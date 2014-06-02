f024f36f0e2dad0937ae5ed11ebaac6fa6d5d485
Campbell Allen <campbell.allen@gmail.com>
2014-06-02 11:19:45 +0100

    fix incorrectly named spec and update model names to make it pass

---
+++ spec/models/set_member_subject_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe SetMemberSubject, :type => :model do
+
+  let(:set_member_subject) { build(:set_member_subject) }
+
+  it "should have a valid factory" do
+    expect(set_member_subject).to be_valid
+  end
+
+  describe "#subject_set" do
+
+    it "must have a subject set" do
+      set_member_subject.subject_set = nil
+      expect(set_member_subject).to_not be_valid
+    end
+
+    it "should belong to a subject set" do
+      expect(set_member_subject.subject_set).to be_a(SubjectSet)
+    end
+  end
+
+  describe "#subject" do
+
+    it "must have a subject" do
+      set_member_subject.subject = nil
+      expect(set_member_subject).to_not be_valid
+    end
+
+    it "should belong to a subject" do
+      expect(set_member_subject.subject).to be_a(Subject)
+    end
+  end
+end
