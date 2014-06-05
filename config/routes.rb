afb0a7a68e09608bc9d369c412c883500e87fa9b
Edward Paget <edward@cassetteta.pe>
2014-06-05 09:56:39 -0500

    Update MIME type. Make routes look nice.
    This changes the mime-type to be compatible with the JSON API spec. It
    also redefines routes closer to what I've been going for with #17.

--- config/routes.rb
+++ config/routes.rb
@@ -6,45 +6,42 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
-    api_version(:module => "V1", :header => {name: "Accept", :value => "application/vnd.zooniverse.v1+json"}) do
-      get "/me", to: 'users#show'
+    api_version(:module => "V1", :header => {name: "Accept", :value => "application/vnd.api+json; version=1"}) do
+      get "/me", to: 'users#show', format: false
 
-      resources :users, except: [:new, :edit] do 
-        resources :collections, except: [:new, :edit]
-        resources :projects, except: [:new, :edit] do
-          resources :classifications, except: [:new, :edit]
-          resources :workflows, except: [:new, :edit]
-          resources :subject_sets, except: [:new, :edit] do 
-            resources :subjects, except: [:new, :edit]
+      resources :users, param: :name, except: [:new, :edit], format: false do
+        resources :collections, only: :index, format: false
+        resources :projects, only: :index, format: false
+        resources :subjects, only: :index, format: false
+        resources :classifications, path: "recents", only: :index, format: false
+      end
+
+      resources :groups, param: :name, except: [:new, :edit], format: false do
+        resources :collections, only: :index, format: false
+        resources :projects, only: :index, format: false
+        resources :subjects, only: :index, format: false
+        resources :members, only: :index, format: false
+        resources :classifications, path: "recents", only: :index, format: false
+      end
+
+      resources :collections, only: [:index, :create], format: false
+      scope "/collections/:owner_name/", format: false do
+        resources :collections, path: '', param: :name, only: [:show, :delete, :update], format: false do
+          resources :subjects, only: :index, format: false
+        end
+      end
+
+      resources :subjects, except: [:new, :edit], format: false
+
+      resources :projects, only: [:index, :create], format: false
+      scope "/projects/:owner_name/" do
+        resources :projects, path: '', param: :name, only: [:show, :update, :delete], format: false do
+          resources :classifications, except: [:new, :edit], format: false
+          resources :workflows, except: [:new, :edit], format: false do
+            resources :subjects, only: :index, format: false
           end
-        end
-      end
-
-      resources :groups, except: [:new, :edit] do
-        resources :collections, except: [:new, :edit]
-        resources :projects, except: [:new, :edit] do
-          resources :classifications, except: [:new, :edit]
-          resources :workflows, except: [:new, :edit]
-          resources :subject_sets, except: [:new, :edit] do 
-            resources :subjects, except: [:new, :edit]
-          end
-        end
-      end
-
-      scope "/collections/:ownername/" do
-        resources :collections, path: '', except: [:new, :edit] do
-          resources :subjects, except: [:new, :edit]
-        end
-      end
-
-      resources :subjects, except: [:new, :edit]
-
-      scope "/projects/:ownername/" do
-        resources :projects, path: '', except: [:new, :edit] do
-          resources :classifications, except: [:new, :edit]
-          resources :workflows, except: [:new, :edit]
-          resources :subject_sets, except: [:new, :edit] do 
-            resources :subjects, except: [:new, :edit]
+          resources :subject_sets, except: [:new, :edit], format: false do 
+            resources :subjects, only: :index, format: false
           end
         end
       end
