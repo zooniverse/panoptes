1530ddd8e31d7d737a61ad7f297b151b95e95322
Campbell Allen <campbell.allen@gmail.com>
2014-06-11 11:00:08 +0100

    Do not hash their login name when scrubbing personal info
    Just leave the login as is since it's not personal information thus avoiding probelms with duplicate login names on revert and uniq index clashes when hashing.

--- lib/user_info_scrubber.rb
+++ lib/user_info_scrubber.rb
@@ -1,5 +1,3 @@
-require "zlib"
-
 class UserInfoScrubber
 
   DELETED_USER_NAME  = 'deleted_user'
@@ -13,10 +11,7 @@ class UserInfoScrubber
       if user.disabled?
         raise ScrubDisabledUserError.new("Can't scrub personal details of a disabled user with id: #{user.id}")
       else
-        hashed_login_string = Zlib.crc32(user.login).to_s
-        user.update_columns( email: DELETED_USER_EMAIL,
-                             display_name: DELETED_USER_NAME,
-                             login: hashed_login_string )
+        user.update_columns( email: DELETED_USER_EMAIL, display_name: DELETED_USER_NAME )
       end
     end
   end
