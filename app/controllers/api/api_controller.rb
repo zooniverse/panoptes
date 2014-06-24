b288994ca03b4f1370bbf2a45ec93a19ba741c58
Edward Paget <edward@cassetteta.pe>
2014-06-24 09:42:50 -0500

    Alias user_for_paper_trail to current user

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -32,15 +32,14 @@ module Api
       @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
     end
 
-    def pundit_user
-      current_resource_owner
-    end
-
     def current_languages
       ( [params[:language]] | 
         (current_resource_owner.try(:languages) || []) |
         parse_http_accept_language ).uniq.compact
     end
+
+    alias_method :pundit_user, :current_resource_owner
+    alias_method :user_for_paper_trail, :current_resource_owner
 
     protected
 
