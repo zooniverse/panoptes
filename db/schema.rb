1d59ef01a494f6ebd650c9f1567900c1cf47137c
Edward Paget <edward@cassetteta.pe>
2014-06-27 12:58:17 -0500

    Add compound index on user_seen_subjects #80

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626155027) do
+ActiveRecord::Schema.define(version: 20140627175457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -213,6 +213,7 @@ ActiveRecord::Schema.define(version: 20140626155027) do
     t.integer  "subject_ids", array: true
   end
 
+  add_index "user_seen_subjects", ["user_id", "workflow_id"], name: "index_user_seen_subjects_on_user_id_and_workflow_id", using: :btree
   add_index "user_seen_subjects", ["user_id"], name: "index_user_seen_subjects_on_user_id", using: :btree
   add_index "user_seen_subjects", ["workflow_id"], name: "index_user_seen_subjects_on_workflow_id", using: :btree
 
