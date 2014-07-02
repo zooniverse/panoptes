1a996a2ccf645ba66d60dca7e0c33d831be5ef5f
Edward Paget <edward@cassetteta.pe>
2014-07-02 09:29:17 -0500

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(version: 20140627175457) do
     t.text     "description"
     t.json     "pages"
     t.json     "example_strings"
-    t.json     "task_strings"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
