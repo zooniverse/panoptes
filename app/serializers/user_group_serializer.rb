e0aab8db03f196133085c3f3128598e0f1d0caaf
Campbell Allen <campbell.allen@gmail.com>
2014-07-04 17:13:27 +0100

    remove the name field from the user / user_group json serializers

--- app/serializers/user_group_serializer.rb
+++ app/serializers/user_group_serializer.rb
@@ -1,5 +1,5 @@
 class UserGroupSerializer
   include RestPack::Serializer
-  attributes :id, :name, :display_name, :classifications_count, :created_at, :updated_at
+  attributes :id, :display_name, :classifications_count, :created_at, :updated_at
   can_include :memberships, :users, :projects, :collections
 end
