44223943959c386470ec1bd7c2d6f9ebb010d25f
Campbell Allen <campbell.allen@gmail.com>
2014-07-04 17:20:14 +0100

    factories should have unique group names

--- spec/factories/user_groups.rb
+++ spec/factories/user_groups.rb
@@ -4,18 +4,21 @@ FactoryGirl.define do
     activated_state :active
 
     factory :user_group_with_users do
+      display_name "A User Group With Users"
       after(:create) do |ug|
         create_list(:membership, 2, user_group: ug)
       end
     end
 
     factory :user_group_with_projects do
+      display_name "A User Group With Projects"
       after(:create) do |ug|
         create_list(:project, 2, owner: ug)
       end
     end
 
     factory :user_group_with_collections do
+      display_name "A User Group With Collections"
       after(:Create) do |ug|
         create_list(:collections, 2, owner: ug)
       end
