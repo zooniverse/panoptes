ae51a5e3945fa090da46f8678eae3c653635f366
Edward Paget <edward@cassetteta.pe>
2014-07-15 13:38:31 -0500

    Regenerate CSRF tokens on log-in log-out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < Devise::SessionsController
   include JSONApiRender
+  after_filter :set_csrf_headers, only: [:create, :destroy]
 
   def create
     respond_to do |format|
@@ -28,5 +29,10 @@ class SessionsController < Devise::SessionsController
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     yield if block_given?
     head :no_content
+  end
+
+  def set_csrf_headers
+    response.headers['X-CSRF-Param'] = request_forgery_protection_token
+    response.headers['X-CSRF-Token'] = form_authenticity_token
   end
 end
