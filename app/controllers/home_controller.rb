1b7696a707d23903b0f7b019125cadbab77c47c5
Edward Paget <edward@cassetteta.pe>
2014-07-16 16:34:18 -0500

    Add content negotiation for root page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < Api::ApiController
 
   def index
-    render json: {}
+    respond_to do |format|
+      format.json { render json: {} }
+      format.json_api { render json_api: {} }
+      format.html { render }
+    end
   end
 end
