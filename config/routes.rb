61970fd25f581f474ac51cd9a11e901135481a47
Edward Paget <edward@cassetteta.pe>
2014-07-16 10:33:27 -0500

    Restrict devise routes for sign in and user creation

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,16 @@ Rails.application.routes.draw do
     controllers applications: 'oauth/applications'
   end
 
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks',
-                                    registrations: 'registrations',
-                                    passwords: 'passwords',
-                                    sessions: 'sessions' }
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks', passwords: 'passwords' }, skip: [ :sessions, :registrations ]
+
+  as :user do
+    get "/users/sign_in" => "sessions#new", as: :new_user_session
+    post "/users/sign_in" => "sessions#create", as: :user_session
+    delete "/users/sign_out" => "sessions#destroy", as: :destroy_user_session
+
+    get "/users/sign_up" => "registrations#new", as: :new_user_registration
+    post "/users" => "registrations#create", as: :user_registration
+  end
 
   namespace :api do
     api_version(:module => "V1", :header => {name: "Accept", :value => "application/vnd.api+json; version=1"}) do
