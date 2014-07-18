83f3e37cf2ea49a7a4fa82e8a1de725b60134de9
Campbell Allen <campbell.allen@gmail.com>
2014-07-18 13:23:15 +0100

    do not serialize the subject owner relation details

--- app/serializers/subject_serializer.rb
+++ app/serializers/subject_serializer.rb
@@ -1,5 +1,5 @@
 class SubjectSerializer
   include RestPack::Serializer
-  attributes :id, :metadata, :locations, :zooniverse_id, :created_at, :updated_at, :owner
+  attributes :id, :metadata, :locations, :zooniverse_id, :created_at, :updated_at
   can_include :owner
 end
