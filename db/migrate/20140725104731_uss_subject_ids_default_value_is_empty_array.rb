4626a790be1fa33d264a848cc5da59b17b525b7b
Campbell Allen <campbell.allen@gmail.com>
2014-07-25 13:03:34 +0100

    set up default emtpy array for user seend subjects subject ids

---
+++ db/migrate/20140725104731_uss_subject_ids_default_value_is_empty_array.rb
@@ -0,0 +1,9 @@
+class UssSubjectIdsDefaultValueIsEmptyArray < ActiveRecord::Migration
+  def up
+    change_column :user_seen_subjects, :subject_ids, :integer, array: true, default: '{}', null: false
+  end
+
+  def down
+    change_column :user_seen_subjects, :subject_ids, :integer, array: true, default: nil, null: true
+  end
+end
