20ef303bd43c9465418bcbc55d6ad15300eec4c6
Edward Paget <edward@cassetteta.pe>
2014-08-11 16:41:14 -0500

    Add Parental Control Specs

--- lib/role_control/parental_controlled.rb
+++ lib/role_control/parental_controlled.rb
@@ -3,23 +3,27 @@ require 'role_control/controlled'
 module RoleControl
   module ParentalControlled
     include RoleControl::Controlled
-      
+    
     def can_by_role_through_parent(action, parent, *additional_roles)
-      question = "can_#{ action }?"
-      can(action) { |enrolled| send(parent).send(question, enrolled) }
+      question = "can_#{ action }?".to_sym
+      can(action) do |enrolled|
+        begin
+          send(parent).send(question, enrolled)
+        rescue StandardError => e
+          nil
+        end
+      end
+      
       unless additional_roles.blank?
-        
         can action, &test_proc(parent, additional_roles)
       end
     end
 
+    protected
+
     def test_proc(parent, add_roles)
       test_proc = super(add_roles)
-      proc do |enrolled|
-        send(parent).instance_eval do
-         test_proc.call(enrolled)
-        end
-      end
+      proc { |enrolled| send(parent).instance_exec(enrolled, &test_proc) }
     end
   end
 end
---
+++ spec/lib/role_control/parental_controlled_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+class TestParentControl
+  extend RoleControl::ParentalControlled
+
+  can_by_role_through_parent :read, :parent
+  can_by_role_through_parent :edit, :parent, :test_parent_role
+
+  attr_reader :parent
+
+  def initialize(parent)
+    @parent = parent
+  end
+end
+
+describe RoleControl::ParentalControlled do
+  setup_role_control_tables
+
+  let(:role_model) { rt = RoleModelTable.new; rt.save!; rt }
+  let(:group_table) { gt = GroupTable.new; gt.save!; gt }
+  let!(:relation) do
+    m = MembershipTable.new
+    m.roles = ['test_parent_role', 'test_role']
+    m.role_model_table_id = role_model.id
+    m.group_table_id = group_table.id
+    m.save!
+  end
+
+  let(:tpc) { TestParentControl.new(group_table) }
+
+  it "returns true when the parent's test would be true" do
+    expect(tpc.can_read?(role_model)).to be_truthy
+  end
+
+  it "returns true when its own role test is met" do
+    expect(tpc.can_edit?(role_model)).to be_truthy
+  end
+
+  it "should call the parent's can method if it exists" do
+    expect(group_table).to receive(:can_read?)
+    tpc.can_read?(role_model)
+  end
+end
