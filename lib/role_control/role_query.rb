b6c9cf08a81d282ed57b130b9e9cf9785e314802
Campbell Allen <campbell.allen@gmail.com>
2014-08-21 14:31:13 +0100

    refactored role_control class for readability

--- lib/role_control/role_query.rb
+++ lib/role_control/role_query.rb
@@ -5,43 +5,65 @@ module RoleControl
     end
 
     def build(actor, target=nil, extra_tests=[])
-      query_value, join_query = join_clause(actor, target)
-      extra_tests << public_test if @public
-      
-      query = @klass.where(where_clause(!!join_query, extra_tests))
-      query = query.joins(join_query) if join_query
-      
-      rebind(query, query_value)
+      @actor = actor
+      @target = target.nil? ? @klass : target
+      @extra_tests = setup_extra_tests(extra_tests)
+      @actor_roles_query = nil
+      build_role_query
     end
 
     private
 
-    def rebind(query, query_values)
-      return query unless query_values
-      query_values.try(:reduce, query) { |q, b| q.bind(b) } 
+    def build_role_query
+      query_bind_values = actor_roles_query_bind_values
+      query = query_without_bind_values
+      return query unless query_bind_values
+      query_bind_values.reduce(query) { |query, value| query.bind(value) }
+    end
+
+    def query_without_bind_values
+      if join_query = join_clause
+        @klass.where(roles_where_clause).joins(join_query)
+      else
+        @klass.where(extra_tests_where_clause)
+      end
+    end
+
+    def setup_extra_tests(extra_tests)
+      if @public
+        extra_tests << table[@roles].eq('{}')
+      else
+        extra_tests
+      end
     end
 
     def table
       @klass.arel_table
     end
-    
+
     def roles_table
       @roles_table ||= Arel::Table.new(:roles_query)
     end
 
-    def role_query(actor, target)
-      target = target.nil? ? @klass : target
-      q = actor.roles_query(target)
-      query_value, arel = q.try(:bind_values), q.try(:arel).try(:as, 'roles_query')
-      [query_value, arel]
+    def actor_roles_query
+      @actor_roles_query ||= @actor.roles_query(@target)
     end
 
-    def join_clause(actor, target)
-      query_value, query = role_query(actor, target)
-      query = table.create_join(query, join_on, Arel::Nodes::OuterJoin) if query
-      [query_value, query]
+    def actor_roles_query_bind_values
+      actor_roles_query.try(:bind_values)
     end
-    
+
+    def actor_roles_query_arel
+      arel = actor_roles_query.try(:arel)
+      arel.try(:as, 'roles_query')
+    end
+
+    def join_clause
+      if arel_query = actor_roles_query_arel
+        table.create_join(arel_query, join_on, Arel::Nodes::OuterJoin)
+      end
+    end
+
     def join_on
       table.create_on(roles_table[join_id].eq(table[:id]))
     end
@@ -50,28 +72,27 @@ module RoleControl
       "#{ @klass.model_name.singular }_id".to_sym
     end
 
-    def where_clause(include_roles, extra_tests)
-      q = include_roles ? roles_test : extra_tests.pop 
-      extra_tests.reduce(q) { |query, test| query.or(test) }
+    def construct_where_clause(query)
+      @extra_tests.reduce(query) { |query, extra_test| query.or(extra_test) }
     end
-    
-    def public_test
-      table[@roles].eq('{}')
-    end
-    
-    def roles_test
-      test = roles_table[:roles].not_eq(nil)
-        .and(roles)
 
-      Arel::Nodes::Grouping.new(test)
+    def roles_where_clause
+      role_test_arel = roles_table[:roles].not_eq(nil).and(roles)
+      grouped_arel = Arel::Nodes::Grouping.new(role_test_arel)
+      construct_where_clause(grouped_arel)
+    end
+
+    def extra_tests_where_clause
+      construct_where_clause(@extra_tests.pop)
     end
 
     def roles
-      if @roles.is_a?(Array)
-        roles_table[:roles].overlap("{#{@roles.join(',')}}")
+      overlap_with = if @roles.is_a?(Array)
+        "{#{@roles.join(',')}}"
       else
-        roles_table[:roles].overlap(table[@roles])
+        table[@roles]
       end
+      roles_table[:roles].overlap(overlap_with)
     end
   end
 end
