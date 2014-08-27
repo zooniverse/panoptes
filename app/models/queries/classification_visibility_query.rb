1d01bf561272462f6834461609845a883a46b666
Edward Paget <edward@cassetteta.pe>
2014-08-27 11:02:38 -0500

    Temporary Workaround for ActiveRecord Bug

--- app/models/queries/classification_visibility_query.rb
+++ app/models/queries/classification_visibility_query.rb
@@ -10,21 +10,21 @@ class ClassificationVisibilityQuery
     query = @parent.where(where_user
                           .or(where_project)
                           .or(where_group))
-    rebind(query, all_binding_values)
+    rebind(query)
   end
 
   private
 
-  def rebind(query, binding_values)
-    binding_values.reduce(query) { |query, value| query.bind(value) }
+  def rebind(query)
+    all_bind_values.reduce(query) { |query, value| query.bind(value) }
   end
-
+  
   def arel_table
     @parent.arel_table
   end
 
-  def all_binding_values
-    project_scope.bind_values.concat(group_scope.bind_values)
+  def all_bind_values
+    project_scope.bind_values | group_scope.bind_values
   end
   
   def where_user
