bd9e402e0d1536d51f5c5ea5e5a9a32d6fe8e2fb
Campbell Allen <campbell.allen@gmail.com>
2014-09-08 17:13:18 +0100

    extract hack to test config ony

--- config/application.rb
+++ config/application.rb
@@ -34,11 +34,5 @@ module Panoptes
         resource '/api/*', headers: :any, methods: [:get, :post, :options, :put, :patch]
       end
     end
-
-    #hack to fix issue with doorkeeper not loading ControllerHelpers in test env
-    # https://github.com/doorkeeper-gem/doorkeeper/issues/375
-    config.to_prepare do
-      Doorkeeper::ApplicationsController.helper Doorkeeper::Helpers::Controller
-    end
   end
 end
