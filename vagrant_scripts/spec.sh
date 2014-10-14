314c0a407d11229168719df671655ff76aaffb70
Edward Paget <edward@cassetteta.pe>
2014-10-14 16:10:21 -0500

    Fix spec script errors

--- vagrant_scripts/spec.sh
+++ vagrant_scripts/spec.sh
@@ -7,21 +7,23 @@ sudo docker inspect test_postgres
 INSPECT_PSQL_STATUS=$?
 
 if [ $INSPECT_PSQL_STATUS = 1 ]; then
-    sudo docker run -d -e DB="panoptes_test" \
-        --name test_postgres -e PG_USER="panoptes" -e \
-        PASS="panoptes" -v /opt/postgresql:/data \
+    sudo docker run -d --name test_postgres \
+        -e DB="panoptes_test" -e PG_USER="panoptes" \
+        -e PASS="panoptes" -v /opt/postgresql:/data \
         zooniverse/postgresql
-fi
 
-echo $INSPECT_SPEC_STATUS
+else
+    sudo docker start test_postgres
+fi
 
 if [ $INSPECT_SPEC_STATUS = 1 ]; then
     IMAGE="zooniverse/ruby:$(</home/vagrant/.ruby-version)"
-    sudo docker run -i -t --name panoptes_spec \
-        --link zookeeper:zookeeper --link test_postgres:postgres \
+    sudo docker run -t --name panoptes_spec \
+        --link zookeeper:zookeeper --link test_postgres:test_postgres \
         -v /home/vagrant/panoptes/:/rails_app/ \
         -e "RAILS_ENV=test" $IMAGE \
-        /bin/bash -c "cd /rails_app && bundle install && bundle exec spec"
+        /bin/bash -c "cd /rails_app && bundle install && rake db:schema:load && bundle exec rspec"
+    sudo docker stop test_postgres
 else
     sudo docker start -a panoptes_spec
 fi
