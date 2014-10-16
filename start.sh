14f1f09a57198b5a19cd8734d115c02fb7b36fb2
Edward Paget <edward@cassetteta.pe>
2014-10-16 11:30:30 -0500

    Update start scrip to link config first

--- start.sh
+++ start.sh
@@ -2,16 +2,15 @@
 
 cd /rails_app
 
+if [ -d "/rails_conf/" ]
+then
+    ln -sf /rails_conf/* ./config/
+fi
 
 if [ "$RAILS_ENV" == "development" ]
 then
     bundle install
     rake db:migrate
-fi
-
-if [ -d "/rails_conf/" ]
-then
-    ln -sf /rails_conf/* ./config/
 fi
 
 exec bundle exec rails s puma -p 80 $*
