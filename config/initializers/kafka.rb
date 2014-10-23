ec7d513d7143dd09e64f60084709420d53764c0e
Edward Paget <edward@cassetteta.pe>
2014-10-23 08:29:03 -0500

    DRY up the Kafka Initializer

--- config/initializers/kafka.rb
+++ config/initializers/kafka.rb
@@ -1,18 +1,13 @@
 require 'multi_kafka_producer'
 
-def kafka_config
-  return @config if @config
-  @config = YAML.load(ERB.new(File.read(Rails.root.join('config/kafka.yml'))).result)
-  @config[Rails.env].symbolize_keys
-rescue Errno::ENOENT
-  @config = {  }
-end
+adapter, kafka_config = begin
+                          config = YAML.load(ERB.new(File.read(Rails.root.join('config/kafka.yml'))).result)
+                          config[Rails.env].symbolize_keys
+                          [ nil, config ]
+                        rescue Errno::ENOENT
+                          [ :null, {  } ]
+                        end
 
-unless kafka_config.empty?
-  MultiKafkaProducer.adapter = nil
-  MultiKafkaProducer.connect(kafka_config[:producer_id],
-                             *kafka_config[:brokers])
-else
-  MultiKafkaProducer.adapter = :null
-  MultiKafkaProducer.connnect(nil, nil)
-end
+MultiKafkaProducer.adapter = adapter
+MultiKafkaProducer.connect(kafka_config[:producer_id],
+                           *kafka_config[:brokers])
