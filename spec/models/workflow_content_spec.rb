40aa17506c3a5afa73250569706e32779416ec93
Edward Paget <edward@cassetteta.pe>
2014-10-28 11:47:23 -0500

    Test Workflow and Content versioning

--- spec/models/workflow_content_spec.rb
+++ spec/models/workflow_content_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe WorkflowContent, :type => :model do
   let(:parent_factory) { :workflow }
 
   it_behaves_like "is translated content"
+
+  describe "versioning" do
+    subject do
+      create(:workflow_content)
+    end
+    
+    it { is_expected.to be_versioned }
+
+    it 'should track changes to strings', versioning: true do
+      new_strings = %w(some stuff)
+      subject.update!(strings: new_strings)
+      expect(subject.previous_version.strings).to_not eq(new_strings)
+    end
+
+    it 'should not track changes to langauges', versioning: true do
+      new_lang = 'en'
+      subject.update!(language: new_lang)
+      expect(subject.previous_version).to be_nil
+    end
+  end
 end
