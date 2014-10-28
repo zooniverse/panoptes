e7048fafac9bd87f461ea5d568efc3d7c7dfddc1
Edward Paget <edward@cassetteta.pe>
2014-10-28 11:50:05 -0500

    Add Versioning Tests to Project Content

--- spec/models/project_content_spec.rb
+++ spec/models/project_content_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe ProjectContent, :type => :model do
   it 'should require a description to be valid' do
     expect(build(:project_content, :description => nil )).to_not be_valid
   end
+
+  describe "versioning" do
+    subject do
+      create(:project_content)
+    end
+    
+    it { is_expected.to be_versioned }
+
+    it 'should track changes to description', versioning: true do
+      new_desc = "a boring old project"
+      subject.update!(description: new_desc)
+      expect(subject.previous_version.description).to_not eq(new_desc)
+    end
+
+    it 'should not track changes to language', versioning: true do
+      new_lang = 'en'
+      subject.update!(language: new_lang)
+      expect(subject.previous_version).to be_nil
+    end
+  end
 end
