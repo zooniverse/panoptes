e1719b037b6e63b595f405b636b8e03f2b2d0e1b
Edward Paget <edward@cassetteta.pe>
2014-10-29 10:21:07 -0500

    Actually use the right environment config

--- config/initializers/kafka.rb
+++ config/initializers/kafka.rb
@@ -2,8 +2,7 @@ require 'multi_kafka_producer'
 
 adapter, kafka_config = begin
                           config = YAML.load(ERB.new(File.read(Rails.root.join('config/kafka.yml'))).result)
-                          config[Rails.env].symbolize_keys
-                          [ nil, config ]
+                          [ nil, config[Rails.env].symbolize_keys ]
                         rescue Errno::ENOENT, NoMethodError
                           [ :null, {  } ]
                         end
