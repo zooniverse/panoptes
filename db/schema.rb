f27644f8727c116c7823b33d157bd945e7e30bb6
Campbell Allen <campbell.allen@gmail.com>
2014-11-07 15:57:16 +0000

    ensure consistent db schema after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105170338) do
+ActiveRecord::Schema.define(version: 20141106180016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
