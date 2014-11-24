d946f07dd345679c8a493250e22dea3d67725d61
Campbell Allen <campbell.allen@gmail.com>
2014-11-24 11:39:13 +0000

    add formatting to setup script

--- db/fig_dev_seed_data/fig_dev_seed_data.rb
+++ db/fig_dev_seed_data/fig_dev_seed_data.rb
@@ -1,10 +1,32 @@
 exit unless Rails.env.development?
-puts "You need to setup an Oauth client application to interact with the API."
-puts "To do this you'll need a local admin user account, the following will setup both for you.\n\n"
-puts "Please enter an password (min 8 chars) for the admin user, make it secure and you'll need to remember this!"
+
+require 'io/console'
+
+def colorize(text, color_code)
+  "\e[#{color_code}m#{text}\e[0m"
+end
+def red(text); colorize(text, 31); end
+def green(text); colorize(text, 32); end
+def yellow(text); colorize(text, 33); end
+
+panoptes = %{
+______  ___   _   _ ___________ _____ _____ _____
+| ___ \\/ _ \\ | \\ | |  _  | ___ \\_   _|  ___/  ___|
+| |_/ / /_\\ \\|  \\| | | | | |_/ / | | | |__ \\ `--.
+|  __/|  _  || . ` | | | |  __/  | | |  __| `--. \\
+| |   | | | || |\\  \\ \\_/ / |     | | | |___/\\__/ /
+\\_|   \\_| |_/\\_| \\_/\\___/\\_|     \\_/ \\____/\\____/
+}
+
+puts yellow("\n\n---------------------------------------------------------------------")
+puts yellow(panoptes)
+puts "\nYou need to setup an Oauth client application to interact with the API."
+puts "In turn this requires a local admin user account."
+puts "This script will setup both for you.\n\n"
+puts yellow("Please enter an password (min 8 chars) for the admin user, make it secure and you'll need to remember this!")
 password = STDIN.noecho(&:gets).chomp
 if password.length < 8
-  abort("Failed: Password must be at least 8 characters long")
+  abort(red("Failed: Password must be at least 8 characters long\n"))
 end
 
 #setup an admin user
@@ -15,9 +37,9 @@ attrs = { display_name: 'Zooniverse Admin',
 admin = User.create(attrs) do |user|
   user.owner_name = OwnerName.new(name: user.login, resource: user)
 end
-puts "Admin details:"
-puts "name: #{admin.login}"
-puts "email: #{admin.email}"
+puts "\nAdmin details:"
+puts "login: #{green(admin.login)}"
+puts "email: #{green(admin.email)}\n"
 
 #setup a doorkeeper first party oauth application with scopes for the client
 app = Doorkeeper::Application.create do |da|
@@ -30,7 +52,8 @@ app = Doorkeeper::Application.create do |da|
   #scoped resources this app has access to
   da.default_scope = ["public", "user", "project", "group", "collection", "classification"]
 end
-puts "Oauth Zooniverse first party app details:"
-puts "client_id: #{app.uid}\n"
+puts "\nOauth Zooniverse first party app details:"
+puts "client_id: #{green(app.uid)}\n"
 puts "Use the client ID above to interact with the api."
-puts "You can reivew these settings via the Oauth Applications page at http(s)://<server_ip:port>//oauth/applications"
+puts "You can review these settings via the Oauth Applications page on your server at:"
+puts yellow("http(s)://<server_ip:port>//oauth/applications\n\n")
