a0594d4d7167f0d4f660ea067a6971a5b3f850b3
Edward Paget <edward@cassetteta.pe>
2014-12-04 12:27:03 -0600

    Use Polymorphic support to simplify VersionSerializer

--- app/serializers/version_serializer.rb
+++ app/serializers/version_serializer.rb
@@ -1,26 +1,10 @@
 class VersionSerializer
   include RestPack::Serializer
-  attributes :id, :changeset, :whodunnit, :created_at, :links
+  attributes :id, :changeset, :whodunnit, :created_at
+
+  can_include :item
 
   def self.model_class
     PaperTrail::Version
-  end
-
-  def item_href
-    "/#{ @model.item.class.model_name.route_key }/#{ @model.item.id }"
-  end
-
-  def item_type
-    @model.item.class.model_name.singular
-  end
-
-  def links
-    { item: { id: @model.item.id.to_s,
-              href: item_href,
-              type: item_type } }
-  end
-
-  def self.links
-    { } 
   end
 end
