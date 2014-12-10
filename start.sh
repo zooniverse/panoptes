170f55c827c2839fcd9315fba5c4644cbad25aec
Adam McMaster <adam@zooniverse.org>
2014-12-10 12:09:44 +0000

    Run sidekiq in start.sh

--- start.sh
+++ start.sh
@@ -22,5 +22,9 @@ if [ "$RAILS_ENV" == "development" ]; then
     exec foreman start
   fi
 else
+  [ -z "$HOME" ] && export HOME=$(pwd)
+  mkdir -p tmp/pids/
+  rm -f tmp/pids/*.pid
+  bundle exec sidekiq &
   exec bundle exec rails s puma -p 80 $*
 fi
