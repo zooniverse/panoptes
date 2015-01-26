20a32dccf4f4c3a38be1a33691c57dd04c0cdcef
Edward Paget <edward@cassetteta.pe>
2015-01-26 11:47:47 -0600

    Add type to user and user_group responses

--- app/serializers/user_group_serializer.rb
+++ app/serializers/user_group_serializer.rb
@@ -1,9 +1,13 @@
 class UserGroupSerializer
   include RestPack::Serializer
-  attributes :id, :name, :display_name, :classifications_count, :created_at, :updated_at
+  attributes :id, :name, :display_name, :classifications_count, :created_at, :updated_at, :type
   can_include :memberships, :users,
               projects: { param: "owner", value: "name" },
               collections: { param: "owner", value: "name" }
 
   can_filter_by :name
+
+  def type
+    "user_groups"
+  end
 end
