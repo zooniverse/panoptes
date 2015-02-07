68761bf6e4e6e4e92549ba7eab03d65f98dd9c38
Edward Paget <edward@cassetteta.pe>
2015-02-06 19:03:54 -1000

    Use JSON Schema for Classification Validation
    Update apiary for new annotation format

--- app/controllers/api/v1/classifications_controller.rb
+++ app/controllers/api/v1/classifications_controller.rb
@@ -3,15 +3,10 @@ class Api::V1::ClassificationsController < Api::ApiController
   doorkeeper_for :show, :index, :destroy, :update, scopes: [:classification]
 
   resource_actions :default
+  
+  schema_type :json_schema
 
   rescue_from RoleControl::AccessDenied, with: :access_denied
-
-  METADATA_PARAMS = [:screen_resolution,
-                     :started_at,
-                     :finished_at,
-                     :user_language,
-                     :workflow_version,
-                     :user_agent]
 
   def create
     super { |classification| lifecycle(:create, classification) }
@@ -67,29 +62,6 @@ class Api::V1::ClassificationsController < Api::ApiController
     lifecycle = ClassificationLifecycle.new(classification)
     lifecycle.update_cellect(cellect_host(classification))
     lifecycle.queue(action)
-  end
-
-  def annotation_params
-    params[:classifications][:annotations].map(&:keys)
-  end
-
-  def create_update_params
-    [ :completed,
-      :gold_standard,
-      metadata: METADATA_PARAMS,
-      annotations: annotation_params ]
-  end
-
-  def create_params
-    param_set = create_update_params | [ links: [:project,
-                                                 :workflow,
-                                                 set_member_subjects: [],
-                                                 subjects: []] ]
-    params.require(:classifications).permit(param_set)
-  end
-
-  def update_params
-    params.require(:classifications).permit(create_update_params)
   end
 
   def completed_error
