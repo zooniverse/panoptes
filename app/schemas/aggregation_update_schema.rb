ac9eb7f1d38421fa889a357e392970d9d4fc41d7
Campbell Allen <campbell.allen@gmail.com>
2015-03-04 11:38:50 +0000

    allow aggregations to be updated after creation
    aggregation will be an interative process that runs at different time scales, e.g. hourly best effort vs nightly full re-run, etc.

---
+++ app/schemas/aggregation_update_schema.rb
@@ -0,0 +1,26 @@
+class AggregationUpdateSchema < JsonSchema
+  schema do
+    type "object"
+    description "An Aggregation for a subject"
+    required "aggregation", "links"
+    additional_properties false
+
+    property "aggregation" do
+      type "object"
+    end
+
+    property "links" do
+      type "object"
+
+      required "subject", "workflow"
+
+      property "subject" do
+        type "integer", "string"
+      end
+
+      property "workflow" do
+        type "integer", "string"
+      end
+    end
+  end
+end
--- spec/controllers/api/v1/aggregations_controller_spec.rb
+++ spec/controllers/api/v1/aggregations_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Api::V1::AggregationsController, type: :controller do
   describe "#index" do
     let(:workflow) { create(:workflow) }
     let!(:aggregations) { create_list(:aggregation, 2, workflow: workflow) }
-    let!(:private_resource) { create(:aggregation) } 
+    let!(:private_resource) { create(:aggregation) }
     let(:authorized_user) { workflow.project.owner }
     let(:n_visible) { 2 }
 
@@ -47,5 +47,34 @@ RSpec.describe Api::V1::AggregationsController, type: :controller do
     end
 
     it_behaves_like "is creatable"
+  end
+
+  describe '#update' do
+    let(:workflow) { create(:workflow_with_subjects) }
+    let(:subject) { workflow.subject_sets.first.subjects.first }
+    let(:authorized_user) { resource.workflow.project.owner }
+    let(:resource) { create(:aggregation, workflow: workflow) }
+    let(:aggregation_results) do
+      { "mean" => "1", "std" => "1", "count" => ["1","1","1"] }
+    end
+    let(:test_attr) { :aggregation }
+    let(:test_attr_value) { aggregation_results }
+    let(:test_relation) { :subject }
+    let(:test_relation_ids) { subject.id }
+    let(:update_params) do
+      { aggregations:
+          {
+            aggregation: aggregation_results,
+            links: {
+              subject: subject.id.to_s,
+              workflow: workflow.id.to_s
+            }
+          }
+      }
+    end
+
+    it_behaves_like "is updatable"
+
+    it_behaves_like "has updatable links"
   end
 end
