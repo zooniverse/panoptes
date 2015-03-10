cf4d3112f2ab40562b545dfe4a9ee661da9320bc
Edward Paget <edward@cassetteta.pe>
2015-03-10 09:18:29 -0500

    Add leading slash to owner link

--- app/serializers/owner_link_serializer.rb
+++ app/serializers/owner_link_serializer.rb
@@ -16,7 +16,7 @@ module OwnerLinkSerializer
     model_name = @model.owner.class.model_name
     data[:links][:owner] = { id: @model.owner.id.to_s,
                              type: model_name.plural,
-                             href: "#{model_name.route_key}/#{@model.owner.id}" }
+                             href: "/#{model_name.route_key}/#{@model.owner.id}" }
     data
   end
 end
