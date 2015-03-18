8fc6eac2633325fa6cacfdd0fceffb94abfbba36
Edward Paget <edward@cassetteta.pe>
2015-03-18 12:58:46 -0500

    Create avatars from Zoo-Home records

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318132024) do
+ActiveRecord::Schema.define(version: 20150318174012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -319,6 +319,7 @@ ActiveRecord::Schema.define(version: 20150318132024) do
     t.boolean  "admin",                       default: false,    null: false
     t.boolean  "banned",                      default: false,    null: false
     t.boolean  "migrated",                    default: false
+    t.text     "avatar"
   end
 
   add_index "users", ["display_name"], name: "index_users_on_display_name", unique: true, using: :btree
