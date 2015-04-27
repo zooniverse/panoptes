0a23d28e270fe79810a107dd1f568d1d23c6ca4e
Campbell Allen <campbell.allen@gmail.com>
2015-04-27 17:39:22 +0100

    scope the workflow param variable correctly
    fixes https://app.honeybadger.io/projects/40595/faults/12434093

--- spec/workers/subject_queue_worker_spec.rb
+++ spec/workers/subject_queue_worker_spec.rb
@@ -10,9 +10,15 @@ RSpec.describe SubjectQueueWorker do
   describe "#perform" do
     context "with no user or set" do
       it 'should create a subject queue with the default number of items' do
-        subject.perform(workflow)
+        subject.perform(workflow.id)
         queue = SubjectQueue.find_by(workflow: workflow)
         expect(queue.set_member_subject_ids.length).to eq(100)
+      end
+    end
+
+    context "when a workflow id string is passed in", :inline do
+      it "should not raise an error" do
+        expect{subject.perform(workflow.id.to_s)}.to_not raise_error
       end
     end
   end
