839453a40806cbd031948880e02525695b21f54e
Edward Paget <edward@cassetteta.pe>
2015-04-28 10:02:06 -0500

    Remove postgresql ports and extra extensions

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,12 @@ ActiveRecord::Schema.define(version: 20150427204917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
-  enable_extension "postgres_fdw"
 
   create_table "access_control_lists", force: :cascade do |t|
     t.integer  "user_group_id"
-    t.string   "roles",                     default: [], null: false, array: true
+    t.string   "roles",         default: [], null: false, array: true
     t.integer  "resource_id"
-    t.string   "resource_type", limit: 255
+    t.string   "resource_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -44,9 +42,9 @@ ActiveRecord::Schema.define(version: 20150427204917) do
 
   create_table "authorizations", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "provider",   limit: 255
-    t.string   "uid",        limit: 255
-    t.string   "token",      limit: 255
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "token"
     t.datetime "expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -77,14 +75,14 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "classifications", ["workflow_id"], name: "index_classifications_on_workflow_id", using: :btree
 
   create_table "collections", force: :cascade do |t|
-    t.string   "name",            limit: 255
+    t.string   "name"
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "activated_state",             default: 0, null: false
-    t.string   "display_name",    limit: 255
+    t.integer  "activated_state", default: 0, null: false
+    t.string   "display_name"
     t.boolean  "private"
-    t.integer  "lock_version",                default: 0
+    t.integer  "lock_version",    default: 0
   end
 
   add_index "collections", ["project_id"], name: "index_collections_on_project_id", using: :btree
@@ -109,14 +107,14 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "memberships", ["user_id"], name: "index_memberships_on_user_id", using: :btree
 
   create_table "oauth_access_grants", force: :cascade do |t|
-    t.integer  "resource_owner_id",             null: false
-    t.integer  "application_id",                null: false
-    t.string   "token",             limit: 255, null: false
-    t.integer  "expires_in",                    null: false
-    t.text     "redirect_uri",                  null: false
-    t.datetime "created_at",                    null: false
+    t.integer  "resource_owner_id", null: false
+    t.integer  "application_id",    null: false
+    t.string   "token",             null: false
+    t.integer  "expires_in",        null: false
+    t.text     "redirect_uri",      null: false
+    t.datetime "created_at",        null: false
     t.datetime "revoked_at"
-    t.string   "scopes",            limit: 255
+    t.string   "scopes"
   end
 
   add_index "oauth_access_grants", ["token"], name: "index_oauth_access_grants_on_token", unique: true, using: :btree
@@ -124,12 +122,12 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   create_table "oauth_access_tokens", force: :cascade do |t|
     t.integer  "resource_owner_id"
     t.integer  "application_id"
-    t.string   "token",             limit: 255, null: false
-    t.string   "refresh_token",     limit: 255
+    t.string   "token",             null: false
+    t.string   "refresh_token"
     t.integer  "expires_in"
     t.datetime "revoked_at"
-    t.datetime "created_at",                    null: false
-    t.string   "scopes",            limit: 255
+    t.datetime "created_at",        null: false
+    t.string   "scopes"
   end
 
   add_index "oauth_access_tokens", ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true, using: :btree
@@ -137,16 +135,16 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "oauth_access_tokens", ["token"], name: "index_oauth_access_tokens_on_token", unique: true, using: :btree
 
   create_table "oauth_applications", force: :cascade do |t|
-    t.string   "name",          limit: 255,              null: false
-    t.string   "uid",           limit: 255,              null: false
-    t.string   "secret",        limit: 255,              null: false
-    t.text     "redirect_uri",                           null: false
+    t.string   "name",                       null: false
+    t.string   "uid",                        null: false
+    t.string   "secret",                     null: false
+    t.text     "redirect_uri",               null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "owner_id"
-    t.string   "owner_type",    limit: 255
-    t.integer  "trust_level",               default: 0,  null: false
-    t.string   "default_scope",             default: [],              array: true
+    t.string   "owner_type"
+    t.integer  "trust_level",   default: 0,  null: false
+    t.string   "default_scope", default: [],              array: true
   end
 
   add_index "oauth_applications", ["owner_id", "owner_type"], name: "index_oauth_applications_on_owner_id_and_owner_type", using: :btree
@@ -154,8 +152,8 @@ ActiveRecord::Schema.define(version: 20150427204917) do
 
   create_table "project_contents", force: :cascade do |t|
     t.integer  "project_id"
-    t.string   "language",          limit: 255
-    t.string   "title",             limit: 255
+    t.string   "language"
+    t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -171,18 +169,18 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "project_contents", ["project_id"], name: "index_project_contents_on_project_id", using: :btree
 
   create_table "projects", force: :cascade do |t|
-    t.string   "name",                  limit: 255
-    t.string   "display_name",          limit: 255
+    t.string   "name"
+    t.string   "display_name"
     t.integer  "user_count"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "classifications_count",             default: 0, null: false
-    t.integer  "activated_state",                   default: 0, null: false
-    t.string   "primary_language",      limit: 255
+    t.integer  "classifications_count", default: 0, null: false
+    t.integer  "activated_state",       default: 0, null: false
+    t.string   "primary_language"
     t.text     "avatar"
     t.text     "background_image"
     t.boolean  "private"
-    t.integer  "lock_version",                      default: 0
+    t.integer  "lock_version",          default: 0
   end
 
   create_table "set_member_subjects", force: :cascade do |t|
@@ -214,13 +212,13 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "subject_queues", ["user_id", "workflow_id"], name: "index_subject_queues_on_user_id_and_workflow_id", unique: true, using: :btree
 
   create_table "subject_sets", force: :cascade do |t|
-    t.string   "display_name",              limit: 255
+    t.string   "display_name"
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "set_member_subjects_count",             default: 0,  null: false
-    t.jsonb    "metadata",                              default: {}
-    t.integer  "lock_version",                          default: 0
+    t.integer  "set_member_subjects_count", default: 0,  null: false
+    t.jsonb    "metadata",                  default: {}
+    t.integer  "lock_version",              default: 0
     t.boolean  "expert_set"
   end
 
@@ -246,15 +244,15 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "subject_workflow_counts", ["workflow_id"], name: "index_subject_workflow_counts_on_workflow_id", using: :btree
 
   create_table "subjects", force: :cascade do |t|
-    t.string   "zooniverse_id",  limit: 255
-    t.jsonb    "metadata",                   default: {}
-    t.jsonb    "locations",                  default: {}
+    t.string   "zooniverse_id"
+    t.jsonb    "metadata",       default: {}
+    t.jsonb    "locations",      default: {}
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "project_id"
     t.boolean  "migrated"
-    t.integer  "lock_version",               default: 0
-    t.string   "upload_user_id", limit: 255
+    t.integer  "lock_version",   default: 0
+    t.string   "upload_user_id"
   end
 
   add_index "subjects", ["project_id"], name: "index_subjects_on_project_id", using: :btree
@@ -273,14 +271,14 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "user_collection_preferences", ["user_id"], name: "index_user_collection_preferences_on_user_id", using: :btree
 
   create_table "user_groups", force: :cascade do |t|
-    t.string   "name",                  limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "classifications_count",             default: 0,    null: false
-    t.integer  "activated_state",                   default: 0,    null: false
-    t.string   "display_name",          limit: 255
-    t.boolean  "private",                           default: true, null: false
-    t.integer  "lock_version",                      default: 0
+    t.integer  "classifications_count", default: 0,    null: false
+    t.integer  "activated_state",       default: 0,    null: false
+    t.string   "display_name"
+    t.boolean  "private",               default: true, null: false
+    t.integer  "lock_version",          default: 0
   end
 
   add_index "user_groups", ["display_name"], name: "index_user_groups_on_display_name", unique: true, using: :btree
@@ -311,31 +309,31 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "user_seen_subjects", ["workflow_id"], name: "index_user_seen_subjects_on_workflow_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                       limit: 255, default: ""
-    t.string   "encrypted_password",          limit: 255, default: "",       null: false
-    t.string   "reset_password_token",        limit: 255
+    t.string   "email",                       default: ""
+    t.string   "encrypted_password",          default: "",       null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                           default: 0,        null: false
+    t.integer  "sign_in_count",               default: 0,        null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",          limit: 255
-    t.string   "last_sign_in_ip",             limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "hash_func",                   limit: 255, default: "bcrypt"
-    t.string   "password_salt",               limit: 255
-    t.string   "display_name",                limit: 255
-    t.string   "zooniverse_id",               limit: 255
-    t.string   "credited_name",               limit: 255
-    t.integer  "classifications_count",                   default: 0,        null: false
-    t.integer  "activated_state",                         default: 0,        null: false
-    t.string   "languages",                               default: [],       null: false, array: true
+    t.string   "hash_func",                   default: "bcrypt"
+    t.string   "password_salt"
+    t.string   "display_name"
+    t.string   "zooniverse_id"
+    t.string   "credited_name"
+    t.integer  "classifications_count",       default: 0,        null: false
+    t.integer  "activated_state",             default: 0,        null: false
+    t.string   "languages",                   default: [],       null: false, array: true
     t.boolean  "global_email_communication"
     t.boolean  "project_email_communication"
-    t.boolean  "admin",                                   default: false,    null: false
-    t.boolean  "banned",                                  default: false,    null: false
-    t.boolean  "migrated",                                default: false
+    t.boolean  "admin",                       default: false,    null: false
+    t.boolean  "banned",                      default: false,    null: false
+    t.boolean  "migrated",                    default: false
     t.text     "avatar"
   end
 
@@ -344,10 +342,10 @@ ActiveRecord::Schema.define(version: 20150427204917) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
-    t.string   "item_type",      limit: 255, null: false
-    t.integer  "item_id",                    null: false
-    t.string   "event",          limit: 255, null: false
-    t.string   "whodunnit",      limit: 255
+    t.string   "item_type",      null: false
+    t.integer  "item_id",        null: false
+    t.string   "event",          null: false
+    t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
     t.text     "object_changes"
@@ -357,30 +355,30 @@ ActiveRecord::Schema.define(version: 20150427204917) do
 
   create_table "workflow_contents", force: :cascade do |t|
     t.integer  "workflow_id"
-    t.string   "language",    limit: 255
+    t.string   "language"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.json     "strings",                 default: {}, null: false
+    t.json     "strings",     default: {}, null: false
   end
 
   add_index "workflow_contents", ["workflow_id"], name: "index_workflow_contents_on_workflow_id", using: :btree
 
   create_table "workflows", force: :cascade do |t|
-    t.string   "display_name",                      limit: 255
-    t.jsonb    "tasks",                                         default: {}
+    t.string   "display_name"
+    t.jsonb    "tasks",                             default: {}
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "classifications_count",                         default: 0,     null: false
-    t.boolean  "pairwise",                                      default: false, null: false
-    t.boolean  "grouped",                                       default: false, null: false
-    t.boolean  "prioritized",                                   default: false, null: false
-    t.string   "primary_language",                  limit: 255
-    t.string   "first_task",                        limit: 255
+    t.integer  "classifications_count",             default: 0,     null: false
+    t.boolean  "pairwise",                          default: false, null: false
+    t.boolean  "grouped",                           default: false, null: false
+    t.boolean  "prioritized",                       default: false, null: false
+    t.string   "primary_language"
+    t.string   "first_task"
     t.integer  "tutorial_subject_id"
-    t.integer  "lock_version",                                  default: 0
-    t.integer  "retired_set_member_subjects_count",             default: 0
-    t.jsonb    "retirement",                                    default: {}
+    t.integer  "lock_version",                      default: 0
+    t.integer  "retired_set_member_subjects_count", default: 0
+    t.jsonb    "retirement",                        default: {}
   end
 
   add_index "workflows", ["project_id"], name: "index_workflows_on_project_id", using: :btree
