21b58d0694848c8e2ca25964f082296a010bd821
Campbell Allen <campbell.allen@gmail.com>
2015-05-03 20:47:14 +0100

    spec the classification csv formatter

--- lib/presenter/formatter_csv_classification.rb
+++ lib/presenter/formatter_csv_classification.rb
@@ -8,10 +8,10 @@ module Formatter
       def self.project_headers
         %w( user_id user_ip workflow_id created_at
             gold_standard expert metadata annotations
-            linked_subjects_data )
+            subject_data )
       end
 
-      def initialize(project, show_user_id: true)
+      def initialize(project, show_user_id: false)
         @project = project
         @show_user_id = show_user_id
       end
@@ -28,14 +28,14 @@ module Formatter
       def user_id
         user_id = classification.user_id
         return user_id if show_user_id
-        "#{user_id}".hash
+        user_id.hash
       end
 
       def user_ip
         classification.user_ip.to_s
       end
 
-      def linked_subjects_data
+      def subject_data
         {}.tap do |subjects_and_metadata|
           subjects = Subject.where(id: classification.subject_ids)
           subjects.each do |subject|
---
+++ spec/lib/presenter/formatter_csv_classification_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+require 'formatter_csv_classification'
+
+RSpec.describe Formatter::CSV::Classification do
+
+  def project_headers
+    %w( user_id user_ip workflow_id created_at
+        gold_standard expert metadata annotations
+        subject_data )
+  end
+
+  def subject_data
+    {
+      "1" => { loudness: 11, brightness: -20, distance_from_earth: "42 light years" }
+    }.to_json
+ end
+
+  def formatted_data
+    [ classification.user_id.hash,
+      classification.user_ip.to_s,
+      classification.workflow_id,
+      classification.created_at,
+      classification.gold_standard,
+      classification.expert_classifier,
+      classification.metadata.to_json,
+      classification.annotations.to_json,
+      subject_data
+    ]
+  end
+
+  let(:project) { build(:project) }
+  let(:classification) { create(:classification, project: project) }
+  let(:formatter) { Formatter::CSV::Classification.new(project) }
+
+  describe "::project_headers?" do
+    it 'should be have the expected headers' do
+      expect(formatter.class.project_headers).to match_array(project_headers)
+    end
+  end
+
+  describe "#to_array" do
+
+    before(:each) do
+      allow(formatter).to receive(:subject_data).and_return(subject_data)
+    end
+
+    it 'return an array formatted classifcation data' do
+      expect(formatter.to_array(classification)).to match_array(formatted_data)
+    end
+
+    context "when the show user id flag is true" do
+
+      it 'return the real user id in the array' do
+        allow(formatter).to receive(:show_user_id).and_return(true)
+        user_id = formatter.to_array(classification)[0]
+        expect(user_id).to eq(classification.user_id)
+      end
+    end
+  end
+end
