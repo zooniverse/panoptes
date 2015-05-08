92a05cde69f0bde2f59eba9dd9d438102285e2a2
Edward Paget <edward@cassetteta.pe>
2015-05-08 12:54:08 -0500

    Remove unused file and add migrations for avatars

--- db/migrate/20150506195759_remove_avatar_background_from_project.rb
+++ db/migrate/20150506195759_remove_avatar_background_from_project.rb
@@ -1,5 +1,26 @@
 class RemoveAvatarBackgroundFromProject < ActiveRecord::Migration
   def change
+    ps_with_avatar_or_background = Project.where.not(avatar: nil)
+      .or(Project.where.not(background_image: nil))
+    total = ps_with_avatar_or_background.count
+    ps_with_avatar_or_background.find_each.with_index do |ps, i|
+      p "#{i+1} of #{total}"
+      if ps.avatar
+        Medium.create!(external_link: true,
+                       content_type: "image/*",
+                       src: ps.avatar,
+                       linked: ps,
+                       type: "project_avatar")
+      end
+
+      if ps.background_image
+        Medium.create!(external_link: true,
+                       content_type: "image/*",
+                       src: ps.background_iamge,
+                       linked: ps,
+                       type: "project_background")
+      end
+    end
     remove_column :projects, :avatar, :text
     remove_column :projects, :background_image, :text
   end
--- db/migrate/20150506195817_remove_avatar_from_user.rb
+++ db/migrate/20150506195817_remove_avatar_from_user.rb
@@ -1,5 +1,15 @@
 class RemoveAvatarFromUser < ActiveRecord::Migration
   def change
+    users_with_avatars = User.where.not(avatar: nil)
+    total = users_with_avatars.count
+    users_with_avatars.find_each.with_index do |u,i|
+      p "#{i+1} of #{total}"
+      Medium.create!(external_link: true,
+                     content_type: "image/*",
+                     src: u.avatar,
+                     linked: u,
+                     type: "user_avatar")
+    end
     remove_column :users, :avatar, :text
   end
 end
