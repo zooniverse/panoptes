fbbb4b3eb6316e259a4d99f7e69a71f8403a44c0
Edward Paget <edward@cassetteta.pe>
2015-05-11 15:29:55 -0500

    Add migration for existing recents

--- db/migrate/20150507212315_create_recents.rb
+++ db/migrate/20150507212315_create_recents.rb
@@ -6,5 +6,13 @@ class CreateRecents < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    total = Classification.count
+    Classification.find_each.with_index do |c,i|
+      p "#{i+1} of #{total}"
+      c.subject_ids.each do |s|
+        Recent.create!(classification: c, subject_id: s)
+      end
+    end
   end
 end
