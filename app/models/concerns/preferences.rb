8e0aa2c1239f7d5f76d04a5c7001bd9484ca8a9d
Campbell Allen <campbell.allen@gmail.com>
2015-05-12 12:21:53 +0100

    do not cascade destroy from the preferences instances
    We do not want to destroy the  user / collection model when we remove the prefernces instance. They are set correctly when removing a user it will remove the linked preferences instances.

--- app/models/concerns/preferences.rb
+++ app/models/concerns/preferences.rb
@@ -2,17 +2,17 @@ module Preferences
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :user, dependent: :destroy
+    belongs_to :user
     validates_presence_of :user
   end
 
   module ClassMethods
     def preferences_for(preference)
       @preferences_for = preference
-      belongs_to @preferences_for, dependent: :destroy
+      belongs_to @preferences_for
       validates_presence_of @preferences_for
     end
-    
+
     def scope_for(action, user, opts={})
       return all if user.is_admin?
       user.send("#{@preferences_for}_preferences")
