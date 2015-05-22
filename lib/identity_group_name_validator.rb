7fd4dde946f1556ea7105fd59347aa8fba3637aa
Campbell Allen <campbell.allen@gmail.com>
2015-05-22 16:08:31 +0100

    report all identity group name field errors

--- lib/identity_group_name_validator.rb
+++ lib/identity_group_name_validator.rb
@@ -2,10 +2,22 @@ class IdentityGroupNameValidator < ActiveModel::Validator
   def validate(user)
     if identity_group = user.try(:identity_membership).try(:user_group)
       unless identity_group.valid?
-        user.errors[:"identity_group.display_name"].concat(identity_group.errors[:name])
+        error_list = name_errors(identity_group)
+        user.errors[:"identity_group.display_name"].concat(error_list)
       end
     else
       user.errors.add(:identity_group, "can't be blank")
+    end
+  end
+
+  private
+
+  def name_errors(identity_group)
+    [].tap do |error_list|
+      %i( display_name name ).each do |attr|
+        attr_errors = identity_group.errors[attr]
+        error_list.concat(attr_errors) unless attr_errors.empty?
+      end
     end
   end
 end
