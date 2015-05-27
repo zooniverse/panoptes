b2f5a36656e6d540cf9bb7da23f2279346b678c3
Edward Paget <edward@cassetteta.pe>
2015-05-27 17:19:22 -0500

    Combine add_subjects into the USS creation method

--- app/models/user_seen_subject.rb
+++ app/models/user_seen_subject.rb
@@ -7,14 +7,10 @@ class UserSeenSubject < ActiveRecord::Base
   def self.add_seen_subjects_for_user(user: nil, workflow: nil, subject_ids: nil)
     uss = where(user: user, workflow: workflow)
     if uss.exists?
-      uss.add_subjects(subject_ids)
+      uss.update_all(["subject_ids = uniq(subject_ids + array[?])", subject_ids])
     else
       uss.create!(subject_ids: subject_ids)
     end
-  end
-
-  def self.add_subjects(subjects)
-    update_all(["subject_ids = uniq(subject_ids + array[?])", subjects])
   end
 
   def subjects_seen?(ids)
