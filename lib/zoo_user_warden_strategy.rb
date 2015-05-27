1f9b608ced32e9de64bca29527be28514d175abe
Campbell Allen <campbell.allen@gmail.com>
2015-05-27 09:35:31 +0100

    remove zoo home warden strategy
    strategies are cascading, do not add the overhead of zoo home connection / import.

--- lib/zoo_user_warden_strategy.rb
+++ lib/zoo_user_warden_strategy.rb
@@ -8,7 +8,7 @@ class ZooUserWardenStrategy < Devise::Strategies::Authenticatable
   def authenticate!
     zoo_home_user = ZooniverseUser.authenticate(params['user']['display_name'],
                                                 params['user']['password'])
-    
+
     if zoo_home_user && imported_user = zoo_home_user.import
       remember_me(imported_user)
       imported_user.after_database_authentication
@@ -20,6 +20,3 @@ class ZooUserWardenStrategy < Devise::Strategies::Authenticatable
     end
   end
 end
-
-
-Warden::Strategies.add(:zoo_user, ZooUserWardenStrategy)
