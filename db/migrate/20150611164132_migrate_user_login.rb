df3ae3cb8fc2aa913a6222a61131f0aebc45e8cf
Michael Parrish <michael@zooniverse.org>
2015-06-11 11:43:09 -0500

    User login data migration

---
+++ db/migrate/20150611164132_migrate_user_login.rb
@@ -0,0 +1,31 @@
+class MigrateUserLogin < ActiveRecord::Migration
+  def up
+    total = User.count
+    validator = LoginUniquenessValidator.new
+
+    User.find_each.with_index do |user, index|
+      puts "#{ index } / #{ total }" if index % 1_000 == 0
+      sanitized_login = User.sanitize_login user.display_name
+
+      user.login = sanitized_login
+      counter = 0
+
+      validator.validate user
+      until user.errors[:login].empty?
+        if user.errors[:login]
+          user.login = "#{ sanitized_login }-#{ counter += 1 }"
+        end
+
+        user.errors[:login].clear
+        validator.validate user
+      end
+
+      user.update_attributes login: user.login
+    end
+
+    change_column :users, :login, :string, null: false
+  end
+
+  def down
+  end
+end
