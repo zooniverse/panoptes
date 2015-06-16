b8027f73235c548ae603970bd5618aa9aaaf49df
Michael Parrish <michael@zooniverse.org>
2015-06-16 10:39:51 -0500

    Revert "add missing lower unique email  index from schema"
    This reverts commit deae020227c965f32ff8b8b9fcc803a26c0007ae.

--- db/migrate/20150616134342_add_lower_index_to_users_email.rb
+++
@@ -1,6 +0,0 @@
-class AddLowerIndexToUsersEmail < ActiveRecord::Migration
-  def change
-    add_index :users, :email, name: "index_users_on_lowercase_email",
-     unique: true, case_sensitive: false
-  end
-end
