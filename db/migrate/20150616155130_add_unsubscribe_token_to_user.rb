4db5dbb31c16f74d8b42192776fd11c16aad0358
Campbell Allen <campbell.allen@gmail.com>
2015-06-23 18:20:10 +0100

    allow null for existing users
    unsubscribe token should get a null: false after migrating

--- db/migrate/20150616155130_add_unsubscribe_token_to_user.rb
+++ db/migrate/20150616155130_add_unsubscribe_token_to_user.rb
@@ -1,6 +1,6 @@
 class AddUnsubscribeTokenToUser < ActiveRecord::Migration
   def change
-    add_column :users, :unsubscribe_token, :string, null: false
+    add_column :users, :unsubscribe_token, :string
     add_index  :users, :unsubscribe_token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -315,7 +315,7 @@ ActiveRecord::Schema.define(version: 20150624155122) do
     t.integer  "project_id"
     t.boolean  "beta_email_communication",    index: {name: "index_users_on_beta_email_communication", where: "(beta_email_communication IS TRUE)"}
     t.string   "login",                       index: {name: "index_users_on_login", unique: true, case_sensitive: false}
-    t.string   "unsubscribe_token",           null: false, index: {name: "index_users_on_unsubscribe_token", unique: true}
+    t.string   "unsubscribe_token",           index: {name: "index_users_on_unsubscribe_token", unique: true}
   end
   add_index "users", ["display_name"], name: "users_display_name_trgm_index", using: :gist, operator_class: "gist_trgm_ops"
 
