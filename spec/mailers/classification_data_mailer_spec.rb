06f43757ac4c29527b0c16ede88edd2edf039b70
Campbell Allen <campbell.allen@gmail.com>
2015-06-24 16:39:15 +0100

    fix rebase error

--- spec/mailers/classification_data_mailer_spec.rb
+++ spec/mailers/classification_data_mailer_spec.rb
@@ -2,5 +2,4 @@ require "spec_helper"
 
 RSpec.describe ClassificationDataMailer, :type => :mailer do
   it_behaves_like "data mailer", "Classification", :classification_data
-  it_behaves_like "email generic unsubscribe links"
 end
