b0779d6a2de4ee019d80b16965b189ac1007932d
Marten Veldthuis <marten@veldthuis.com>
2015-06-24 12:14:20 +0200

    Port specs to AnnotationForCsv

---
+++ spec/lib/formatter/csv/annotation_for_csv_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe Formatter::Csv::AnnotationForCsv do
+  let(:classification) { create(:classification) }
+
+  let(:annotation) do
+    {
+      "task" => "interest",
+      "value" => [{"x"=>1, "y"=>2, "tool"=>1},
+                  {"x"=>3, "y"=>4, "tool"=>2},
+                  {"x"=>5, "y"=>6, "tool"=>1}]
+    }
+  end
+
+  it 'adds the tool labels for drawing tasks' do
+    formatted = described_class.new(classification, annotation).to_h
+    expect(formatted["value"][0]["tool_label"]).to eq("Green")
+    expect(formatted["value"][1]["tool_label"]).to eq("Blue")
+    expect(formatted["value"][2]["tool_label"]).to eq("Green")
+  end
+end
