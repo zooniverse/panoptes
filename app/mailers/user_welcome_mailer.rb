dfe3a577e0de0ee9e72ec90ab48b5e7358b1d7b5
Campbell Allen <campbell.allen@gmail.com>
2015-06-25 18:06:19 +0100

    welcome mailer copy

--- app/mailers/user_welcome_mailer.rb
+++ app/mailers/user_welcome_mailer.rb
@@ -1,17 +1,13 @@
 class UserWelcomeMailer < ApplicationMailer
+  layout false
 
-  DEFAULT_SUBJECT = "Welcome to the Zooniverse!"
+  SITE_NAME = "the Zooniverse"
+  DEFAULT_SUBJECT = "Welcome to #{SITE_NAME}! Your Account & What’s Next"
 
-  def welcome_user(user)
+  def welcome_user(user, project=nil)
     @user = user
     @email_to = user.email
-    mail(to: @email_to, subject: DEFAULT_SUBJECT)
-  end
-
-  def project_welcome_user(user, project)
-    @user = user
-    @project = project
-    @email_to = user.email
+    @welcome_location = project ? project : SITE_NAME
     mail(to: @email_to, subject: DEFAULT_SUBJECT)
   end
 end
