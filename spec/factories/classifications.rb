2e421b544c49e0e102986c96e3475fcc77c44745
Campbell Allen <campbell.allen@gmail.com>
2015-06-26 11:03:19 +0100

    reduce number of created associations
    creating a list of subject_set cascades builds down the factory association chain. Quick and dirty attempt to avoid persisting these records but it needs more work to fix it properly.

--- spec/factories/classifications.rb
+++ spec/factories/classifications.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :classification do
+    transient do
+      build_real_subjects true
+    end
+
     metadata({
               user_agent: "cURL",
               started_at: 2.minutes.ago.to_s,
@@ -16,7 +20,15 @@ FactoryGirl.define do
     user
     project
     workflow
-    subject_ids { create_list(:set_member_subject, 2).map(&:subject).map(&:id) }
+    subject_ids do
+      if build_real_subjects
+        #TODO: clean this up -> this cascading create_list always builds:
+        # 7 Projects, 3 Workflows, 2 SubjectSets, 2 Subjects
+        create_list(:set_member_subject, 2).map(&:subject).map(&:id)
+      else
+        (1..10).to_a.sample(2)
+      end
+    end
 
     factory(:classification_with_recents) do
       after(:build) do |c|
