12b296e0dc89d41807eba15846ec57bbc854b05f
Campbell Allen <campbell.allen@gmail.com>
2015-07-02 14:25:41 +0100

    drop index if it already exists

--- db/migrate/20150630144332_add_zooniverse_id_index_to_users.rb
+++ db/migrate/20150630144332_add_zooniverse_id_index_to_users.rb
@@ -1,5 +1,8 @@
 class AddZooniverseIdIndexToUsers < ActiveRecord::Migration
   def change
+    if index_exists?(:users, :zooniverse_id)
+      remove_index :users, column: :zooniverse_id
+    end
     add_index :users, :zooniverse_id, unique: true
   end
 end
