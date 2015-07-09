710ff799eb07b7ee9ee613f8c5cec88bd8c48323
Campbell Allen <campbell.allen@gmail.com>
2015-07-09 16:24:20 +0100

    do not welcome users without an email
    Users created via the API and then deactivated / deleted will not have an email. Staging tests this process and may try to fire emails with a to field.

--- app/workers/user_welcome_mailer_worker.rb
+++ app/workers/user_welcome_mailer_worker.rb
@@ -4,7 +4,8 @@ class UserWelcomeMailerWorker
   attr_reader :user, :project
 
   def perform(user_id, project_id=nil)
-    if @user = User.find(user_id)
+    @user = User.find(user_id)
+    if @user && !@user.email.blank?
       @project_name = if project_id
         Project.find(project_id).try(:display_name)
       end
