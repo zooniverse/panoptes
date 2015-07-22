dcfc19410d1a089439bb55851bc51d63afb4a024
Campbell Allen <campbell.allen@gmail.com>
2015-07-22 17:00:06 +0100

    return sms to classify when user has seen all non-retired

--- lib/set_member_subject_selector.rb
+++ lib/set_member_subject_selector.rb
@@ -8,16 +8,11 @@ class SetMemberSubjectSelector
   end
 
   def set_member_subjects
-    if select_from_all?
-      workflow.set_member_subjects.select(SELECT_FIELDS)
+    to_classify = select_set_member_subjects_to_classify
+    if to_classify.empty?
+      select_all_workflow_set_member_subjects
     else
-      SetMemberSubject.select(SELECT_FIELDS)
-        .joins(subject_set: {workflows: :user_seen_subjects})
-        .joins("LEFT OUTER JOIN subject_workflow_counts ON subject_workflow_counts.set_member_subject_id = set_member_subjects.id")
-        .where(user_seen_subjects: {user_id: user.id},
-               workflows: {id: workflow.id})
-        .where(never_seen_before_or_not_retired)
-        .where.not('"set_member_subjects"."subject_id" = ANY("user_seen_subjects"."subject_ids")')
+      to_classify
     end
   end
 
@@ -34,12 +29,25 @@ class SetMemberSubjectSelector
     !user.user_seen_subjects.where(workflow: workflow).exists?
   end
 
-  def never_seen_before_or_not_retired
-    table = SubjectWorkflowCount.arel_table
-
-    never_seen_before = table[:workflow_id].eq(nil)
-    not_retired = table[:workflow_id].eq(workflow.id).and(table[:retired_at].eq(nil))
-
-    never_seen_before.or(not_retired)
+  def select_set_member_subjects_to_classify
+    if select_from_all?
+      select_all_workflow_set_member_subjects
+    else
+      select_non_retired_unseen_for_user
+    end
   end
+
+  def select_all_workflow_set_member_subjects
+    workflow.set_member_subjects.select(SELECT_FIELDS)
+  end
+
+  def select_non_retired_unseen_for_user
+    SetMemberSubject.select(SELECT_FIELDS)
+      .joins(subject_set: {workflows: :user_seen_subjects})
+      .joins("LEFT OUTER JOIN subject_workflow_counts ON subject_workflow_counts.set_member_subject_id = set_member_subjects.id")
+      .where(user_seen_subjects: {user_id: user.id},
+             workflows: {id: workflow.id},
+             subject_workflow_counts: {workflow_id: workflow.id, retired_at: nil})
+      .where.not('"set_member_subjects"."subject_id" = ANY("user_seen_subjects"."subject_ids")')
+    end
 end
--- spec/lib/set_member_subject_selector_spec.rb
+++ spec/lib/set_member_subject_selector_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe SetMemberSubjectSelector do
   let(:count) { create(:subject_workflow_count) }
   let(:user) { create(:user) }
+  let(:seen_subject) { create(:subject, subject_sets: [count.set_member_subject.subject_set]) }
 
   before do
     count.workflow.subject_sets = [count.set_member_subject.subject_set]
@@ -12,18 +13,35 @@ describe SetMemberSubjectSelector do
   context 'when there is a user and they have participated before' do
     before { allow_any_instance_of(SetMemberSubjectSelector).to receive(:select_from_all?).and_return(false) }
 
+    let(:sms_to_classify) { SetMemberSubjectSelector.new(count.workflow, user).set_member_subjects }
+
     it 'does not include subjects that have been seen' do
       seen_subject = create(:subject, subject_sets: [count.set_member_subject.subject_set])
       create(:user_seen_subject, user: user, workflow: count.workflow, subject_ids: [seen_subject.id])
-
-      sms = SetMemberSubjectSelector.new(count.workflow, user).set_member_subjects
-      expect(sms).to eq([count.set_member_subject])
+      expect(sms_to_classify).to eq([count.set_member_subject])
     end
 
     it 'does not include subjects that are retired' do
       count.retire!
-      sms = SetMemberSubjectSelector.new(count.workflow, user).set_member_subjects
-      expect(sms).to be_empty
+      expect(sms_to_classify).to be_empty
+    end
+
+    context "when all the user has seen all non-retired" do
+
+      before(:each) do
+        create(:user_seen_subject, user: user, workflow: count.workflow, subject_ids: [seen_subject.id])
+        create(:subject_workflow_count, set_member_subject: seen_subject.set_member_subjects.first, workflow: count.workflow)
+        count.retire!
+      end
+
+      it "should return something to classify" do
+        expect(sms_to_classify).to_not be_empty
+      end
+
+      it "should select from the whole set of set_member_subjects" do
+        expect_any_instance_of(SetMemberSubjectSelector).to receive(:select_all_workflow_set_member_subjects)
+        sms_to_classify
+      end
     end
   end
 end
