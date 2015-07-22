f86ae15a413419641a5489ce931ca38ae11b9e7e
Marten Veldthuis <marten@veldthuis.com>
2015-07-22 20:03:39 +0200

    Convert subject factory to use traits

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -8,17 +8,19 @@ FactoryGirl.define do
               brightness: -20,
               loudness: 11})
 
-    after(:create) do |s|
-      create_list(:medium, 2, linked: s)
+    trait :with_mediums do
+      after(:create) do |s|
+        create_list(:medium, 2, linked: s)
+      end
     end
 
-    factory :subject_with_collections do
+    trait :with_collections do
       after(:create) do |s|
         create_list(:collection, 2, subjects: [s])
       end
     end
 
-    factory :subject_with_subject_sets do
+    trait :with_subject_sets do
       after(:create) do |s|
         create_list(:set_member_subject, 2, subject: s)
       end
