7ff6e00f82e0a8c7b8067d9c1298cf5df51af688
Campbell Allen <campbell.allen@gmail.com>
2015-08-18 12:46:35 +0100

    move summate method to model

--- app/models/user_project_preference.rb
+++ app/models/user_project_preference.rb
@@ -2,4 +2,18 @@ class UserProjectPreference < ActiveRecord::Base
   include Preferences
 
   preferences_for :project, :classifiers_count
+
+  def summated_activity_count
+    if legacy_count.blank?
+      activity_count
+    else
+      valid_legacy_count_values.reduce(:+)
+    end
+  end
+
+  private
+
+  def valid_legacy_count_values
+    legacy_count.values.compact
+  end
 end
--- spec/factories/user_project_preferences.rb
+++ spec/factories/user_project_preferences.rb
@@ -9,5 +9,14 @@ FactoryGirl.define do
     email_communication true
     preferences '{"tutorial": "done"}'
     activity_count 19
+
+    factory :legacy_user_project_preference do
+      activity_count nil
+      legacy_count '{"bars": 19, "candels": 10}'
+
+      factory :busted_legacy_user_project_preference do
+        legacy_count '{"":null, "radio":"4"}'
+      end
+    end
   end
 end
