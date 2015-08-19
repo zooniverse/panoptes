c4e79c1c9e20cd151d8af6c98d439b96bd73a62d
Campbell Allen <campbell.allen@gmail.com>
2015-08-19 12:32:34 +0100

    add subject get locations to dump

--- spec/lib/formatter/csv/subject_spec.rb
+++ spec/lib/formatter/csv/subject_spec.rb
@@ -4,10 +4,21 @@ RSpec.describe Formatter::Csv::Subject do
   let(:project) { create(:project) }
   let(:workflow) { create(:workflow, project: project) }
   let(:subject_set) { create(:subject_set, project: project, workflows: [workflow]) }
-  let(:sms) { create(:set_member_subject, subject_set: subject_set) }
+  let(:loc_subject) do
+    create(:subject, :with_mediums, project: project, uploader: project.owner)
+  end
+  let(:sms) { create(:set_member_subject, subject_set: subject_set, subject: loc_subject) }
   let!(:swc) do
     create :subject_workflow_count, classifications_count: 10, workflow: workflow,
       set_member_subject: sms, retired_at: DateTime.now
+  end
+
+  def ordered_subject_locations
+    {}.tap do |locs|
+      Medium.all.order(:id).each_with_index.map do |m, index|
+        locs[index] = m.get_url
+      end
+    end
   end
 
   let(:fields) do
@@ -15,13 +26,14 @@ RSpec.describe Formatter::Csv::Subject do
      project.id,
      [workflow.id].to_json,
      subject_set.id,
+     ordered_subject_locations.to_json,
      sms.subject.metadata.to_json,
      {workflow.id => 10}.to_json,
      [workflow.id].to_json]
   end
 
   let(:header) do
-    %w(subject_id project_id workflow_ids subject_set_id metadata classifications_by_workflow retired_in_workflow)
+    %w(subject_id project_id workflow_ids subject_set_id locations metadata classifications_by_workflow retired_in_workflow)
   end
 
   describe "::project_headers" do
