3446e9a5152ed0b3d38d0eb56496d9aee4257628
Edward Paget <edward@cassetteta.pe>
2015-08-19 12:26:08 -0500

    Make sure subject exports and aggregation exports
    ...are private

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -113,7 +113,7 @@ class Api::V1::MediaController < Api::ApiController
 
   def controlled_scope
     case media_name
-    when "classifications_export"
+    when "classifications_export", "subjects_export", "aggregations_export"
       :update
     else
       action_name.to_sym
