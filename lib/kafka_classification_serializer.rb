0da969c7c1419dad21c4585c08c0010ddc126fdb
Marten Veldthuis <marten@veldthuis.com>
2015-08-20 12:07:15 +0100

    Remove embed: :ids

--- lib/kafka_classification_serializer.rb
+++ lib/kafka_classification_serializer.rb
@@ -6,10 +6,10 @@ class KafkaClassificationSerializer < ActiveModel::Serializer
 
   attributes :id, :annotations, :created_at, :metadata
 
-  belongs_to :project, serializer: KafkaProjectSerializer, embed: :ids
+  belongs_to :project, serializer: KafkaProjectSerializer
   belongs_to :user, serializer: KafkaUserSerializer
   belongs_to :workflow, serializer: KafkaWorkflowSerializer
-  has_many   :subjects, serializer: KafkaSubjectSerializer, embed: :ids
+  has_many   :subjects, serializer: KafkaSubjectSerializer
 
   def metadata
     object.metadata.merge(workflow_version: object.workflow_version)
