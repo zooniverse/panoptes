3d00a5dd6fafff4f756a8012e63275b9c53e2a37
Campbell Allen <campbell.allen@gmail.com>
2015-08-24 18:23:28 +0100

    correct live / reified version numbers

--- lib/model_version.rb
+++ lib/model_version.rb
@@ -1,8 +1,11 @@
 module ModelVersion
 
   def self.index_number(versioned_model)
-    if versioned_model && last_version = versioned_model.versions.last
-      last_version.index + 1
+    return unless versioned_model
+    if versioned_model.live?
+      versioned_model.versions.last.index + 1
+    elsif versioned_model.respond_to?(:version)
+      versioned_model.version.index
     else
       1
     end
