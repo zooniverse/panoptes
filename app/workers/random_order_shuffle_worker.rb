0a92ac0e4f5bcd9fa16592bae7444acb64323108
Campbell Allen <campbell.allen@gmail.com>
2015-09-14 11:35:51 +0100

    add rate limiter to index rebuild worker
    using sidekiq-congestion rolling window rate limiter with reschedule, defaulting to 5 req's every 60 secs with a window of 12s.

--- app/workers/random_order_shuffle_worker.rb
+++ app/workers/random_order_shuffle_worker.rb
@@ -1,6 +1,13 @@
 class RandomOrderShuffleWorker
   include Sidekiq::Worker
 
+  sidekiq_options congestion: {
+    interval: 1.minute,
+    max_in_interval: 5,
+    min_delay: 12,
+    reject_with: :reschedule
+  }
+
   def perform(sms_ids)
     SetMemberSubject.where(id: Array.wrap(sms_ids)).update_all("random = RANDOM()")
   end
