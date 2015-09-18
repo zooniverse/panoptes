a139078d81478c7f342de3b2a8997c08d5c02714
Campbell Allen <campbell.allen@gmail.com>
2015-09-18 15:31:02 +0100

    refactor set member subject selector
    make it more readable

--- lib/set_member_subject_selector.rb
+++ lib/set_member_subject_selector.rb
@@ -10,11 +10,17 @@ class SetMemberSubjectSelector
   end
 
   def set_member_subjects
-    to_classify = select_set_member_subjects_to_classify
-    if user && !to_classify.exists?
-      to_classify = SetMemberSubject.unseen_for_user_by_workflow(user, workflow)
+    selection = if !user && !workflow.finished?
+                  select_non_retired
+                elsif select_from_all?
+                  select_all_workflow_set_member_subjects
+                else
+                  select_non_retired_unseen_for_user
+                end
+    if all_user_unseen_data_is_retired?(selection)
+      selection = select_unseen_for_user
     end
-    to_classify
+    selection.select(SELECT_FIELDS)
   end
 
   def select_from_all?
@@ -26,19 +32,12 @@ class SetMemberSubjectSelector
 
   private
 
-  def user_has_not_seen_workflow_subjects?
-    !user.user_seen_subjects.where(workflow: workflow).exists?
+  def all_user_unseen_data_is_retired?(selection)
+    user && !selection.exists?
   end
 
-  def select_set_member_subjects_to_classify
-    selection = if !user && !workflow.finished?
-                  select_non_retired
-                elsif select_from_all?
-                  select_all_workflow_set_member_subjects
-                else
-                  select_non_retired_unseen_for_user
-                end
-    selection.select(SELECT_FIELDS)
+  def user_has_not_seen_workflow_subjects?
+    !user.user_seen_subjects.where(workflow: workflow).exists?
   end
 
   def select_non_retired
@@ -49,8 +48,11 @@ class SetMemberSubjectSelector
     workflow.set_member_subjects
   end
 
-  def select_non_retired_unseen_for_user
+  def select_unseen_for_user
     SetMemberSubject.unseen_for_user_by_workflow(user, workflow)
-    .merge(SetMemberSubject.non_retired_for_workflow(workflow))
+  end
+
+  def select_non_retired_unseen_for_user
+    select_unseen_for_user.merge(select_non_retired)
   end
 end
