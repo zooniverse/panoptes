b11fdf2ae05ee73b6d1c35d43b3325c58c1c0bd2
Marten Veldthuis <marten@veldthuis.com>
2015-11-06 16:40:14 +0000

    Add worker to recalculate counters based on project launch date

---
+++ app/workers/reset_project_counters_worker.rb
@@ -0,0 +1,37 @@
+class ResetProjectCountersWorker
+  include Sidekiq::Worker
+
+  def perform(project_id)
+    project = Project.find(project_id)
+
+    return unless project.launch_date
+
+    project.classifications_count = project.classifications.where("created_at > ?", project.launch_date).count
+
+    project.classifiers_count = UserProjectPreference.connection.execute(<<-SQL)[0]["count"]
+      SELECT COUNT(DISTINCT(upp.user_id)) count FROM user_project_preferences upp
+      INNER JOIN classifications c ON c.user_id = upp.user_id
+      INNER JOIN projects p ON p.id = upp.project_id
+      WHERE upp.project_id = #{project.id} AND c.project_id = #{project.id} AND c.created_at >= p.launch_date;
+    SQL
+
+    project.workflows.pluck(:id).each do |workflow_id|
+      SubjectWorkflowCount.connection.execute <<-SQL
+        UPDATE subject_workflow_counts swc
+        SET classifications_count = sub.actual
+        FROM (
+          SELECT subject_workflow_counts.id, COUNT(cs.classification_id) actual FROM subject_workflow_counts
+          INNER JOIN classification_subjects cs ON cs.subject_id = subject_workflow_counts.subject_id
+          INNER JOIN classifications c ON c.id = cs.classification_id
+          INNER JOIN projects p ON p.id = c.project_id
+          WHERE subject_workflow_counts.workflow_id = #{workflow_id}
+            AND c.created_at >= p.launch_date
+          GROUP BY subject_workflow_counts.id
+        ) as sub
+        WHERE sub.id = swc.id;
+      SQL
+    end
+
+    project.save!
+  end
+end
