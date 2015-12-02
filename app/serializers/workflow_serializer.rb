a52b22c2cb9153465486d2f238296ce18a6fc36f
Campbell Allen <campbell.allen@gmail.com>
2015-12-02 15:16:43 +0000

    update workflow serialized response
    added public_gold_standard and added default for null finished_at

--- app/serializers/workflow_serializer.rb
+++ app/serializers/workflow_serializer.rb
@@ -9,7 +9,7 @@ class WorkflowSerializer
              :created_at, :updated_at, :finished_at, :first_task, :primary_language,
              :version, :content_language, :prioritized, :grouped, :pairwise,
              :retirement, :retired_set_member_subjects_count, :href, :active,
-             :aggregation, :configuration
+             :aggregation, :configuration, :public_gold_standard
 
   can_include :project, :subject_sets, :tutorial_subject, :expert_subject_sets
 
