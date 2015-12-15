9ab500e609da1081b2911b0ba0ac1e4f50ee927f
Campbell Allen <campbell.allen@gmail.com>
2015-12-15 17:40:59 +0000

    cellect / db selection strategies with fallbacks in the worker
    Allow a requested strategy in the worker to override a heirarchy of strategy configs combined with a CELLECT_ON configuration directive to enable disable this feature.  Heirarchy looks like this:
    
    1. override if available and known
    2. workflow config object to run the strategy
    3. subject set size > than min pool used for for larger projects
    4. graceful fallback to pg if failure as last resort

--- app/workers/enqueue_subject_queue_worker.rb
+++ app/workers/enqueue_subject_queue_worker.rb
@@ -7,14 +7,14 @@ class EnqueueSubjectQueueWorker
     }
   })
 
-  attr_reader :workflow, :user, :subject_set_id, :limit, :strategy
+  attr_reader :workflow, :user, :subject_set_id, :limit, :selection_strategy
 
-  def perform(workflow_id, user_id=nil, subject_set_id=nil, limit=SubjectQueue::DEFAULT_LENGTH, strategy=nil)
+  def perform(workflow_id, user_id=nil, subject_set_id=nil, limit=SubjectQueue::DEFAULT_LENGTH, strategy_override=nil)
     @workflow = Workflow.find(workflow_id)
     @user = User.find(user_id) if user_id
     @subject_set_id = subject_set_id
     @limit = limit
-    @strategy = strategy
+    @selection_strategy = strategy(strategy_override)
 
     begin
       subject_ids = selected_subject_ids.compact
@@ -29,19 +29,41 @@ class EnqueueSubjectQueueWorker
     nil
   end
 
+  def strategy(requested)
+    return nil unless Panoptes.cellect_on
+    return requested if requested == :cellect
+    case
+    when workflow_strategy
+      workflow_strategy
+    when cellect_size_subject_space?
+       :cellect
+    else
+      nil
+    end
+  end
+
   private
 
   def selected_subject_ids
-    case strategy
+    ids = case selection_strategy
     when :cellect
       Subjects::CellectClient.get_subjects({}, workflow.try(:id), user.try(:id), subject_set_id, limit)
     else
       default_strategy_ids
     end
+    Array.wrap(ids)
   end
 
   def default_strategy_ids
     Subjects::PostgresqlSelection.new(workflow, user)
     .select(limit: limit, subject_set_id: subject_set_id)
+  end
+
+  def workflow_strategy
+    @workflow_strategy ||= workflow.configuration.with_indifferent_access[:selection_strategy]
+  end
+
+  def cellect_size_subject_space?
+    workflow.set_member_subjects.count >= Panoptes.cellect_min_pool_size
   end
 end
--- spec/workers/enqueue_subject_queue_worker_spec.rb
+++ spec/workers/enqueue_subject_queue_worker_spec.rb
@@ -35,13 +35,6 @@ RSpec.describe EnqueueSubjectQueueWorker do
       end
     end
 
-    context "when the strategy param is not set" do
-      it "should fall back to postgresql strategy" do
-        expect_any_instance_of(Subjects::PostgresqlSelection).to receive(:select)
-        subject.perform(workflow.id)
-      end
-    end
-
     context "with a user" do
 
       it 'should create a subject queue for the user' do
@@ -60,20 +53,39 @@ RSpec.describe EnqueueSubjectQueueWorker do
       end
     end
 
-    context "when selecting via cellect selection strategy" do
-      it 'should attempt to queue the selected set' do
-        allow_any_instance_of(Subjects::CellectClient).to receive(:get_subjects).and_return([1])
+    context "when selecting via strategy param" do
+      let(:result_ids) { [1] }
+
+      it 'should fallback from the override strategy' do
+        allow_any_instance_of(Subjects::PostgresqlSelection).to receive(:select).and_return(result_ids)
         expect(SubjectQueue).to receive(:enqueue)
-        subject.perform(workflow.id)
+        subject.perform(workflow.id, nil, nil, nil, :unknown_strategy)
       end
 
-      context "when the cellect client can't reach a server" do
+      context "with cellect" do
+        let(:run_selection) { subject.perform(workflow.id, nil, nil, nil, :cellect) }
+        before do
+          allow(Panoptes).to receive(:cellect_on).and_return(true)
+        end
 
-        it "should fall back to postgres strategy" do
-          allow(Subjects::CellectClient).to receive(:get_subjects)
-            .and_raise(Subjects::CellectClient::ConnectionError)
-          expect_any_instance_of(Subjects::PostgresqlSelection).to receive(:select)
-          subject.perform(workflow.id, nil, nil, nil, :cellect)
+        it 'should use the cellect client' do
+          expect(Subjects::CellectClient).to receive(:get_subjects).and_return(result_ids)
+          run_selection
+        end
+
+        it 'should attempt to queue the selected set' do
+          allow(Subjects::CellectClient).to receive(:get_subjects).and_return(result_ids)
+          expect(SubjectQueue).to receive(:enqueue)
+          run_selection
+        end
+
+        context "when the cellect client can't reach a server" do
+          it "should fall back to postgres strategy" do
+            allow(Subjects::CellectClient).to receive(:get_subjects)
+              .and_raise(Subjects::CellectClient::ConnectionError)
+            expect_any_instance_of(Subjects::PostgresqlSelection).to receive(:select)
+            run_selection
+          end
         end
       end
     end
@@ -98,6 +110,76 @@ RSpec.describe EnqueueSubjectQueueWorker do
         it 'should not attempt to queue an empty set' do
           expect(SubjectQueue).to_not receive(:enqueue)
           subject.perform(workflow.id)
+        end
+      end
+    end
+  end
+
+  describe "#strategy" do
+    let(:config) {  { selection_strategy: :cellect } }
+    let(:cellect_size) { Panoptes.cellect_min_pool_size }
+
+    before do
+      allow(subject).to receive(:workflow).and_return(workflow)
+    end
+
+    it "should fall back to postgresql strategy when not set" do
+      expect(subject.strategy(nil)).to eq(nil)
+    end
+
+    context "when Cellect config is on" do
+      before do
+        allow(Panoptes).to receive(:cellect_on).and_return(true)
+      end
+
+      it "should use the supplied strategy" do
+        expect(subject.strategy(:cellect)).to eq(:cellect)
+      end
+
+      context "when the workflow config has a selection strategy" do
+        it "should use the workflow config strategy" do
+          allow(workflow).to receive(:configuration).and_return(config)
+          expect(subject.strategy(nil)).to eq(:cellect)
+        end
+      end
+
+      context "when the number of set_member_subjects is large" do
+        it "should use the cellect strategy" do
+          allow(workflow).to receive_message_chain("set_member_subjects.count") { cellect_size }
+          expect(subject.strategy(nil)).to eq(:cellect)
+        end
+      end
+
+      context "with a workflow config and large subject set size" do
+        it "should only use the worfklow strategy", :aggregate_failures do
+          allow(workflow).to receive(:configuration).and_return(config)
+          allow(workflow).to receive_message_chain("set_member_subjects.count") { cellect_size }
+          expect(workflow).not_to receive(:set_member_subjects)
+          expect(subject.strategy(nil)).to eq(:cellect)
+        end
+      end
+    end
+
+    context "when Cellect Config is off" do
+      before do
+        allow(Panoptes).to receive(:cellect_on).and_return(false)
+      end
+
+      it "should never use cellect" do
+        expect(subject.strategy(:cellect)).to eq(nil)
+      end
+
+      context "when the workflow config has a selection strategy" do
+        it "should not ask the workflow" do
+          expect(subject).not_to receive(:workflow_strategy)
+          expect(subject.strategy(nil)).to eq(nil)
+        end
+      end
+
+      context "when the number of set_member_subjects is large" do
+        it "should not query the cellect size method" do
+          expect(subject).not_to receive(:cellect_size_subject_space?)
+          expect(subject.strategy(nil)).to eq(nil)
         end
       end
     end
