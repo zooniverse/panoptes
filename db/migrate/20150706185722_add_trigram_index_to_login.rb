e4b11674e9f911e3c5fe3c930ab7d2d9dff1d1fc
Campbell Allen <campbell.allen@gmail.com>
2015-12-31 15:25:05 +0000

    fix busted old migrations
    allow the schema to be rebuilt from migrations, this should be backwards compatible but i didn’t invest much time in it.

--- db/migrate/20150706185722_add_trigram_index_to_login.rb
+++ db/migrate/20150706185722_add_trigram_index_to_login.rb
@@ -1,7 +1,9 @@
 class AddTrigramIndexToLogin < ActiveRecord::Migration
-  def change
+  def up
     remove_index :users, name: "users_display_name_trgm_index"
-    add_index :users, name: "users_idx_trgm_login_display_name", using: :gin, operator_class: :gin_trgm_ops,
-      expression: %((coalesce("users"."login"::text, '') || ' ' || coalesce("users"."display_name"::text, '')) gin_trgm_ops)
+    execute <<-SQL
+      CREATE INDEX users_idx_trgm_login_display_name ON users
+      USING gin((coalesce("users"."login"::text, '') || ' ' || coalesce("users"."display_name"::text, '')) gin_trgm_ops);
+    SQL
   end
 end
