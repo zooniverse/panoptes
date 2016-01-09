7e8e4aa8313ae135b0a92e23b6892b0c78cc1a61
Campbell Allen <campbell.allen@gmail.com>
2016-01-09 16:50:48 +0000

    reload worker should try 3 times and give up

--- app/workers/reload_cellect_worker.rb
+++ app/workers/reload_cellect_worker.rb
@@ -2,12 +2,13 @@ require 'subjects/cellect_client'
 
 class ReloadCellectWorker
   include Sidekiq::Worker
+  sidekiq_options retry: 3
 
   def perform(workflow_id)
     workflow = Workflow.find(workflow_id)
     if Panoptes.use_cellect?(workflow)
       Subjects::CellectClient.reload_workflow(workflow_id)
     end
-  rescue Subjects::CellectClient::ConnectionError, ActiveRecord::RecordNotFound
+  rescue ActiveRecord::RecordNotFound
   end
 end
