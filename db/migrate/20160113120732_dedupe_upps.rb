3a18b8db1e383a7bc98dd6abd64a8c888cc337e7
Marten Veldthuis <marten@veldthuis.com>
2016-01-13 13:25:45 +0000

    Do the index concurrently to avoid lock

--- db/migrate/20160113120732_dedupe_upps.rb
+++ db/migrate/20160113120732_dedupe_upps.rb
@@ -1,19 +1,19 @@
 class DedupeUpps < ActiveRecord::Migration
-  def change
-    UserProjectPreference.transaction do
-      UserProjectPreference.connection.execute <<-SQL
-        DELETE FROM user_project_preferences
-        WHERE id IN (
-          SELECT id
-          FROM (
-            SELECT id, ROW_NUMBER() OVER (PARTITION BY user_id, project_id ORDER BY id ASC) AS row
-            FROM user_project_preferences
-          ) dupes
-          WHERE dupes.row > 1
-        );
-      SQL
+  disable_ddl_transaction!
 
-      add_index :user_project_preferences, [:user_id, :project_id], unique: true
-    end
+  def change
+    UserProjectPreference.connection.execute <<-SQL
+      DELETE FROM user_project_preferences
+      WHERE id IN (
+        SELECT id
+        FROM (
+          SELECT id, ROW_NUMBER() OVER (PARTITION BY user_id, project_id ORDER BY id ASC) AS row
+          FROM user_project_preferences
+        ) dupes
+        WHERE dupes.row > 1
+      );
+    SQL
+
+    add_index :user_project_preferences, [:user_id, :project_id], unique: true, algorithm: :concurrently
   end
 end
