95a2ded4595b549f6dcdb02f594113099aa4eb14
Campbell Allen <campbell.allen@gmail.com>
2016-02-10 15:52:42 +0000

    format event time to iso8601 before pushing to kafka

--- lib/event_stream.rb
+++ lib/event_stream.rb
@@ -2,8 +2,18 @@ module EventStream
   def self.push(event_type, event_id: SecureRandom.uuid, event_time: Time.now.utc, **metadata)
     event_type = "panoptes.#{event_type}"
     event_id   = "#{event_type}.#{event_id}"
-    event_data = metadata.merge(event_id: event_id, event_time: event_time, event_type: event_type).to_json
+    event_data = metadata.merge(
+      event_id: event_id,
+      event_time: formatted_time(event_time),
+      event_type: event_type
+    ).to_json
 
     MultiKafkaProducer.publish('events', [event_id, event_data])
+  end
+
+  def self.formatted_time(time)
+    return time.iso8601 if time.respond_to?(:iso8601)
+    valid_time_str = Time.parse(time)
+    valid_time_str.iso8601
   end
 end
