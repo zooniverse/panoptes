3805118a52442d70b7cef60cb5b1e7a30be10b97
Marten Veldthuis <marten@veldthuis.com>
2016-03-07 17:40:29 +0000

    Remove duplicate specs (moved to Random selector)

--- spec/lib/subjects/postgresql_selection_spec.rb
+++ spec/lib/subjects/postgresql_selection_spec.rb
@@ -26,20 +26,6 @@ RSpec.describe Subjects::PostgresqlSelection do
     describe "#select" do
       describe "random selection" do
         it_behaves_like "select for incomplete_project"
-
-        it "should reassign the random attribute after selection" do
-          allow(Panoptes::SubjectSelection).to receive(:index_rebuild_rate).and_return(1)
-          expect(RandomOrderShuffleWorker).to receive(:perform_async).once
-          subject.select
-        end
-
-        it "should give up trying to construct a random list after set number of attempts" do
-          unreachable_limit = SetMemberSubject.count + 1
-          allow_any_instance_of(subject.class).to receive(:available_count).and_return(unreachable_limit + 1)
-          allow_any_instance_of(subject.class).to receive(:limit).and_return(unreachable_limit)
-          results = subject.select
-          expect(results).to eq(results)
-        end
       end
 
       context "grouped selection" do
