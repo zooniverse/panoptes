46e49deb50531adefe8fa654649475ff053f7a92
Marten Veldthuis <marten@veldthuis.com>
2016-03-09 10:02:04 +0000

    Do not send emails when recipients list is empty

--- app/workers/workflow_content_data_mailer_worker.rb
+++ app/workers/workflow_content_data_mailer_worker.rb
@@ -4,6 +4,7 @@ class WorkflowContentDataMailerWorker
   sidekiq_options queue: :data_high
 
   def perform(project_id, s3_url, emails)
+    return unless emails.present?
     WorkflowContentDataMailer.workflow_content_data(Project.find(project_id), s3_url.to_s, emails).deliver
   end
 end
--- spec/workers/subject_data_mailer_worker_spec.rb
+++ spec/workers/subject_data_mailer_worker_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe SubjectDataMailerWorker do
   it 'should deliver the mail' do
     expect{ subject.perform(project.id, s3_url, ["ed.paget@gmail.com"]) }.to change{ ActionMailer::Base.deliveries.count }.by(1)
   end
+
+  context 'when there are no recipients' do
+    it 'does not call the mailer' do
+      expect(SubjectDataMailer).to receive(:subject_data).never
+      subject.perform(project.id, s3_url, [])
+    end
+  end
 end
