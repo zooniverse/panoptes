0ce0e5771a7b2ac8476fe1d84fcc778755df0641
Alex Bowyer <alex.bowyer@gmail.com>
2016-03-18 15:19:08 +0000

    Add sorting of subject sets
    Allow display_name sorting for subject sets

--- app/serializers/subject_set_serializer.rb
+++ app/serializers/subject_set_serializer.rb
@@ -5,6 +5,6 @@ class SubjectSetSerializer
   attributes :id, :display_name, :set_member_subjects_count, :metadata,
     :created_at, :updated_at, :href
   can_include :project, :workflows
-
+  can_sort_by :display_name
   can_filter_by :display_name
 end
