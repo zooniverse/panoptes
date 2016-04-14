48dbce511b14aff4493125db6f2fe0fc9d03e42c
Marten Veldthuis <marten@veldthuis.com>
2016-04-14 15:04:07 +0100

    Use compose and import_filters

--- app/operations/projects/create_classifications_export.rb
+++ app/operations/projects/create_classifications_export.rb
@@ -1,10 +1,10 @@
 module Projects
   class CreateClassificationsExport < Operation
     object :project
-    hash :media, strip: false
+    import_filters CreateOrUpdateMedium, only: :media
 
     def execute
-      medium = CreateOrUpdateMedium.run!(inputs.merge(type: :classifications_export))
+      medium = compose(CreateOrUpdateMedium, inputs.merge(type: :classifications_export))
       ClassificationsDumpWorker.perform_async(project.id, medium.id, api_user.id)
       medium
     end
--- app/operations/projects/create_subjects_export.rb
+++ app/operations/projects/create_subjects_export.rb
@@ -1,10 +1,10 @@
 module Projects
   class CreateSubjectsExport < Operation
     object :project
-    hash :media, strip: false
+    import_filters CreateOrUpdateMedium, only: :media
 
     def execute
-      medium = CreateOrUpdateMedium.run!(inputs.merge(type: :subjects_export))
+      medium = compose(CreateOrUpdateMedium, inputs.merge(type: :subjects_export))
       SubjectsDumpWorker.perform_async(project.id, medium.id, api_user.id)
       medium
     end
