d5c000cf6e3a8b357e1e1efe8d20fa295c89a7a0
Campbell Allen <campbell.allen@gmail.com>
2016-04-19 15:09:45 +0100

    simplify the owner filter join and eager load the identity group users
    the previous query was creating 2 inner joins on the access_control_lists and creating duplicates on the join which was affecting the overal sql count queries, i.e. 1 result but count was 2.

--- app/controllers/concerns/filter_by_owner.rb
+++ app/controllers/concerns/filter_by_owner.rb
@@ -10,11 +10,9 @@ module FilterByOwner
     unless owner_filter.blank?
       groups = UserGroup.where(UserGroup.arel_table[:name].lower.in(owner_filter.map(&:downcase)))
       @controlled_resources = controlled_resources
-        .eager_load(:owner)
-        .joins(:owner)
-        .joins(:access_control_lists)
-        .where(access_control_lists: {user_group: groups})
-        .where.overlap(access_control_lists: { roles: ["owner"] })
+      .eager_load(owner: [:users])
+      .joins(:owner)
+      .where(access_control_lists: { user_group: groups })
     end
   end
 end
