37f8ce405b370e943d355c093ae20c073a0bba1e
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2016-05-05 09:55:16 -0500

    Single and multiple working after changes. On to drawing/combo/dropdown

--- lib/formatter/csv/annotation_for_csv.rb
+++ lib/formatter/csv/annotation_for_csv.rb
@@ -10,23 +10,42 @@ module Formatter
       end
 
       def to_h
-        annotation.merge! "task_label" => task_label
-
-        case task["type"]
-        when "drawing"
-          value_with_tool = (annotation["value"] || []).map do |drawn_item|
-            drawn_item.merge "tool_label" => tool_label(drawn_item)
-          end
-          annotation.merge!("value" => value_with_tool)
-        when /single/i
-          annotation.merge!("value" => answer_label)
-        when /multiple/i
-          annotation.merge!("value" => answer_labels)
-        end
-        annotation
+        send(task["type"])
       end
 
       private
+
+      def drawing
+        # annotation.merge! "task_label" => task_label
+        # value_with_tool = (annotation["value"] || []).map do |drawn_item|
+        #  drawn_item.merge "tool_label" => tool_label(drawn_item)
+        # end
+        # annotation.merge!("value" => value_with_tool)
+      end
+
+      def simple
+        {}.tap do |new_annotation|
+          new_annotation['task'] = annotation['task']
+          new_annotation['task_label'] = task_label
+          new_annotation['value'] = task['type'] == 'multiple' ? answer_labels : answer_label
+        end
+      end
+
+      alias_method :single, :simple
+      alias_method :multiple, :simple
+
+      def combo
+        # {}.tap do |new_annotation|
+        #   annotation['value'].each do |subtask|
+        #     binding.pry
+
+            # anno = annotation_by_task(subtask).last
+            # answer_string = anno['answers'][subtask['value']]['label']
+            # new_annotation['value'] = translate(answer_string)
+            # new_annotation['task_label'] = translate(anno['question'])
+          # end
+        # end
+      end
 
       def task_label
         translate(task["question"] || task["instruction"])
@@ -69,10 +88,16 @@ module Formatter
         classification.workflow_version.split(".")[1].to_i
       end
 
+      def annotation_by_task(subtask)
+        workflow_at_version.tasks.find do |key, task|
+          key == subtask['task']
+        end
+      end
+
       def task
         return @task if @task
         task_annotation = workflow_at_version.tasks.find do |key, task|
-          key == annotation["task"]
+         key == annotation["task"]
         end
         @task = task_annotation.try(:last) || {}
       end
