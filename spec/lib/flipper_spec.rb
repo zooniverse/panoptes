ed0533c336e4fd1dbf7bb62895a1525f410b53a2
Campbell Allen <campbell.allen@gmail.com>
2016-06-13 14:26:28 +0100

    clear the cached feature before each test
    Panoptes.flipper uses cached DSL instance that internally memoizes the features for re-use. DSL in master has a remove method but not released as a gem yet.

--- spec/lib/flipper_spec.rb
+++ spec/lib/flipper_spec.rb
@@ -3,15 +3,22 @@ require 'spec_helper'
 describe Flipper do
 
   describe 'feature flippin' do
-    let(:feature) { :test_feature }
+    let(:feature_name) { :test_feature }
+
+    before do
+      # Panoptes.flipper.remove(feature_name) once
+      # https://github.com/jnunemaker/flipper/pull/126 is released
+      feature = Panoptes.flipper[feature_name]
+      feature.adapter.remove(feature)
+    end
 
     it "should not be enabled by default" do
-      expect(Panoptes.flipper[feature].enabled?).to be_falsey
+      expect(Panoptes.flipper[feature_name].enabled?).to be_falsey
     end
 
     it "should enable features being turned on" do
-      Panoptes.flipper[feature].enable
-      expect(Panoptes.flipper[feature].enabled?).to be_truthy
+      Panoptes.flipper[feature_name].enable
+      expect(Panoptes.flipper[feature_name].enabled?).to be_truthy
     end
   end
 end
