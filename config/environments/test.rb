16e06997f8ea7681e92adf6ac5dd567fb2679d1e
Campbell Allen <campbell.allen@gmail.com>
2016-08-11 19:12:10 +0100

    do not cache data in tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  #turn off the rails cache store
+  config.cache_store = :null_store
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
