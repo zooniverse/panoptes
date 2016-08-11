aafccf9c01edd9cb0c131385c2460381fe07151c
Campbell Allen <campbell.allen@gmail.com>
2016-08-11 19:14:10 +0100

    add simple rails caching to the cellect workflow ids lookup

--- app/controllers/cellect_controller.rb
+++ app/controllers/cellect_controller.rb
@@ -1,6 +1,8 @@
 class CellectController < ApplicationController
   before_filter :html_to_json_override
 
+  EXPIRY = 10.minutes.freeze
+
   def workflows
     respond_to do |format|
       format.json do
@@ -25,7 +27,11 @@ class CellectController < ApplicationController
   end
 
   def all_cellect_workflow_ids
-     workflow_ids_using_cellect | Workflow.using_cellect.pluck(:id)
+    cache_key = "#{Rails.env}#all_cellect_workflow_ids"
+    Rails.cache.fetch(cache_key, expires_in: EXPIRY) do
+      workflow_ids_using_cellect | Workflow.using_cellect.pluck(:id)
+    end
+  end
 
   def html_to_json_override
     request.format = :json if request.format == :html
