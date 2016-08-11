350e2dfdab0dff97693c105b84791895dafb1879
Campbell Allen <campbell.allen@gmail.com>
2016-08-11 18:16:21 +0100

    return all workflows using cellect
    including the ones that are manually set and the ones that qualify when their subjects count get beyond the threshold

--- app/controllers/cellect_controller.rb
+++ app/controllers/cellect_controller.rb
@@ -2,9 +2,27 @@ class CellectController < ApplicationController
   def workflows
     respond_to do |format|
       format.json do
-        cellect_workflows = Workflow.all
-        render json: { workflow_ids: cellect_workflows.pluck(:id).as_json }
+        render json: { workflow_ids: all_cellect_workflow_ids.as_json }
       end
     end
+  end
+
+  private
+
+  def launched_workflows
+    Workflow
+      .joins(:project)
+      .where("projects.launch_approved IS TRUE")
+      .select(:id, :use_cellect)
+  end
+
+  def workflow_ids_using_cellect
+    launched_workflows.collect do |w|
+      w.using_cellect? ? w.id : nil
+    end.compact
+  end
+
+  def all_cellect_workflow_ids
+     workflow_ids_using_cellect | Workflow.using_cellect.pluck(:id)
   end
 end
--- spec/controllers/cellect_controller_spec.rb
+++ spec/controllers/cellect_controller_spec.rb
@@ -1,28 +1,32 @@
 require 'spec_helper'
 
-describe CellectController, type: :controller, focus: true do
+describe CellectController, type: :controller do
   let(:cellect_workflow) do
-    create(:workflow, configuration: { selection_strategy: :cellect})
+    create(:workflow, use_cellect: true)
   end
   let(:non_cellect_workflow) { create(:workflow) }
 
   describe "GET 'workflows'" do
+    let(:run_get) { get 'workflows', format: :json }
+
     context "as json" do
       before(:each) do
         cellect_workflow
         non_cellect_workflow
-        get 'workflows', format: :json
       end
 
       it "returns success" do
+        run_get
         expect(response).to be_success
       end
 
       it "returns the expected json header" do
+        run_get
         expect(response.content_type).to eq("application/json")
       end
 
       it "should respond with only the cellect workflow" do
+        run_get
         expect(json_response).to eq({"workflow_ids"=>[cellect_workflow.id]})
       end
 
@@ -34,7 +38,8 @@ describe CellectController, type: :controller, focus: true do
         end
 
         it "should respond with all the workflows" do
-          expect(json_response).to eq({"workflow_ids"=>[Workflow.all.pluck(:id)]})
+          run_get
+          expect(json_response).to eq({"workflow_ids"=>Workflow.all.pluck(:id)})
         end
       end
     end
