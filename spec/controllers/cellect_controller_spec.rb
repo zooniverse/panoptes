42d2a3230cee18a9f83759ee290bbaf207ff830a
Michael Parrish <mtparrish@gmail.com>
2016-08-15 10:54:21 -0500

    Update CellectController responses to be more usable (#1906)
    * Update CellectController responses to be more usable
    
    * Probably fix travis failure
    
    * Probably fix travis
    
    * Probably fix travis failure

--- spec/controllers/cellect_controller_spec.rb
+++ spec/controllers/cellect_controller_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe CellectController, type: :controller do
   let(:cellect_workflow) do
-    create(:workflow, use_cellect: true)
+    create(:workflow_with_subjects, use_cellect: true)
   end
-  let(:non_cellect_workflow) { create(:workflow) }
+  let(:non_cellect_workflow) { create(:workflow_with_subjects) }
 
   describe "GET 'workflows'" do
     let(:run_get) { get 'workflows', format: :json }
@@ -27,8 +27,8 @@ describe CellectController, type: :controller do
 
       it "should respond with only the cellect workflow" do
         run_get
-        expected = { workflow_ids: [cellect_workflow.id] }.as_json
-        expect(json_response).to eq(expected)
+        expected = [cellect_workflow.slice(:id, :pairwise, :grouped, :prioritized)].as_json
+        expect(json_response['workflows']).to eql(expected)
       end
 
       context "with a workflow that satifies the cellect subjects critera" do
@@ -40,7 +40,8 @@ describe CellectController, type: :controller do
 
         it "should respond with all the workflows" do
           run_get
-          expected = { workflow_ids: Workflow.all.pluck(:id) }.as_json
+          workflows = Workflow.all.map{ |w| w.slice(:id, :pairwise, :grouped, :prioritized) }
+          expected = { workflows: workflows }.as_json
           expect(json_response).to eq(expected)
         end
       end
@@ -59,33 +60,38 @@ describe CellectController, type: :controller do
     end
 
     context "as json" do
-      let(:subject_ids) { cellect_workflow.subjects.map(&:id) }
+      let(:subjects) do
+        cellect_workflow.set_member_subjects.map do |s|
+          { 'id' => s.subject_id, 'priority' => s.priority }
+        end
+      end
 
       before do
         cellect_workflow
         another_cellect_workflow
       end
 
-      it "should respond with only the subject ids of the params workflow" do
-        expect(subject_ids.count > 0).to be_truthy
+      it "should respond with only the subjects of the params workflow" do
+        expect(subjects.count > 0).to be_truthy
         run_get
-        expect(json_response["subject_ids"]).to match_array(subject_ids)
+        expect(json_response["subjects"]).to match_array(subjects)
       end
 
       context "with a retired subject" do
-        let(:retired_id) { subject_ids.sample }
+        let(:retired_subject) { cellect_workflow.subjects.all.sample }
         let!(:retired_swc) do
           create(:subject_workflow_count,
-            subject_id: retired_id,
+            subject_id: retired_subject.id,
             workflow_id: cellect_workflow.id,
             retired_at: DateTime.now
           )
         end
 
-        it "should not respond with retired_ids" do
-          non_retired_ids = subject_ids - [retired_id]
+        it "should not respond with retired subjects" do
+          non_retired_ids = subjects.map{ |s| s['id'] } - [retired_subject.id]
           run_get
-          expect(json_response["subject_ids"]).to match_array(non_retired_ids)
+          response_ids = json_response["subjects"].map{ |s| s['id'] }
+          expect(response_ids).to match_array(non_retired_ids)
         end
       end
 
@@ -94,7 +100,7 @@ describe CellectController, type: :controller do
 
         it "should respond with an empty array" do
           run_get
-          expect(json_response).to eq({ subject_ids: [] }.as_json)
+          expect(json_response).to eq({ subjects: [] }.as_json)
         end
       end
     end
