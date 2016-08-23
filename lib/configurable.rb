e9694fc479051d6ec4c7fd26074d0a6e46b6795e
Marten Veldthuis <marten@veldthuis.com>
2016-08-23 15:34:02 +0200

    Specify which config fields exist

--- lib/configurable.rb
+++ lib/configurable.rb
@@ -6,6 +6,12 @@ module Configurable
   end
 
   module ClassMethods
+    def configure(key, options = {})
+      @fields ||= {}
+      @fields[key] = options
+      cattr_accessor key
+    end
+
     def config_file=(file_name)
       @config_file = "config/#{file_name}.yml"
     end
@@ -15,11 +21,14 @@ module Configurable
     end
 
     def load_configuration
-      self.host = ENV["#{@api_prefix}_API_HOST"] || configuration[:host]
-      self.user_id = (ENV["#{@api_prefix}_API_USER"] || configuration[:user]).to_i
-      self.application_id = (ENV["#{@api_prefix}_API_APPLICATION"] || configuration[:application]).to_i
-    end
+      @fields.each do |key, options|
+        value = ENV["#{@api_prefix}_#{key.upcase}"]
+        value ||= configuration[options[:file_field || key]]
+        value = value.to_i if options[:type] == :integer
 
+        self.public_send("#{key}=", value)
+      end
+    end
 
     def configuration
       @configuration ||= begin
