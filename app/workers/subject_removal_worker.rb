add92df9d3d931b7f86528dd5033a51c23b9d8b1
Campbell Allen <campbell.allen@gmail.com>
2016-08-25 18:09:25 +0100

    skip the subject removal temporarily
    cleaning up subjects is causing really high cpu on the db, skipping the cleanup in the meantime instead just leaving them as inactive.

--- app/workers/subject_removal_worker.rb
+++ app/workers/subject_removal_worker.rb
@@ -6,6 +6,8 @@ class SubjectRemovalWorker
   sidekiq_options queue: :data_low
 
   def perform(subject_id)
-    Subjects::Remover.new(subject_id).cleanup
+    #TODO: figure out why these subject deletes are maxing out the DB CPU
+    # in the meantime just move on and leave them as inactive
+    # Subjects::Remover.new(subject_id).cleanup
   end
 end
