28619407bd06911490cb1614b1423ae2588f3395
Campbell Allen <campbell.allen@gmail.com>
2016-08-25 18:29:37 +0100

    skip the subject removal temporarily (#1925)
    cleaning up subjects is causing really high cpu on the db, skipping the cleanup in the meantime instead just leaving them as inactive.

--- spec/workers/subject_removal_worker_spec.rb
+++ spec/workers/subject_removal_worker_spec.rb
@@ -3,10 +3,17 @@ require 'spec_helper'
 RSpec.describe SubjectRemovalWorker do
   let(:subject_id) { 1 }
 
-  it 'should call the orphan remover cleanup' do
-    remover = instance_double(Subjects::Remover)
-    expect(Subjects::Remover).to receive(:new).with(subject_id).and_return(remover)
-    expect(remover).to receive(:cleanup)
+  # TODO: reinstate this when the cleanup code is not killing the db
+  # it 'should call the orphan remover cleanup' do
+  #   remover = instance_double(Subjects::Remover)
+  #   expect(Subjects::Remover).to receive(:new).with(subject_id).and_return(remover)
+  #   expect(remover).to receive(:cleanup)
+  #   subject.perform(subject_id)
+  # end
+
+  # remove this when the above is fixed
+  it 'should not call the orphan remover cleanup', :focus do
+    expect(Subjects::Remover).not_to receive(:new)
     subject.perform(subject_id)
   end
 end
