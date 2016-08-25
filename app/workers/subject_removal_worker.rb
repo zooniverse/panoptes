3e449fe31d079d12d572da2ca005152576a35174
Campbell Allen <campbell.allen@gmail.com>
2016-08-25 15:43:09 +0100

    lower queue job priority for the subject removal worker

--- app/workers/subject_removal_worker.rb
+++ app/workers/subject_removal_worker.rb
@@ -3,7 +3,7 @@ require 'subjects/remover'
 class SubjectRemovalWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :data_medium
+  sidekiq_options queue: :data_low
 
   def perform(subject_id)
     Subjects::Remover.new(subject_id).cleanup
