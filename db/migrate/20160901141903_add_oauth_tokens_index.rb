93b0c6edf7ef2a0ca36f2f2fae969d75740ceddb
Campbell Allen <campbell.allen@gmail.com>
2016-09-01 17:38:46 +0100

    remove old doorkeeper grants / tokens code before index migration runs

--- db/migrate/20160901141903_add_oauth_tokens_index.rb
+++ db/migrate/20160901141903_add_oauth_tokens_index.rb
@@ -2,6 +2,8 @@ class AddOauthTokensIndex < ActiveRecord::Migration
   disable_ddl_transaction!
 
   def change
+    Doorkeeper::AccessCleanup.new.cleanup!
+
     unless index_exists?(:oauth_access_tokens, [:application_id, :resource_owner_id])
       add_index :oauth_access_tokens, [:application_id, :resource_owner_id],
         name: 'index_oauth_access_tokens_on_app_id_and_owner_id',
