a1d6e382e55dc34ddc63457a71140fe6c4ddb204
Campbell Allen <campbell.allen@gmail.com>
2016-09-01 18:21:12 +0100

    revert the temp fix start running these again

--- spec/workers/project_classifications_count_worker_spec.rb
+++ spec/workers/project_classifications_count_worker_spec.rb
@@ -6,28 +6,23 @@ RSpec.describe ProjectClassificationsCountWorker do
   let!(:project) { workflow.project }
 
   describe "#perform" do
-    # temp fix - remove this once we've cleared the jam
-    # and figured out how to do the counting for certain projects
-    it "should return nil" do
-      expect(worker.perform(project.id)).to be_nil
+
+    it 'calls the workflow counter to update the workflow counts' do
+      expect_any_instance_of(WorkflowCounter)
+        .to receive(:classifications)
+      expect_any_instance_of(Workflow)
+        .to receive(:update_column)
+        .with(:classifications_count, anything)
+      worker.perform(project.id)
     end
 
-    # it 'calls the workflow counter to update the workflow counts' do
-    #   expect_any_instance_of(WorkflowCounter)
-    #     .to receive(:classifications)
-    #   expect_any_instance_of(Workflow)
-    #     .to receive(:update_column)
-    #     .with(:classifications_count, anything)
-    #   worker.perform(project.id)
-    # end
-    #
-    # it 'calls the project counter to update the project counts' do
-    #   expect_any_instance_of(ProjectCounter)
-    #     .to receive(:classifications)
-    #   expect_any_instance_of(Project)
-    #     .to receive(:update_column)
-    #     .with(:classifications_count, anything)
-    #   worker.perform(project.id)
-    # end
+    it 'calls the project counter to update the project counts' do
+      expect_any_instance_of(ProjectCounter)
+        .to receive(:classifications)
+      expect_any_instance_of(Project)
+        .to receive(:update_column)
+        .with(:classifications_count, anything)
+      worker.perform(project.id)
+    end
   end
 end
