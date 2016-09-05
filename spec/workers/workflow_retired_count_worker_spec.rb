c0bb3d1e7b8cbb1888212f0ce5e2ec84941e31a2
Campbell Allen <campbell.allen@gmail.com>
2016-09-05 11:33:01 +0100

    use the counter in the retired count worker and rate limit it
    added rate limiting for apps using the workflow.retire_subjects route

--- spec/workers/workflow_retired_count_worker_spec.rb
+++ spec/workers/workflow_retired_count_worker_spec.rb
@@ -1,37 +1,23 @@
 require "spec_helper"
 
 RSpec.describe WorkflowRetiredCountWorker do
-  let(:workflows) { create_list(:workflow, 2, subject_sets: []) }
-  let(:subject_set) { create(:subject_set, workflows: workflows) }
-  let(:sms) { create_list(:set_member_subject, 4, subject_set: subject_set) }
-
   subject(:worker) { WorkflowRetiredCountWorker.new }
+  let(:workflow) { create(:workflow) }
 
   describe "#perform" do
-    let(:workflow) { workflows.first }
-    let(:non_retired_swc) do
-      create(:subject_workflow_status, subject: sms.last.subject, workflow: workflow, link_subject_sets: false)
-    end
-    before do
-      workflow.update! retired_set_member_subjects_count: 100
-      sms.take(2).each do |s|
-        opts = { subject: s.subject, workflow: workflow, retired_at: Time.now, link_subject_sets: false}
-        create(:subject_workflow_status, opts)
-      end
+
+    it 'should update the workflow retired count' do
+      allow(Workflow).to receive(:find).and_return(workflow)
+      expect(workflow)
+        .to receive(:update_column)
+        .with(:retired_set_member_subjects_count, anything)
+      worker.perform(workflow.id)
     end
 
-    it 'should reset the workflow retired count' do
-      non_retired_swc
-      expect do
-        worker.perform(workflow.id)
-      end.to change{Workflow.find(workflow.id).retired_set_member_subjects_count}.from(100).to(2)
-    end
-
-    it 'should respect the project launch date' do
-      workflow.project.update_column(:launch_date, DateTime.now)
-      expect do
-        worker.perform(workflow.id)
-      end.to change{Workflow.find(workflow.id).retired_set_member_subjects_count}.from(100).to(0)
+    it 'should use a workflow counter to do it' do
+      counter = instance_double(WorkflowCounter, retired_subjects: 0)
+      expect(WorkflowCounter).to receive(:new).with(workflow).and_return(counter)
+      worker.perform(workflow.id)
     end
   end
 end
