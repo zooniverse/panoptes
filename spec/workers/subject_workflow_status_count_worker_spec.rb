4d4f3c01de10d10bfa07328d1b7d0b81506ad5c5
Campbell Allen <campbell.allen@gmail.com>
2016-09-05 16:06:13 +0100

    add a rate limited sws classifications_count worker
    this will be used to update the counts to their correct value in the databse and rate limited to try and avoid contention on the locking updates to the counter column

---
+++ spec/workers/subject_workflow_status_count_worker_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+RSpec.describe SubjectWorkflowStatusCountWorker do
+  let(:worker) { described_class.new }
+  let(:sws) { create(:subject_workflow_status) }
+
+  it "should be rate limited with defaults" do
+    opts = worker.class.get_sidekiq_options['congestion']
+    expect(opts[:interval]).to eq(30)
+    expect(opts[:max_in_interval]).to eq(1)
+    expect(opts[:min_delay]).to eq(0)
+    expect(opts[:reject_with]).to eq(:cancel)
+  end
+
+  describe "#perform" do
+
+    it 'should call the counter to update the classifications_count' do
+      expect_any_instance_of(SubjectWorkflowCounter)
+        .to receive(:classifications)
+      expect_any_instance_of(SubjectWorkflowStatus)
+        .to receive(:update_column)
+        .with(:classifications_count, anything)
+      worker.perform(sws.id)
+    end
+  end
+end
