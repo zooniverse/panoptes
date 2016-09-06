7257f6f4572549b92f26e3811cfb745c53f518fd
Campbell Allen <campbell.allen@gmail.com>
2016-09-06 18:05:20 +0100

    reset the retired_subject_count on the worklfow too

--- spec/workers/reset_project_counters_worker_spec.rb
+++ spec/workers/reset_project_counters_worker_spec.rb
@@ -23,7 +23,7 @@ describe ResetProjectCountersWorker do
     create(:classification, classification_attrs.merge(user: user2, created_at: project.launch_date + 1.week))
 
     project.update_columns classifications_count: 3, classifiers_count: 3
-    workflow.update_columns classifications_count: 3
+    workflow.update_columns classifications_count: 3, retired_set_member_subjects_count: 2
 
     create :user_project_preference, user: user1, project: project
     create :user_project_preference, user: user2, project: project
@@ -45,6 +45,11 @@ describe ResetProjectCountersWorker do
   it 'resets workflow classifications count' do
     described_class.new.perform(project.id)
     expect { workflow.reload }.to change { workflow.classifications_count }.from(3).to(1)
+  end
+
+  it 'resets retired subjects count' do
+    described_class.new.perform(project.id)
+    expect { workflow.reload }.to change { workflow.retired_subjects_count }.from(2).to(0)
   end
 
   it 'resets subject workflow counters' do
