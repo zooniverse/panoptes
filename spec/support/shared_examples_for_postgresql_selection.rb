8244ee3a3a6507a712133b1b3db3c3d05363a274
Campbell Allen <campbell.allen@gmail.com>
2016-09-07 17:47:11 +0100

    refactor to cleanup cached count state

--- spec/support/shared_examples_for_postgresql_selection.rb
+++ spec/support/shared_examples_for_postgresql_selection.rb
@@ -1,21 +1,8 @@
 shared_examples "select for incomplete_project" do
   let(:args) { opts }
   let(:selector) { Subjects::PostgresqlSelection.new(workflow, user, args) }
-  let(:sms_scope) do
-    if ss_id = args[:subject_set_id]
-      SetMemberSubject.where(subject_set_id: ss_id)
-    else
-      SetMemberSubject.all
-    end
-  end
   let(:unseen_count) do
-    scoped_seen_count = if ss_id = args[:subject_set_id]
-                          group_sms = SetMemberSubject.where(subject_set_id: ss_id)
-                          group_sms.where(subject_id: uss.subject_ids).count
-                        else
-                          seen_count
-                        end
-    sms_scope.count - scoped_seen_count
+    sms_count - seen_count
   end
 
   def run_limit_selection(limit)
@@ -31,7 +18,6 @@ shared_examples "select for incomplete_project" do
     let(:args) { opts.merge(limit: limit) }
     let(:uss) do
       subject_ids = sms_scope.sample(seen_count).map(&:subject_id)
-      create(:classification, user: user, workflow: workflow, subject_ids: subject_ids)
       create(:user_seen_subject, user: user, subject_ids: subject_ids, workflow: workflow)
     end
 
@@ -48,7 +34,7 @@ shared_examples "select for incomplete_project" do
     context "with a limit of 10" do
       let(:limit) { 10 }
 
-      it 'should no have duplicates' do
+      it 'should not have duplicates' do
         result = selector.select
         expect(result).to match_array(result.to_a.uniq)
       end
@@ -67,7 +53,6 @@ shared_examples "select for incomplete_project" do
     let(:seen_count) { 20 }
     let!(:uss) do
       subject_ids = sms_scope.sample(seen_count).map(&:subject_id)
-      create(:classification, user: user, workflow: workflow, subject_ids: subject_ids)
       create(:user_seen_subject, user: user, subject_ids: subject_ids, workflow: workflow)
     end
 
