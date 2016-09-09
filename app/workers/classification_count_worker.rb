44472b7bd426a237b2b57dd636f56d4850a4ce49
Marten Veldthuis <marten@veldthuis.com>
2016-09-09 11:08:12 +0100

    Fix specs

--- app/workers/classification_count_worker.rb
+++ app/workers/classification_count_worker.rb
@@ -6,16 +6,18 @@ class ClassificationCountWorker
   def perform(subject_id, workflow_id, was_update=false)
     workflow = Workflow.find(workflow_id)
 
-    Workflow.transaction do
-      if workflow.project.live
+    if workflow.project.live
+      count = nil
+
+      Workflow.transaction do
         count = SubjectWorkflowStatus.find_or_create_by!(subject_id: subject_id, workflow_id: workflow_id)
         count.class.increment_counter(:classifications_count, count.id) unless was_update
       end
-    end
 
-    SubjectWorkflowStatusCountWorker.perform_async(count.id)
-    ProjectClassificationsCountWorker.perform_async(workflow.project.id)
-    RetirementWorker.perform_async(count.id)
+      ProjectClassificationsCountWorker.perform_async(workflow.project.id)
+      SubjectWorkflowStatusCountWorker.perform_async(count.id)
+      RetirementWorker.perform_async(count.id)
+    end
   rescue ActiveRecord::RecordNotFound
     nil
   end
