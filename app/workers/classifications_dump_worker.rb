c9a6ac85b8acd05201f187d74cca00d2fdf0256a
Campbell Allen <campbell.allen@gmail.com>
2016-09-20 19:08:09 +0100

    make the code read good
    be a good person to your future self and make everyone happy

--- app/workers/classifications_dump_worker.rb
+++ app/workers/classifications_dump_worker.rb
@@ -41,7 +41,7 @@ class ClassificationsDumpWorker
     sql = "SELECT classification_id, subject_id FROM classification_subjects where classification_id IN (#{classification_ids})"
     c_s_ids = ActiveRecord::Base.connection.select_rows(sql)
     cache.reset_classification_subjects(c_s_ids)
-    subject_ids = c_s_ids.map { |cs| cs.last }
+    subject_ids = c_s_ids.map { |_, subject_id| subject_id }
     cache.reset_subjects(Subject.unscoped.where(id: subject_ids).load)
     subject_ids
   end
