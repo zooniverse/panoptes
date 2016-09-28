0f2808cea3870f0eb134d4fe4b4a60b1bfb6b79c
Campbell Allen <campbell.allen@gmail.com>
2016-09-28 17:37:46 +0100

    reinstate subject removal

--- app/workers/subject_removal_worker.rb
+++ app/workers/subject_removal_worker.rb
@@ -6,8 +6,6 @@ class SubjectRemovalWorker
   sidekiq_options queue: :data_low
 
   def perform(subject_id)
-    #TODO: figure out why these subject deletes are maxing out the DB CPU
-    # in the meantime just move on and leave them as inactive
-    # Subjects::Remover.new(subject_id).cleanup
+    Subjects::Remover.new(subject_id).cleanup
   end
 end
--- spec/workers/subject_removal_worker_spec.rb
+++ spec/workers/subject_removal_worker_spec.rb
@@ -3,17 +3,10 @@ require 'spec_helper'
 RSpec.describe SubjectRemovalWorker do
   let(:subject_id) { 1 }
 
-  # TODO: reinstate this when the cleanup code is not killing the db
-  # it 'should call the orphan remover cleanup' do
-  #   remover = instance_double(Subjects::Remover)
-  #   expect(Subjects::Remover).to receive(:new).with(subject_id).and_return(remover)
-  #   expect(remover).to receive(:cleanup)
-  #   subject.perform(subject_id)
-  # end
-
-  # remove this when the above is fixed
-  it 'should not call the orphan remover cleanup' do
-    expect(Subjects::Remover).not_to receive(:new)
+  it 'should call the orphan remover cleanup' do
+    remover = instance_double(Subjects::Remover)
+    expect(Subjects::Remover).to receive(:new).with(subject_id).and_return(remover)
+    expect(remover).to receive(:cleanup)
     subject.perform(subject_id)
   end
 end
