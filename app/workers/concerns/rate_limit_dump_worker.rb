5114fa245849b8cbaafbae9db94e44fa8b966721
Marten Veldthuis <marten@veldthuis.com>
2016-10-18 14:09:06 +0100

    Fix congestion config for dump workers

--- app/workers/concerns/rate_limit_dump_worker.rb
+++ app/workers/concerns/rate_limit_dump_worker.rb
@@ -3,10 +3,10 @@ module RateLimitDumpWorker
 
   included do
     sidekiq_options congestion: Panoptes::CongestionControlConfig.dump_worker.congestion_opts.merge({
-      key: ->(project_id, medium_id, requester_id = nil) {
-        "project_#{ project_id }_#{medium_id}_data_dump_worker"
+      key: ->(resource_id, resource_type, medium_id, requester_id = nil) {
+        "#{resource_type}_#{resource_id}_#{medium_id}_data_dump_worker"
       },
-      enabled: ->(project_id, medium_id, requester_id = nil) {
+      enabled: ->(resource_id, resource_type, medium_id, requester_id = nil) {
         requester_id.blank? || !User.find(requester_id).admin?
       }
     })
