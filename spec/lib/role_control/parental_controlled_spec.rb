96fb3d7935e035838e0ed65698c238d4f8f8fb39
Campbell Allen <campbell.allen@gmail.com>
2016-10-26 16:29:15 +0100

    skip parent scope_for eager load when parental controlled
    just join on the parent object for parental access, don't attempt to eager_load relations that may conflict with the join as well.

--- spec/lib/role_control/parental_controlled_spec.rb
+++ spec/lib/role_control/parental_controlled_spec.rb
@@ -3,11 +3,34 @@ require 'spec_helper'
 
 describe RoleControl::ParentalControlled do
   setup_role_control_tables
-
-  let(:enrolled_actor) { EnrolledActorTable.create! }
-  let(:controlled) { ControlledTable.create! }
-  let!(:relation) { create_roles_join_instance(%w(test_parent_role test_role), controlled, enrolled_actor)  }
+  let(:user) { create(:user) }
+  let(:enrolled_actor) { ApiUser.new(user) }
+  let(:parent) { ControlledTable.create! }
+  let(:controlled) { TestParentControlTable.create! }
 
   describe "::scope_for" do
+    let(:klass) { controlled.class }
+
+    before do
+      controlled
+      create_roles_join_instance(%w(test_role), parent, user)
+    end
+
+    after do
+      klass.scope_for(:update, enrolled_actor)
+    end
+
+    it "should call join on the parent" do
+      expect(klass)
+        .to receive(:joins)
+        .with(parent.class.model_name.singular.to_sym)
+        .and_call_original
+    end
+
+    it "should call scope_for on the parent and skip eager loads" do
+      expect(parent.class)
+        .to receive(:scope_for)
+        .with(:update, enrolled_actor, {skip_eager_load: true})
+    end
   end
 end
