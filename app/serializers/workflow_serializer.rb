eebf6c77f3c781afd40a749c67a33f993e98d636
Campbell Allen <campbell.allen@gmail.com>
2016-11-11 17:41:58 +0000

    add eager load to workflow serializer with experiment

--- app/serializers/workflow_serializer.rb
+++ app/serializers/workflow_serializer.rb
@@ -6,6 +6,8 @@ class WorkflowSerializer
   include FilterHasMany
   include MediaLinksSerializer
 
+  EAGER_LOADS = %i(project subject_sets expert_subject_sets tutorial_subject attached_images).freeze
+
   attributes :id, :display_name, :tasks, :classifications_count, :subjects_count,
              :created_at, :updated_at, :finished_at, :first_task, :primary_language,
              :version, :content_language, :prioritized, :grouped, :pairwise,
@@ -17,6 +19,17 @@ class WorkflowSerializer
   media_include :attached_images, classifications_export: { include: false }
 
   can_filter_by :active
+
+  def self.page(params = {}, scope = nil, context = {})
+    experiment_name = "eager_load_workflows"
+    CodeExperiment.run(experiment_name) do |e|
+      # e.run_if { Panoptes.flipper[experiment_name].enabled? }
+      e.use { super(params, scope, context) }
+      e.try { super(params, scope.eager_load(*EAGER_LOADS), context) }
+      #skip the mismatch reporting...we just want perf metrics
+      e.ignore { true }
+    end
+  end
 
   def self.links
     links = super
