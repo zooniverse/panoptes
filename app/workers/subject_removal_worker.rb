b3ee313e67a6e3e1fc799caacd1ba077b6f77aa9
Campbell Allen <campbell.allen@gmail.com>
2016-11-11 10:31:42 +0000

    add feature flag to subject removal
    provide a way to turn this behaviour on/off at runtime

--- app/workers/subject_removal_worker.rb
+++ app/workers/subject_removal_worker.rb
@@ -6,6 +6,8 @@ class SubjectRemovalWorker
   sidekiq_options queue: :data_low
 
   def perform(subject_id)
-    Subjects::Remover.new(subject_id).cleanup
+    if Panoptes.flipper["remove_orphan_subjects"].enabled?
+      Subjects::Remover.new(subject_id).cleanup
+    end
   end
 end
--- spec/workers/subject_removal_worker_spec.rb
+++ spec/workers/subject_removal_worker_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 RSpec.describe SubjectRemovalWorker do
   let(:subject_id) { 1 }
+  let(:feature_name) { "remove_orphan_subjects" }
+  let(:remover) { instance_double(Subjects::Remover) }
 
-  it 'should call the orphan remover cleanup' do
-    remover = instance_double(Subjects::Remover)
+  it 'should call the orphan remover cleanup when enabled' do
+    Panoptes.flipper[feature_name].enable
     expect(Subjects::Remover).to receive(:new).with(subject_id).and_return(remover)
     expect(remover).to receive(:cleanup)
+    subject.perform(subject_id)
+  end
+
+  it 'should not call the orphan remover cleanup when disabled' do
+    expect(Subjects::Remover).not_to receive(:new)
+    expect(remover).not_to receive(:cleanup)
     subject.perform(subject_id)
   end
 end
