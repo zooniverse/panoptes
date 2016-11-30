d6c9d4cae0e0be9464385ba5f878704285f17f8c
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2016-11-30 12:53:49 -0600

    Send emails to the right people

--- app/controllers/api/v1/project_roles_controller.rb
+++ app/controllers/api/v1/project_roles_controller.rb
@@ -10,15 +10,31 @@ class Api::V1::ProjectRolesController < Api::ApiController
   end
 
   def update
-    super do
-      UserAddedToProjectMailerWorker.perform_async(api_user.id, controlled_resource.resource.id, roles) if new_roles_present?(roles)
-    end
+    UserAddedToProjectMailerWorker.perform_async(acl_user_id, controlled_resource.resource.id, roles) if new_roles_present?(roles)
+    super
+  end
+
+  def create
+    UserAddedToProjectMailerWorker.perform_async(new_user_id, project_id, roles) if new_roles_present?(roles)
+    super
   end
 
   private
 
   def roles
     params[:project_roles][:roles]
+  end
+
+  def acl_user_id
+    controlled_resource.user_group.users.first.id.to_i
+  end
+
+  def new_user_id
+    params[:project_roles][:links][:user].to_i
+  end
+
+  def project_id
+    params[:project_roles][:links][:project].to_i
   end
 
   def new_roles_present?(roles)
--- spec/factories/access_control_lists.rb
+++ spec/factories/access_control_lists.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :access_control_list do
-    user_group
+    association :user_group, factory: :user_group_with_users
     roles ["collaborator"]
     association :resource, factory: :project
   end
