db7707e868aeec4a469390b049e07ca86033adc3
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2016-12-13 10:51:01 -0600

    Adds display_name backfill for existing tutorials

--- db/migrate/20161212205412_add_display_name_to_tutorials.rb
+++ db/migrate/20161212205412_add_display_name_to_tutorials.rb
@@ -1,5 +1,9 @@
 class AddDisplayNameToTutorials < ActiveRecord::Migration
   def change
     add_column :tutorials, :display_name, :text, default: ""
+    Tutorial.find_each do |tut|
+      tut.display_name = ""
+      tut.save!
+    end
   end
 end
