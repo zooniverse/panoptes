0bcd91a3979bee34001ecafa0a5ebc8e1160c618
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2016-12-13 11:08:48 -0600

    More efficient table update

--- db/migrate/20161212205412_add_display_name_to_tutorials.rb
+++ db/migrate/20161212205412_add_display_name_to_tutorials.rb
@@ -1,9 +1,6 @@
 class AddDisplayNameToTutorials < ActiveRecord::Migration
   def change
     add_column :tutorials, :display_name, :text, default: ""
-    Tutorial.find_each do |tut|
-      tut.display_name = ""
-      tut.save!
-    end
+    Tutorial.update_all(display_name: "")
   end
 end
