7439043ca0adfe4bc2bc0d04098ae98f1170c6c4
Campbell Allen <campbell.allen@gmail.com>
2017-01-04 14:57:46 +0000

    preload the subjects association to avoid n+1

--- app/serializers/classification_serializer.rb
+++ app/serializers/classification_serializer.rb
@@ -3,6 +3,10 @@ class ClassificationSerializer
   attributes :id, :annotations, :created_at, :metadata, :href
   can_include :project, :user, :user_group, :workflow
 
+  def self.page(params = {}, scope = nil, context = {})
+    super(params, scope.preload(:subjects), context)
+  end
+
   def metadata
     @model.metadata.merge(workflow_version: @model.workflow_version)
   end
