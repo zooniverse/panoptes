283bc1d99b1b8af6752e4b9ac687efa7e18436c0
Campbell Allen <campbell.allen@gmail.com>
2017-01-04 17:47:09 +0000

    make the shared helper generic

--- spec/support/panoptes_restpack_serializer.rb
+++ spec/support/panoptes_restpack_serializer.rb
@@ -4,15 +4,15 @@ shared_examples "a panoptes restpack serializer" do
   end
 
   it "should not preload when no included relations" do
-    expect_any_instance_of(Workflow::ActiveRecord_Relation).not_to receive(:preload)
-    WorkflowSerializer.page({}, Workflow.all, {})
+    expect_any_instance_of(resource.class::ActiveRecord_Relation).not_to receive(:preload)
+    described_class.page({}, Workflow.all, {})
   end
 
   it "should preload included relations" do
-    expect_any_instance_of(Workflow::ActiveRecord_Relation)
+    expect_any_instance_of(resource.class::ActiveRecord_Relation)
       .to receive(:preload)
       .with(*preloads)
       .and_call_original
-    WorkflowSerializer.page({include: includes}, Workflow.all, {})
+    described_class.page({include: includes}, Workflow.all, {})
   end
 end
