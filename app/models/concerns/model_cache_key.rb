3d5edbcb91fbe1a252df5026b1d4d82a2a881172
Campbell Allen <campbell.allen@gmail.com>
2017-01-16 14:51:06 +0000

    don’t to_s in string interpolation

--- app/models/concerns/model_cache_key.rb
+++ app/models/concerns/model_cache_key.rb
@@ -1,5 +1,5 @@
 module ModelCacheKey
   def model_cache_key(method_name)
-    "#{self.class.to_s}/#{id.to_i}/#{updated_at.to_i}/#{method_name}"
+    "#{self.class}/#{id.to_i}/#{updated_at.to_i}/#{method_name}"
   end
 end
