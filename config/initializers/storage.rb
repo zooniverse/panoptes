65e0c900315aecb075365711c22b867da098b182
Campbell Allen <campbell.allen@gmail.com>
2017-01-17 18:26:20 +0000

    ensure the storage config is modifiable

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,10 +1,11 @@
 module Panoptes
   module StorageAdapter
     def self.configuration
-      return @configuration.dup if @configuration
+      return @configuration if @configuration
       begin
         file = Rails.root.join('config/storage.yml')
         @configuration = YAML.load(File.read(file))[Rails.env].symbolize_keys
+        @configuration.freeze
       rescue Errno::ENOENT, NoMethodError
         {adapter: "default"}
       end
@@ -13,4 +14,4 @@ module Panoptes
 end
 
 config = Panoptes::StorageAdapter.configuration
-MediaStorage.adapter(config.delete(:adapter), **config)
+MediaStorage.adapter(config[:adapter], **config.except(:adapter))
