7bf63057fbc9ff11681cc660e20b41969afaaf6d
Campbell Allen <campbell.allen@gmail.com>
2017-01-17 22:17:14 +0000

    handle redis connection pool timeouts

--- lib/subjects/cellect_client.rb
+++ lib/subjects/cellect_client.rb
@@ -81,7 +81,7 @@ module Subjects
         super(action, params) do
           params[:host] = @session.reset_host
         end
-      rescue Redis::CannotConnectError, Subjects::CellectSession::NoHostError
+      rescue Redis::CannotConnectError, Subjects::CellectSession::NoHostError, Timeout::Error
         raise ConnectionError, "Cellect can't find a server host"
       end
     end
