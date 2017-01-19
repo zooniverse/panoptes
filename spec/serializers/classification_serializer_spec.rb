5130abab2fe68da5d105901f80dee37dd7111482
Campbell Allen <campbell.allen@gmail.com>
2017-01-19 12:04:19 +0000

    avoid count queries on classifications end point that timeout / stress the db (#2145)
    * avoid large count queries on classifications end point
    
    manually set the page values based on paging params to ensure we don't break clients that use the next/prev hrefs
    
    * add docs on missing page count information for classifications

--- spec/serializers/classification_serializer_spec.rb
+++ spec/serializers/classification_serializer_spec.rb
@@ -10,4 +10,24 @@ describe ClassificationSerializer do
       .and_call_original
     ClassificationSerializer.page({}, Classification.all, {})
   end
+
+  describe "avoid heavy count queries on paging" do
+    it "should manually deal with the paging information" do
+      result = ClassificationSerializer.page({}, Classification.all, {})
+      meta = result[:meta][:classifications]
+      expect(meta[:count]).to eq(0)
+      expect(meta[:page_count]).to eq(0)
+      expect(meta[:previous_page]).to eq(0)
+      expect(meta[:next_page]).to eq(2)
+    end
+
+    it "should handle the the previous page information" do
+      result = ClassificationSerializer.page({page: 2}, Classification.all, {})
+      meta = result[:meta][:classifications]
+      expect(meta[:count]).to eq(0)
+      expect(meta[:page_count]).to eq(0)
+      expect(meta[:previous_page]).to eq(1)
+      expect(meta[:next_page]).to eq(3)
+    end
+  end
 end
