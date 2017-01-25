d24f1f53be9659277ca383696e102896b2f5d37a
Marten Veldthuis <marten@veldthuis.com>
2017-01-25 14:00:14 +0000

    Stream the updated_at for workflows and projects

--- app/serializers/event_stream_serializers/project_serializer.rb
+++ app/serializers/event_stream_serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 module EventStreamSerializers
   class ProjectSerializer < ActiveModel::Serializer
-    attributes :id, :display_name, :created_at
+    attributes :id, :display_name, :created_at, :updated_at
   end
 end
