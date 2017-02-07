e3714ca7d5925ccae561387dbeb2e0b50fbaacf0
Campbell Allen <campbell.allen@gmail.com>
2017-02-07 16:08:09 +0000

    cleanup invalid recents in rake task not migration

--- db/migrate/20170118141452_add_recents_foreign_keys.rb
+++ db/migrate/20170118141452_add_recents_foreign_keys.rb
@@ -1,13 +1,6 @@
 class AddRecentsForeignKeys < ActiveRecord::Migration
 
   def change
-    # remove all recents that have no traceable user in the classification
-    # should have picked these up in the rake task to backfill :(
-    Recent.where(user_id: nil)
-      .joins(:classification)
-      .where(classifications: { user_id: nil})
-      .delete_all
-
     add_foreign_key :recents, :projects
     add_foreign_key :recents, :workflows
     add_foreign_key :recents, :users
