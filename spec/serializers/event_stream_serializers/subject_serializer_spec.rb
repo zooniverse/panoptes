a68178881ef37aebaff0d5314e93f73734b9c7f0
Marten Veldthuis <marten@veldthuis.com>
2017-02-07 11:45:55 +0000

    Add subject locations into stream

---
+++ spec/serializers/event_stream_serializers/subject_serializer_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe EventStreamSerializers::SubjectSerializer do
+  let(:subject) { create(:subject, :with_mediums) }
+  let(:serializer) { described_class.new(subject) }
+  let(:adapter) { Serialization::V1Adapter.new(serializer) }
+
+  it 'serializes the subject locations' do
+    expected = subject.ordered_locations.map do |loc|
+      { "#{loc.content_type}" => loc.url_for_format(:get) }
+    end
+    expect(adapter.as_json["subjects"][0][:locations]).to eq(expected)
+  end
+end
