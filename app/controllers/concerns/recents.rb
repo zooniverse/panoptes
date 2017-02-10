ff0a6e65450a7102a5e2b80bfdba1634631d20f4
Campbell Allen <campbell.allen@gmail.com>
2017-02-10 17:18:11 +0000

    simply the recent scope
    fk filtering is provided by restpack serializer

--- app/controllers/concerns/recents.rb
+++ app/controllers/concerns/recents.rb
@@ -2,13 +2,10 @@ module Recents
   def recents
     ps = params.dup
     ps.delete "#{resource_name}_id"
-    render json_api: RecentSerializer.page(ps, recent_scope, { type: resource_sym.to_s, owner_id: resource_ids })
-  end
-
-  def recent_scope
-    scope = Recent.where(:"#{resource_name}_id" => resource_ids)
-    scope = scope.where(project_id: params[:project_id]) if params.key?(:project_id)
-    scope = scope.where(workflow_id: params[:workflow_id]) if params.key?(:workflow_id)
-    scope
+    render json_api: RecentSerializer.page(
+      ps,
+      Recent.where(:"#{resource_name}_id" => resource_ids),
+      { type: resource_sym.to_s, owner_id: resource_ids }
+    )
   end
 end
