8ef0224a09db324678113cca06d8b384d24c31ab
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2017-02-15 10:02:45 -0600

    Moved specs to model, added contributor role to show/index

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -63,4 +63,29 @@ describe Collection, type: :model do
       expect(collection.subjects).to all( be_a(Subject) )
     end
   end
+
+  context "collaboration" do
+    let(:contributor) { ApiUser.new(create(:user)) }
+    let(:collab_collection) { create(:collection) }
+    let(:subjects) { create_list(:subject, 4) }
+
+    describe "user is a collaborator" do
+      let!(:acl) do
+        create(:access_control_list,
+               user_group: contributor.user.identity_group,
+               resource: collab_collection,
+               roles: ["contributor"])
+      end
+
+      it "allows contributor to update links" do
+        expect(Collection.scope_for(:update_links, contributor)).to include(collab_collection)
+      end
+    end
+
+    describe "user is unauthorized" do
+      it "does not allow non-contributor to update links" do
+        expect(Collection.scope_for(:update_links, contributor)).to_not include(collab_collection)
+      end
+    end
+  end
 end
