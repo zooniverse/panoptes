9115d3d71384760a16ce5ce5531b6659603b7af8
Campbell Allen <campbell.allen@gmail.com>
2017-02-16 15:11:48 +0000

    add paging collection routes

--- spec/serializers/classification_serializer_spec.rb
+++ spec/serializers/classification_serializer_spec.rb
@@ -11,6 +11,20 @@ describe ClassificationSerializer do
     ClassificationSerializer.page({}, Classification.all, {})
   end
 
+  describe "nested collection routes" do
+    it "should return the correct href urls" do
+      %w(gold_standard incomplete project).each do |collection_route|
+        context = {url_suffix: collection_route}
+        result = ClassificationSerializer.page({}, Classification.all, context)
+        meta = result[:meta][:classifications]
+        expect(meta[:first_href]).to eq("/classifications/#{collection_route}")
+        expect(meta[:previous_href]).to eq("/classifications/#{collection_route}?page=0")
+        expect(meta[:next_href]).to eq("/classifications/#{collection_route}?page=2")
+        expect(meta[:last_href]).to eq("/classifications/#{collection_route}?page=0")
+      end
+    end
+  end
+
   describe "avoid heavy count queries on paging" do
     it "should manually deal with the paging information" do
       result = ClassificationSerializer.page({}, Classification.all, {})
