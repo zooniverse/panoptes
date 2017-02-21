8cae5787a3b61c9028f3da227f83e20e379a6fcd
Campbell Allen <campbell.allen@gmail.com>
2017-02-21 18:22:18 +0000

    remove selection context from subject serializer

--- spec/serializers/subject_serializer_spec.rb
+++ spec/serializers/subject_serializer_spec.rb
@@ -36,31 +36,4 @@ describe SubjectSerializer do
       expect(expected).to match_array(result_locs)
     end
   end
-
-  context "subject selection" do
-    let(:selection_context) { { select_context: true } }
-    let(:run_serializer) do
-      SubjectSerializer.single({}, Subject.all, selection_context)
-    end
-
-    describe "seen, retired, finished selection contexts" do
-      it "should run the lookups if the feature flag is off" do
-        expect_any_instance_of(SubjectSerializer).to receive(:retired)
-        expect_any_instance_of(SubjectSerializer).to receive(:already_seen)
-        expect_any_instance_of(SubjectSerializer).to receive(:finished_workflow)
-        run_serializer
-      end
-
-      context "when skip select context lookup feature flag is on" do
-        let(:selection_context) { { select_context: false } }
-
-        it "should not run the lookups if the feature flag is on" do
-          expect_any_instance_of(SubjectSerializer).not_to receive(:retired)
-          expect_any_instance_of(SubjectSerializer).not_to receive(:already_seen)
-          expect_any_instance_of(SubjectSerializer).not_to receive(:finished_workflow)
-          run_serializer
-        end
-      end
-    end
-  end
 end
