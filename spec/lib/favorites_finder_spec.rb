f07dc9d10fe7dc66f485992216b18991465f56a7
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2017-03-07 11:38:33 +0000

    Removed unnecessary collection builds

--- spec/lib/favorites_finder_spec.rb
+++ spec/lib/favorites_finder_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe FavoritesFinder do
   let(:non_fav_subject) { subjects.first }
   let(:fav_subject) { subjects.last }
   let(:subject_ids) { subjects.map(&:id) }
-  let!(:collection) do
+  let(:collection) do
     create(:collection, owner: user, subjects: [fav_subject], favorite: true, projects: [project])
   end
   context "user has no favorites" do
     it "returns favorite as false" do
-      user.collections.destroy_all
       expect(FavoritesFinder.new(user, project, subject_ids).find_favorites).to eq([])
     end
   end
 
   context "user has favorites" do
+    before { collection }
     it "favorite returns true for favorited subjects" do
       expect(FavoritesFinder.new(user, project, subject_ids).find_favorites).to include(fav_subject.id)
     end
