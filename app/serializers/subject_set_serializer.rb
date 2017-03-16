5fe1222ea7761b303194534270658dd4baad3a66
Campbell Allen <campbell.allen@gmail.com>
2017-03-16 16:36:18 +0000

    setup has_many_filtering using customized serializer code and preload data

--- app/serializers/subject_set_serializer.rb
+++ app/serializers/subject_set_serializer.rb
@@ -1,10 +1,13 @@
 class SubjectSetSerializer
-  include RestPack::Serializer
+  include Serialization::PanoptesRestpack
   include FilterHasMany
 
   attributes :id, :display_name, :set_member_subjects_count, :metadata,
     :created_at, :updated_at, :href
+
   can_include :project, :workflows
   can_sort_by :display_name
   can_filter_by :display_name
+
+  preload :workflows
 end
