9699d4109ba1c8ce249060f8a91ce84cb4437094
Campbell Allen <campbell.allen@gmail.com>
2017-03-20 21:55:02 +0000

    add gold standard annotation model

---
+++ app/models/gold_standard_annotation.rb
@@ -0,0 +1,16 @@
+class GoldStandardAnnotation < ActiveRecord::Base
+  belongs_to :workflow
+  belongs_to :subject
+  belongs_to :project
+  belongs_to :user
+  belongs_to :classification
+
+  validates_presence_of
+    :workflow,
+    :subject,
+    :project,
+    :user,
+    :classification,
+    :metadata,
+    :annotations
+end
