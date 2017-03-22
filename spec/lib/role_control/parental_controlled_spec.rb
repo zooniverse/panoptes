6d9d5b14173482957107befba2851acd343353b2
Campbell Allen <campbell.allen@gmail.com>
2017-03-22 10:26:06 +0000

    test no joins for default public / private scopes
    renamed the feature flag to indicate we are testing this feature and added the behaviour to the default access scopes

--- spec/lib/role_control/parental_controlled_spec.rb
+++ spec/lib/role_control/parental_controlled_spec.rb
@@ -7,20 +7,15 @@ describe RoleControl::ParentalControlled do
   let(:enrolled_actor) { ApiUser.new(user) }
   let(:parent) { ControlledTable.create! }
   let(:controlled) { TestParentControlTable.create! }
+  let(:klass) { controlled.class }
 
-  describe "::scope_for" do
-    let(:klass) { controlled.class }
+  before do
+    controlled
+    create_roles_join_instance(%w(test_role), parent, user)
+  end
 
-    before do
-      controlled
-      create_roles_join_instance(%w(test_role), parent, user)
-    end
-
-    after do
-      klass.scope_for(:update, enrolled_actor)
-    end
-
-    context "without no join parent scope feature flag" do
+  shared_examples_for "a parental controlled" do
+    context "without test no join parent scope feature flag" do
       it "should call join on the parent" do
         expect(klass)
           .to receive(:joins)
@@ -29,9 +24,9 @@ describe RoleControl::ParentalControlled do
       end
     end
 
-    context "with no join parent scope feature flag" do
+    context "with test no join parent scope feature flag" do
       it "should call filter on belongs_to parent fk" do
-        Panoptes.flipper[:no_join_parental_scope].enable
+        Panoptes.flipper[:test_no_join_parental_scope].enable
         parent_scope_for = parent.class.scope_for(:update, enrolled_actor, {})
         parent_fk = klass.reflect_on_association(parent.model_name.singular).foreign_key
         expect(klass)
@@ -40,6 +35,32 @@ describe RoleControl::ParentalControlled do
           .and_call_original
       end
     end
+  end
+
+  describe "::public_scope" do
+    let(:parent) { ControlledTable.create!(private: false) }
+
+    it_behaves_like "a parental controlled" do
+      after do
+        klass.public_scope
+      end
+    end
+  end
+
+  describe "::private_scope" do
+    it_behaves_like "a parental controlled" do
+      after do
+        klass.private_scope
+      end
+    end
+  end
+
+  describe "::scope_for" do
+    after do
+      klass.scope_for(:update, enrolled_actor)
+    end
+
+    it_behaves_like "a parental controlled"
 
     it "should call scope_for on the parent" do
       expect(parent.class).to receive(:scope_for).with(:update, enrolled_actor, {})
