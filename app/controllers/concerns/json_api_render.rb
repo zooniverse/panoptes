89a99f841ffbf37069286916f811f84d94336d1f
Campbell Allen <campbell.allen@gmail.com>
2017-03-23 23:54:18 +0000

    use extracted HttpCacheable class
    use the extracted class to determine the public nature of the controlled resources and the appropriate cache directive

--- app/controllers/concerns/json_api_render.rb
+++ app/controllers/concerns/json_api_render.rb
@@ -1,32 +1,20 @@
 module JSONApiRender
   extend ActiveSupport::Concern
 
-  CACHEABLE_RESOURCES = { subjects: "public max-age: 60" }.freeze
-
   included do
     ActionController.add_renderer :json_api do |obj, options|
       response_body = JSONApiResponse.format_response_body(obj)
       if options[:generate_response_obj_etag]
         self.headers["ETag"] = JSONApiResponse.response_etag_header(response_body)
       end
-      resource_cache_directive = CACHEABLE_RESOURCES[resource_sym]
-      if resource_cache_directive && options[:add_http_cache] == "true"
 
-        #TODO: move this to some infelctor or render options
-        parent_class = resource_class.parent_class
-        parent_resource_ids = controlled_resources.map do |cr|
-          cr.send resource_class.parent_foreign_key
-        end
-
-        contains_private_data = parent_class
-          .private_scope
-          .where(id: parent_resource_ids)
-          .exists?
-
-        if !contains_private_data
-          self.headers["Cache-Control"] = resource_cache_directive
+      if options[:add_http_cache] == "true"
+        http_cacheable = HttpCacheable.new(controlled_resources)
+        if http_cacheable.public_resources?
+          self.headers["Cache-Control"] = http_cacheable.resource_cache_directive
         end
       end
+
       self.content_type ||= Mime::Type.lookup("application/vnd.api+json; version=1")
       self.response_body = response_body
     end
