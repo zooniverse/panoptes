c4ae146ff315b86c1f258d6560ba34eef088cf03
Campbell Allen <campbell.allen@gmail.com>
2017-03-26 16:59:38 +0100

    do not validate the subjects on classification save

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -8,7 +8,9 @@ class Classification < ActiveRecord::Base
 
   has_many :recents, dependent: :destroy
 
-  has_and_belongs_to_many :subjects, join_table: :classification_subjects
+  has_and_belongs_to_many :subjects,
+    join_table: :classification_subjects,
+    validate: false
 
   enum expert_classifier: [:expert, :owner]
 
