5fc9552e00ca4acbca58a8968a23aca5e0b61f45
Campbell Allen <campbell.allen@gmail.com>
2017-03-30 16:56:20 +0100

    modify cached serailizer to have an overridable cache_key class method

--- app/serializers/concerns/cached_serializer.rb
+++ app/serializers/concerns/cached_serializer.rb
@@ -4,7 +4,7 @@ module CachedSerializer
   module ClassMethods
     def as_json(model, context)
       if Panoptes.flipper["cached_serializer"].enabled?
-        cache_key = "#{model.class.to_s}/#{model.id}/#{model.updated_at.to_i}/context-#{Digest::MD5.hexdigest(context.to_json)}"
+        cache_key = serializer_cache_key(model, Digest::MD5.hexdigest(context.to_json))
 
         Rails.cache.fetch(cache_key) do
           super
@@ -12,6 +12,10 @@ module CachedSerializer
       else
         super
       end
+    end
+
+    def serializer_cache_key(model, context_hash)
+      "#{model.class.to_s}/#{model.id}/#{model.updated_at.to_i}/context-#{context_hash}"
     end
   end
 end
