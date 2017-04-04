3f32299d7ad8990ab58d7829fbf06355f3ea0c87
Campbell Allen <campbell.allen@gmail.com>
2017-04-04 10:45:23 +1000

    move to IN sub select queries instead of JOINS on parental controlled (#2259)
    * move to ANY queries instead of JOINS on parental controlled
    
    remove the feature flag conditional behaviour and implement the ANY avoiding join scopes for queries.
    
    * use select sub queries instead of pluck and another query
    
    * ensure we have the parent_relation available
    
    lost this in my rebase

--- lib/role_control/parental_controlled.rb
+++ lib/role_control/parental_controlled.rb
@@ -4,24 +4,8 @@ module RoleControl
     include RoleControl::Controlled
 
     included do
-      scope :private_scope, lambda {
-        private_scope = parent_class.private_scope
-
-        if Panoptes.flipper["test_no_join_parental_scope"].enabled?
-          where(parent_foreign_key => private_scope.select(:id))
-        else
-          joins(@parent).merge(private_scope)
-        end
-      }
-      scope :public_scope, lambda {
-        public_scope = parent_class.public_scope
-
-        if Panoptes.flipper["test_no_join_parental_scope"].enabled?
-          where(parent_foreign_key => public_scope.select(:id))
-        else
-          joins(@parent).merge(public_scope)
-        end
-      }
+      scope :private_scope, -> { parent_scope(parent_class.private_scope) }
+      scope :public_scope,  -> { parent_scope(parent_class.public_scope) }
     end
 
     module ClassMethods
@@ -36,22 +20,20 @@ module RoleControl
         @parent_class ||= @parent.to_s.camelize.constantize
       end
 
+      def parent_relation
+       @parent
+     end
+
       def parent_foreign_key
         reflect_on_association(@parent).foreign_key
       end
 
-      def parent_relation
-        @parent
+      def parent_scope(scope)
+        where(parent_foreign_key => scope.select(:id))
       end
 
       def scope_for(action, user, opts={})
-        parent_scope = parent_class.scope_for(action, user, opts)
-
-        if Panoptes.flipper["test_no_join_parental_scope"].enabled?
-          where(parent_foreign_key => parent_scope.select(:id))
-        else
-          joins(@parent).merge(parent_scope)
-        end
+        parent_scope(parent_class.scope_for(action, user, opts))
       end
     end
   end
--- spec/lib/role_control/parental_controlled_spec.rb
+++ spec/lib/role_control/parental_controlled_spec.rb
@@ -15,27 +15,11 @@ describe RoleControl::ParentalControlled do
   end
 
   shared_examples_for "a parental controlled" do
-    context "without test no join parent scope feature flag" do
-      it "should call join on the parent" do
-        expect(klass)
-          .to receive(:joins)
-          .with(parent.class.model_name.singular.to_sym)
-          .and_call_original
-      end
-    end
-
-    context "with test no join scope feature flag" do
-      let(:parent_fk) do
-        klass.reflect_on_association(parent.model_name.singular).foreign_key
-      end
-
-      it "should call filter on belongs_to parent fk" do
-        Panoptes.flipper[:test_no_join_parental_scope].enable
-        expect(klass)
-          .to receive(:where)
-          .with(parent_fk => sub_select_scope.select(:id))
-          .and_call_original
-      end
+    it "should call filter on belongs_to parent fk" do
+      expect(klass)
+        .to receive(:where)
+        .with(klass.parent_foreign_key => parent_select_scope.select(:id))
+        .and_call_original
     end
   end
 
@@ -43,7 +27,7 @@ describe RoleControl::ParentalControlled do
     let(:parent) { ControlledTable.create!(private: false) }
 
     it_behaves_like "a parental controlled" do
-      let(:sub_select_scope) { parent.class.public_scope }
+      let(:parent_select_scope) { parent.class.public_scope }
       after do
         klass.public_scope
       end
@@ -52,7 +36,7 @@ describe RoleControl::ParentalControlled do
 
   describe "::private_scope" do
     it_behaves_like "a parental controlled" do
-      let(:sub_select_scope) { parent.class.private_scope }
+      let(:parent_select_scope) { parent.class.private_scope }
       after do
         klass.private_scope
       end
@@ -60,7 +44,9 @@ describe RoleControl::ParentalControlled do
   end
 
   describe "::scope_for" do
-    let(:sub_select_scope) { parent.class.scope_for(:update, enrolled_actor, {}) }
+    let(:parent_select_scope) do
+      parent.class.scope_for(:update, enrolled_actor, {})
+    end
     after do
       klass.scope_for(:update, enrolled_actor, {})
     end
@@ -68,7 +54,10 @@ describe RoleControl::ParentalControlled do
     it_behaves_like "a parental controlled"
 
     it "should call scope_for on the parent" do
-      expect(parent.class).to receive(:scope_for).with(:update, enrolled_actor, {})
+      expect(parent.class)
+        .to receive(:scope_for)
+        .with(:update, enrolled_actor, {})
+        .and_call_original
     end
   end
 end
