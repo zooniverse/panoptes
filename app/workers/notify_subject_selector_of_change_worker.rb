b2a664fc3a498d002c549bda03d68d4f3e1aeab0
Marten Veldthuis <marten@veldthuis.com>
2017-04-10 16:25:05 +0100

    Restore pre-SGL queue configs

--- app/workers/notify_subject_selector_of_change_worker.rb
+++ app/workers/notify_subject_selector_of_change_worker.rb
@@ -1,9 +1,7 @@
 class NotifySubjectSelectorOfChangeWorker
   include Sidekiq::Worker
 
-  # SGL-PRIORITY
-  # sidekiq_options retry: 3, queue: :data_high
-  sidekiq_options retry: 3, queue: :high
+  sidekiq_options retry: 3, queue: :data_high
 
   def perform(workflow_id)
     workflow = Workflow.find(workflow_id)
