25c118b198074945a2842d23e095e10c799742e5
Marten Veldthuis <marten@veldthuis.com>
2017-05-10 13:55:00 +0100

    Add tests for ClassificationsExportSegments::Create

--- app/operations/classifications_export_segments/create.rb
+++ app/operations/classifications_export_segments/create.rb
@@ -1,17 +1,37 @@
-module ClassificationExportSegments
+module ClassificationsExportSegments
   class Create < Operation
     integer :workflow_id
-    object :requester
 
     def execute
       workflow = Workflow.find(workflow_id)
       previous_segment = workflow.latest_classifications_export_segment
 
-      segment = previous_segment.next_segment
-      segment.requester = requester
-      segment.create!
+      segment = if previous_segment
+                  previous_segment.next_segment
+                else
+                  initial_segment(workflow)
+                end
 
-      ClassificationsExportSegmentWorker.perform_async(segment.id, segment.class.name.underscore)
+      segment.requester = requester
+
+      if segment.save
+        ClassificationsExportSegmentWorker.perform_async(segment.id, segment.class.name.underscore)
+        segment
+      else
+        nil
+      end
+    end
+
+    private
+
+    def initial_segment(workflow)
+      segment = workflow.classifications_export_segments.build(project_id: workflow.project_id)
+      segment.set_first_last_classifications(nil)
+      segment
+    end
+
+    def requester
+      api_user.user
     end
   end
 end
