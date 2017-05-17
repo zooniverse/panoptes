bd77c50ea5d125226f4d1ca34f337ab58c85f46b
Campbell Allen <campbell.allen@gmail.com>
2017-05-17 16:04:01 +0100

    test using autoload vs eager load
    Ensure we only add the sub dirs with */. Also we really want to eager load our code for COW benefits and to avoid lookup on first constant encounters.

--- config/application.rb
+++ config/application.rb
@@ -13,11 +13,22 @@ Bundler.require(*Rails.groups)
 
 module Panoptes
   class Application < Rails::Application
-    config.autoload_paths += Dir[Rails.root.join('app', 'models', '**/')]
-    config.autoload_paths += Dir[Rails.root.join('app', 'serializers', '**/')]
-    config.autoload_paths += Dir[Rails.root.join('app', 'workers', '**/')]
-    config.autoload_paths += Dir[Rails.root.join('app', 'operations', '**/')]
-    config.autoload_paths += Dir[Rails.root.join('lib', '**/')]
+    # config.eager_load_paths += Dir[Rails.root.join('app', 'models', '*/')]
+    # config.eager_load_paths += Dir[Rails.root.join('app', 'workers', '*/')]
+    # config.eager_load_paths += Dir[Rails.root.join('app', 'operations', '*/')]
+    # config.eager_load_paths += Dir[Rails.root.join('app', 'serializers', '*/')]
+    # config.eager_load_paths += Dir[Rails.root.join('app', 'formatters', '*/')]
+
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '*/')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'workers', '*/')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'operations', '*/')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'serializers', '*/')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'formatters', '*/')]
+
+    # config.autoload_paths += Dir[Rails.root.join('lib', '**/')]
+    config.eager_load_paths += Dir[Rails.root.join('lib', '**/')]
+
+    # config.eager_load_namespaces << ::Formatters::Csv::Classification
 
     config.action_dispatch.perform_deep_munge = false
     config.middleware.insert_before ActionDispatch::ParamsParser, "RejectPatchRequests"
