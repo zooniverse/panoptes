8295433e58c29d97146f1f2ea3f32567a787c7ca
Campbell Allen <campbell.allen@gmail.com>
2017-05-24 10:03:32 +0100

    test the cleaner methods are called in the worker
    rely on the unit tests for the Doorkeeper::AccessCleanup, test the worker behaviour not the cleaner class.

--- spec/workers/cleanup_expired_access_tokens_worker_spec.rb
+++ spec/workers/cleanup_expired_access_tokens_worker_spec.rb
@@ -2,9 +2,6 @@ require 'spec_helper'
 
 describe CleanupExpiredAccessTokensWorker do
   let(:worker) { described_class.new }
-  let(:user) { create(:user) }
-  let(:access_token) { create(:access_token, resource_owner_id: user.id) }
-  let(:access_grant) { create(:access_grant, resource_owner_id: user.id) }
 
   it{ is_expected.to be_a Sidekiq::Worker }
 
@@ -15,46 +12,12 @@ describe CleanupExpiredAccessTokensWorker do
   end
 
   describe "perform" do
-    before do
-      access_token
-    end
+    let(:cleaner) { instance_double(Doorkeeper::AccessCleanup) }
 
-    def make_old(resource)
-      resource.update_column(:created_at, resource.created_at - 31.days)
-    end
-
-    it 'should not cleanup valid tokens' do
+    it 'should tell doorkeeper to cleanup', :focus do
+      allow(Doorkeeper::AccessCleanup).to receive(:new).and_return(cleaner)
+      expect(cleaner).to receive(:cleanup!)
       worker.perform
-      expect { access_token.reload }.not_to raise_error
-    end
-
-    it 'should not cleanup valid access grant' do
-      worker.perform
-      expect { access_token.reload }.not_to raise_error
-    end
-
-    it "should cleanup revoked tokens" do
-      access_token.revoke
-      worker.perform
-      expect { access_token.reload }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-
-    it "should cleanup old tokens" do
-      make_old(access_token)
-      worker.perform
-      expect { access_token.reload }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-
-    it "should cleanup the revoked access grants" do
-      access_grant.revoke
-      worker.perform
-      expect { access_grant.reload }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-
-    it "should cleanup the old access grants" do
-      make_old(access_grant)
-      worker.perform
-      expect { access_grant.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
