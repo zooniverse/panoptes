999d156f3b6d8b8178a887c07220b4eb4e7ab8cf
Zach <zach.wolfenbarger@gmail.com>
2017-06-08 09:43:27 -0500

    Saves all orgs to ensure non-blank slug attributes before adding unique index (#2336)

--- db/migrate/20170524205300_add_index_to_organization_slug.rb
+++ db/migrate/20170524205300_add_index_to_organization_slug.rb
@@ -1,5 +1,7 @@
 class AddIndexToOrganizationSlug < ActiveRecord::Migration
   def change
+    # Ensure that all slugs are populated by acts_as_url callback
+    Organization.all.each(&:save)
     add_index :organizations, :slug, unique: true
   end
 end
