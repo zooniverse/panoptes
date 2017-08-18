0261e02766da6a88a33289cdb3784c90b97b272e
Campbell Allen <campbell.allen@gmail.com>
2017-08-18 15:30:39 +0100

    better controller before_filter naming

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_devise_permitted_parameters, if: :devise_controller?
 
   def unknown_route
     exception = ActionController::RoutingError.new("Not Found")
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
+  def configure_devise_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:email, :password, :password_confirmation, :login, :display_name,
                :credited_name, :global_email_communication,
