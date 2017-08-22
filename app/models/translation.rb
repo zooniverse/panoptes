29202aa1496abdf5cf7ae00eaa1e2260f612716d
Campbell Allen <campbell.allen@gmail.com>
2017-08-22 11:42:29 +0100

    remove translation model authorization
    do not use role controlled model code to deal with authorizations.
    this would need ACL's setup on each translated resource.

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,34 +1,8 @@
 class Translation < ActiveRecord::Base
-  include RoleControl::Controlled
-
   belongs_to :translated, polymorphic: true, required: true
   validate :validate_strings
 
-  can_by_role :index, roles: [ :owner, :translator, :collaborator ]
-
-  def self.private_query(action, target, roles)
-    user_group_memberships = memberships_query(action, target)
-      .select(:user_group_id)
-    # check the ACL roles for projects and orgs
-    # this should cover all translatable resources available
-    AccessControlList
-      .where(user_group_id: user_group_memberships)
-      .where(resource_type: %w(Project Organization))
-      .select(:resource_id)
-      .where.overlap(roles: roles)
-  end
-
-  def self.user_can_access_scope(private_query, public_flag)
-    # TODO: convert this to lookup projects or orgs
-    # union all i guess on this but taking into account that the ids
-    # from the private query may match projects / orgs we don't actually
-    # have access too
-    scope = where(id: private_query.select(:resource_id))
-    scope = scope.or(public_scope) if public_flag
-    scope
-  end
-
-  # Look at adding in paper trail change tracking for laguage / strings here
+  # TODO: Look at adding in paper trail change tracking for laguage / strings here
 
   private
 
