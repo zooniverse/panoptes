58f1c37ebd8911088ae4f5b7c443237c5f2ea6df
Campbell Allen <campbell.allen@gmail.com>
2017-08-25 17:26:05 +0100

    add specs for translations index controller

--- spec/controllers/api/v1/translations_controller_spec.rb
+++ spec/controllers/api/v1/translations_controller_spec.rb
@@ -6,21 +6,25 @@ RSpec.describe Api::V1::TranslationsController, type: :controller do
   # TODO: expand to worklows, tutorials, field guides, etc
   %i(project).each do |resource_type|
     let(:resource) { create(:translation) }
-    let(:api_resource_name) { resource_type.to_s}
+    let(:api_resource_name) { "translations" }
     let(:api_resource_attributes) { %w(id strings language) }
-    let(:api_resource_links) { %w(workflow_contents.workflow) }
+    let(:api_resource_links) { %w(translated.project) }
     let(:scopes) { [ resource_type ] }
 
     describe "#index", :focus do
-      let!(:private_resource) do
-        create(resource_type, private: true)
-      end
-
       it_behaves_like "is indexable" do
         let(:index_params) do
-          { translated_id: resource.translated_id, translated_type: "project" }
+          resource
+          # passing a translated id will filter the project resources
+          # TODO: add this as a specific filteirng spec on owning resources
+          # { translated_id: resource.translated_id, translated_type: resource_type.to_s }
+          { translated_type: resource_type.to_s }
         end
         let(:n_visible) { 2 }
+        # TODO: remove the setup for all the specs here...can we do this?
+        let!(:private_resource) do
+          create(:translation, translated: create(:private_project))
+        end
       end
     end
   end
