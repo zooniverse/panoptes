7252e5c114772e290b210adf6cfcd78e6151ecb1
Campbell Allen <campbell.allen@gmail.com>
2017-08-29 16:40:21 +0100

    add more complex factory examples
    different type with more complex json strings nesting

--- spec/factories/translations.rb
+++ spec/factories/translations.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :translation do
+  factory :translation, aliases: [:project_translation]do
     strings({
       title: "A test Project",
       description: "Some Lorem Ipsum",
@@ -13,5 +13,30 @@ FactoryGirl.define do
     })
     association :translated, factory: :project
     language "en-GB"
+
+    factory :workflow_translation do
+      strings({
+        display_name: "A translated Workflow name",
+        tasks: [
+          interest: {
+            type: 'drawing',
+            question: "Draw a circle",
+            help: "Duh?",
+            # the tool lables array might be best un-wound to avoid ambiguity
+            # "tool_lablel.2" => "Blue",
+            tool_labels: %w(Red Green Blue)
+          },
+          shape: {
+            question: "What shape is this galaxy",
+            help: "Duh?",
+            # the answers array might be best un-wound to avoid ambiguity
+            # "answers.0" => "Smooth",
+            answers: ["Smooth", "Features", "Star or artifact"]
+          }
+        ]
+      })
+
+      association :translated, factory: :workflow
+    end
   end
 end
