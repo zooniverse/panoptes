f392a2b220809fa57a3b4313fb22d0ec2abdbb5d
Campbell Allen <campbell.allen@gmail.com>
2017-11-01 16:02:09 +0000

    all routes have to supply the translated_type param
    this wires up the authorisation scopes for translation resources

--- lib/routes/constraints/translations.rb
+++ lib/routes/constraints/translations.rb
@@ -1,41 +1,28 @@
 module Routes
   module Constraints
     class Translations
-      attr_reader :request, :params
+      attr_reader :params
 
       def matches?(request)
-        @request = request
         @params = request.params
 
-        if create_route? || index_route?
-          has_translated_type_param?
-        elsif show_route? || update_route?
-          valid_show_id?
+        if member_route?
+           valid_show_id? && has_translated_type_param?
         else
-          false
+          # all polymorphic controllers need to supply the translated resource
+          # type to correctly wire up the controller authorization scopes
+          has_translated_type_param?
         end
       end
 
       private
 
-      def show_route?
+      def member_route?
         params.key?(:id)
       end
 
-      def update_route?
-         request.put? || request.patch?
-      end
-
-      def create_route?
-        request.post?
-      end
-
-      def index_route?
-        request.get? && !show_route?
-      end
-
       def has_translated_type_param?
-        !!params.dig(:translations, :translated_type)
+        params.key?(:translated_type)
       end
 
       def valid_show_id?
