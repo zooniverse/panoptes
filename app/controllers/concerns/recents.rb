e744572570ea3fa59cd7c961465eeae1cf2c9ae4
Campbell Allen <campbell.allen@gmail.com>
2017-11-02 13:26:29 +0000

    use common read replica entry point for dumps and recents

--- app/controllers/concerns/recents.rb
+++ app/controllers/concerns/recents.rb
@@ -2,24 +2,12 @@ module Recents
   def recents
     ps = params.dup
     ps.delete "#{resource_name}_id"
-    read_recents_from_database do
+    DatabaseReplica.read("read_recents_from_read_slave") do
       render json_api: RecentSerializer.page(
         ps,
         Recent.where(:"#{resource_name}_id" => resource_ids),
         { url_prefix: "#{resource_sym.to_s.pluralize}/#{resource_ids}" }
       )
-    end
-  end
-
-  private
-
-  def read_recents_from_database
-    if Panoptes.flipper.enabled?("read_recents_from_read_slave")
-      Slavery.on_slave do
-        yield
-      end
-    else
-      yield
     end
   end
 end
