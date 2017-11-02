728b94dc0a244bbe3d213eef9a5676a1e219565e
Campbell Allen <campbell.allen@gmail.com>
2017-11-02 10:30:15 +0000

    feature flag read recents from read replica
    some of the recents queries can be costly as they invert the order on created at before selecting the first page. push this to the read replica where we can

--- app/controllers/concerns/recents.rb
+++ app/controllers/concerns/recents.rb
@@ -2,10 +2,24 @@ module Recents
   def recents
     ps = params.dup
     ps.delete "#{resource_name}_id"
-    render json_api: RecentSerializer.page(
-      ps,
-      Recent.where(:"#{resource_name}_id" => resource_ids),
-      { url_prefix: "#{resource_sym.to_s.pluralize}/#{resource_ids}" }
-    )
+    read_recents_from_database do
+      render json_api: RecentSerializer.page(
+        ps,
+        Recent.where(:"#{resource_name}_id" => resource_ids),
+        { url_prefix: "#{resource_sym.to_s.pluralize}/#{resource_ids}" }
+      )
+    end
+  end
+
+  private
+
+  def read_recents_from_database
+    if Panoptes.flipper.enabled?("read_recents_from_read_slave")
+      Slavery.on_slave do
+        yield
+      end
+    else
+      yield
+    end
   end
 end
