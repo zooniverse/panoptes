d8ddcd461f186c81557e043514cb52f86f1fbe14
Campbell Allen <campbell.allen@gmail.com>
2017-11-09 15:41:08 +0000

    extract out the file path spec

--- spec/models/csv_dumps/direct_to_s3_spec.rb
+++ spec/models/csv_dumps/direct_to_s3_spec.rb
@@ -12,6 +12,7 @@ describe CsvDumps::DirectToS3 do
       content_disposition: "attachment; filename=\"full_email_list.csv\""
     }
   end
+  let(:file_path) { "/tmp/foobar" }
 
   it "push the file to s3 the correct bucket location via a custom storage adapter" do
     expect(adapter).to receive(:stored_path)
@@ -20,6 +21,14 @@ describe CsvDumps::DirectToS3 do
     expect(adapter).to receive(:put_file)
                          .with(s3_path, an_instance_of(String), s3_opts)
 
-    direct_to_s3.put_file("/tmp/foobar")
+  it "push the file to s3 the correct bucket location via a custom storage adapter" do
+    expect(adapter)
+      .to receive(:stored_path)
+      .with("application/x-gzip", "email_exports")
+      .and_call_original
+    expect(adapter)
+      .to receive(:put_file)
+      .with(s3_path, file_path, s3_opts)
+    direct_to_s3.put_file(file_path)
   end
 end
