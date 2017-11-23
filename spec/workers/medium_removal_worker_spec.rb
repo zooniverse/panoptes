e17523f45354dddbd7af6e24984bfd66d2766783
Campbell Allen <campbell.allen@gmail.com>
2017-11-23 21:14:53 +0000

    use new api for access denied error

--- spec/workers/medium_removal_worker_spec.rb
+++ spec/workers/medium_removal_worker_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe MediumRemovalWorker do
   end
 
   it 'should skip any access denied media paths' do
-    allow(MediaStorage).to receive(:delete_file).and_raise(AWS::S3::Errors::AccessDenied)
+    allow(MediaStorage)
+      .to receive(:delete_file)
+      .and_raise(Aws::S3::Errors::AccessDenied.new(:s3, "fake denied"))
     expect { subject.perform('test/path.txt') }.not_to raise_error
   end
 end
