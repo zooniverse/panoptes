04a9bb12e3daaf45176d3e1dbdbb999ae562990c
Marten Veldthuis <marten@veldthuis.com>
2017-12-08 11:05:26 +0000

    Remove subject queue model

--- spec/factories/subject_queues.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :subject_queue do
-    user
-    workflow
-    set_member_subject_ids []
-  end
-end
