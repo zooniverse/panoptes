5205313ed1aa09f44e0b93ebdd9866e3f0c5da9a
Adam McMaster <adam@zooniverse.org>
2017-12-11 10:17:23 +0000

    Remove a couple of other JISC-related bits

--- spec/workers/unsubscribe_worker_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe UnsubscribeWorker do
-  let(:user) { create(:user) }
-  it 'should send mail' do
-    expect{ subject.perform(user.email) }.to change{ ActionMailer::Base.deliveries.count }.by(1)
-  end
-end
