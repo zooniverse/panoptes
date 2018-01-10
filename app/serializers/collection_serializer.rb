892d951c018e55962ab7380cc423306b8da9aa2c
Marten Veldthuis <marten@veldthuis.com>
2018-01-10 12:52:18 +0000

    Support habtm_projects for create action

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -11,8 +11,8 @@ class CollectionSerializer
   # Do not include the BelongsToMany :projects relation
   # as this can't be preloaded (custom AR relation)
   # Note: this won't allow ?include=projects side loading of the resource
-  # TODO: After PR #2563, remove comment and just add "projects" to the can_include list
-  can_include :owner, :collection_roles, :subjects
+  # TODO: After PR #2563, remove comment and rename "habtm_projects" in the can_include list
+  can_include :owner, :collection_roles, :subjects, :habtm_projects
 
   can_filter_by :display_name, :slug, :favorite
   can_sort_by :display_name
