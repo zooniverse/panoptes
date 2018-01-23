d4628a9143d831c12442cd6bcdda7a214c930eb8
Campbell Allen <campbell.allen@gmail.com>
2018-01-23 11:13:58 +0000

    remove old indexes from previous migration

--- db/structure.sql
+++ db/structure.sql
@@ -2929,21 +2929,7 @@ CREATE UNIQUE INDEX index_tags_on_name ON tags USING btree (name);
 
 
 --
--- Name: index_translations_on_language; Type: INDEX; Schema: public; Owner: -; Tablespace:
---
-
-CREATE INDEX index_translations_on_language ON translations USING btree (language);
-
-
---
--- Name: index_translations_on_translated_type_and_translated_id; Type: INDEX; Schema: public; Owner: -; Tablespace:
---
-
-CREATE INDEX index_translations_on_translated_type_and_translated_id ON translations USING btree (translated_type, translated_id);
-
-
---
--- Name: index_tutorials_on_kind; Type: INDEX; Schema: public; Owner: -; Tablespace:
+-- Name: index_tutorials_on_kind; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE INDEX index_tutorials_on_kind ON tutorials USING btree (kind);
