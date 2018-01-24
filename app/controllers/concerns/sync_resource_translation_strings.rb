1214fcfb349c309489e7793777f82406c4670191
Campbell Allen <campbell.allen@gmail.com>
2018-01-24 13:42:58 +0000

    better handling of different model language attributes

--- app/controllers/concerns/sync_resource_translation_strings.rb
+++ app/controllers/concerns/sync_resource_translation_strings.rb
@@ -12,12 +12,19 @@ module SyncResourceTranslationStrings
       TranslationSyncWorker.perform_async(
         resource.class.name,
         resource.id,
-        resource.primary_language
+        translatable_language(resource)
       )
     end
   end
 
   def translatable_resources
     controlled_resources | created_resources
+
+  def translatable_language(resource)
+    if resource.respond_to?(:primary_language)
+      resource.primary_language
+    else
+      resource.language
+    end
   end
 end
