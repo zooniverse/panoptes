fc6f8fc56fbdc15940b53aca9c58e7d28f206623
Campbell Allen <campbell.allen@gmail.com>
2018-02-01 17:13:12 +0000

    copy edits on the gold standard annotation class note

--- app/models/gold_standard_annotation.rb
+++ app/models/gold_standard_annotation.rb
@@ -1,14 +1,14 @@
 # This class contains the historical gold standard classification annotations.
 # They have been ported to this table as a read only resource, as they are
-# used to provide accuracy feeback for users in specific Zooniverse
-# projects. This this feeback mechanism changed from GS annotations to
+# used to provide accuracy feedback for users in specific Zooniverse
+# projects. This feedback mechanism changed from GS annotations to
 # standardized subject metadata instead so teams can selfservice via uploads.
 #
 # Thus this is a snapshot of the GS data to support only specific Zoo projects
 # (see the project / workflow ids for the data in this table)
 #
 # Long term this can probably go, but please check that the projects have
-# been retired and don't need this data for the feeback mechanism.
+# been retired and don't need this data for the feedback mechanism.
 class GoldStandardAnnotation < ActiveRecord::Base
   belongs_to :workflow
   belongs_to :subject
