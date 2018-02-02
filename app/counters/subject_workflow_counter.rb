9973a0eb1fb479c643dd51c86e1ad1afaf68b71d
Campbell Allen <campbell.allen@gmail.com>
2018-02-02 12:10:47 +0000

    remove code experiment on subject workflow counters
    switch to the proposed filtering on classification index scopes to avoid counting duplicate, incomplete and gold standard data.

--- app/counters/subject_workflow_counter.rb
+++ app/counters/subject_workflow_counter.rb
@@ -7,42 +7,18 @@ class SubjectWorkflowCounter
   end
 
   def classifications
-    experiment_name = "subject_workflow_counter_skip_gs_and_seen_before"
-    CodeExperiment.run(experiment_name) do |e|
-      e.run_if { Panoptes.flipper[experiment_name].enabled? }
+    scope = Classification
+      .where(workflow: swc.workflow_id)
+      .joins("INNER JOIN classification_subjects cs ON cs.classification_id = classifications.id")
+      .where("cs.subject_id = ?", swc.subject_id)
+      .where("gold_standard IS NOT TRUE")
+      .where.not("metadata ? 'seen_before'")
+      .complete
 
-      e.use do
-        scope = Classification
-          .where(workflow: swc.workflow_id)
-          .joins("INNER JOIN classification_subjects cs ON cs.classification_id = classifications.id")
-          .where("cs.subject_id = ?", swc.subject_id)
-
-        count_classifications(scope)
-      end
-
-      e.try do
-        scope = Classification
-          .where(workflow: swc.workflow_id)
-          .joins("INNER JOIN classification_subjects cs ON cs.classification_id = classifications.id")
-          .where("cs.subject_id = ?", swc.subject_id)
-          .where("gold_standard IS NOT TRUE")
-          .where.not("metadata ? 'seen_before'")
-          .complete
-
-        count_classifications(scope)
-      end
-
-      # skip the mismatch reporting...we just want perf metrics
-      e.ignore { true }
-    end
-  end
-
-  private
-
-  def count_classifications(scope)
     if launch_date = swc.project.launch_date
       scope = scope.where("classifications.created_at >= ?", launch_date)
     end
+
     scope.count
   end
 end
