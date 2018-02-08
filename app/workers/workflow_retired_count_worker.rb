bc84f65fd2189a5ed03b81e30648abdcaf5c6a0d
Campbell Allen <campbell.allen@gmail.com>
2018-02-08 11:32:58 +0000

    only run 1 retired count worker every 10 seconds
    space them out to only allow 1 per 10 seconds every 10 seconds, without the min_delay these can run more than expected, see https://github.com/parrish/Congestion/blob/012bbd29370bb3133680ddf6f0de909356fe6697/lib/congestion/rate_limiter.rb#L41 for more details.

--- app/workers/workflow_retired_count_worker.rb
+++ app/workers/workflow_retired_count_worker.rb
@@ -6,7 +6,7 @@ class WorkflowRetiredCountWorker
   sidekiq_options congestion: {
     interval: 10,
     max_in_interval: 1,
-    min_delay: 0,
+    min_delay: 10,
     reject_with: :reschedule,
     key: ->(workflow_id) { "workflow_#{workflow_id}_retired_count_worker" }
   }
