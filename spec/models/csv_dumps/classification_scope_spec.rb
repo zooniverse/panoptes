f2a0e907e7e21da9a60f1a0fa38f773a30649eaa
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2018-02-08 17:03:00 -0600

    Process only undeleted workflows in cls exports (#2662)
    Classification dump workers should only export classifications from workflows that have not been deleted (via the Activatable module). That is, only workflows with `activated_state: "active"` should have their classifications included.
    
    This should not be confused with `active`, which is a flag researchers can set. Those classifications are correctly included already.

--- spec/models/csv_dumps/classification_scope_spec.rb
+++ spec/models/csv_dumps/classification_scope_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe CsvDumps::ClassificationScope do
   let(:workflow) { create(:workflow) }
+  let(:inactive_workflow) { create(:workflow, activated_state: "inactive") }
   let(:project) { workflow.project }
   let(:subject) { create(:subject, project: project, subject_sets: [create(:subject_set, workflows: [workflow])]) }
   let(:classifications) do
     create_list(:classification, 2, project: project, workflow: workflow, subjects: [subject])
   end
+  let!(:unincluded_classification) { create(:classification, project: project, workflow: inactive_workflow, subjects: [subject]) }
   let(:cache) { ClassificationDumpCache.new }
 
   let(:scope) { described_class.new(project, cache) }
 
-  it "should find all the classifications" do
+  it "should find all correctly scoped classifications" do
     expect { |b| scope.each(&b) }.to yield_successive_args(*classifications)
   end
 end
