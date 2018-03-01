ce2f6c5219d7b419c85dd0e24ed540d7c42adc65
Marten Veldthuis <marten@veldthuis.com>
2018-03-01 15:10:05 +0000

    Refactor dump workers (#2687)
    * Remove DumpWorker module
    
    * Convert DumpMailer module into object
    
    * Fix DumpMailer specs

--- app/workers/workflows_dump_worker.rb
+++ app/workers/workflows_dump_worker.rb
@@ -2,11 +2,23 @@ require 'csv'
 
 class WorkflowsDumpWorker
   include Sidekiq::Worker
-  include DumpWorker
-  include DumpMailerWorker
   include RateLimitDumpWorker
 
   sidekiq_options queue: :data_high
+
+  attr_reader :resource, :medium, :scope, :processor
+
+  def perform(resource_id, resource_type, medium_id=nil, requester_id=nil, *args)
+    raise ApiErrors::FeatureDisabled unless Panoptes.flipper[:dump_worker_exports].enabled?
+
+    if @resource = CsvDumps::FindsDumpResource.find(resource_type, resource_id)
+      @medium = CsvDumps::FindsMedium.new(medium_id, @resource, dump_target).medium
+      scope = get_scope(resource)
+      @processor = CsvDumps::DumpProcessor.new(formatter, scope, medium)
+      @processor.execute
+      DumpMailer.new(resource, medium, dump_target).send_email
+    end
+  end
 
   def formatter
     @formatter ||= Formatter::Csv::Workflow.new
@@ -14,5 +26,9 @@ class WorkflowsDumpWorker
 
   def get_scope(resource)
     @scope ||= CsvDumps::WorkflowScope.new(resource)
+  end
+
+  def dump_target
+    "workflows"
   end
 end
--- spec/workers/concerns/dump_mailer_spec.rb
+++ spec/workers/concerns/dump_mailer_spec.rb
@@ -1,37 +1,20 @@
 require 'spec_helper'
 
-describe DumpMailerWorker do
+describe DumpMailer do
   let(:users) { create_list(:user, 2) }
   let(:owner) { users.sample }
   let(:resource) do
     double(id: 1, class: Workflow, communication_emails: [owner.email])
   end
   let(:metadata) { {"recipients" => users.map(&:id) } }
-  let(:medium) {double(id: 2, get_url: nil, metadata: metadata) }
-  let(:worker_class) do
-    Class.new do
-      include DumpWorker
-      include DumpMailerWorker
-
-      attr_reader :project, :medium
-
-      def initialize(resource, medium)
-        @resource = resource
-        @medium = medium
-      end
-
-      def dump_target
-        "classifications"
-      end
-    end
-  end
+  let(:medium) { double(id: 2, get_url: nil, metadata: metadata) }
+  let(:dump_mailer) { described_class.new(resource, medium, "classifications") }
 
   describe 'queueing notification emails' do
-    let(:worker) { worker_class.new(resource, medium) }
     let(:expected_emails) { users.map(&:email) }
 
     before do
-      expect(worker.mailer)
+      expect(ClassificationDataMailerWorker)
       .to receive(:perform_async)
       .with(resource.id, "workflow", nil, expected_emails)
       .once
@@ -39,7 +22,7 @@ describe DumpMailerWorker do
     end
 
     it 'queues up an email job' do
-      worker.send_email
+      dump_mailer.send_email
     end
 
     context "with no specified recipients" do
@@ -47,7 +30,7 @@ describe DumpMailerWorker do
       let(:expected_emails) { [ owner.email ] }
 
       it 'queues an email to the owner' do
-        worker.send_email
+        dump_mailer.send_email
       end
     end
   end
