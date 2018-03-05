9f2c20d0d5685f7e1487c5331517f54f3ddfe622
Campbell Allen <campbell.allen@gmail.com>
2018-03-05 13:33:20 -0800

    memberships controller use explicit auth code

--- app/controllers/api/v1/memberships_controller.rb
+++ app/controllers/api/v1/memberships_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::MembershipsController < Api::ApiController
-  include RoleControl::RoledController
-  before_filter :require_login
+  attr_reader :auth_scheme
+  delegate :check_controller_resources,
+    :controlled_resources,
+    :controlled_resource,
+    to: :auth_scheme
+
+  prepend_before_filter :require_login
   require_authentication :all, scopes: [:group]
+
+  before_action :setup_auth_scheme, except: :create
+  before_action :check_controller_resources, except: :create
+
   resource_actions :index, :show, :create, :update, :deactivate
   schema_type :strong_params
 
@@ -19,7 +28,10 @@ class Api::V1::MembershipsController < Api::ApiController
 
   private
 
-  def add_active_resources_scope
-    false
+  def setup_auth_scheme
+    @auth_scheme = RoleControl::ControlledResources.new(
+      self,
+      add_active_resources_scope: false
+    )
   end
 end
