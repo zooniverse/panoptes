4672616b3da5bdb983487ebdc2e8ccb13afda0f2
Campbell Allen <campbell.allen@gmail.com>
2018-04-04 16:25:15 +0100

    remove the backfill operation outside a transaction

--- db/migrate/20180403150901_add_ux_testing_email_field_to_user.rb
+++ db/migrate/20180403150901_add_ux_testing_email_field_to_user.rb
@@ -1,8 +1,6 @@
 class AddUxTestingEmailFieldToUser < ActiveRecord::Migration
   def change
     add_column :users, :ux_testing_email_communication, :boolean
-    User.update_all(ux_testing_email_communication: false)
-
     reversible do |dir|
       dir.up do
         change_column_default(:users, :ux_testing_email_communication, false)
