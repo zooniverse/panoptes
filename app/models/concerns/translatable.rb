e87d175ee3094d8fe72185342272b08cc538bb28
Campbell Allen <campbell.allen@gmail.com>
2018-04-26 16:29:07 +0100

    add primary_content as an association on resources with content models
    this is a step towards only having 1 content model for any resource, i.e. the primary one. Once this is out then we can drop the the has_many content_association entirely. Unfortunately it’s not so simple as there are specs, etc that use build multiple language content models to be fixed.
    
    Also staging has some resources with translated content models that need to be cleaned up before orphaning them by dropping the has_many content_association here (production looks good).

--- app/models/concerns/translatable.rb
+++ app/models/concerns/translatable.rb
@@ -4,9 +4,18 @@ module Translatable
   included do
     validates :primary_language, format: {with: /\A[a-z]{2}(\z|-[A-z]{2})/}
     has_many content_association, autosave: true, inverse_of: name.downcase.to_sym, dependent: :destroy
+    has_one :primary_content, -> (translatable) { where(language: translatable.primary_language) }, class_name: content_model
     can_be_linked content_model.name.underscore.to_sym, :scope_for, :translate, :user
 
+    before_validation do |translatable|
+      primary_content_association = translatable.content_association.find do |content|
+        content.language == translatable.primary_language
+      end
+      translatable.primary_content = primary_content_association
+    end
+
     validates content_association, presence: true
+    validates :primary_content, presence: true
   end
 
   module ClassMethods
@@ -25,16 +34,5 @@ module Translatable
 
   def content_association
     @content_association ||= send(self.class.content_association)
-  end
-
-  # TODO: this should become the association instead of the has_many
-  def primary_content
-    @primary_content ||= if content_association.loaded?
-      content_association.to_a.find do |content|
-        content.language == primary_language
-      end
-    else
-      content_association.find_by(language: primary_language)
-    end
   end
 end
