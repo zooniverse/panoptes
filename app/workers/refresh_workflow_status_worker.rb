ada81ccce7cc100ed9fd2ce19d999543dc7bc579
Campbell Allen <campbell.allen@gmail.com>
2018-05-17 13:15:55 +0100

    unfinish then schedule a retired count worker
    use the unique & congestion sidekiq middleware to avoid these jobs running at the same time.

--- app/workers/refresh_workflow_status_worker.rb
+++ app/workers/refresh_workflow_status_worker.rb
@@ -15,11 +15,9 @@ class RefreshWorkflowStatusWorker
 
   def perform(workflow_id)
     workflow = Workflow.find(workflow_id)
-    # run these in band and in order to ensure
-    # we don't have state race conditions
-    # between workers and/or db transactions
+    # run the first worker manually to ensure we don't have state race
+    # conditions between workers and/or db transactions
     UnfinishWorkflowWorker.new.perform(workflow.id)
-    WorkflowRetiredCountWorker.new.perform(workflow.id)
-    CalculateProjectCompletenessWorker.new.perform(workflow.project_id)
+    WorkflowRetiredCountWorker.perform_async(workflow.id)
   end
 end
--- spec/workers/refresh_workflow_status_worker_spec.rb
+++ spec/workers/refresh_workflow_status_worker_spec.rb
@@ -3,24 +3,13 @@ require 'spec_helper'
 RSpec.describe RefreshWorkflowStatusWorker do
   let(:worker) { described_class.new }
   let(:workflow) { create(:workflow) }
-  let(:unfinish_worker_double) { double(:peform) }
-  let(:retired_count_worker_double) { double(:peform) }
-  let(:completeness_worker_double) { double(:peform) }
-  let(:klass_doubles) do
-    {
-      UnfinishWorkflowWorker => unfinish_worker_double,
-      WorkflowRetiredCountWorker => retired_count_worker_double,
-      CalculateProjectCompletenessWorker => completeness_worker_double
-    }
-  end
 
   describe "#perform" do
+    let(:unfinish_worker_double) { double(:peform) }
     before do
-      klass_doubles.each do |klass, double|
-        allow(klass)
-          .to receive(:new)
-          .and_return(double)
-      end
+      allow(UnfinishWorkflowWorker)
+      .to receive(:new)
+      .and_return(unfinish_worker_double)
     end
 
     it "should call a chain of ordered workers" do
@@ -28,13 +17,9 @@ RSpec.describe RefreshWorkflowStatusWorker do
         .to receive(:perform)
         .with(workflow.id)
         .ordered
-      expect(retired_count_worker_double)
-        .to receive(:perform)
+      expect(WorkflowRetiredCountWorker)
+        .to receive(:perform_async)
         .with(workflow.id)
-        .ordered
-      expect(completeness_worker_double)
-        .to receive(:perform)
-        .with(workflow.project_id)
         .ordered
       worker.perform(workflow.id)
     end
