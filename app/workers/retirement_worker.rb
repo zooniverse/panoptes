c9c1a9c75aec8fc05ff732385240ae90037b7e47
Campbell Allen <campbell.allen@gmail.com>
2018-05-18 16:25:29 +0100

    allow a reason to propogate for sws retirement
    use 1 worker to run the retirement event and handle the post event worker calls instead of two.

--- app/workers/retirement_worker.rb
+++ app/workers/retirement_worker.rb
@@ -3,10 +3,10 @@ class RetirementWorker
 
   sidekiq_options queue: :high
 
-  def perform(status_id)
-    status = load_subject_workflow_status(status_id)
+  def perform(status_id, reason="classification_count")
+    status = SubjectWorkflowStatus.where(id: status_id).first
     if status&.retire?
-      status.retire!("classification_count")
+      status.retire!(reason)
 
       workflow_id = status.workflow_id
       WorkflowRetiredCountWorker.perform_async(workflow_id)
@@ -14,11 +14,5 @@ class RetirementWorker
       NotifySubjectSelectorOfRetirementWorker
         .perform_async(status.subject_id, workflow_id)
     end
-  end
-
-  private
-
-  def load_subject_workflow_status(status_id)
-    SubjectWorkflowStatus.where(id: status_id).eager_load(:workflow).first
   end
 end
