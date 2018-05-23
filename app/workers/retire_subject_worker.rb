aeeb63acd42880e818a8ea562a949005ea987432
Campbell Allen <campbell.allen@gmail.com>
2018-05-23 14:28:19 +0100

    Consolidate retirement worker behaviour (#2773)
    * allow a reason to propogate for sws retirement
    
    use 1 worker to run the retirement event and handle the post event worker calls instead of two.
    
    * use the canonical retirement worker
    
    don't duplicate behaviour, allow the operation to use this worker via the API to specify the context for creating a SubjectWorkflowStatus resource and then request retirement on that resource via the retirement worker.

--- app/workers/retire_subject_worker.rb
+++ app/workers/retire_subject_worker.rb
@@ -1,30 +1,28 @@
 class RetireSubjectWorker
   include Sidekiq::Worker
+  attr_reader :workflow
 
   sidekiq_options queue: :high
 
   def perform(workflow_id, subject_ids, reason=nil)
-    workflow = Workflow.find(workflow_id)
-    retired_subjects = 0
-
-    Workflow.transaction do
-      subject_ids.each do |subject_id|
-        begin
-          if workflow.retire_subject(subject_id, reason)
-            retired_subjects += 1
-          end
-        rescue ActiveRecord::RecordInvalid
-        end
-      end
+    begin
+      @workflow = Workflow.find(workflow_id)
+    rescue ActiveRecord::RecordNotFound
+      return nil
     end
 
-    if retired_subjects > 0
-      WorkflowRetiredCountWorker.perform_async(workflow.id)
-
-      subject_ids.each do |subject_id|
-        NotifySubjectSelectorOfRetirementWorker.perform_async(subject_id, workflow.id)
-      end
+    Array.wrap(subject_ids).each do |subject_id|
+      count = subject_workflow_status(subject_id)
+      RetirementWorker.perform_async(count.id, reason)
     end
-  rescue ActiveRecord::RecordNotFound
+  end
+
+  private
+
+  def subject_workflow_status(subject_id)
+    workflow
+    .subject_workflow_statuses
+    .where(subject_id: subject_id)
+    .first_or_create!
   end
 end
--- spec/workers/retirement_worker_spec.rb
+++ spec/workers/retirement_worker_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe RetirementWorker do
   let(:workflow) { create :workflow, subject_sets: [sms.subject_set] }
   let(:count) { create(:subject_workflow_status, subject: sms.subject, workflow: workflow) }
 
-  describe "#perform" do
+  describe "#perform"  do
     context "sms is retireable" do
       before(:each) do
         allow_any_instance_of(SubjectWorkflowStatus).to receive(:retire?).and_return(true)
@@ -18,10 +18,23 @@ RSpec.describe RetirementWorker do
         expect(sms.retired_workflows).to include(workflow)
       end
 
-      it 'should record a reason for retirement' do
+      it 'should record a default reason for retirement' do
         expect { worker.perform(count) }.to change {
           count.reload.retirement_reason
         }.to("classification_count")
+      end
+
+      it 'should record the reason for retirement' do
+        reason = "nothing_here"
+        expect { worker.perform(count, reason) }.to change {
+          count.reload.retirement_reason
+        }.to(reason)
+      end
+
+      it 'should allow a nil reason for retirement' do
+        expect { worker.perform(count, nil) }.not_to change {
+          count.reload.retirement_reason
+        }
       end
 
       it 'should call the workflow retired counter worker' do
@@ -46,6 +59,23 @@ RSpec.describe RetirementWorker do
       end
     end
 
+    shared_examples "it does not run the post retirement workers" do
+      it 'should not queue a notify selector retirement' do
+        expect(NotifySubjectSelectorOfRetirementWorker).not_to receive(:perform_async)
+        worker.perform(count)
+      end
+
+      it 'should not queue a retired count worker' do
+        expect(WorkflowRetiredCountWorker).not_to receive(:perform_async)
+        worker.perform(count)
+      end
+
+      it "should not call the publish retire event worker" do
+        expect(PublishRetirementEventWorker).not_to receive(:perform_async)
+        worker.perform(count.id)
+      end
+    end
+
     context "sms is not retireable" do
       it 'should not retire subject for the workflow' do
         allow_any_instance_of(SubjectWorkflowStatus).to receive(:retire?).and_return(false)
@@ -53,6 +83,8 @@ RSpec.describe RetirementWorker do
         sms.reload
         expect(sms.retired_workflows).to_not include(workflow)
       end
+
+      it_behaves_like "it does not run the post retirement workers"
     end
 
     context 'when the sms is already retired' do
@@ -65,6 +97,8 @@ RSpec.describe RetirementWorker do
         expect(count).to_not receive :retire!
         worker.perform count.id
       end
+
+      it_behaves_like "it does not run the post retirement workers"
     end
   end
 end
