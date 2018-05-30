d5d1e1416ce74abf1c82234beea4716240e4b709
Campbell Allen <campbell.allen@gmail.com>
2018-05-30 17:37:40 +0100

    move operation to use the workflow_id
    avoid serialising the workflow resource from the db just to use the id that we have in params. The controlled_resources.exists? check still runs to ensure we are allowed to retire for this workflow, but that doesn’t instantiate any resources from the db so we can trust the authorization code and simply use the params in the operation.

--- spec/operations/workflows/retire_subjects_spec.rb
+++ spec/operations/workflows/retire_subjects_spec.rb
@@ -6,14 +6,20 @@ describe Workflows::RetireSubjects do
   let(:subject_set) { create(:subject_set, project: workflow.project, workflows: [workflow]) }
   let(:subject_set_id) { subject_set.id }
   let(:subject1) { create(:subject, subject_sets: [subject_set]) }
-
+  let(:params) do
+    {
+      workflow_id: workflow.id,
+      subject_id: subject1.id,
+      retirement_reason: "other"
+    }
+  end
   let(:operation) { described_class.with(api_user: api_user) }
 
   it "should call the retire subject worker with the subject_id" do
     expect(RetireSubjectWorker)
       .to receive(:perform_async)
       .with(workflow.id, [ subject1.id ], "other")
-    operation.run! workflow: workflow, subject_id: subject1.id, retirement_reason: "other"
+    operation.run!(params)
   end
 
   it "should call the retire subject worker with the subject_ids" do
@@ -22,23 +28,29 @@ describe Workflows::RetireSubjects do
     expect(RetireSubjectWorker)
       .to receive(:perform_async)
       .with(workflow.id, subject_ids, nil)
-    operation.run! workflow: workflow, subject_ids: subject_ids
+    run_params = params.except(:subject_id, :retirement_reason)
+    operation.run!(run_params.merge(subject_ids: subject_ids))
   end
 
   it 'rewrites the reason blank to nothing here' do
     expect(RetireSubjectWorker)
       .to receive(:perform_async)
       .with(workflow.id, [subject1.id], "nothing_here")
-    operation.run! workflow: workflow, subject_id: subject1.id, retirement_reason: "blank"
+    operation.run!(params.merge(retirement_reason: "blank"))
   end
 
-  it 'is invalid with an invalid retirement reason' do
-    result = operation.run workflow: workflow, subject_id: subject1.id, retirement_reason: "nope"
+  it 'is invalid with a missing workflow_id param' do
+    result = operation.run(params.except(:workflow_id))
     expect(result).not_to be_valid
   end
 
-  it 'is valid with a missing parameter' do
-    result = operation.run workflow: workflow, subject_id: subject1.id
+  it 'is invalid with an invalid retirement reason' do
+    result = operation.run(params.merge(retirement_reason: "nope"))
+    expect(result).not_to be_valid
+  end
+
+  it 'is valid with an allowed missing parameter' do
+    result = operation.run(params.except(:retirement_reason))
     expect(result).to be_valid
   end
 end
