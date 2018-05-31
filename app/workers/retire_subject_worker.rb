ece3173ec988cbced9e8cc23dd6ebaa8c63cd53d
Campbell Allen <campbell.allen@gmail.com>
2018-05-31 15:39:54 +0100

    avoid repeat loading of the workflow (#2786)
    we really only want to know if the workflow exists we don’t need the whole resource to do this

--- app/workers/retire_subject_worker.rb
+++ app/workers/retire_subject_worker.rb
@@ -1,28 +1,28 @@
 class RetireSubjectWorker
   include Sidekiq::Worker
-  attr_reader :workflow
+  attr_reader :workflow_id
 
   sidekiq_options queue: :high
 
   def perform(workflow_id, subject_ids, reason=nil)
-    begin
-      @workflow = Workflow.find(workflow_id)
-    rescue ActiveRecord::RecordNotFound
-      return nil
-    end
-
-    Array.wrap(subject_ids).each do |subject_id|
-      count = subject_workflow_status(subject_id)
-      RetirementWorker.perform_async(count.id, reason)
+    @workflow_id = workflow_id
+    if workflow_exists?
+      Array.wrap(subject_ids).each do |subject_id|
+        count = subject_workflow_status(subject_id)
+        RetirementWorker.perform_async(count.id, reason)
+      end
     end
   end
 
   private
 
+  def workflow_exists?
+    Workflow.where(id: workflow_id).exists?
+  end
+
   def subject_workflow_status(subject_id)
-    workflow
-    .subject_workflow_statuses
-    .where(subject_id: subject_id)
+    SubjectWorkflowStatus
+    .where(workflow_id: workflow_id, subject_id: subject_id)
     .first_or_create!
   end
 end
