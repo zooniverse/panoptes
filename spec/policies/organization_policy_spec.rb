19a4e750bcf92f6e189fffebabecabfa59abd860
Marten Veldthuis <marten@veldthuis.com>
2018-06-14 16:17:59 +0100

    Add tests for the other scopes in OrganizationPolicy

--- spec/policies/organization_policy_spec.rb
+++ spec/policies/organization_policy_spec.rb
@@ -70,5 +70,114 @@ describe OrganizationPolicy do
         end
       end
     end
+
+    describe 'update, destroy, update_links, destroy_links' do
+      let(:resolved_scope) do
+        Pundit.policy!(api_user, Organization).scope_for(:update)
+      end
+
+      context 'for an anonymous user' do
+        let(:api_user) { ApiUser.new(anonymous_user) }
+
+        it "includes nothing" do
+          listed_organization.save!
+          unlisted_organization.save!
+          expect(resolved_scope).to be_empty
+        end
+      end
+
+      context 'for a normal user' do
+        let(:api_user) { ApiUser.new(logged_in_user) }
+
+        it "includes nothing" do
+          listed_organization.save!
+          unlisted_organization.save!
+          expect(resolved_scope).to be_empty
+        end
+      end
+
+      context 'for the resource owner' do
+        let(:api_user) { ApiUser.new(resource_owner) }
+
+        it "includes owned organizations" do
+          listed_organization.save!
+          unlisted_organization.save!
+          expect(resolved_scope).to include(listed_organization, unlisted_organization)
+        end
+      end
+
+      context 'for an admin' do
+        let(:admin_user) { create :user, admin: true }
+        let(:api_user) { ApiUser.new(admin_user, admin: true) }
+
+        it 'includes all organizations' do
+          listed_organization.save!
+          unlisted_organization.save!
+          expect(resolved_scope).to include(listed_organization, unlisted_organization)
+        end
+      end
+    end
+
+    describe 'translate' do
+      let(:resolved_scope) do
+        Pundit.policy!(api_user, Organization).scope_for(:translate)
+      end
+
+      context 'for an anonymous user' do
+        let(:api_user) { ApiUser.new(anonymous_user) }
+
+        it "includes nothing" do
+          listed_organization.save!
+          unlisted_organization.save!
+          expect(resolved_scope).to be_empty
+        end
+      end
+
+      context 'for a normal user' do
+        let(:api_user) { ApiUser.new(logged_in_user) }
+
+        it "includes nothing" do
+          listed_organization.save!
+          unlisted_organization.save!
+          expect(resolved_scope).to be_empty
+        end
+      end
+
+      context 'for the resource owner' do
+        let(:api_user) { ApiUser.new(resource_owner) }
+
+        it "includes owned organizations" do
+          listed_organization.save!
+          unlisted_organization.save!
+          expect(resolved_scope).to include(listed_organization, unlisted_organization)
+        end
+      end
+
+      context 'for a translator' do
+        let(:translator) { create(:user) }
+        let(:api_user) { ApiUser.new(translator) }
+
+        it "includes owned organizations" do
+          listed_organization.save!
+          unlisted_organization.save!
+
+          create :access_control_list, user_group: translator.identity_group, resource: listed_organization, roles: ['translator']
+          create :access_control_list, user_group: translator.identity_group, resource: unlisted_organization, roles: ['translator']
+
+          expect(resolved_scope).to include(listed_organization, unlisted_organization)
+        end
+      end
+
+      context 'for an admin' do
+        let(:admin_user) { create :user, admin: true }
+        let(:api_user) { ApiUser.new(admin_user, admin: true) }
+
+        it 'includes all organizations' do
+          listed_organization.save!
+          unlisted_organization.save!
+          expect(resolved_scope).to include(listed_organization, unlisted_organization)
+        end
+      end
+    end
   end
 end
