b8b8152933a9f05ca906b3623b8b381e6d5e2aef
Campbell Allen <campbell.allen@gmail.com>
2018-06-14 17:45:59 +0100

    convert favourites finder to a class method

--- lib/favorites_finder.rb
+++ lib/favorites_finder.rb
@@ -1,13 +1,11 @@
 class FavoritesFinder
-  def initialize(user, project, subject_ids)
-    @user = user
-    @project = project
-    @subject_ids = subject_ids
-  end
 
-  def find_favorites
-    return [] if @user.nil? || Panoptes.flipper[:skip_favorites_finder].enabled?
-    fav_collections = @user.favorite_collections_for_project(@project)
+  def self.find_favorites(user, project_id, subject_ids)
+    if user.nil? || Panoptes.flipper[:skip_favorites_finder].enabled?
+      return []
+    end
+
+    fav_collections = user.favorite_collections_for_project(project_id)
     fav_collections.joins(:subjects).where(subjects: {id: @subject_ids}).pluck("subjects.id")
   end
 end
