9528f836762a90e9dbed27c5f72f87e084f7cbca
Marten Veldthuis <marten@veldthuis.com>
2018-06-19 15:03:45 +0100

    Convert subjects to pundit

---
+++ app/policies/subject_policy.rb
@@ -0,0 +1,10 @@
+class SubjectPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve(action)
+      parent_scope = Project.scope_for(action, user)
+      scope.where(project_id: parent_scope.select(:id))
+    end
+  end
+
+  scope :index, :show, :update, :destroy, :update_links, :destroy_links, :versions, :version, with: Scope
+end
