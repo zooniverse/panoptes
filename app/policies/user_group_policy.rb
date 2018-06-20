29611977891da54552763ab40b41cf006629966f
Marten Veldthuis <marten@veldthuis.com>
2018-06-20 14:20:36 +0100

    Convert UserGroup to Pundit

---
+++ app/policies/user_group_policy.rb
@@ -0,0 +1,33 @@
+class UserGroupPolicy < ApplicationPolicy
+  # The old can_by_role scopes used to take into accuont a `project_editor` and `collection_editor`.
+  # Neither of these exist in the database, and therefore these are no longer included now.
+
+  class Scope < Scope
+    def private_query(action, roles)
+      scope.joins(:memberships).merge(user.memberships_for(action, model))
+        .where(memberships: { identity: false })
+    end
+
+    def user_can_access_scope(private_query)
+      accessible = scope
+      accessible = accessible.where(id: private_query.select(:id))
+      accessible = accessible.or(public_scope) if public_flag
+      accessible
+    end
+  end
+
+  class ReadScope < Scope
+    roles_for_private_scope %i(group_admin group_member)
+
+    def public_scope
+      scope.where(private: false)
+    end
+  end
+
+  class WriteScope < Scope
+    roles_for_private_scope %i(group_admin)
+  end
+
+  scope :index, :show, with: ReadScope
+  scope :update, :destroy, :update_links, :destroy_links, with: WriteScope
+end
---
+++ spec/policies/user_group_policy_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe UserGroupPolicy do
+  describe 'scopes' do
+    let(:anonymous_user) { nil }
+    let(:logged_in_user) { create(:user) }
+    let(:group_admin) { create(:user) }
+
+    let(:public_user_group) { create(:user_group, admin: group_admin, private: false) }
+    let(:private_user_group) { create(:user_group, admin: group_admin, private: true) }
+
+    before do
+      public_user_group.save!
+      private_user_group.save!
+    end
+
+    subject do
+      PunditScopeTester.new(UserGroup, api_user)
+    end
+
+    context 'as an anonymous user' do
+      let(:api_user) { ApiUser.new(anonymous_user) }
+
+      its(:index) { is_expected.to match_array(public_user_group) }
+      its(:show) { is_expected.to match_array(public_user_group) }
+      its(:update) { is_expected.to be_empty }
+      its(:destroy) { is_expected.to be_empty }
+      its(:update_links) { is_expected.to be_empty }
+      its(:destroy_links) { is_expected.to be_empty }
+    end
+
+    context 'as a normal user' do
+      let(:api_user) { ApiUser.new(logged_in_user) }
+
+      its(:index) { is_expected.to match_array(public_user_group) }
+      its(:show) { is_expected.to match_array(public_user_group) }
+      its(:update) { is_expected.to be_empty }
+      its(:destroy) { is_expected.to be_empty }
+      its(:update_links) { is_expected.to be_empty }
+      its(:destroy_links) { is_expected.to be_empty }
+    end
+
+    context 'as a group member' do
+      let(:api_user) { ApiUser.new(logged_in_user) }
+
+      before do
+        create :membership, user: logged_in_user, user_group: private_user_group, roles: ['group_member']
+      end
+
+      its(:index) { is_expected.to match_array([public_user_group, private_user_group]) }
+      its(:show) { is_expected.to match_array([public_user_group, private_user_group]) }
+      its(:update) { is_expected.to be_empty }
+      its(:destroy) { is_expected.to be_empty }
+      its(:update_links) { is_expected.to be_empty }
+      its(:destroy_links) { is_expected.to be_empty }
+    end
+
+    context 'as the group admin' do
+      let(:api_user) { ApiUser.new(group_admin) }
+
+      its(:index) { is_expected.to match_array([public_user_group, private_user_group]) }
+      its(:show) { is_expected.to match_array([public_user_group, private_user_group]) }
+      its(:update) { is_expected.to match_array([public_user_group, private_user_group]) }
+      its(:destroy) { is_expected.to match_array([public_user_group, private_user_group]) }
+      its(:update_links) { is_expected.to match_array([public_user_group, private_user_group]) }
+      its(:destroy_links) { is_expected.to match_array([public_user_group, private_user_group]) }
+    end
+  end
+end
