ff54b09001f562cd0eeed3b6fe56619677c1f681
Campbell Allen <campbell.allen@gmail.com>
2018-06-26 10:16:19 +0100

    add worker to create linked status records for a subject set

---
+++ app/workers/subject_set_statuses_create_worker.rb
@@ -0,0 +1,24 @@
+class SubjectSetStatusesCreateWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :data_high, unique: :until_executed
+
+  def perform(subject_set_id, workflow_id)
+    set_is_linked_to_workflow = SubjectSetsWorkflow.where(
+      subject_set_id: subject_set_id,
+      workflow_id: workflow_id
+    ).exists?
+    return unless set_is_linked_to_workflow
+
+    linked_subject_select_scope = SetMemberSubject
+      .where(subject_set_id: subject_set_id)
+      .select(:id,:subject_id)
+
+    linked_subject_select_scope.find_each do |sms|
+      SubjectWorkflowStatusCreateWorker.perform_async(
+        sms.subject_id,
+        workflow_id
+      )
+    end
+  end
+end
