457a1fd261c7687c2f866f6554895fca0662e7b1
Marten Veldthuis <marten@veldthuis.com>
2018-07-04 12:16:18 +0100

    Convert ACL over to Pundit

--- app/controllers/api/v1/organization_roles_controller.rb
+++ app/controllers/api/v1/organization_roles_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::OrganizationRolesController < Api::ApiController
-  include JsonApiController::LegacyPolicy
+  include JsonApiController::PunditPolicy
   include RolesController
 
   require_authentication :create, :update, :destroy, scopes: [:organization]
