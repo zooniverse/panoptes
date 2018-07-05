e526a2f630b78ac74ab6cded3ba94bc4407479de
Marten Veldthuis <marten@veldthuis.com>
2018-07-05 15:53:01 +0100

    Fix scope tester

--- spec/support/pundit_scope_tester.rb
+++ spec/support/pundit_scope_tester.rb
@@ -44,11 +44,38 @@ class PunditScopeTester
     Pundit.policy!(@user, @klass).scope_for(:deactivate)
   end
 
+  # Used by classifications controller:
   def project
     Pundit.policy!(@user, @klass).scope_for(:project)
   end
 
+  # Used by classifications controller:
   def incomplete
     Pundit.policy!(@user, @klass).scope_for(:incomplete)
+  end
+
+  # Used by projects controller:
+  def create_classifications_export
+    Pundit.policy!(@user, @klass).scope_for(:create_classifications_export)
+  end
+
+  # Used by projects controller:
+  def create_subjects_export
+    Pundit.policy!(@user, @klass).scope_for(:create_subjects_export)
+  end
+
+  # Used by projects controller:
+  def create_workflows_export
+    Pundit.policy!(@user, @klass).scope_for(:create_workflows_export)
+  end
+
+  # Used by projects controller:
+  def create_workflow_contents_export
+    Pundit.policy!(@user, @klass).scope_for(:create_workflow_contents_export)
+  end
+
+  # Used by projects controller:
+  def retire_subjects
+    Pundit.policy!(@user, @klass).scope_for(:retire_subjects)
   end
 end
