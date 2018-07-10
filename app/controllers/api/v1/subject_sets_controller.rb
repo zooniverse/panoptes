1233ca1d00969760e4fb6a1765c9c6d51645bc86
Marten Veldthuis <marten@veldthuis.com>
2018-07-10 10:53:13 +0100

    Use Pundit to find collections

--- app/controllers/api/v1/subject_sets_controller.rb
+++ app/controllers/api/v1/subject_sets_controller.rb
@@ -74,7 +74,7 @@ class Api::V1::SubjectSetsController < Api::ApiController
   def build_resource_for_create(create_params)
     super do |_, link_params|
       if collection_id = link_params.delete("collection")
-        if collection = Collection.scope_for(:show, api_user).where(id: collection_id).first
+        if collection = Pundit.policy!(api_user, Collection).scope_for(:show).where(id: collection_id).first
           link_params["subjects"] = collection.subjects
         else
           raise ActiveRecord::RecordNotFound, "No Record Found for Collection with id: #{collection_id}"
