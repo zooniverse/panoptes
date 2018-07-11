c4700c5e3b2ce6acc0eadae7bc57676ab266ae1b
Marten Veldthuis <marten@veldthuis.com>
2018-07-11 14:38:48 +0100

    Add index concurrently

--- db/migrate/20180710151618_make_uss_index_unique.rb
+++ db/migrate/20180710151618_make_uss_index_unique.rb
@@ -3,6 +3,6 @@ class MakeUssIndexUnique < ActiveRecord::Migration
 
   def change
     remove_index :user_seen_subjects, column: [:user_id, :workflow_id]
-    add_index :user_seen_subjects, [:user_id, :workflow_id], unique: true
+    add_index :user_seen_subjects, [:user_id, :workflow_id], unique: true, algorithm: :concurrently
   end
 end
