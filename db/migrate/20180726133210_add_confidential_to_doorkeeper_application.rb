1b3b668f1dd1ffa588f4d454f4e78239252ee151
Campbell Allen <campbell.allen@gmail.com>
2018-07-26 16:47:42 +0100

    add note on what this migration default value means
    some of our apps (first_party password / session grants and implicit) will require the non-default value of confidential = false

--- db/migrate/20180726133210_add_confidential_to_doorkeeper_application.rb
+++ db/migrate/20180726133210_add_confidential_to_doorkeeper_application.rb
@@ -6,6 +6,11 @@ class AddConfidentialToDoorkeeperApplication < ActiveRecord::Migration
       :boolean,
       null: false,
       default: true # maintaining backwards compatibility: require secrets
+      # setting all the existing apps to confidential will break login for grants (password)
+      # that do not supply a client_secret, e.g. our main zooniverse.org UI and api clients
+      # these apps will require a confidential = false setting
+      # all implicit apps will require confidential = false as well
+      # apps that can keep secrets and use them to authenticate will require the default value
     )
   end
 end
