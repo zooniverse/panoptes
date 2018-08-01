967acecabe07df0f32e74b5ac82284bcf9f2d3c3
Campbell Allen <campbell.allen@gmail.com>
2018-08-01 17:42:46 +0100

    explicitly test organization_page policy scopes

--- app/policies/organization_page_policy.rb
+++ app/policies/organization_page_policy.rb
@@ -6,15 +6,8 @@ class OrganizationPagePolicy < ApplicationPolicy
     end
   end
 
-  class TranslateScope < Scope
-    def resolve(_action)
-      parent_scope = policy_for(Organization).scope_for(:translate)
-      scope.where(organization_id: parent_scope.select(:id))
-    end
-  end
-
-  scope :index, :show, with: Scope
-  scope :update, :destroy, :update_links, :destroy_links, :versions, :version, with: TranslateScope
+  scope :index, :show, :update, :destroy, :translate, :update_links,
+        :destroy_links, :versions, :version, with: Scope
 
   def linkable_organizations
     policy_for(Organization).scope_for(:update)
