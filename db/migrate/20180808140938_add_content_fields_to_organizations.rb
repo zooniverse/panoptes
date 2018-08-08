3d6e6b005928649a0ef53c805da41e36faa66056
Marten Veldthuis <marten@veldthuis.com>
2018-08-08 16:28:22 +0100

    Add content columns to parent tables

---
+++ db/migrate/20180808140938_add_content_fields_to_organizations.rb
@@ -0,0 +1,9 @@
+class AddContentFieldsToOrganizations < ActiveRecord::Migration
+  def change
+    add_column :organizations, :title, :string
+    add_column :organizations, :description, :string
+    add_column :organizations, :introduction, :text
+    add_column :organizations, :url_labels, :jsonb
+    add_column :organizations, :announcement, :string
+  end
+end
