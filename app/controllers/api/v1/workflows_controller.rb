dbaa6c9c187abcca284ee3206715ce3c5a0b97c2
Campbell Allen <campbell.allen@gmail.com>
2018-08-14 12:46:16 +0100

    use the safe operator
    the linter prefers this over the explicit comparison, which seems strange but i'll keep it happy.

--- app/controllers/api/v1/workflows_controller.rb
+++ app/controllers/api/v1/workflows_controller.rb
@@ -99,7 +99,7 @@ class Api::V1::WorkflowsController < Api::ApiController
       RefreshWorkflowStatusWorker.perform_async(workflow.id)
     end
 
-    if relation_index == 0
+    if relation_index&.zero?
       NotifySubjectSelectorOfChangeWorker.perform_async(workflow.id)
     end
   end
