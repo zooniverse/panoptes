1b0383dca5ae30a1e5db964b6c257d82b83c3aa9
Campbell Allen <campbell.allen@gmail.com>
2018-08-15 19:03:52 +0100

    clean up empty linked subject sws records
    sws records are created for each subject to optimize the selection paths 
    for internal selector https://github.com/zooniverse/Panoptes/pull/2822
    
    We should only remove subjects that have no classification counts or 
    retirement status from external systems.

--- lib/subjects/remover.rb
+++ lib/subjects/remover.rb
@@ -8,11 +8,13 @@ module Subjects
 
     def cleanup
       if can_be_removed?
-        locations = orphan.locations
-        set_member_subjects = orphan.set_member_subjects
-        workflow_ids = orphan.workflows.pluck(:id)
+        locations = orphan_subject.locations
+        set_member_subjects = orphan_subject.set_member_subjects
+        workflow_ids = orphan_subject.workflows.pluck(:id)
         ActiveRecord::Base.transaction do
-          orphan.delete
+          # clean up the linked sws records, https://github.com/zooniverse/Panoptes/pull/2822
+          orphan_subject_sws_scope.delete_all
+          orphan_subject.delete
           locations.map(&:destroy)
           set_member_subjects.map(&:destroy)
         end
@@ -26,17 +28,31 @@ module Subjects
     private
 
     def can_be_removed?
-      !!orphan && no_talk_discussions?
+      # subject has been collected or classified
+      return false unless orphan_subject_scope.exists?
+
+      # subject has been talked about
+      return false unless no_talk_discussions?
+
+      # subject has been counted or retired via a SubjectWorkflowStatus record
+      if has_not_been_counted_or_retired?
+        true
+      else
+        false
+      end
     end
 
-    def orphan
-      @orphan ||= Subject
-       .where(id: subject_id)
-       .joins("LEFT OUTER JOIN classification_subjects ON classification_subjects.subject_id = subjects.id")
-       .where("classification_subjects.subject_id IS NULL")
-       .joins("LEFT OUTER JOIN collections_subjects ON collections_subjects.subject_id = subjects.id")
-       .where("collections_subjects.subject_id IS NULL")
-       .first
+    def orphan_subject_scope
+      Subject
+      .where(id: subject_id)
+      .joins("LEFT OUTER JOIN classification_subjects ON classification_subjects.subject_id = subjects.id")
+      .where("classification_subjects.subject_id IS NULL")
+      .joins("LEFT OUTER JOIN collections_subjects ON collections_subjects.subject_id = subjects.id")
+      .where("collections_subjects.subject_id IS NULL")
+    end
+
+    def orphan_subject
+      @orphan_subject ||= orphan_subject_scope.first
     end
 
     def no_talk_discussions?
@@ -49,8 +65,20 @@ module Subjects
 
     def notify_subject_selector(workflow_ids)
       workflow_ids.each do |workflow_id|
-        NotifySubjectSelectorOfRetirementWorker.perform_async(orphan.id, workflow_id)
+        NotifySubjectSelectorOfRetirementWorker.perform_async(orphan_subject.id, workflow_id)
       end
+    end
+
+    def orphan_subject_sws_scope
+      SubjectWorkflowStatus.where(subject_id: subject_id)
+    end
+
+    def orphan_subject_sws_counted_or_retired_scope
+      orphan_subject_sws_scope.where("classifications_count > 0 OR retired_at IS NOT NULL")
+    end
+
+    def has_not_been_counted_or_retired?
+      !orphan_subject_sws_counted_or_retired_scope.exists?
     end
   end
 end
