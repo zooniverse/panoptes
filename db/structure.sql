94cf726bbafba7a063b6e9ebea9711879608501c
Marten Veldthuis <marten@veldthuis.com>
2018-08-21 14:05:03 +0100

    Add columns to keep content in Project

--- db/structure.sql
+++ db/structure.sql
@@ -913,7 +913,13 @@ CREATE TABLE public.projects (
     state integer,
     organization_id integer,
     mobile_friendly boolean DEFAULT false NOT NULL,
-    featured boolean DEFAULT false NOT NULL
+    featured boolean DEFAULT false NOT NULL,
+    title character varying,
+    description text,
+    introduction text,
+    url_labels jsonb,
+    workflow_description text,
+    researcher_quote text
 );
 
 
@@ -4165,6 +4171,8 @@ INSERT INTO schema_migrations (version) VALUES ('20180724112620');
 INSERT INTO schema_migrations (version) VALUES ('20180726133210');
 
 INSERT INTO schema_migrations (version) VALUES ('20180808140938');
+
+INSERT INTO schema_migrations (version) VALUES ('20180821125430');
 
 INSERT INTO schema_migrations (version) VALUES ('20180821151555');
 
