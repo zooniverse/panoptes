113b48da5c2f6879b37204ab7132a8cdffcda23c
Campbell Allen <campbell.allen@gmail.com>
2018-08-21 16:19:05 +0100

    Cleanup subject placeholder SubjectWorkflowStatus records when removing subjects (#2899)
    * spec out linked sws record removal
    
    * default sws record should be a 0 count one
    
    * clean up empty linked subject sws records
    
    sws records are created for each subject to optimize the selection paths 
    for internal selector https://github.com/zooniverse/Panoptes/pull/2822
    
    We should only remove subjects that have no classification counts or 
    retirement status from external systems.
    
    * fix spec when creating linked sws records
    
    * fix spec wording to reflect the context
    
    * modify how panoptes client is setup
    
    allow easy test double setup via dep injection
    
    * simplify the logic around removal testing
    
    make the method names readable without negating the logic when testing 
    subject removal candidates
    
    * split out orphan subject scope method
    
    allow this scope to be used directly when debugging
    
    * update spec to generic selector naming
    
    * remove comments after clarifying method names

--- lib/subjects/remover.rb
+++ lib/subjects/remover.rb
@@ -1,18 +1,21 @@
 module Subjects
   class Remover
-    attr_reader :subject_id
+    attr_reader :subject_id, :panoptes_client
 
-    def initialize(subject_id)
+    def initialize(subject_id, client=nil)
       @subject_id = subject_id
+      @panoptes_client = client || Panoptes::Client.new(env: Rails.env)
     end
 
     def cleanup
       if can_be_removed?
-        locations = orphan.locations
-        set_member_subjects = orphan.set_member_subjects
-        workflow_ids = orphan.workflows.pluck(:id)
+        locations = orphan_subject.locations
+        set_member_subjects = orphan_subject.set_member_subjects
+        workflow_ids = orphan_subject.workflows.pluck(:id)
         ActiveRecord::Base.transaction do
-          orphan.delete
+          # clean up the linked sws records, https://github.com/zooniverse/Panoptes/pull/2822
+          orphan_subject_sws_scope.delete_all
+          orphan_subject.delete
           locations.map(&:destroy)
           set_member_subjects.map(&:destroy)
         end
@@ -26,31 +29,54 @@ module Subjects
     private
 
     def can_be_removed?
-      !!orphan && no_talk_discussions?
+      return false if has_been_collected_or_classified?
+
+      return false if has_been_talked_about?
+
+      return false if has_been_counted_or_retired?
+
+      # subject has no record of use in zooniverse
+      true
     end
 
-    def orphan
-      @orphan ||= Subject
-       .where(id: subject_id)
-       .joins("LEFT OUTER JOIN classification_subjects ON classification_subjects.subject_id = subjects.id")
-       .where("classification_subjects.subject_id IS NULL")
-       .joins("LEFT OUTER JOIN collections_subjects ON collections_subjects.subject_id = subjects.id")
-       .where("collections_subjects.subject_id IS NULL")
-       .first
+    def orphan_subject_scope
+      Subject
+      .where(id: subject_id)
+      .joins("LEFT OUTER JOIN classification_subjects ON classification_subjects.subject_id = subjects.id")
+      .where("classification_subjects.subject_id IS NULL")
+      .joins("LEFT OUTER JOIN collections_subjects ON collections_subjects.subject_id = subjects.id")
+      .where("collections_subjects.subject_id IS NULL")
     end
 
-    def no_talk_discussions?
-      panoptes_client.discussions(focus_id: subject_id, focus_type: 'Subject').empty?
+    def orphan_subject
+      @orphan_subject ||= orphan_subject_scope.first
     end
 
-    def panoptes_client
-      @client ||= Panoptes::Client.new(env: Rails.env)
+    def has_been_collected_or_classified?
+      !orphan_subject
+    end
+
+    def has_been_talked_about?
+      panoptes_client.discussions(
+        focus_id: subject_id,
+        focus_type: 'Subject'
+      ).any?
     end
 
     def notify_subject_selector(workflow_ids)
       workflow_ids.each do |workflow_id|
-        NotifySubjectSelectorOfRetirementWorker.perform_async(orphan.id, workflow_id)
+        NotifySubjectSelectorOfRetirementWorker.perform_async(orphan_subject.id, workflow_id)
       end
+    end
+
+    def orphan_subject_sws_scope
+      SubjectWorkflowStatus.where(subject_id: subject_id)
+    end
+
+    def has_been_counted_or_retired?
+      orphan_subject_sws_scope
+      .where("classifications_count > 0 OR retired_at IS NOT NULL")
+      .exists?
     end
   end
 end
--- spec/lib/subjects/remover_spec.rb
+++ spec/lib/subjects/remover_spec.rb
@@ -7,34 +7,39 @@ RSpec.describe Subjects::Remover do
   end
   let(:subjects) { subject_set.subjects }
   let(:subject) { subjects.sample }
-  let(:remover) { Subjects::Remover.new(subject.id) }
+  let!(:linked_sws) do
+    create(
+      :subject_workflow_status,
+      workflow: workflow,
+      subject: subject,
+      classifications_count: 0
+    )
+  end
+  let(:panoptes_client) { instance_double(Panoptes::Client) }
+  let(:remover) { Subjects::Remover.new(subject.id, panoptes_client) }
 
   describe "#cleanup" do
-
     describe "testing the client configuration" do
-
       it "should setup the panoptes client with the correct env" do
         expect(Panoptes::Client)
           .to receive(:new)
           .with(env: Rails.env)
-          .and_call_original
-        remover.cleanup
+        Subjects::Remover.new(subject.id)
       end
     end
 
     context "with a client test double testing the client configuration" do
-      let(:panoptes_client) { instance_double(Panoptes::Client) }
       let(:discussions) { [] }
 
       before do
         allow(panoptes_client)
-          .to receive(:discussions)
-          .with(focus_id: subject.id, focus_type: "Subject")
-          .and_return(discussions)
-        allow(remover).to receive(:panoptes_client).and_return(panoptes_client)
+        .to receive(:discussions)
+        .with(focus_id: subject.id, focus_type: "Subject")
+        .and_return(discussions)
       end
 
       context "without a real subject" do
+        let(:linked_sws) { nil }
         let(:subject) { double(id: 100) }
 
         it "should ignore non existant subject ids" do
@@ -65,6 +70,35 @@ RSpec.describe Subjects::Remover do
         expect { Subject.find(subject.id) }.to raise_error(ActiveRecord::RecordNotFound)
       end
 
+      context "with a non-zero count sws record" do
+        let(:linked_sws) do
+          create(
+            :subject_workflow_status,
+            workflow: workflow,
+            subject: subject,
+            classifications_count: 10
+          )
+        end
+        it "should not remove a subject that has a non-zero count a sws record" do
+          expect(remover.cleanup).to be_falsey
+        end
+      end
+
+      context "with a retired count sws record" do
+        let(:linked_sws) do
+          create(
+            :subject_workflow_status,
+            workflow: workflow,
+            subject: subject,
+            retired_at: Time.now,
+            retirement_reason: :flagged
+          )
+        end
+        it "should not remove a subject that has a retired sws record" do
+          expect(remover.cleanup).to be_falsey
+        end
+      end
+
       it "should remove the associated set_member_subjects" do
         sms_ids = subject.set_member_subjects.map(&:id)
         remover.cleanup
@@ -78,7 +112,7 @@ RSpec.describe Subjects::Remover do
         expect { Medium.find(media_ids) }.to raise_error(ActiveRecord::RecordNotFound)
       end
 
-      it "notify cellect about the subject removal" do
+      it "notify selector service about the subject removal" do
         expect(NotifySubjectSelectorOfRetirementWorker)
           .to receive(:perform_async)
           .with(subject.id, workflow.id)
