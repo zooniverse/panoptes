802738663c5706aeb7b639902d6106a11cd4daab
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-08-24 11:57:46 +0100

    Add mailer preview paths to development config file
    By default rails looks for mailer preview files in
    test/mailers/previews/. However we have our previews in
    spec/mailers/previews so I have updated the config file for the
    development environment to point to the location of the previews in
    spec.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
 end
