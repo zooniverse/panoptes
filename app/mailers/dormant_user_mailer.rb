1685b6cfb802ecbe9d47cac95d145c91c57e5797
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-08-30 11:42:56 +0100

    Alter text in dormant mailer if upp is true
    If a user has classified before, then we want the dormant mailer to have
    the option of linking them to their last project.
    
    - If the user does have a previous classification then user project
    preference (upp) will the true. If so then the id of the last project
    classified is passed to @last_project in dormant user mailer.
    
    - In the mailer text, if @last_project holds a project id then the
    display name for that project and some surrounding text is given.
    Otherwise it is left out and only a link to the main project page is
    given.
    
    - I will also add the project slug for the @last_project but not in this
    commit.

--- app/mailers/dormant_user_mailer.rb
+++ app/mailers/dormant_user_mailer.rb
@@ -1,13 +1,22 @@
 class DormantUserMailer < ApplicationMailer
-  layout false
 
   DEFAULT_SUBJECT = "Come back to the Zooniverse".freeze
 
   def email_dormant_user(user)
     @user = user
     @email_to = user.email
-    @last_project= UserProjectPreference.where(user_id: user.id).first
+    @last_project = last_classified_project(user.id)
     mail(to: @email_to, subject: DEFAULT_SUBJECT)
   end
 
+   # this is a target to move into the user model? or maybe the UPP model?
+   #  instead of re-writing this here and in user.rb
+  def last_classified_project(user_id)
+    upp = UserProjectPreference
+      .where(user_id: user_id)
+      .where.not(email_communication: nil)
+      .order(updated_at: :desc)
+      .first
+    upp ? upp.project : nil
+  end
 end
