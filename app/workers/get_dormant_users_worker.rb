409643a28e16d7cd525c06a968a67264cb4e246d
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-09-05 15:46:13 +0100

    Add worker to that gets all dormant users
    This worker get all dormant users within a subset and passes each one to
    the dormant user mailer worker

---
+++ app/workers/get_dormant_users_worker.rb
@@ -0,0 +1,11 @@
+class GetDormantUsersWorker
+  include Sidekiq::Worker
+
+  def perform(num_days_since_activity, ending_in)
+    selected_users = User.subset_selection(ending_in)
+    User.dormant(num_days_since_activity, selected_users) do |dormant_user|
+      DormantUserMailerWorker.perform_async(dormant_user.id)
+    end
+
+  end
+end
