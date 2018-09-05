de9f87d8fba6521b48026f8637323e51c929ab52
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-09-05 16:59:00 +0100

    Enforce normal case for variable numbers

--- spec/workers/get_dormant_users_worker_spec.rb
+++ spec/workers/get_dormant_users_worker_spec.rb
@@ -4,8 +4,8 @@ describe GetDormantUsersWorker do
   let(:worker) { described_class.new }
 
   it "should email dormant users in subselection" do
-    dormant_user_1 = create(:user, id: 45, current_sign_in_at: 5.days.ago)
-    dormant_user_2 = create(:user, id: 5, current_sign_in_at: 5.days.ago)
+    dormant_user1 = create(:user, id: 45, current_sign_in_at: 5.days.ago)
+    dormant_user2 = create(:user, id: 5, current_sign_in_at: 5.days.ago)
 
     expect(DormantUserMailerWorker).to receive(:perform_async).with(dormant_user_1.id)
     expect(DormantUserMailerWorker).to receive(:perform_async).with(dormant_user_2.id)
