ae311495474c27091f41a0828234affa478655fe
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-09-05 16:55:16 +0100

    Remove blank line

--- app/workers/get_dormant_users_worker.rb
+++ app/workers/get_dormant_users_worker.rb
@@ -6,6 +6,5 @@ class GetDormantUsersWorker
     User.dormant(num_days_since_activity, selected_users) do |dormant_user|
       DormantUserMailerWorker.perform_async(dormant_user.id)
     end
-
   end
 end
