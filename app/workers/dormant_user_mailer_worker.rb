5c21c130c87f11236a87324bad29a2b7961c2cc4
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-09-07 15:23:17 +0100

    Remove safe navigator and instance variable
    Addressing a couple of comments made about a previous commit on this
    file:
    
    - removed the safe navigator to check if the user exists and recieves
    emails. Uneeded to check if user exists as if the user does not exist
    then an exception will be raised. This should never happen so if it does
    it would be good if the exception is raised instead of being passed
    over.
    
    - Conversion of the instance variable @user to a normal variable

--- app/workers/dormant_user_mailer_worker.rb
+++ app/workers/dormant_user_mailer_worker.rb
@@ -4,9 +4,9 @@ class DormantUserMailerWorker
   attr_reader :user
 
   def perform(user_id)
-    @user = User.find(user_id)
-    if @user&.receives_email?
-      DormantUserMailer.email_dormant_user(@user).deliver
+    user = User.find(user_id)
+    if user.receives_email?
+      DormantUserMailer.email_dormant_user(user).deliver
     end
   end
 end
