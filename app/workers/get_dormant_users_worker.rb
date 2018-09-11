57da7b97d2de86490478dddd9d288fbd06307d17
Rona Costello <ronacostello@plants.ox.ac.uk>
2018-09-11 15:03:38 +0100

    Raise error if get dormant users is run on staging

--- app/workers/get_dormant_users_worker.rb
+++ app/workers/get_dormant_users_worker.rb
@@ -1,7 +1,9 @@
 class GetDormantUsersWorker
+  class DoNotRunOnStagingError < StandardError; end
   include Sidekiq::Worker
 
   def perform(num_days_since_activity, ending_in)
+    raise DoNotRunOnStagingError if Rails.env.staging?
     selected_users = User.subset_selection(ending_in)
     User.dormant(num_days_since_activity, selected_users) do |dormant_user|
       DormantUserMailerWorker.perform_async(dormant_user.id)
--- spec/workers/get_dormant_users_worker_spec.rb
+++ spec/workers/get_dormant_users_worker_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe GetDormantUsersWorker do
   let(:worker) { described_class.new }
 
+  it "should raise error if running on staging" do
+    allow(Rails).to receive(:env) { "staging".inquiry }
+    expect { worker.perform(5, 5) }.to raise_error(GetDormantUsersWorker::DoNotRunOnStagingError)
+  end
+
   it "should email dormant users in subselection" do
     dormant_user1 = create(:user, id: 45, current_sign_in_at: 5.days.ago)
     dormant_user2 = create(:user, id: 5, current_sign_in_at: 5.days.ago)
