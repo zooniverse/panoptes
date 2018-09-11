9dc218acb3e6b8b556265fc8a84c46a395b02357
Campbell Allen <campbell.allen@gmail.com>
2018-09-11 15:12:35 +0100

    add space for short circut

--- app/workers/get_dormant_users_worker.rb
+++ app/workers/get_dormant_users_worker.rb
@@ -4,6 +4,7 @@ class GetDormantUsersWorker
 
   def perform(num_days_since_activity, ending_in)
     raise DoNotRunOnStagingError if Rails.env.staging?
+
     selected_users = User.subset_selection(ending_in)
     User.dormant(num_days_since_activity, selected_users) do |dormant_user|
       DormantUserMailerWorker.perform_async(dormant_user.id)
