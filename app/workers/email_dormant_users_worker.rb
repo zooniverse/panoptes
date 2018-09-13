ab9b07e33f1e1be81702271b7d5dd6885957ae40
Campbell Allen <campbell.allen@gmail.com>
2018-09-13 11:06:04 +0100

    clarify the ending in scope param name

--- app/workers/email_dormant_users_worker.rb
+++ app/workers/email_dormant_users_worker.rb
@@ -3,10 +3,10 @@ class EmailDormantUsersWorker
 
   class DoNotRunOnStagingError < StandardError; end
 
-  def perform(num_days_since_activity, ending_in)
+  def perform(num_days_since_activity, only_user_ids_ending_in)
     raise DoNotRunOnStagingError if Rails.env.staging?
 
-    user_ids_ending_in_scope = User.subset_selection(ending_in)
+    user_ids_ending_in_scope = User.subset_selection(only_user_ids_ending_in)
     User.dormant(num_days_since_activity, user_ids_ending_in_scope) do |dormant_user|
       DormantUserMailerWorker.perform_async(dormant_user.id)
     end
