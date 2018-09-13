b4f36a8266aabf4e48381e1c85a6911d7aff9a72
Campbell Allen <campbell.allen@gmail.com>
2018-09-13 11:05:11 +0100

    clarify the scope param name

--- app/workers/email_dormant_users_worker.rb
+++ app/workers/email_dormant_users_worker.rb
@@ -6,8 +6,8 @@ class EmailDormantUsersWorker
   def perform(num_days_since_activity, ending_in)
     raise DoNotRunOnStagingError if Rails.env.staging?
 
-    selected_users = User.subset_selection(ending_in)
-    User.dormant(num_days_since_activity, selected_users) do |dormant_user|
+    user_ids_ending_in_scope = User.subset_selection(ending_in)
+    User.dormant(num_days_since_activity, user_ids_ending_in_scope) do |dormant_user|
       DormantUserMailerWorker.perform_async(dormant_user.id)
     end
   end
