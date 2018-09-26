7329fc5c6937efa9366891baf07671a87a5f679f
Campbell Allen <campbell.allen@gmail.com>
2018-09-26 11:34:40 +0100

    optimize the workflow activity period count query
    refactor the worker internals to rely on a nested class that calculates 
    the workflow classifications for the activity period in question. This 
    uses a last id classification query to restrict the search space of the 
    classifications and allow us to filter on the created_at field and thus 
    we can remove the index that this used to rely on

--- app/workers/calculate_project_activity_worker.rb
+++ app/workers/calculate_project_activity_worker.rb
@@ -3,13 +3,17 @@ class CalculateProjectActivityWorker
 
   sidekiq_options queue: :data_medium
 
-  def perform(project_id)
+  def perform(project_id, period=nil)
     project = Project.find(project_id)
     Project.transaction do
       project_activity = 0
       project.workflows.each do |workflow|
-        activity_count = workflow_activity(workflow)
-        workflow.update_columns activity: activity_count
+        activity_period = WorkflowActivityPeriod.new(workflow, period)
+        activity_count = activity_period.count
+        workflow.update_columns(
+          activity: activity_count,
+          activity_classification_id: activity_period.earliest_activity_classification_id
+        )
         project_activity += activity_count
       end
       project.update_columns activity: project_activity
@@ -18,7 +22,34 @@ class CalculateProjectActivityWorker
     nil
   end
 
-  def workflow_activity(workflow, period=24.hours.ago)
-    workflow.classifications.where("created_at >= ?", period).count
+  class WorkflowActivityPeriod
+    attr_reader :workflow, :period
+    ACTIVITY_PERIOD = 24.hours.ago
+
+    def initialize(workflow, period)
+      @workflow = workflow
+      @period = period || ACTIVITY_PERIOD
+    end
+
+    def count
+      activity_scope.count
+    end
+
+    def earliest_activity_classification_id
+      earliest_classification_id = activity_scope.select(:id).first
+      earliest_classification_id&.id
+    end
+
+    private
+
+    def activity_scope
+      workflow_period_activity_scope.after_id(
+        workflow.activity_classification_id
+      )
+    end
+
+    def workflow_period_activity_scope
+      workflow.classifications.where("created_at >= ?", period)
+    end
   end
 end
--- spec/workers/calculate_project_activity_worker_spec.rb
+++ spec/workers/calculate_project_activity_worker_spec.rb
@@ -5,39 +5,104 @@ describe CalculateProjectActivityWorker do
   let(:workflow) { create(:workflow) }
   let(:project) { workflow.project }
   let(:user) { project.user }
-
-  let(:classifications) do
+  let(:in_period_classifications) do
     create_list(:classification, 2, project: project, workflow: workflow)
+  end
+  let(:non_normal_period_classification) do
     c = create(:classification, project: project, workflow: workflow)
     c.update_column(:created_at, 25.hours.ago)
+    c
+  end
+  let(:classifications) do
+    [ non_normal_period_classification, in_period_classifications ].flatten
   end
 
   describe '#perform' do
-    it 'should update the workflow and project activity' do
-      classifications
-      count = worker.workflow_activity(workflow)
-      expect_any_instance_of(Workflow).to receive(:update_columns).with(activity: count)
-      expect_any_instance_of(Project).to receive(:update_columns).with(activity: count)
-      worker.perform(project.id)
-    end
-
     context "when it can't find the project" do
       it "should fail quickly" do
         expect(Project).not_to receive(:transaction)
         worker.perform("-1")
       end
     end
+
+    context "with classifications and the tracked activity classification id" do
+      before do
+        classifications
+        workflow.update_column(:activity_classification_id, 1)
+      end
+      let(:activity_count) { 2 }
+
+      it 'should update the workflow activity attributes' do
+        expect_any_instance_of(Workflow)
+          .to receive(:update_columns)
+          .with({
+            activity: activity_count,
+            activity_classification_id: in_period_classifications.first.id
+          })
+        worker.perform(project.id)
+      end
+
+      it 'should update the project activity attributes' do
+        expect_any_instance_of(Project)
+          .to receive(:update_columns)
+          .with({activity: activity_count})
+        worker.perform(project.id)
+      end
+
+      context "with a longer activity period" do
+        it 'should have different counts and track a different activity classification' do
+          expect_any_instance_of(Workflow)
+            .to receive(:update_columns)
+            .with({
+              activity: 3,
+              activity_classification_id: non_normal_period_classification.id
+            })
+          worker.perform(project.id, 48.hours.ago)
+        end
+      end
+    end
   end
 
-  describe '#workflow_activity' do
+  describe CalculateProjectActivityWorker::WorkflowActivityPeriod do
+    let(:period) do
+      CalculateProjectActivityWorker::WorkflowActivityPeriod::ACTIVITY_PERIOD
+    end
+    subject { described_class.new(workflow, period) }
+
     it 'returns 0 when no classifications have been made yet' do
-      expect(worker.workflow_activity(workflow)).to eq(0)
+      expect(subject.count).to eq(0)
     end
 
-    context "with classifications" do
-      it 'should only count classification in last 24 hours' do
+    context "with classifications and the tracked activity classification id" do
+      before do
         classifications
-        expect(worker.workflow_activity(workflow)).to eq(2)
+        workflow.update_column(:activity_classification_id, 1)
+      end
+
+      it 'should only count classification in last 24 hours' do
+        expect(subject.count).to eq(2)
+      end
+
+      context "with a longer activity period" do
+        let(:period) { 48.hours.ago }
+
+        it 'should only count classification in last 48 hours' do
+          expect(subject.count).to eq(3)
+        end
+      end
+
+      context "with a later activity_classification_id" do
+        it 'should only count the classifications after the id' do
+          id = in_period_classifications.first.id
+          workflow.update_column(:activity_classification_id, id)
+          expect(subject.count).to eq(1)
+        end
+
+        it 'should only count the classifications after the id' do
+          id = in_period_classifications.last.id
+          workflow.update_column(:activity_classification_id, id)
+          expect(subject.count).to eq(0)
+        end
       end
     end
   end
