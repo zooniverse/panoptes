22d5cb61497b2de612a7085c5ddbebb204fb3225
Marten Veldthuis <marten@veldthuis.com>
2018-10-23 16:08:16 +0100

    Add versioning to organizations (#2977)

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -13,6 +13,7 @@ class Organization < ActiveRecord::Base
   has_one :avatar, -> { where(type: "organization_avatar") }, class_name: "Medium", as: :linked
   has_one :background, -> { where(type: "organization_background") }, class_name: "Medium", as: :linked
   has_many :organization_roles, ->{ where.not("access_control_lists.roles = '{}'") }, class_name: "AccessControlList", as: :resource
+  has_many :organization_versions, dependent: :destroy
   has_many :pages, class_name: "OrganizationPage", dependent: :destroy
   has_many :attached_images, -> { where(type: "organization_attached_image") }, class_name: "Medium",
     as: :linked
@@ -26,6 +27,15 @@ class Organization < ActiveRecord::Base
   def self.translatable_attributes
     %i(display_name title description introduction announcement url_labels)
   end
+
+  after_save :save_version
+
+  def save_version
+    if (changes.keys & %w(display_name description introduction urls url_labels announcement)).present?
+      OrganizationVersion.create_from(self)
+    end
+  end
+
 
   def retired_subjects_count
     projects.joins(:active_workflows).sum("workflows.retired_set_member_subjects_count")
