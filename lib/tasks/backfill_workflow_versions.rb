1e75bdcd5093754cbe2e43ca5f6baa48ed6a849b
Marten Veldthuis <marten@veldthuis.com>
2018-11-08 14:55:35 +0000

    Backfill workflow version records from Papertrail=

--- lib/tasks/backfill_workflow_versions.rb
+++ lib/tasks/backfill_workflow_versions.rb
@@ -3,10 +3,35 @@
 module Tasks
   class BackfillWorkflowVersions
     def backfill(workflow)
-      workflow.versions.each do |version|
-        workflow_at_version = version.reify
+      workflow_content = workflow.primary_content
+
+      workflow.versions.each_with_index do |w_version, w_index|
+        workflow_at_version = w_version.reify
+
+        # reify returns nil for create. I think it's not correct to then just
+        # set it to the actual workflow, since that is always the latest version
+        # but it is what the dump cache is doing.
+        workflow_at_version ||= workflow
+
+        # It is hard to figure out which combinations of major/minor actually
+        # existed. For now I'm opting to simply generate all permutations.
+        # I'd love to have a discussion on how to do this more wisely.
+        workflow_content.versions.each_with_index do |wc_version, wc_index|
+          workflow_content_at_version = wc_version.reify
+
+          # reify returns nil for create. I think it's not correct to then just
+          # set it to the actual workflow, since that is always the latest version
+          # but it is what the dump cache is doing.
+          workflow_content_at_version ||= workflow_content
+
+          workflow.workflow_versions.create! \
+            tasks: workflow_at_version.tasks,
+            first_task: workflow_at_version.first_task,
+            strings: workflow_content_at_version.strings,
+            major_number: w_index,
+            minor_number: wc_index
+        end
       end
     end
-    # Code here
   end
 end
--- spec/tasks/backfill_workflow_versions_spec.rb
+++ spec/tasks/backfill_workflow_versions_spec.rb
@@ -3,18 +3,16 @@
 require 'spec_helper'
 
 describe Tasks::BackfillWorkflowVersions do
-
   it 'creates workflow_versions for major changes', :versioning do
     workflow = create :workflow
-    workflow.tasks['interest.next'] = "none"
+    workflow.tasks['interest']['next'] = "none"
     workflow.save!
-    workflow.tasks['interest.next'] = "shape"
+    workflow.tasks['interest']['next'] = "shape"
     workflow.save!
 
     workflow.workflow_versions.destroy_all
 
     described_class.new.backfill(workflow)
-
-    binding.pry
+    expect(workflow.reload.workflow_versions.count).to eq(3)
   end
 end
