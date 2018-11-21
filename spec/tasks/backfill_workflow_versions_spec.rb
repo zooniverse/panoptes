cebaca0a9b0aefec974dc238cf5348649ca2f187
Marten Veldthuis <marten@veldthuis.com>
2018-11-21 14:11:21 +0000

    Test for backfilling content changes

--- spec/tasks/backfill_workflow_versions_spec.rb
+++ spec/tasks/backfill_workflow_versions_spec.rb
@@ -15,4 +15,17 @@ describe Tasks::BackfillWorkflowVersions do
     described_class.new.backfill(workflow)
     expect(workflow.reload.workflow_versions.count).to eq(3)
   end
+
+  it 'creates workflow_versions for minor changes', :versioning do
+    workflow = create :workflow
+    workflow.primary_content.strings['interest.title'] = 'Foo'
+    workflow.primary_content.save!
+    workflow.primary_content.strings['interest.title'] = 'Bar'
+    workflow.primary_content.save!
+
+    workflow.workflow_versions.destroy_all
+
+    described_class.new.backfill(workflow)
+    expect(workflow.reload.workflow_versions.count).to eq(3)
+  end
 end
