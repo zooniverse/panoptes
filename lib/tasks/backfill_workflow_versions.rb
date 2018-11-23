7933f9735215ce8d6273133f74b3da2044dcc0fc
Marten Veldthuis <marten@veldthuis.com>
2018-11-23 15:07:09 +0000

    Fix backfilling

--- lib/tasks/backfill_workflow_versions.rb
+++ lib/tasks/backfill_workflow_versions.rb
@@ -5,31 +5,23 @@ module Tasks
     def backfill(workflow)
       workflow_content = workflow.primary_content
 
-      workflow.versions.each_with_index do |w_version, w_index|
-        workflow_at_version = w_version.reify
+      # It is hard to figure out which combinations of major/minor actually
+      # existed. For now I'm opting to simply generate all permutations.
+      # I'd love to have a discussion on how to do this more wisely.
+      #
+      workflow_versions = workflow.versions[1..-1].map(&:reify) + [workflow]
+      workflow_versions.each_with_index do |workflow_at_version, w_index|
 
-        # reify returns nil for create. I think it's not correct to then just
-        # set it to the actual workflow, since that is always the latest version
-        # but it is what the dump cache is doing.
-        workflow_at_version ||= workflow
-
-        # It is hard to figure out which combinations of major/minor actually
-        # existed. For now I'm opting to simply generate all permutations.
-        # I'd love to have a discussion on how to do this more wisely.
-        workflow_content.versions.each_with_index do |wc_version, wc_index|
-          workflow_content_at_version = wc_version.reify
-
-          # reify returns nil for create. I think it's not correct to then just
-          # set it to the actual workflow, since that is always the latest version
-          # but it is what the dump cache is doing.
-          workflow_content_at_version ||= workflow_content
+        workflow_content_versions = workflow_content.versions[1..-1].map(&:reify) + [workflow_content]
+        workflow_content_versions.each_with_index do |workflow_content_at_version, wc_index|
 
           workflow.workflow_versions.create! \
             tasks: workflow_at_version.tasks,
             first_task: workflow_at_version.first_task,
             strings: workflow_content_at_version.strings,
-            major_number: w_index,
-            minor_number: wc_index
+            major_number: w_index + 1,
+            minor_number: wc_index + 1
+
         end
       end
     end
--- spec/tasks/backfill_workflow_versions_spec.rb
+++ spec/tasks/backfill_workflow_versions_spec.rb
@@ -4,28 +4,34 @@ require 'spec_helper'
 
 describe Tasks::BackfillWorkflowVersions do
   it 'creates workflow_versions for major changes', :versioning do
-    workflow = create :workflow
-    workflow.tasks['interest']['next'] = "none"
-    workflow.save!
-    workflow.tasks['interest']['next'] = "shape"
-    workflow.save!
+    workflow = create :workflow, tasks: {status: "one"}
+    workflow.update! tasks: {status: "two"}
+    workflow.update! tasks: {status: "three"}
 
     workflow.workflow_versions.destroy_all
 
     described_class.new.backfill(workflow)
-    expect(workflow.reload.workflow_versions.count).to eq(3)
+    workflow.reload
+
+    expect(workflow.workflow_versions.count).to eq(3)
+    expect(workflow.workflow_versions.find_by(major_number: 1, minor_number: 1).tasks).to eq("status" => "one")
+    expect(workflow.workflow_versions.find_by(major_number: 2, minor_number: 1).tasks).to eq("status" => "two")
+    expect(workflow.workflow_versions.find_by(major_number: 3, minor_number: 1).tasks).to eq("status" => "three")
   end
 
   it 'creates workflow_versions for minor changes', :versioning do
     workflow = create :workflow
-    workflow.primary_content.strings['interest.title'] = 'Foo'
-    workflow.primary_content.save!
-    workflow.primary_content.strings['interest.title'] = 'Bar'
-    workflow.primary_content.save!
+    workflow.primary_content.update! strings: workflow.primary_content.strings.merge('interest.question' => 'Foo')
+    workflow.primary_content.update! strings: workflow.primary_content.strings.merge('interest.question' => 'Bar')
 
     workflow.workflow_versions.destroy_all
 
     described_class.new.backfill(workflow)
-    expect(workflow.reload.workflow_versions.count).to eq(3)
+    workflow.reload
+
+    expect(workflow.workflow_versions.count).to eq(3)
+    expect(workflow.workflow_versions.find_by(major_number: 1, minor_number: 1).strings['interest.question']).to eq("Draw a circle")
+    expect(workflow.workflow_versions.find_by(major_number: 1, minor_number: 2).strings['interest.question']).to eq("Foo")
+    expect(workflow.workflow_versions.find_by(major_number: 1, minor_number: 3).strings['interest.question']).to eq("Bar")
   end
 end
