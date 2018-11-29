92148638e75e052f65b285c56501b6b3b38c9054
Marten Veldthuis <marten@veldthuis.com>
2018-11-29 10:27:45 +0000

    Backfill when version already exists

--- lib/tasks/backfill_workflow_versions.rb
+++ lib/tasks/backfill_workflow_versions.rb
@@ -15,13 +15,12 @@ module Tasks
         workflow_content_versions = workflow_content.versions[1..-1].map(&:reify) + [workflow_content]
         workflow_content_versions.each_with_index do |workflow_content_at_version, wc_index|
 
-          workflow.workflow_versions.create! \
-            tasks: workflow_at_version.tasks,
-            first_task: workflow_at_version.first_task,
-            strings: workflow_content_at_version.strings,
-            major_number: w_index + 1,
-            minor_number: wc_index + 1
-
+          workflow_version = workflow.workflow_versions.find_or_initialize_by(major_number: w_index + 1,
+                                                                              minor_number: wc_index + 1)
+          workflow_version.tasks = workflow_at_version.tasks
+          workflow_version.first_task = workflow_at_version.first_task
+          workflow_version.strings = workflow_content_at_version.strings
+          workflow_version.save!
         end
       end
     end
