7bd5ae5e5b0aa9f1002368f3eb5eb51dc2fdd53b
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-03-22 14:43:45 -0500

    Select, but be more specific about what to count.

--- app/workers/subject_set_statuses_create_worker.rb
+++ app/workers/subject_set_statuses_create_worker.rb
@@ -12,9 +12,11 @@ class SubjectSetStatusesCreateWorker
     ).exists?
     return unless set_is_linked_to_workflow
 
-    linked_subject_select_scope = SetMemberSubject.where(subject_set_id: subject_set_id)
-    duration = linked_subject_select_scope.count * 4
+    linked_subject_select_scope = SetMemberSubject
+      .where(subject_set_id: subject_set_id)
+      .select(:id,:subject_id)
 
+    duration = linked_subject_select_scope.count(:id) * 4
     linked_subject_select_scope.find_each do |sms|
       SubjectWorkflowStatusCreateWorker.perform_in(
         duration.seconds*rand,
