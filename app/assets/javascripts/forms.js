0a0792a70b00b3f8f76da72f9d24b148ed8a039f
Campbell Allen <campbell.allen@gmail.com>
2019-04-05 18:45:13 +0100

    handle form page re-loading events
    ensure the form changes the wordring correctly based on the trips back from the server

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -3,12 +3,7 @@ $(function () {
   var DEFAULT_EMAIL_TEXT = "Email (required)";
   var DEFAULT_SUBSCRIPTION_TEXT = "It’s okay to send me email every once in a while.";
 
-  $(document).ready(function() {
-    $("#privacy_text").text(DEFAULT_PRIVACY_TEXT);
-    $("#agreement_text").text(DEFAULT_SUBSCRIPTION_TEXT);
-    $("#email_text label").text(DEFAULT_EMAIL_TEXT);
-  });
-  $("#under_age").change(function() {
+  function changeRegistrationFormWording() {
     if ($("#under_age").is(':checked')) {
       $("#privacy_text").text("I confirm I am the parent/guardian and give permission for my child to register by providing my email address as the main contact address. Both I and my child understand and agree to the");
       $("#agreement_text").text("If you agree, we will periodically send email promoting new research-related projects or other information relating to our research. We will not use your contact information for commercial purposes.");
@@ -20,5 +15,16 @@ $(function () {
       $("#anonymous_text").text("");
       $("#email_text label").text(DEFAULT_EMAIL_TEXT);
     }
-    });
+  }
+
+  $(document).ready(function() {
+    $("#privacy_text").text(DEFAULT_PRIVACY_TEXT);
+    $("#agreement_text").text(DEFAULT_SUBSCRIPTION_TEXT);
+    $("#email_text label").text(DEFAULT_EMAIL_TEXT);
+    // ensure the form changes on re-load if the under_age checkbox is set
+    changeRegistrationFormWording();
   });
+  $("#under_age").change(function() {
+    changeRegistrationFormWording();
+  });
+});
