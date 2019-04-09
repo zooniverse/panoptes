984a13ae40eea5faac193a382a215a6b25e41b7c
Campbell Allen <campbell.allen@gmail.com>
2019-04-09 22:15:40 +0100

    remove unused relation
    subject workflow retirement should happen on the recommended workflow retire_subjects route

--- app/serializers/set_member_subject_serializer.rb
+++ app/serializers/set_member_subject_serializer.rb
@@ -2,5 +2,5 @@ class SetMemberSubjectSerializer
   include Serialization::PanoptesRestpack
 
   attributes :id, :created_at, :updated_at, :priority, :href
-  can_include :subject_set, :subject, :retired_workflows
+  can_include :subject_set, :subject
 end
