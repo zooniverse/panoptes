238dff2c4ad887f265c97017c620125ed2438fb9
Campbell Allen <campbell.allen@gmail.com>
2019-04-09 22:24:50 +0100

    preload tutorial links

--- app/serializers/tutorial_serializer.rb
+++ app/serializers/tutorial_serializer.rb
@@ -5,8 +5,9 @@ class TutorialSerializer
 
   attributes :steps, :href, :id, :created_at, :updated_at, :language, :kind, :display_name
 
-  can_include :project
-  can_include :workflows
+  can_include :project, :workflows
   media_include :attached_images
   can_filter_by :language
+
+  preload :workflows, :attached_images
 end
