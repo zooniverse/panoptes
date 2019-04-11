3a452aa162f887a6efccbab73e7bbb796968d822
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2019-04-11 16:47:09 +0100

    Fix syntax on update_all

--- db/migrate/20190411125709_change_workflow_steps_default.rb
+++ db/migrate/20190411125709_change_workflow_steps_default.rb
@@ -5,7 +5,7 @@ class ChangeWorkflowStepsDefault < ActiveRecord::Migration
         change_column_default(:workflows, :steps, [])
         Workflow.select(:id).find_in_batches do |workflows|
           workflows_update_scope = Workflow.where(id: workflows)
-          workflows_update_scope.update_all(:steps, [])
+          workflows_update_scope.update_all(steps: [])
         end
       end
     end
