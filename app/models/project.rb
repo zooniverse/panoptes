77b011849ae088ea9adadcf2faadf963a4669c98
Campbell Allen <campbell.allen@gmail.com>
2019-05-07 12:38:24 +0100

    format the relation lambda sytnax

--- app/models/project.rb
+++ app/models/project.rb
@@ -13,17 +13,14 @@ class Project < ActiveRecord::Base
   has_many :tutorials
   has_many :field_guides, dependent: :destroy
   belongs_to :organization
-
   # uses the activated_state enum on the workflow
-  has_many :workflows, -> {
-    where(education_api: false).active
-  }, dependent: :restrict_with_exception
-
+  has_many :workflows,
+    -> { where(education_api: false).active},
+    dependent: :restrict_with_exception
   # use both the activated_state and active attribute on the workflow
-  has_many :active_workflows, -> {
-    where(active: true, education_api: false).active
-  }, class_name: "Workflow"
-
+  has_many :active_workflows,
+    -> { where(active: true, education_api: false).active },
+    class_name: "Workflow"
   has_many :subject_sets, dependent: :destroy
   has_many :live_subject_sets, through: :active_workflows, source: 'subject_sets'
   has_many :classifications, dependent: :restrict_with_exception
