e83fc6aeac3bfca3844eeb295a0db34d71207f05
Campbell Allen <campbell.allen@gmail.com>
2019-06-12 10:48:36 +0100

    add ability to make more subjects

--- spec/factories/subject_sets.rb
+++ spec/factories/subject_sets.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :subject_set do
     transient do
       num_workflows 1
+      num_subjects 2
     end
 
     sequence(:display_name) { |n| "Subject Set #{n}" }
@@ -16,8 +17,8 @@ FactoryBot.define do
     end
 
     factory :subject_set_with_subjects do
-      after(:create) do |set|
-        2.times do |i|
+      after(:create) do |set, evaluator|
+        evaluator.num_subjects.times do |i|
           subject = create(:subject, project: set.project, uploader: set.project.owner)
           create(:set_member_subject, subject_set: set, subject: subject)
         end
