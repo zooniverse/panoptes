9051a981ec6fbc0fdac72bacb7b276953f7e9a7b
Campbell Allen <campbell.allen@gmail.com>
2019-06-13 17:00:59 +0100

    switch to sanitize the sql string vs use bind params

--- app/workers/subject_metadata_worker.rb
+++ app/workers/subject_metadata_worker.rb
@@ -8,13 +8,24 @@ class SubjectMetadataWorker
       FROM   subjects
       WHERE  subjects.id = set_member_subjects.subject_id
       AND    subjects.metadata ? '#priority'
-      AND    set_member_subjects.subject_set_id = $1
+      AND    set_member_subjects.subject_set_id = ?
     SQL
-
-    ActiveRecord::Base.connection.exec_update(
+    # handle incorrect bind params for non preparted statements
+    # in exec_update, fixed in rails 5
+    # https://github.com/rails/rails/issues/24893
+    # https://github.com/rails/rails/issues/34183
+    bound_update_sms_priority_sql = ActiveRecord::Base.send(
+      :replace_bind_variables,
       update_sms_priority_sql,
-      "SQL",
-      [[nil, subject_set_id]]
+      [subject_set_id]
     )
+
+    # Note: In Rails 5 use bound params, [[nil,subject_set_id]]
+    ActiveRecord::Base.connection.exec_update(
+      bound_update_sms_priority_sql,
+      "SQL",
+      []
+    )
+binding.pry
   end
 end
