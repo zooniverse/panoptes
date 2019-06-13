6c8b11dacba695ab27c950eb8c112a89c06aa017
Campbell Allen <campbell.allen@gmail.com>
2019-06-13 19:08:24 +0100

    replace named bind vars
    use internal Sanitization method replace_named_bind_variables to safely inject the subject_set_id

--- app/workers/subject_metadata_worker.rb
+++ app/workers/subject_metadata_worker.rb
@@ -8,16 +8,18 @@ class SubjectMetadataWorker
       FROM   subjects
       WHERE  subjects.id = set_member_subjects.subject_id
       AND    subjects.metadata ? '#priority'
-      AND    set_member_subjects.subject_set_id = ?
+      AND    set_member_subjects.subject_set_id = :subject_set_id
     SQL
+    # replace the above SQL :subject_set_id named bind var with $1
+    #
     # handle incorrect bind params for non preparted statements
     # in exec_update, fixed in rails 5
     # https://github.com/rails/rails/issues/24893
     # https://github.com/rails/rails/issues/34183
     bound_update_sms_priority_sql = ActiveRecord::Base.send(
-      :replace_bind_variables,
+      :replace_named_bind_variables,
       update_sms_priority_sql,
-      [subject_set_id]
+      {subject_set_id: subject_set_id}
     )
 
     # Note: In Rails 5 use bound params, [[nil,subject_set_id]]
@@ -26,6 +28,5 @@ class SubjectMetadataWorker
       "SQL",
       []
     )
-binding.pry
   end
 end
