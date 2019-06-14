f96dad40b9dee77d5ef0043367cc853401cfdad1
Campbell Allen <campbell.allen@gmail.com>
2019-06-14 17:20:48 +0100

    clarify variable and comment wording

--- lib/subjects/postgresql_selection.rb
+++ lib/subjects/postgresql_selection.rb
@@ -26,14 +26,14 @@ module Subjects
 
     def available
       query = Subjects::SetMemberSubjectSelector.new(workflow, user).set_member_subjects
-      subject_set_scope = if workflow.grouped
-                            # respect the user wishes if they want to selection from a training set
+      subject_set_ids = if workflow.grouped
+                            # respect the user if they want to select from a training set
                             opts[:subject_set_id]
                           else
-                            # by default do not select data from training sets
+                            # default mode: do not select from training sets
                             workflow.non_training_subject_sets.pluck(:id)
                           end
-      query.where(subject_set_id: subject_set_scope)
+      query.where(subject_set_id: subject_set_ids)
     end
 
     def limit
