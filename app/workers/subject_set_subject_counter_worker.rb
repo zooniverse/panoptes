8c228a928519eeed9490b750786e59766ee90808
Campbell Allen <campbell.allen@gmail.com>
2019-06-24 16:17:24 +0100

    count the workflow's subjects after counting a sets
    ensure we recount the workflow subjects after counting a linked set's subjects.

--- app/workers/subject_set_subject_counter_worker.rb
+++ app/workers/subject_set_subject_counter_worker.rb
@@ -12,8 +12,14 @@ class SubjectSetSubjectCounterWorker
     lock: :until_executing
 
   def perform(subject_set_id)
+    # recount this set's subjects
     set = SubjectSet.find(subject_set_id)
     set.update_column(:set_member_subjects_count, set.set_member_subjects.count)
     set.touch
+
+    # recount the subjects for each workflow this set is in
+    set.workflow_ids.each do |workflow_id|
+      WorkflowSubjectsCountWorker.perform_async(workflow_id)
+    end
   end
 end
