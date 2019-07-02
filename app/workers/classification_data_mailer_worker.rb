58db214b2ba94c8e3439c3600dec5d9c15c9be73
Campbell Allen <campbell.allen@gmail.com>
2019-07-02 09:19:57 +0100

    Remove s3 download export link (#3147)
    * change wording to download from data exports page
    
    * provide the data exports lab url to emailers
    
    * add data-exports suffix to url
    
    * switch param name from s3_url to url

--- app/workers/classification_data_mailer_worker.rb
+++ app/workers/classification_data_mailer_worker.rb
@@ -3,11 +3,11 @@ class ClassificationDataMailerWorker
 
   sidekiq_options queue: :data_high
 
-  def perform(resource_id, resource_type, s3_url, emails)
+  def perform(resource_id, resource_type, url, emails)
     return unless emails.present?
     ClassificationDataMailer.classification_data(
       resource_type.camelize.constantize.find(resource_id),
-      s3_url.to_s,
+      url.to_s,
       emails
     ).deliver
   end
--- spec/workers/subject_data_mailer_worker_spec.rb
+++ spec/workers/subject_data_mailer_worker_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 
 RSpec.describe SubjectDataMailerWorker do
   let(:project) { create(:project) }
-  let(:s3_url) { "https://fake.s3.url.example.com" }
+  let(:url) { "https://www.zooniverse.org/lab/123" }
 
   it 'should deliver the mail' do
-    expect{ subject.perform(project.id, "project", s3_url, ["zach@zooniverse.org"]) }.to change{ ActionMailer::Base.deliveries.count }.by(1)
+    expect{ subject.perform(project.id, "project", url, ["zach@zooniverse.org"]) }.to change{ ActionMailer::Base.deliveries.count }.by(1)
   end
 
   context 'when there are no recipients' do
     it 'does not call the mailer' do
       expect(SubjectDataMailer).to receive(:subject_data).never
-      subject.perform(project.id, "project", s3_url, [])
+      subject.perform(project.id, "project", url, [])
     end
   end
 end
