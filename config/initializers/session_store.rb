94b03a76b165b081248de678dd714c2a5b97a563
Campbell Allen <campbell.allen@gmail.com>
2019-09-20 17:48:57 +0100

    allow cookie reuse across zooniverse subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_Panoptes_session'
+Rails.application.config.session_store :cookie_store,
+    key: '_Panoptes_session',
+    domain: {
+      production: '.zooniverse.org',
+      staging: '.zooniverse.org'
+    }.fetch(Rails.env.to_sym, :all)
