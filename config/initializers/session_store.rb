58c27ec0d8910462a49be39fde5c3de46fb28f3a
Campbell Allen <campbell.allen@gmail.com>
2019-09-26 15:29:03 +0100

    allow cookie reuse across zooniverse subdomains (#3176)
    * allow cookie reuse across zooniverse subdomains
    
    * clarify the code for domains setup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_Panoptes_session'
+domain =
+  if %w(production staging).include?(Rails.env)
+    # allow zooniverse.org subdomains for staging & production
+    '.zooniverse.org'
+  else
+    # allow all subdomains for dev & test
+    :all
+  end
+
+Rails.application.config.session_store :cookie_store,
+    key: '_Panoptes_session',
+    domain: domain
