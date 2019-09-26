8c0b571cccafc174e5798c491a0c024509e1b0a0
Campbell Allen <campbell.allen@gmail.com>
2019-09-26 15:13:40 +0100

    clarify the code for domains setup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
+domain =
+  if %w(production staging).include?(Rails.env)
+    # allow zooniverse.org subdomains for staging & production
+    '.zooniverse.org'
+  else
+    # allow all subdomains for dev & test
+    :all
+  end
+
 Rails.application.config.session_store :cookie_store,
     key: '_Panoptes_session',
-    domain: {
-      production: '.zooniverse.org',
-      staging: '.zooniverse.org'
-    }.fetch(Rails.env.to_sym, :all)
+    domain: domain
