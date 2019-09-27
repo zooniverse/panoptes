9c8f8170397cb2bfee8b6dd1e9f4e19591dfc0c1
Campbell Allen <campbell.allen@gmail.com>
2019-09-27 12:25:23 +0100

    Revert "allow cookie reuse across zooniverse subdomains (#3176)"
    This reverts commit 58c27ec0d8910462a49be39fde5c3de46fb28f3a.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,14 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-domain =
-  if %w(production staging).include?(Rails.env)
-    # allow zooniverse.org subdomains for staging & production
-    '.zooniverse.org'
-  else
-    # allow all subdomains for dev & test
-    :all
-  end
-
-Rails.application.config.session_store :cookie_store,
-    key: '_Panoptes_session',
-    domain: domain
+Rails.application.config.session_store :cookie_store, key: '_Panoptes_session'
