ce212a3d6778d27e6d161b9cf66194085755ea9e
Jim O'Donnell <jim@zooniverse.org>
2020-02-13 12:59:10 +0000

    Add project status to the cards route Add project.status and project.completeness to the fields returned by /projects?cards=true.

--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -106,7 +106,7 @@ describe Api::V1::ProjectsController, type: :controller do
         describe "cards only" do
           let(:index_options) { { cards: true } }
           let(:card_attrs) do
-            ["id", "display_name", "description", "slug", "redirect", "avatar_src", "links", "updated_at", "classifications_count", "launch_approved"]
+            ["id", "display_name", "description", "slug", "redirect", "avatar_src", "links", "updated_at", "classifications_count", "launch_approved", "state", "completeness"]
           end
 
           it "should return only serialise the card data" do
