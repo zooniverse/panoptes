029dca9d5315b95b83b215f7a6864285756157a8
Nicole Ciemniak <nciemniak@gmail.com>
2020-03-26 15:16:11 -0500

    add validator to ensure only 1 fav per collection

---
+++ lib/validators/one_favorite_per_owner_validator.rb
@@ -0,0 +1,14 @@
+module Validators
+  # Validator for Collection model
+  # Validate that only one favorite collection is created per owner for each project
+  class OneFavoritePerOwnerValidator < ActiveModel::Validator
+    def validate(new_record)
+      records = new_record.owner.collections
+      if records && new_record.favorite
+        records.each do |record|
+          new_record.errors[:favorite] = 'Only one favorite collection can be created per owner for each project' if record.favorite
+        end
+      end
+    end
+  end
+end
