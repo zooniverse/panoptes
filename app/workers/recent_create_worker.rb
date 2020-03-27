46c5b531cf9f879ef1bb97bfa05e0b7d1504abb8
Campbell Allen <campbell.allen@gmail.com>
2020-03-27 19:09:41 +0000

    mark old recents for delete
    once there are more than the capped collection size, mark all recents with an ID greater than the top 50 (default) for deletion

--- app/workers/recent_create_worker.rb
+++ app/workers/recent_create_worker.rb
@@ -4,5 +4,18 @@ class RecentCreateWorker
   def perform(classification_id)
     classification = Classification.find(classification_id)
     Recent.create_from_classification(classification)
+
+    # TODO: move this to the Recent class
+    # TODO: provide a constant / env var to modify
+    #       the capped collection size
+    # find the latest 50(default) classifications
+    # and mark any with an id > the last one
+    # for removal and thus cleanup
+    user_project_recent_scope = Recent.where(
+      project_id: classification.project_id,
+      user_id: classification.user_id
+    ).order(id: 'ASC')
+    last_id = user_project_recent_scope.limit(1).pluck(:id)
+    user_project_recent_scope.where("id > ?", last_id).update_all(mark_remove: true)
   end
 end
--- spec/workers/recent_create_worker_spec.rb
+++ spec/workers/recent_create_worker_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe RecentCreateWorker do
       .with(classification)
     subject.perform(classification.id)
   end
+
+  it 'should mark all recents past the capped ammount for deletion' do
+    # TODO: allow the capped collection size
+    # to be stubbed to only have to cleanup 1 out of 2
+    classification = create(:classification_with_recents)
+    classification.recents.last.id
+    recent_to_be_marked = classification.recents.last.id
+    expect {
+      subject.perform(classification.id)
+    }.to change {
+        Recent.where(
+          id: recent_to_be_marked,
+          mark_remove: true
+        ).count
+      }.from(0).to(1)
+  end
+
 end
