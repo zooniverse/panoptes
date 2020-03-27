336cf8cb9b0062a89873142b349275504f4f7a2b
Campbell Allen <campbell.allen@gmail.com>
2020-03-27 18:21:43 +0000

    add mark_remove column to recents
    start work on ability to mark and sweep old recents (capped collection style)

---
+++ db/migrate/20200327164455_add_mark_remove_column_to_recents.rb
@@ -0,0 +1,17 @@
+class AddMarkRemoveColumnToRecents < ActiveRecord::Migration
+  disable_ddl_transaction!
+  def up
+    add_column :recents, :mark_remove, :boolean
+    change_column_default :recents, :mark_remove, false
+    remove_foreign_key :recents, :classifications
+
+    Recent.find_in_batches do |recents|
+      recents.update_all(mark_remove: false)
+      sleep(0.01) # throttle the updates to the table
+    end
+  end
+
+  def down
+    remove_column :recents, :mark_remove
+  end
+end
