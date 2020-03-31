2457bbf679190402e42547020d95c85caa60a5da
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2020-03-31 18:09:51 -0500

    storage.yml

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,14 +1,12 @@
 module Panoptes
   module StorageAdapter
     def self.configuration
-      return @configuration if @configuration
-      begin
-        file = Rails.root.join('config/storage.yml')
-        @configuration = YAML.load(File.read(file))[Rails.env].symbolize_keys
-        @configuration.freeze
-      rescue Errno::ENOENT, NoMethodError
-        {adapter: "default"}
-      end
+      @configuration ||=
+        {
+          adapter: ENV['STORAGE_ADAPTER'] || 'test',
+          bucket: ENV['STORAGE_BUCKET'],
+          prefix: ENV['STORAGE_PREFIX']
+        }
     end
   end
 end
