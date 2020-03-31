d890d92c09bcf0bbc4497a76a0ac57c40c6decdd
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2020-03-31 15:42:37 -0500

    cors_config.yml

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,30 @@
 require 'ostruct'
 module Panoptes
   def self.cors_config
-    @cors_config ||=OpenStruct
-      .new(**begin
-        file = Rails.root.join('config/cors_config.yml')
-        YAML.load(File.read(file))[Rails.env].symbolize_keys
-      rescue Errno::ENOENT, NoMethodError
-        {  }
-      end)
+    @cors_config ||= OpenStruct.new({
+                      headers: :any,
+                      request_methods: %w[ delete get post options put head ],
+                      expose: %w[ ETag X-CSRF-Param X-CSRF-Token ],
+                      max_age: 300,
+                      allows: [
+                        { "origins" => '*', "resource" => '/api/*' },
+                        { "origins" => '*', "resource" => '/graphql' },
+                        {
+                          "origins" => ENV['CORS_ORIGINS_USERS'] || default_origins,
+                          "resource" => '/users*',
+                          "credentials" => true
+                        },
+                        {
+                          "origins" => ENV['CORS_ORIGINS_OAUTH'] || default_origins,
+                          "resource" => '/oauth/*',
+                          "credentials" => true
+                        }
+                      ]
+                    })
+  end
+
+  def self.default_origins
+    /^https?:\/\/(127\.0\.0\.1|localhost|[a-z0-9-]+\.local)(:\d+)?$/
   end
 end
 
