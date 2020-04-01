8f79eb85d8a73ed06676e839f84f2df754bece6c
Campbell Allen <campbell.allen@gmail.com>
2020-04-01 19:04:59 +0100

    add spec for flow control with yield

--- spec/models/csv_dumps/dump_processor_spec.rb
+++ spec/models/csv_dumps/dump_processor_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe CsvDumps::DumpProcessor do
   let(:formatter) { double("Formatter", headers: false).tap { |f| allow(f).to receive(:to_rows) { |model| [model] } } }
   let(:scope) { [] }
   let(:medium) { double("Medium", put_file: true, metadata: {}, save!: true) }
-  let(:csv_dump) { double(CsvDump, cleanup!: true, gzip!: true) }
+  let(:csv_dump) { double(CsvDump, cleanup!: true, gzip!: true, '<<': true) }
   let(:processor) { described_class.new(formatter, scope, medium, csv_dump) }
 
   let(:project_file_name) do
@@ -53,6 +53,24 @@ RSpec.describe CsvDumps::DumpProcessor do
       allow(formatter).to receive(:headers).and_raise("something")
       expect(medium).not_to receive(:save!)
       expect { processor.execute }.to raise_error("something")
+    end
+  end
+
+  # rubocop:disable RSpec/MultipleExpectations
+  it 'passes the provided block to perform_dump' do
+    my_proc = proc { }
+    expect(processor).to receive(:execute) do |&block|
+      expect(block).to be(my_proc)
+    end
+    processor.execute(&my_proc)
+  end
+  # rubocop:enable RSpec/MultipleExpectations
+
+  describe '#perform_dump' do
+    let(:scope) { [Classification.new] }
+
+    it 'yields with the formatter if a block is passed' do
+      expect { |b| processor.perform_dump(&b) }.to yield_with_args
     end
   end
 end
