3d84dbc4969251b6fc71068c50da85a09efd789e
Campbell Allen <campbell.allen@gmail.com>
2020-04-02 18:34:02 +0100

    streamline kaminari max page limit

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,13 +1,3 @@
-module Panoptes
-  def self.page_size_limits
-    @page_limits ||= { max: ENV['PAGE_SIZE_LIMIT'] || 100 }
-  end
-
-  def self.max_page_size_limit
-    page_size_limits[:max]
-  end
-end
-
 Kaminari.configure do |config|
-  config.max_per_page = Panoptes.max_page_size_limit || nil
+  config.max_per_page = ENV.fetch('PAGE_SIZE_LIMIT', 100)
 end
