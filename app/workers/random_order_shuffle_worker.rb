df1f37ee8de60a986f419db4f76d9b48c150e614
Campbell Allen <campbell.allen@gmail.com>
2020-04-02 19:04:28 +0100

    streamline selection config and inline to worker

--- app/workers/random_order_shuffle_worker.rb
+++ app/workers/random_order_shuffle_worker.rb
@@ -1,8 +1,16 @@
 class RandomOrderShuffleWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :data_medium,
-    congestion: Panoptes::SubjectSelection.random_order_shuffle_worker_opts
+  sidekiq_options(
+    queue: :data_medium,
+    congestion:
+      {
+        interval: ENV.fetch('SELECTION_RANDOM_INTERVAL', 60),
+        max_in_interval: ENV.fetch('SELECTION_RANDOM_MAX_IN_INTERVAL', 1),
+        min_delay: ENV.fetch('SELECTION_RANDOM_MIN_DELAY', 30),
+        reject_with: :cancel
+      }
+  )
 
   def perform(sms_ids)
     SetMemberSubject.where(id: Array.wrap(sms_ids)).update_all("random = RANDOM()")
--- config/initializers/subject_selection_config.rb
+++ config/initializers/subject_selection_config.rb
@@ -1,32 +1,13 @@
+# frozen_string_literal: true
+
 module Panoptes
   module SubjectSelection
-    def self.config
-      @config ||=
-        {
-          focus_set_window_size: ENV['SELECTION_FOCUS_SET_WINDOW_SIZE'] || 1000,
-          index_rebuild_rate: ENV['SELECTION_INDEX_REBUILD_RATE'] || 0.01,
-          random_order_shuffle_worker_opts:
-            {
-              interval: ENV['SELECTION_RANDOM_INTERVAL'] || 60,
-              max_in_interval: ENV['SELECTION_RANDOM_MAX_IN_INTERVAL'] || 1,
-              MIN_DELAY: ENV['selection_RANDOM_MIN_DELAY'] || 30,
-              reject_with: :cancel
-            }
-        }
-    end
-
     def self.focus_set_window_size
-      config[:focus_set_window_size]
+      ENV.fetch('SELECTION_FOCUS_SET_WINDOW_SIZE', 1000)
     end
 
     def self.index_rebuild_rate
-      config[:index_rebuild_rate]
-    end
-
-    def self.random_order_shuffle_worker_opts
-      config.fetch(:random_order_shuffle_worker_opts, {}).symbolize_keys
+      ENV.fetch('SELECTION_INDEX_REBUILD_RATE', 0.01)
     end
   end
 end
-
-Panoptes::SubjectSelection.config
