1fd1ae6af83dcbeac201c3621b96444072a705e9
Campbell Allen <campbell.allen@gmail.com>
2020-04-02 18:46:42 +0100

    streamline frontend redirect urls

--- config/initializers/redirect_urls.rb
+++ config/initializers/redirect_urls.rb
@@ -1,27 +1,13 @@
 module Panoptes
-  def self.frontend_configuration
-    @frontend_conf ||= {
-      base: ENV['REDIRECTS_BASE'] || 'http://localhost:2727',
-      password_reset: ENV['REDIRECTS_PASSWORD_RESET'] || '/#/reset-password',
-      unsubscribe: ENV['REDIRECTS_UNSUBSCRIBE'] || '/#/unsubscribe'
-    }
-  end
-
   def self.frontend_url
-    frontend_configuration[:base]
+    ENV.fetch('REDIRECTS_BASE', 'http://localhost:2727')
   end
 
   def self.password_reset_redirect
-    if reset = frontend_configuration[:password_reset]
-      "#{frontend_url}#{reset}"
-    end
+    "#{frontend_url}#{ENV.fetch('REDIRECTS_PASSWORD_RESET', '/#/reset-password')}"
   end
 
   def self.unsubscribe_redirect
-    if unsubscribe = frontend_configuration[:unsubscribe]
-      "#{frontend_url}#{unsubscribe}"
-    end
+    "#{frontend_url}#{ENV.fetch('REDIRECTS_UNSUBSCRIBE', '/#/unsubscribe')}"
   end
 end
-
-Panoptes.frontend_configuration
