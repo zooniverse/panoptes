3b16d21f66d595d42b767642849d7456db721770
Campbell Allen <campbell.allen@gmail.com>
2020-04-09 20:15:27 +0100

    mark and sweep all old recents
    remove all recents that are older than a period (14 days) during execution

--- app/workers/recent_sweeper_worker.rb
+++ app/workers/recent_sweeper_worker.rb
@@ -10,11 +10,24 @@ class RecentSweeperWorker
   recurrence { hourly }
 
   def perform
+    # mark any recents older than 14 days, keep the recents table clean
+    mark_old_recents(Recent.first_older_than(14.days))
+
     # https://api.rubyonrails.org/classes/ActiveRecord/Batches.html#method-i-in_batches
     # move this method to in_batches when migrating past rails 4
     recent_ids_to_delete = Recent.where(mark_remove: true).select(:id)
     recent_ids_to_delete.find_in_batches do |recents|
       Recent.where(id: recents).destroy_all
+    end
+  end
+
+  def mark_old_recents(old_recent)
+    return unless old_recent
+
+    old_recents = Recent.where('id <= ?', old_recent.id).select(:id)
+
+    old_recents.find_in_batches do |recents|
+      Recent.where(id: recents).update_all(mark_remove: true)
     end
   end
 end
--- spec/workers/recent_sweeper_worker_spec.rb
+++ spec/workers/recent_sweeper_worker_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe RecentSweeperWorker do
     expect(worker.class.schedule.to_s).to eq('Hourly')
   end
 
-  it 'removes all recents marked for removal' do
+   it 'removes all recents marked for removal' do
     expect {
       worker.perform
     }.to change {
@@ -31,5 +31,15 @@ RSpec.describe RecentSweeperWorker do
     }.not_to change {
       Recent.where(mark_remove: false).count
     }
+  end
+
+  context 'with a recent older than 14 days' do
+    it 'sweeps it up' do
+      old_recent = create(:recent) do |r|
+        r.update_column(:created_at, Time.now.utc - 15.days)
+      end
+      worker.perform
+      expect { old_recent.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
 end
