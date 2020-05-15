0584a81bf2513675414bc5b97067404c6994328f
Nicole Ciemniak <nciemniak@gmail.com>
2020-05-15 11:52:56 -0500

    add cam's comment about duplicate favs

--- lib/validators/one_favorite_per_owner_validator.rb
+++ lib/validators/one_favorite_per_owner_validator.rb
@@ -1,6 +1,10 @@
 module Validators
-  # Validator for Collection model
-  # Validate that only one favorite collection is created per owner for each project
+  # validator for Collection model
+  # validate that only one favorite collection is created per owner for each project.
+  # this validation technique may allow a duplicate favorite record be inserted into the database
+  # due to the db the read isolation level of this transaction vs concurrent transactions
+  # and the fact our validations exist in ruby land and not strictly enforced at the database layer
+  # it's low probablity but worth noting here if folks wonder how it can happen
   class OneFavoritePerOwnerValidator < ActiveModel::Validator
     def validate(new_record)
       return unless new_record.owner && new_record.favorite
