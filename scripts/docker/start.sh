75472bbe63339b6852f77615aaa7a322b9f97fe7
Campbell Allen <campbell.allen@gmail.com>
2020-05-20 09:31:47 +0100

    remove AWS emergency mode cruft
    not used in k8s env

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -15,13 +15,6 @@ rm -f tmp/pids/*.pid
 if [ "$RAILS_ENV" == "development" ]; then
   exec bundle exec foreman start
 else
-  USER_DATA=$(curl --fail http://169.254.169.254/latest/user-data || echo "")
-
-  if [ "$USER_DATA" == "EMERGENCY_MODE" ]
-  then
-    git pull
-  fi
-
   if [ ! -d public/assets ]
   then
       bundle exec rake assets:precompile
