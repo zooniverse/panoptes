a2534f0bdc6645aa32f79d90eb6788b3fcd41a64
Campbell Allen <campbell.allen@gmail.com>
2020-06-28 23:14:42 +0100

    gracefull fallback for missing cache data

--- lib/formatter/caching.rb
+++ lib/formatter/caching.rb
@@ -12,15 +12,13 @@ module Formatter
     private
 
     def respond_to_missing?(name, include_private=false)
-      return true if @cache_resource.respond_to?(name, include_private)
-
       @formatter.respond_to?(name, include_private)
     end
 
     # rubocop:disable Style/MethodMissingSuper
-    def method_missing(method, *args, &block)
+    def method_missing(method, *_args, &_block)
       # look in the cache_resource data hash for the attribute name
-      if (cache_resource_value = @cache_resource.data[method.to_s])
+      if (cache_resource_value = @cache_resource&.data&.dig(method.to_s))
         cache_resource_value
       else
         # fallback to the formatter if we didn't find the data in the cache
--- spec/lib/formatter/caching_spec.rb
+++ spec/lib/formatter/caching_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Formatter::Caching do
   let(:formatter) do
     double(
       'Formatter',
-      cached_attribute: 'this should not be returned',
+      cached_attribute: 'fallback when cache data missing',
       non_cached_attribute: 'this is from the formatter'
     )
   end
@@ -22,5 +22,21 @@ RSpec.describe Formatter::Caching do
 
   it 'returns formatter value when no matching attribue in cache resource' do
     expect(caching_formatter.non_cached_attribute).to eq('this is from the formatter')
+  end
+
+  context 'when no cached_resource data' do
+    let(:cache_resource) { instance_double('CachedExport', data: nil) }
+
+    it 'safely fallsback to formatter' do
+      expect(caching_formatter.cached_attribute).to eq('fallback when cache data missing')
+    end
+  end
+
+  context 'when no cached_resource' do
+    let(:caching_formatter) { described_class.new(nil, formatter) }
+
+    it 'safely fallsback to formatter' do
+      expect(caching_formatter.cached_attribute).to eq('fallback when cache data missing')
+    end
   end
 end
