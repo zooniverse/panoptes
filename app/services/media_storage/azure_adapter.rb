9dfe02b1ad89a92cab3324a541f1273b53e88000
Nicole Ciemniak <nciemniak@gmail.com>
2020-07-02 11:52:38 -0500

    switch to using signed_uri method

--- app/services/media_storage/azure_adapter.rb
+++ app/services/media_storage/azure_adapter.rb
@@ -4,7 +4,7 @@ module MediaStorage
 
     def initialize(opts={})
       @storage_account_name = opts[:storage_account_name]
-      @container_name = opts[:storage_container] || Rails.env
+      @container = opts[:storage_container] || Rails.env
       @get_expiration = opts.dig(:expiration, :get) || DEFAULT_EXPIRES_IN
       @put_expiration = opts.dig(:expiration, :put) || DEFAULT_EXPIRES_IN
 
@@ -12,14 +12,13 @@ module MediaStorage
         storage_account_name: opts[:storage_account_name],
         storage_access_key: opts[:storage_access_key]
       )
-      @sas_generator = Azure::Storage::Common::Core::Auth::SharedAccessSignature.new(
+      @signer = Azure::Storage::Common::Core::Auth::SharedAccessSignature.new(
         opts[:storage_account_name],
         opts[:storage_access_key]
       )
     end
 
     def stored_path(content_type, medium_type, *path_prefix)
-      # do we want to change anything about how the stored_path is generated? this is copied from aws
       extension = get_extension(content_type)
       path = "#{medium_type}/"
       path += "#{path_prefix.join('/')}/" unless path_prefix.empty?
@@ -27,43 +26,39 @@ module MediaStorage
     end
 
     def get_path(path, opts={})
-      # to do: add in expiration and other opts
-      # to do: narrow down which permissions we want to provide
       expires_in = opts[:get_expires] || @get_expiration # time in minutes
       expiry_time = get_expiry_time(expires_in)
-      sas_token = @sas_generator.generate_service_sas_token(
-        path,
+
+      @signer.signed_uri(
+        generate_uri(path), false,
         service: 'b', # blob
-        resource: 'b', # blob
         permissions: 'rcw', # read create write
-        expiry: expiry_time
+        expiry: expiry_time,
       )
-      generate_sas_url(path, sas_token)
     end
 
     def put_path(path, opts={})
       content_type = opts[:content_type]
       expires_in = opts[:put_expires] || @put_expiration # time in minutes
       expiry_time = get_expiry_time(expires_in)
-      sas_token = @sas_generator.generate_service_sas_token(
-        path,
+
+      @signer.signed_uri(
+        generate_uri(path), false,
         service: 'b', # blob
-        resource: 'b', # blob
         permissions: 'rcw', # read create write
         expiry: expiry_time,
         content_type: content_type
       )
-      generate_sas_url(path, sas_token)
     end
 
     def put_file(path, file_path, opts={})
       # to do: implement options
       content = get_file_contents file_path
-      @client.create_block_blob(@container_name, path, content)
+      @client.create_block_blob(@container, path, content)
     end
 
     def delete_file(path)
-      @client.delete_blob(@container_name, path)
+      @client.delete_blob(@container, path)
     end
 
     def encrypted_bucket?
@@ -81,11 +76,11 @@ module MediaStorage
 
     # @param expires_in [int]: time increment in minutes
     def get_expiry_time(expires_in)
-      Time.now.utc.iso8601 + expires_in.minutes # required format is UTC time zone, ISO 8601
+      Time.now.utc.advance(minutes: expires_in).iso8601 # required format is UTC time zone, ISO 8601
     end
 
-    def generate_sas_url(path, sas_token)
-      "https://#{@storage_account_name}.blob.core.windows.net/#{path}?#{sas_token}"
+    def generate_uri(path)
+      URI("https://#{@storage_account_name}.blob.core.windows.net/#{@container}/#{path}")
     end
   end
 end
