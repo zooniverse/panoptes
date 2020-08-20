713fc2b851092ea5074a9f505a14a67c0929e1ed
Campbell Allen <campbell.allen@gmail.com>
2020-08-20 17:49:40 +0100

    add stored path media_url constructor

--- spec/services/media_storage/stored_path_spec.rb
+++ spec/services/media_storage/stored_path_spec.rb
@@ -3,30 +3,41 @@
 require 'spec_helper'
 
 RSpec.describe MediaStorage::StoredPath do
-  describe '.url_from_src', :focus do
+  describe '.media_url' do
+    let(:url) { 'https://media.storage.domain/path' }
+    let(:result) do
+      described_class.media_url(url, stored_path)
+    end
+
     context 'with a migrated aws stored path' do
-      let(:stored_path) { 'panoptes-uploads.zooniverse.org/production/user_avatar/1e5fc9b5-86f1-4df3-986f-549f02f969a5.jpeg' }
-      let(:result) { described_class.url_from_src(stored_path) }
+      let(:stored_path) do
+        'panoptes-uploads.zooniverse.org/production/user_avatar/1e5fc9b5-86f1-4df3-986f-549f02f969a5.jpeg'
+      end
+      let(:expected_url) do
+        "#{url}/production/user_avatar/1e5fc9b5-86f1-4df3-986f-549f02f969a5.jpeg"
+      end
 
       it 'returns a url without the old path prefix' do
-        expect(result).not_to include('panoptes-uploads.zooniverse.org/production/')
+        expect(result).not_to include('panoptes-uploads.zooniverse.org/')
       end
 
       it 'returns the url with our custom domain path prefix' do
-        expect(result).to include('https://custom-domain.org/')
+        expect(result).to include(url)
       end
 
       it 'returns the expected url' do
-        expect(result).to eq('https://custom-domain.org/user_avatar/1e5fc9b5-86f1-4df3-986f-549f02f969a5.jpeg')
+        expect(result).to eq(expected_url)
       end
     end
 
     context 'with an azure native stored path (no old aws prefixes)' do
-      let(:stored_path) { 'subject_location/f2eb4dbc-1353-4598-b1f6-3bf9e9a14169.jpeg' }
-      let(:result) { described_class.url_from_src(stored_path) }
+      let(:stored_path) { 'production/subject_location/f2eb4dbc-1353-4598-b1f6-3bf9e9a14169.jpeg' }
+      let(:expected_url) do
+        "#{url}/production/subject_location/f2eb4dbc-1353-4598-b1f6-3bf9e9a14169.jpeg"
+      end
 
       it 'returns the expected url' do
-        expect(result).to eq('https://custom-domain.org/subject_location/f2eb4dbc-1353-4598-b1f6-3bf9e9a14169.jpeg')
+        expect(result).to eq(expected_url)
       end
     end
   end
