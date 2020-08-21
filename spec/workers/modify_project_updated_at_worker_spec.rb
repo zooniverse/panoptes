c9e2e5c86931edc7ce39153d019469aa6dfdf31c
Will Granger <twgranger@gmail.com>
2020-08-21 09:18:47 -0500

    Resolve small linter issues

--- spec/workers/modify_project_updated_at_worker_spec.rb
+++ spec/workers/modify_project_updated_at_worker_spec.rb
@@ -1,17 +1,16 @@
 require 'spec_helper'
 
 describe ModifyProjectUpdatedAtWorker do
-    let(:worker) { described_class.new }
-    let(:project) { create :project }
+  let(:worker) { described_class.new }
+  let(:project) { create :project }
 
-    describe 'project touch timestamp' do
-        before do
-          allow(Project).to receive(:find).and_return(project)
-        end
-    
-        it 'touches the project timestamp on update' do
-          expect(project).to have_received(:touch).once
-          worker.perform(project)
-        end
+  describe 'project touch timestamp' do
+    before do
+      allow(Project).to receive(:find).and_return(project)
     end
+    it 'touches the project timestamp on update' do
+      expect(project).to have_received(:touch).once
+      worker.perform(project)
+     end
+  end
 end
