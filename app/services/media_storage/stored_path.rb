1fabe3c3ae29955d07c8fdb82b273381660bbcaa
nciemniak <nciemniak@gmail.com>
2020-10-20 12:53:00 -0500

    Remove env prefix from stored path if present (#3488)

--- app/services/media_storage/stored_path.rb
+++ app/services/media_storage/stored_path.rb
@@ -7,14 +7,20 @@ module MediaStorage
     # but leave intact any non old s3 URL, i.e. new azure ones
     def self.media_url(url, stored_path)
       uri = URI("https://#{stored_path}")
+      # if the parse succeeds, we know we have a valid domain prefix,
+      # i.e. it is an old s3 stored path - hence, we need to rewrite the url
       PublicSuffix.parse(uri.host)
-      # we have a valid domain prefix here so remove it
-      # to allow us to construct the URL correctly
-      File.join(url, uri.path)
+      uri_path = uri.path
+
+      env_prefix = '/' + Rails.env
+      # remove env prefix if present
+      uri_path = uri_path.sub(env_prefix, '') if uri_path.start_with? env_prefix
+
+      File.join(url, uri_path)
     rescue PublicSuffix::DomainNotAllowed
       # failure here indicates we do not have
       # a valid domain prefix in the stored path
-      # e.g. production/user_avatar/1e5fc9b5-86f1-4df3-986f-549f02f969a5.jpeg
+      # e.g. /user_avatar/1e5fc9b5-86f1-4df3-986f-549f02f969a5.jpeg
       # so we do not need to rewrite the URL
       File.join(url, stored_path)
     end
