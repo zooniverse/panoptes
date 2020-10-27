43921bab5a65c530f1c5cc24774e0d4d4d0b205e
Campbell Allen <campbell.allen@gmail.com>
2020-10-27 09:54:12 +0000

    control ratio of real and training subjects in fallback selection (#3486)
    * control ratio of real and training subjects in fallback selection
    
    by default provide at least 1 training subject for every 10 real subjects in fallback selection
    
    avoid showing the user mostly training subjects
    
    * change to denominator wording
    
    clarify the use of the training subjects limit as a denominator used as a ratio on the supplied limit
    
    * respect the original request limit
    
    * respect request limit when no training data exists

--- lib/subjects/fallback_selection.rb
+++ lib/subjects/fallback_selection.rb
@@ -2,30 +2,73 @@ module Subjects
   class FallbackSelection
     attr_reader :workflow, :limit, :options
 
+    def self.num_training_subjects_denominator
+      ENV.fetch('FALLBACK_NUM_TRAINING_SUBJECTS_DENOMINATOR', 10)
+    end
+
     def initialize(workflow, limit, options = {})
       @workflow, @limit, @options = workflow, limit, options
     end
 
     def any_workflow_data
-      any_workflow_data_scope
-        .limit(limit)
-        .pluck("set_member_subjects.subject_id")
-        .shuffle
+      if workflow.grouped
+        fallback_grouped_selection.shuffle
+      else
+        fallback_selection.shuffle
+      end
     end
 
     private
 
-    def any_workflow_data_scope
-      scope = workflow.set_member_subjects
-      if workflow.grouped
-        if subject_set_id = options[:subject_set_id]
-          scope = scope.where(subject_set_id: subject_set_id)
-        else
-          msg = "subject_set_id parameter missing for grouped workflow"
-          raise Subjects::Selector::MissingParameter.new(msg)
-        end
+    def fallback_grouped_selection
+      unless (subject_set_id = options[:subject_set_id])
+        raise Subjects::Selector::MissingParameter, 'subject_set_id parameter missing for grouped workflow'
       end
-      scope
+
+      workflow
+        .set_member_subjects
+        .where(subject_set_id: subject_set_id)
+        .limit(limit)
+        .pluck('set_member_subjects.subject_id')
+    end
+
+    def fallback_selection
+      selected_training_subject_ids = training_subject_ids
+      if selected_training_subject_ids.empty?
+        non_training_subject_ids
+      else
+        # respect the request limit and return the correct mix of
+        # training and non-training subject ids
+        selected_non_training_subject_ids = non_training_subject_ids.take(non_training_limit)
+        selected_non_training_subject_ids | selected_training_subject_ids
+      end
+    end
+
+    def non_training_subject_ids
+      SetMemberSubject
+        .where(subject_set_id: workflow.non_training_subject_set_ids)
+        .limit(limit)
+        .pluck('set_member_subjects.subject_id')
+    end
+
+    def training_subject_ids
+      return [] if workflow.training_set_ids.empty?
+
+      SetMemberSubject
+        .where(subject_set_id: workflow.training_set_ids)
+        .limit(training_limit)
+        .pluck('set_member_subjects.subject_id')
+    end
+
+    # by default a ~10:1 ratio of real subjects to training
+    # modify this ration by num_training_subjects_limit class method
+    def training_limit
+      @training_limit ||= (limit.to_f / self.class.num_training_subjects_denominator).round
+    end
+
+    # respect the original request limit
+    def non_training_limit
+      limit - training_limit
     end
   end
 end
