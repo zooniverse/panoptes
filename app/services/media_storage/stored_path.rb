6287de9674f6c684c522e432a0016ff36ad27532
Nicole Ciemniak <nciemniak@gmail.com>
2020-11-03 15:13:09 -0600

    add tests update comments

--- app/services/media_storage/stored_path.rb
+++ app/services/media_storage/stored_path.rb
@@ -1,21 +1,26 @@
 # frozen_string_literal: true
 
 module MediaStorage
+  # StoredPath: module used by AzureAdapter for constructing the
+  # stored path/url of the media resource in azure
+  #
+  # Old S3 paths will include the panoptes-uploads domain prefix
+  # and the environment in their path, for example:
+  # panoptes-uploads.zooniverse.org/staging/subject_location/49f02f969a5.jpeg
+  # These URLs need to be rewritten such that domain prefix and env get removed
+  # Azure native paths do not need to be rewritten
   module StoredPath
-    # Construct the media access URL
-    # and ensure we remove the old aws s3 domain prefix in the stored paths
-    # but leave intact any non old s3 URL, i.e. new azure ones
+    # Construct path and join to the passed in URL
     def self.media_url(url, stored_path)
       uri = URI("https://#{stored_path}")
       # if the parse succeeds, we know we have a valid domain prefix,
-      # i.e. it is an old s3 stored path - hence, we need to rewrite the url
+      # i.e. it is an old s3 stored path and we need to rewrite it
       PublicSuffix.parse(uri.host)
 
       uri_path = path_without_env_prefix(uri.path)
       File.join(url, uri_path)
     rescue PublicSuffix::DomainNotAllowed
-      # failure here indicates we do not have
-      # a valid domain prefix in the stored path
+      # if parse fails, we do not have a valid domain prefix in the stored path
       # e.g. /user_avatar/1e5fc9b5-86f1-4df3-986f-549f02f969a5.jpeg
       # so we do not need to rewrite the URL
       File.join(url, stored_path)
@@ -23,19 +28,15 @@ module MediaStorage
 
     def self.media_path(stored_path)
       uri = URI("https://#{stored_path}")
-      # if the parse succeeds, we know we have a valid domain prefix,
-      # i.e. it is an old s3 stored path - hence, we need to extract just the azure path
       PublicSuffix.parse(uri.host)
+      # S3 path
       path_without_env_prefix(uri.path)
     rescue PublicSuffix::DomainNotAllowed
-      # failure here indicates we do not have a valid domain prefix and dont need to rewrite
-      # return path as is
+      # azure path
       stored_path
     end
 
-    private
-
-    def path_without_env_prefix(uri_path)
+    private_class_method def self.path_without_env_prefix(uri_path)
       # remove env prefix if present
       env_prefix = '/' + Rails.env
       uri_path.sub(env_prefix, '') if uri_path.start_with? env_prefix
