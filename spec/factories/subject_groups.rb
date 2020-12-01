1c08b3625bcde012031d31a9744878f3d1489e2c
Campbell Allen <campbell.allen@gmail.com>
2020-12-01 17:38:14 +0000

    use matching classname to make association pre-loading work

--- spec/factories/subject_groups.rb
+++ spec/factories/subject_groups.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
 
     after(:build) do |subject_group|
       member = build(:subject_group_member, subject_group: subject_group)
-      subject_group.members << member
+      subject_group.subject_group_members << member
     end
   end
 end
