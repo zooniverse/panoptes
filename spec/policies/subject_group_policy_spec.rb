3972cab8349f761e3f32ad9f2a6b1799f355e0cc
Campbell Allen <campbell.allen@gmail.com>
2020-12-02 16:04:09 +0000

    add SubjectGroup resource access policies

---
+++ spec/policies/subject_group_policy_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe SubjectGroupPolicy do
+  describe 'scopes' do
+    let(:anonymous_user) { nil }
+    let(:logged_in_user) { create(:user) }
+    let(:resource_owner) { create(:user) }
+
+    let(:public_project)  { build(:project, owner: resource_owner) }
+    let(:private_project) { build(:project, owner: resource_owner, private: true) }
+
+    let(:public_subject_group) { build(:subject_group, project: public_project) }
+    let(:private_subject_group) { build(:subject_group, project: private_project) }
+
+    let(:resolved_scope) do
+      Pundit.policy!(api_user, SubjectGroup).scope_for(:index)
+    end
+
+    before do
+      public_subject_group.save!
+      private_subject_group.save!
+    end
+
+    context 'for an anonymous user' do
+      let(:api_user) { ApiUser.new(anonymous_user) }
+
+      it 'includes subject_groups from public projects' do
+        expect(resolved_scope).to match_array(public_subject_group)
+      end
+    end
+
+    context 'for a normal user' do
+      let(:api_user) { ApiUser.new(logged_in_user) }
+
+      it 'includes public_subject_groups from public projects' do
+        expect(resolved_scope).to match_array(public_subject_group)
+      end
+    end
+
+    context 'for the resource owner' do
+      let(:api_user) { ApiUser.new(resource_owner) }
+
+      it 'includes public_subject_groups from public projects' do
+        expect(resolved_scope).to include(public_subject_group)
+      end
+
+      it 'includes public_subject_groups from owned private projects' do
+        expect(resolved_scope).to include(private_subject_group)
+      end
+    end
+
+    context 'for an admin' do
+      let(:admin_user) { create :user, admin: true }
+      let(:api_user) { ApiUser.new(admin_user, admin: true) }
+
+      it 'includes everything' do
+        expect(resolved_scope).to include(public_subject_group, private_subject_group)
+      end
+    end
+  end
+end
