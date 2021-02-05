6f408ddf3d3bca7e5229be2edda3bf236ce4cba4
Campbell Allen <campbell.allen@gmail.com>
2021-02-05 13:37:07 +0000

    fix translations when project copying
    ensure we correctly setup the translations when copying a project.

--- lib/project_copier.rb
+++ lib/project_copier.rb
@@ -16,15 +16,27 @@ class ProjectCopier
 
     copied_project = project.deep_clone include: INCLUDE_ASSOCIATIONS, except: EXCLUDE_ATTRIBUTES
     copied_project.owner = user
-    copied_project.configuration.delete(:template)
 
     if user == project.owner
       copied_project.display_name += " (copy)"
     end
 
     copied_project.assign_attributes(launch_approved: false, live: false)
-    copied_project.configuration[:source_project_id] = project.id
-    copied_project.save!
+    # reset the project's configuration but record the source project id
+    copied_project.configuration = { source_project_id: project.id }
+    # reset the copied translations to not have any old string versions
+    copied_project.translations.each { |tr| tr.string_versions = {} }
+
+    Project.transaction(requires_new: true) do
+      # save the project and create the project versions for use in translation strings
+      copied_project.save!
+      # update all the translation strings versions to match the latest project_version resource
+      copied_project.translations.each do |translation|
+        translated_strings = TranslationStrings.new(copied_project).extract
+        translation.update_strings_and_versions(translated_strings, copied_project.latest_version_id)
+        translation.save!
+      end
+    end
     copied_project
   end
 end
