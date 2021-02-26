66a1a1f04f9a66622cd564ed5f9581d288726b37
Campbell Allen <campbell.allen@gmail.com>
2021-02-26 18:41:35 +0000

    make FK change migration reversible

--- db/migrate/20210226173243_add_on_delete_constraint_to_subject_sets_import.rb
+++ db/migrate/20210226173243_add_on_delete_constraint_to_subject_sets_import.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 class AddOnDeleteConstraintToSubjectSetsImport < ActiveRecord::Migration
   def change
-    remove_foreign_key :subject_set_imports, :subject_sets
-    add_foreign_key :subject_set_imports, :subject_sets, on_update: :cascade, on_delete: :cascade, validate: false
+    reversible do |dir|
+      dir.up do
+        remove_foreign_key :subject_set_imports, :subject_sets
+        add_foreign_key :subject_set_imports, :subject_sets, on_update: :cascade, on_delete: :cascade, validate: false
+      end
+
+      dir.down do
+        remove_foreign_key :subject_set_imports, :subject_sets
+        add_foreign_key :subject_set_imports, :subject_sets
+      end
+    end
   end
 end
