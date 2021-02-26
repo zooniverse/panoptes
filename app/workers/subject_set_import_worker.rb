9cbe44fb72565c63cdc471ec64f6e7023367a032
Campbell Allen <campbell.allen@gmail.com>
2021-02-26 18:07:00 +0000

    update the subject set subject counts post import (#3578)

--- app/workers/subject_set_import_worker.rb
+++ app/workers/subject_set_import_worker.rb
@@ -4,5 +4,6 @@ class SubjectSetImportWorker
   def perform(subject_set_import_id)
     subject_set_import = SubjectSetImport.find(subject_set_import_id)
     subject_set_import.import!
+    SubjectSetSubjectCounterWorker.new.perform(subject_set_import.subject_set_id)
   end
 end
