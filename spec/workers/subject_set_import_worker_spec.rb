2341d7d42cdf0e1c08e1f4ed6ea40a49ab393aaa
Campbell Allen <campbell.allen@gmail.com>
2021-02-26 17:00:58 +0000

    update the subject set subject counts post import

---
+++ spec/workers/subject_set_import_worker_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe SubjectSetImportWorker do
+  describe '#perform' do
+    let(:import_double) do
+      instance_double(SubjectSetImport, id: 1, import!: true, subject_set_id: 1)
+    end
+    let(:count_worker_double) do
+      instance_double(SubjectSetSubjectCounterWorker, perform: true)
+    end
+
+    before do
+      allow(SubjectSetImport).to receive(:find).and_return(import_double)
+      allow(SubjectSetSubjectCounterWorker).to receive(:new).and_return(count_worker_double)
+    end
+
+    it 'runs the subjet set import code' do
+      described_class.new.perform(import_double.id)
+      expect(import_double).to have_received(:import!)
+    end
+
+    it 'calls the subject set counter worker inline' do
+      described_class.new.perform(import_double.id)
+      expect(count_worker_double).to have_received(:perform).with(import_double.subject_set_id)
+    end
+  end
+end
