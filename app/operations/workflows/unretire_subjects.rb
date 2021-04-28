af738e3144666f4b4f57391f7d426a8c276e411d
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-04-28 14:43:43 -0500

    update operation class

--- app/operations/workflows/unretire_subjects.rb
+++ app/operations/workflows/unretire_subjects.rb
@@ -1,5 +1,24 @@
 module Workflows
     class UnretireSubjects < Operation
-        
+        validates :workflow_id, presence: true
+
+        integer :workflow_id
+        integer :subject_id, default: nil
+        array :subject_ids, default: [] do
+            integer
+        end
+
+        def execute
+            return if subject_ids.empty?
+            puts "MDY114 WORKFLOW ID #{workflow_id}"
+            SubjectWorkflowStatus.where.not(retired_at: nil).where(workflow_id: workflow_id, subject_id: subject_ids).update_all(retired_at: nil, retirement_reason: nil)
+            RefreshWorkflowStatusWorker.perform_async(workflow_id)
+            NotifySubjectSelectorOfChangeWorker.perform_async(workflow_id)
+        end
+
+        def subject_ids 
+            @cached_subject_ids ||= Array.wrap(@subject_ids) | Array.wrap(@subject_id)
+        end
+
     end
 end
--- spec/operations/workflows/unretire_subjects_spec.rb
+++ spec/operations/workflows/unretire_subjects_spec.rb
@@ -6,6 +6,7 @@ describe Workflows::UnretireSubjects do
     let(:subject_set) { create(:subject_set, project: workflow.project, workflows: [workflow]) }
     let(:subject_set_id) { subject_set.id }
 
-    it "should unretire given subjects with subhect ids for the workflow" do 
+    it "should unretire given subjects with subject ids for the workflow", :focus => true do 
+        
     end
 end
