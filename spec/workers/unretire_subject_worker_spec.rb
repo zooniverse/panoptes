c5ce7ea1640915a6d149fe31e98a2bc00f7ad5f3
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-04-30 14:54:20 -0500

    WIP update worker test with have_received double

--- spec/workers/unretire_subject_worker_spec.rb
+++ spec/workers/unretire_subject_worker_spec.rb
@@ -5,23 +5,35 @@ require 'spec_helper'
 RSpec.describe UnretireSubjectWorker do
   let(:worker) { described_class.new }
   let(:workflow) { create(:workflow_with_subjects, num_sets: 1) }
-  let(:subject) { workflow.subjects.first }
+  let(:subject1) { workflow.subjects.first }
   let(:sms) { subject.set_member_subject.first }
   let(:set) { sms.subject_set }
-  let(:status) { create(:subject_workflow_status, subject: subject, workflow: workflow, retired_at: 1.day.ago, retirement_reason: 'other') }
+  let(:status) { create(:subject_workflow_status, subject: subject1, workflow: workflow, retired_at: 1.day.ago, retirement_reason: 'other') }
 
   describe '#perform' do
     context 'when subjects are unretireable' do
       it 'sets retired_at to nil' do
-        expect { worker.perform(workflow.id, [subject.id]) }.to change {
+        expect { worker.perform(workflow.id, [subject1.id]) }.to change {
           status.reload.retired_at
         }.to(nil)
       end
 
       it 'sets retirement_reason to nil' do
-        expect { worker.perform(workflow.id, [subject.id]) }.to change {
+        expect { worker.perform(workflow.id, [subject1.id]) }.to change {
           status.reload.retirement_reason
         }.to(nil)
+      end
+
+      it 'calls RefreshWorkflowStatusWorker with workflow id' do
+        allow(RefreshWorkflowStatusWorker).to receive(:perform_async).and_return(true)
+        worker.perform(workflow.id, [subject1.id])
+        expect(RefreshWorkflowStatusWorker).to have_received(:perform_async).with(workflow.id)
+      end
+
+      it 'calls NotifySubjectSelectorOfChangeWorker with workflow id' do
+        allow(NotifySubjectSelectorOfChangeWorker).to receive(:perform_async).and_return(true)
+        worker.perform(workflow.id, [subject1.id])
+        expect(NotifySubjectSelectorOfChangeWorker).to have_received(:perform_async).with(workflow.id)
       end
     end
   end
