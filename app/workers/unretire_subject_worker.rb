b2819b4ab6c9abf4396d353f7093e76004958394
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-05-04 10:11:52 -0500

    remove unused vars and styling spacing

--- app/workers/unretire_subject_worker.rb
+++ app/workers/unretire_subject_worker.rb
@@ -6,7 +6,7 @@ class UnretireSubjectWorker
   sidekiq_options queue: :high
 
   def perform(workflow_id, subject_ids)
-    return unless  Workflow.exists?(id: workflow_id)
+    return unless Workflow.exists?(id: workflow_id)
 
     SubjectWorkflowStatus.where.not(retired_at: nil).where(workflow_id: workflow_id, subject_id: subject_ids).update_all(retired_at: nil,
                                                                                                                          retirement_reason: nil)
--- spec/operations/workflows/unretire_subjects_spec.rb
+++ spec/operations/workflows/unretire_subjects_spec.rb
@@ -6,10 +6,8 @@ describe Workflows::UnretireSubjects do
   let(:api_user) { ApiUser.new(build_stubbed(:user)) }
   let(:workflow) { create(:workflow) }
   let(:subject_set) { create(:subject_set, project: workflow.project, workflows: [workflow]) }
-  let(:subject_set_id) { subject_set.id }
   let(:subject1) { create(:subject, subject_sets: [subject_set]) }
   let(:subject2) { create(:subject, subject_sets: [subject_set]) }
-  let(:subject_workflow_count) { create }
   let(:params) do
     {
       workflow_id: workflow.id,
