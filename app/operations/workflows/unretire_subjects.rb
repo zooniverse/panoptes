ecf7b53c4ae46ec1644872388b42f530f429fce5
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-05-06 12:07:50 -0500

    WIPupdating operator to add to subject_ids

--- app/operations/workflows/unretire_subjects.rb
+++ app/operations/workflows/unretire_subjects.rb
@@ -8,16 +8,32 @@ module Workflows
     integer :subject_id, default: nil
     array :subject_ids, default: [] do
       integer
-    end
+    end;
+    integer :subject_set_id, default: nil
+    array :subject_set_ids, default: [] do 
+      integer
+    end;
 
     def execute
       return if cached_subject_ids.empty?
-
+      puts "MDY114 CACHED SUBJECT IDS BEFORE #{cached_subject_ids}"
       UnretireSubjectWorker.perform_async(workflow_id, cached_subject_ids)
     end
 
     def cached_subject_ids
       @cached_subject_ids ||= Array.wrap(@subject_ids) | Array.wrap(@subject_id)
+      if !subject_set_subject_ids.empty?
+        puts 'MDY114 HITS CHECK'
+        @cached_subject_ids.push(*subject_set_subject_ids)
+      end
+      return @cached_subject_ids
+    end
+
+    def subject_set_subject_ids
+      puts 'MDY114 SUBJECT SET IDS'
+
+      set_ids ||= Array.wrap(@subject_set_ids) | Array.wrap(@subject_set_id)
+      SetMemberSubject.where(subject_set_id: set_ids).pluck(:subject_id)
     end
   end
 end
