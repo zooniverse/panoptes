ca0a72311f1ef2633bc3a0ec7de653f3e424d2c9
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-05-07 09:48:30 -0500

    updating per reviews

--- app/operations/workflows/unretire_subjects.rb
+++ app/operations/workflows/unretire_subjects.rb
@@ -21,13 +21,15 @@ module Workflows
     end
 
     def cached_subject_ids
-      cached_subject_ids ||= Array.wrap(@subject_ids) | Array.wrap(@subject_id)
-      cached_subject_ids.push(*subject_set_subject_ids) unless subject_set_subject_ids.empty?
-      @cached_subject_ids = cached_subject_ids
+      @cached_subject_ids ||= subject_ids | subject_set_subject_ids
+    end
+
+    def subject_ids
+      Array.wrap(@subject_ids) | Array.wrap(@subject_id)
     end
 
     def subject_set_subject_ids
-      set_ids ||= Array.wrap(@subject_set_ids) | Array.wrap(@subject_set_id)
+      set_ids = Array.wrap(@subject_set_ids) | Array.wrap(@subject_set_id)
       SetMemberSubject.where(subject_set_id: set_ids).pluck(:subject_id)
     end
   end
