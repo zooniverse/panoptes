19010a208c045dd7c576bf95e742919197508ba2
Campbell Allen <campbell.allen@gmail.com>
2021-07-29 17:25:30 +0100

    fix linter warnings

--- spec/counters/subject_set_workflow_counter_spec.rb
+++ spec/counters/subject_set_workflow_counter_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe SubjectSetWorkflowCounter do
   let(:subject_set) { create(:subject_set_with_subjects, num_workflows: 1, num_subjects: 2) }
   let(:workflow) { subject_set.workflows.first }
-  let(:counter) { SubjectSetWorkflowCounter.new(subject_set.id, workflow.id) }
+  let(:counter) { described_class.new(subject_set.id, workflow.id) }
 
   describe 'retired_subjects' do
     it 'returns 0 if there are none' do
@@ -13,10 +13,14 @@ describe SubjectSetWorkflowCounter do
     end
 
     context 'with retired_subjects' do
-      let(:subject) { subject_set.subjects.first }
+      let(:subject_to_retire) { subject_set.subjects.first }
 
       before do
-        SubjectWorkflowStatus.create(workflow_id: workflow.id, subject_id: subject.id, retired_at: Time.now.utc)
+        SubjectWorkflowStatus.create(
+          workflow_id: workflow.id,
+          subject_id: subject_to_retire.id,
+          retired_at: Time.now.utc
+        )
       end
 
       it 'returns 1' do
