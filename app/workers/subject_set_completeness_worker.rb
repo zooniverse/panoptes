2fb86ac72bf3e980a30bc5fe3def9a37b2384798
Campbell Allen <campbell.allen@gmail.com>
2021-07-30 11:14:28 +0100

    use the readreplica via feature flag

--- app/workers/subject_set_completeness_worker.rb
+++ app/workers/subject_set_completeness_worker.rb
@@ -20,12 +20,15 @@ class SubjectSetCompletenessWorker
     subject_set = SubjectSet.find(subject_set_id)
     workflow = Workflow.find_without_json_attrs(workflow_id)
 
-    # find the count of all retired subjects in a known subject set
-    # in the context of a known workflow
-    retired_subjects_count = SubjectSetWorkflowCounter.new(subject_set.id, workflow.id).retired_subjects * 1.0
-    total_subjects_count = subject_set.set_member_subjects_count * 1.0
-    # clamp the value between 0.0 and 1.0, i.e. 0 to 100% complete
-    retired_subjects_completeness = (0.0..1.0).clamp(retired_subjects_count / total_subjects_count)
+    # find the count of all retired subjects, for a known subject set, in the context of a known workflow
+    # using the read replica if the feature flag is enabled
+    retired_subjects_completeness = 0.0
+    DatabaseReplica.read('subject_set_completeness_from_read_replica') do
+      retired_subjects_count = SubjectSetWorkflowCounter.new(subject_set.id, workflow.id).retired_subjects * 1.0
+      total_subjects_count = subject_set.set_member_subjects_count * 1.0
+      # calculate and clamp the completeness value between 0.0 and 1.0, i.e. 0 to 100%
+      retired_subjects_completeness = (0.0..1.0).clamp(retired_subjects_count / total_subjects_count)
+    end
 
     # store these per workflow completeness metric in a json object keyed by the workflow id
     # use the atomic DB json operator to avoid clobbering data in the jsonb attribute by other updates
