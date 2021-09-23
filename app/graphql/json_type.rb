7245b7bc53a120a74a0d4fac1bc03d028aa66181
Campbell Allen <campbell.allen@gmail.com>
2021-09-23 14:33:59 +0100

    remove custom JSON type

--- app/graphql/json_type.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-
-JsonType = GraphQL::ScalarType.define do
-  name "JsonType"
-  description "Arbitrary JSON object"
-
-  coerce_input ->(value, ctx) { value }
-  coerce_result ->(value, ctx) { value }
-end
