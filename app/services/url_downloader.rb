771c746dae779deb3d71a5ffc8efa69a0352e422
Campbell Allen <campbell.allen@gmail.com>
2021-10-07 13:41:16 +0100

    provide a line count of the manifest import file

--- app/services/url_downloader.rb
+++ app/services/url_downloader.rb
@@ -4,10 +4,14 @@ class UrlDownloader
       HTTParty.get(url, stream_body: true) do |fragment|
         file.write(fragment)
       end
-
+      # rewind the file post writing
+      file.rewind
+      # count the number of lines in the file (including header)
+      num_lines = file.each_line.inject(0) { |counter, _line| counter + 1 }
+      # rewind post counting lines
       file.rewind
 
-      yield file
+      yield file, num_lines
     end
   end
 end
