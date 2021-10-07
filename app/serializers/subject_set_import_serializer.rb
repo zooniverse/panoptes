40bd91090bb1f660270272fe3b7ac0f71b6ca42e
Campbell Allen <campbell.allen@gmail.com>
2021-10-07 16:05:24 +0100

    add progress metric to SubjectSetImport serailizer

--- app/serializers/subject_set_import_serializer.rb
+++ app/serializers/subject_set_import_serializer.rb
@@ -1,9 +1,17 @@
 class SubjectSetImportSerializer
   include Serialization::PanoptesRestpack
   include CachedSerializer
+  using Refinements::RangeClamping
 
-  attributes :id, :href, :created_at, :updated_at, :source_url
+  attributes :id, :href, :created_at, :updated_at, :source_url, :imported_count, :manifest_count, :progress
   can_include :subject_set, :user
 
   can_filter_by :subject_set, :user
+
+  def progress
+    binding.pry
+    # calculate and clamp the progress value between 0.0 and 1.0, i.e. 0 to 100%
+    progress = (imported_count * 1.0) / (manifest_count * 1.0)
+    (0.0..1.0).clamp(progress)
+  end
 end
