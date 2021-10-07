c274dbde047a3374cbf8b441d1af8b10bdc49ccb
Campbell Allen <campbell.allen@gmail.com>
2021-10-07 16:34:48 +0100

    add frozen strings magic comment

--- db/migrate/20211007125705_add_manifest_count_to_subject_set_import.rb
+++ db/migrate/20211007125705_add_manifest_count_to_subject_set_import.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddManifestCountToSubjectSetImport < ActiveRecord::Migration
   def change
     add_column :subject_set_imports, :manifest_count, :integer, default: 0
