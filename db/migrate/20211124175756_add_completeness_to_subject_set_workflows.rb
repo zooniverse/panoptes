980bbfea499182af2194a71a59056955a587e4a2
Campbell Allen <campbell.allen@gmail.com>
2021-11-24 19:31:37 +0000

    add magic frozen string literal

--- db/migrate/20211124175756_add_completeness_to_subject_set_workflows.rb
+++ db/migrate/20211124175756_add_completeness_to_subject_set_workflows.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddCompletenessToSubjectSetWorkflows < ActiveRecord::Migration
   def change
     add_column :subject_sets_workflows, :completeness, :decimal, default: 0.0
