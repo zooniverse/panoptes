3e2e740d69bb789706e933f1a2c37940dbdee69d
Campbell Allen <campbell.allen@gmail.com>
2021-12-01 17:20:34 +0000

    add frozen magic comment

--- db/migrate/20211201164326_add_project_run_subject_set_completion_events.rb
+++ db/migrate/20211201164326_add_project_run_subject_set_completion_events.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddProjectRunSubjectSetCompletionEvents < ActiveRecord::Migration
   def change
     add_column :projects, :run_subject_set_completion_events, :boolean, default: false
