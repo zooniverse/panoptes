7ffcca3e8a3d035217bb6ab1cbe4e29449c71f02
Campbell Allen <campbell.allen@gmail.com>
2021-12-15 15:16:42 +0000

    call the import! method with the manifest_row_count

--- spec/models/subject_set_import_spec.rb
+++ spec/models/subject_set_import_spec.rb
@@ -18,19 +18,19 @@ describe SubjectSetImport, type: :model do
   let(:subject_set_import) do
     described_class.create(source_url: source_url, subject_set: subject_set, user: user)
   end
-  let(:num_lines) { 3 }
+  let(:manifest_row_count) { 2 }
 
   before do
     allow(UrlDownloader).to receive(:stream).with(source_url).and_yield(csv_file)
   end
 
   it 'imports subjects to the set' do
-    subject_set_import.import!
+    subject_set_import.import!(manifest_row_count)
     expect(subject_set.subjects.count).to eq(2)
   end
 
   it 'stores the count of imported subjects' do
-    subject_set_import.import!
+    subject_set_import.import!(manifest_row_count)
     expect(subject_set_import.imported_count).to eq(2)
   end
 
@@ -38,7 +38,7 @@ describe SubjectSetImport, type: :model do
   it 'correctly records the progress status during import' do
     allow(subject_set_import).to receive(:save_imported_row_count)
     allow(SubjectSetImport::ProgressUpdateCadence).to receive(:calculate).and_return(2)
-    subject_set_import.import!(2)
+    subject_set_import.import!(manifest_row_count)
     # twice: once when the progress is mod 2 == 0 and once at the end
     expect(subject_set_import).to have_received(:save_imported_row_count).twice
   end
@@ -51,16 +51,16 @@ describe SubjectSetImport, type: :model do
     end
 
     it 'continues processing' do
-      expect { subject_set_import.import! }.not_to raise_error
+      expect { subject_set_import.import!(manifest_row_count) }.not_to raise_error
     end
 
     it 'stores the failed count' do
-      subject_set_import.import!
+      subject_set_import.import!(manifest_row_count)
       expect(subject_set_import.failed_count).to eq(2)
     end
 
     it 'stores the failed UUIDs' do
-      subject_set_import.import!
+      subject_set_import.import!(manifest_row_count)
       expect(subject_set_import.failed_uuids).to match_array(%w[1 2])
     end
   end
