a834885a0599ffe66fa781551adfbbcdcb918b8a
Campbell Allen <campbell.allen@gmail.com>
2021-12-16 12:03:21 +0000

    rework the block

--- app/models/user_seen_subject.rb
+++ app/models/user_seen_subject.rb
@@ -6,16 +6,11 @@ class UserSeenSubject < ActiveRecord::Base
 
   def self.add_seen_subjects_for_user(user: nil, workflow: nil, subject_ids: nil)
     newly_created = false
-    uss = where(user: user, workflow: workflow).first_or_create! do |uss_to_create|
-      newly_created = true
-      uss_to_create.user = user
-      uss_to_create.workflow = workflow
-      uss_to_create.subject_ids = subject_ids
-    end
-
+    # find or create the USS record and track if it's newly created
+    uss = where(user: user, workflow: workflow).first_or_create!(user: user, workflow: workflow, subject_ids: subject_ids) { |_| newly_created = true }
     return uss if newly_created
 
-    # existing records we need to uniquely update the subject_ids using an atomic update via update_all
+    # uniquely update the subject_ids on existing records using atomic update (update_all)
     where(user: user, workflow: workflow).update_all(['subject_ids = uniq(subject_ids + array[?])', subject_ids])
     # touch the updated_at timestamp on the record to track when the subject_ids were last modified
     uss.touch
