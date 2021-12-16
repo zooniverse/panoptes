b517eca7d10f31f45e4e941d3155739c76acb3a4
Campbell Allen <campbell.allen@gmail.com>
2021-12-16 15:56:01 +0000

    track when a user has seen a subject (#3754)
    * track when a user has seen a subject
    
    touch any existing uss record when we update the subject_ids tracking object
    
    * rework the block

--- app/models/user_seen_subject.rb
+++ app/models/user_seen_subject.rb
@@ -5,12 +5,15 @@ class UserSeenSubject < ActiveRecord::Base
   validates_presence_of :user, :workflow
 
   def self.add_seen_subjects_for_user(user: nil, workflow: nil, subject_ids: nil)
-    uss = where(user: user, workflow: workflow)
-    if uss.exists?
-      uss.update_all(["subject_ids = uniq(subject_ids + array[?])", subject_ids])
-    else
-      uss.create!(subject_ids: subject_ids)
-    end
+    newly_created = false
+    # find or create the USS record and track if it's newly created
+    uss = where(user: user, workflow: workflow).first_or_create!(user: user, workflow: workflow, subject_ids: subject_ids) { |_| newly_created = true }
+    return uss if newly_created
+
+    # uniquely update the subject_ids on existing records using atomic update (update_all)
+    where(user: user, workflow: workflow).update_all(['subject_ids = uniq(subject_ids + array[?])', subject_ids])
+    # touch the updated_at timestamp on the record to track when the subject_ids were last modified
+    uss.touch
   end
 
   def self.count_user_activity(user_id, workflow_ids=[])
