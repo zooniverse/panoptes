9e1382cd825bda22d3639b6f8cc69c9c42e8da12
Campbell Allen <campbell.allen@gmail.com>
2022-01-10 12:05:57 +0000

    avoid reporting kinesis payload error for specific projects (#3761)
    some projects don't use the downstream services (caesar etc) - these we can't avoid noisy error reports by not reporting

--- app/workers/publish_classification_worker.rb
+++ app/workers/publish_classification_worker.rb
@@ -23,10 +23,14 @@ class PublishClassificationWorker
   end
 
   def publish_to_kinesis!
-    ZooStream.publish(event: "classification",
-                      data: kinesis_data,
-                      linked: kinesis_linked,
-                      shard_by: [classification.workflow_id, classification.subject_ids].flatten.join("-"))
+    ZooStream.publish(
+      event: "classification",
+      data: kinesis_data,
+      linked: kinesis_linked,
+      shard_by: [classification.workflow_id, classification.subject_ids].flatten.join("-")
+    )
+  rescue Aws::Kinesis::Errors::ValidationException => e
+    handle_kinesis_error(e)
   end
 
   def kinesis_data
@@ -35,5 +39,19 @@ class PublishClassificationWorker
 
   def kinesis_linked
     serialized_classification[:linked]
+  end
+
+  def ignore_project_ids_list
+    ENV.fetch('KINESIS_PAYLOAD_SIZE_ERROR_PROJECT_ID_INGORE_LIST', '').split(',')
+  end
+
+  def handle_kinesis_error(error)
+    # Aws::Kinesis::Errors::ValidationException are broad and contain the specific error data in message
+    # therefore only test for these data payload size errors
+    # all others we raise on to keep visibility to kinesis publishing errors
+    raise error unless error.message.include?("Value at 'data' failed to satisfy constraint: Member must have length less than or equal to")
+
+    # re-raise if this classification's project isn't in the ignore list
+    raise error unless ignore_project_ids_list.include?(classification.project_id.to_s)
   end
 end
