533c5f9e8f3ff373a0ec695243478764c7963880
Campbell Allen <campbell.allen@gmail.com>
2022-01-20 14:29:30 +0000

    add manifest dowload error handling

--- app/operations/subject_set_imports/count_manifest_rows.rb
+++ app/operations/subject_set_imports/count_manifest_rows.rb
@@ -2,17 +2,17 @@
 
 module SubjectSetImports
   class CountManifestRows < Operation
-    class LimitExceeded < ApiErrors::PanoptesApiError; end
+    class LimitExceeded < StandardError; end
+    class ManifestError < StandardError; end
     string :source_url
     integer :manifest_count, default: -> {
-      UrlDownloader.stream(source_url) do |io|
-        csv_import = SubjectSetImport::CsvImport.new(io)
-        csv_import.count
-      end
+      count_manifest_data_rows
     }
     integer :manifest_row_count_limit, default: -> {
       ENV.fetch('SUBJECT_SET_IMPORT_MANIFEST_ROW_LIMIT', 10000).to_i
     }
+
+    attr_reader :manifest
 
     def execute
       return manifest_count if user_is_admin || manifest_is_under_limit
@@ -32,6 +32,15 @@ module SubjectSetImports
 
     def manifest_is_under_limit
       manifest_count <= manifest_row_count_limit
+    end
+
+    def count_manifest_data_rows
+      UrlDownloader.stream(source_url) do |io|
+        csv_import = SubjectSetImport::CsvImport.new(manifest)
+        csv_import.count
+      end
+    rescue UrlDownloader::Failed => e
+      raise ManifestError, "Failed to download manifest: #{source_url}"
     end
   end
 end
