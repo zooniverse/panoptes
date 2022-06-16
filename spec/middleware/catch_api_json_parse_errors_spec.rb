1a8e91fb32bb5b387efb67ce2b4858ccfc6daa1d
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-06-16 11:21:13 -0500

    update catch_ap_json_parse_errors_spec from hound sniffs and rubocop autofixes

--- spec/middleware/catch_api_json_parse_errors_spec.rb
+++ spec/middleware/catch_api_json_parse_errors_spec.rb
@@ -1,40 +1,42 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe CatchApiJsonParseErrors do
   let(:app) { double({ call: true }) }
   let(:good_env) do
     {
-     'CONTENT_TYPE' => 'application/json; charset=utf-8',
-     'HTTP_ACCEPT' => 'application/vnd.api+json; version=1'
+      'CONTENT_TYPE' => 'application/json; charset=utf-8',
+      'HTTP_ACCEPT' => 'application/vnd.api+json; version=1'
     }
   end
 
   let(:bad_env) do
     {
-     'CONTENT_TYPE' => 'text/plain; charset=utf-8',
-     'HTTP_ACCEPT' => 'application/vnd.api+json; version=1'
+      'CONTENT_TYPE' => 'text/plain; charset=utf-8',
+      'HTTP_ACCEPT' => 'application/vnd.api+json; version=1'
     }
   end
 
-  let(:middle) { CatchApiJsonParseErrors.new(app) }
+  let(:middle) { described_class.new(app) }
 
-  context "call does not error" do
-    it 'should return true' do
+  context 'when call does not error' do
+    it 'returns true' do
       expect(middle.call(good_env)).to be true
     end
   end
 
-  context "call errors with a StandardError" do
-    before(:each) do
+  context 'when call errors with a StandardError' do
+    before do
       allow(app).to receive(:call).and_raise(StandardError)
     end
 
-    it 'should allow the error to propogate' do
-      expect{ middle.call(good_env) }.to raise_error(StandardError)
+    it 'allows the error to propogate' do
+      expect { middle.call(good_env) }.to raise_error(StandardError)
     end
   end
 
-  context "call errors with ParseError" do
+  context 'when call errors with ParseError' do
     let(:error) {
       # raise an error here to simulate the HTTP request data parsers blowing up
       # https://github.com/rails/rails/commit/b3d41eae4b138d6c9d38bd9c1cbe033802c0e21d
@@ -43,23 +45,23 @@ describe CatchApiJsonParseErrors do
       begin
         # this would really be a specific parser error in reality, by Rails 5.1 should be ActionDispatch::Http::Parameters::ParseError
         raise StandardError
-      rescue
+      rescue StandardError
         return ActionDispatch::ParamsParser::ParseError.new('test', 'test')
       end
-     }
+    }
 
-    before(:each) do
+    before do
       allow(app).to receive(:call).and_raise(error)
     end
 
     context 'when the call is not JSON' do
       it 'reraises the error' do
-        expect{ middle.call(bad_env) }.to raise_error(ActionDispatch::ParamsParser::ParseError)
+        expect { middle.call(bad_env) }.to raise_error(ActionDispatch::ParamsParser::ParseError)
       end
     end
 
-    context "when the call is JSON" do
-      let(:request) {  middle.call(good_env) }
+    context 'when the call is JSON' do
+      let(:request) { middle.call(good_env) }
       let(:status) { 400 }
       let(:msg) { /There was a problem in the JSON you submitted/ }
 
