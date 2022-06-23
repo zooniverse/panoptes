1065043ec0d827b13245eeacbd8f1a7ff321a504
Campbell Allen <campbell.allen@gmail.com>
2022-06-23 14:59:35 +0100

    fix the available query using incorrect bind params
    AR seems to have a bug where the subject_set_id bind params were incorrectly being set - fixing this then caused the limit bind params to be incorrectly set.
    
    instead use 2 queries to solve this and look at fixing longer term post rails 5.0

--- lib/subjects/postgresql_selection.rb
+++ lib/subjects/postgresql_selection.rb
@@ -23,13 +23,46 @@ module Subjects
     def available
       query = Subjects::SetMemberSubjectSelector.new(workflow, user).set_member_subjects
       subject_set_ids = if workflow.grouped
-                            # respect the user if they want to select from a training set
-                            opts[:subject_set_id]
-                          else
-                            # default mode: do not select from training sets
-                            workflow.non_training_subject_sets.pluck(:id)
-                          end
-      query.where(subject_set_id: subject_set_ids)
+                          # respect the user if they want to select from a training set
+                          Array.wrap(opts[:subject_set_id])
+                        else
+                          # default mode: do not select from training sets
+                          workflow.non_training_subject_sets.pluck(:id)
+                        end
+      # Handle a bug when the rails 5.0 AR scope bind params merge in rails 5.0 (works find in 4.2)
+      #
+      # failed fix 1 - ensure the subject_set_id param clause is before the id clause
+      #
+      # SetMemberSubject.where(subject_set_id: subject_set_ids, id: query).select(:id)
+      #
+      # as that fixes the incorrect subject_set_id bind param - fails to bind limit correctly later on :(
+      #
+      # failed fix 2 - leave as is but wrap subject set ids in array
+      #
+      # query.where(subject_set_id: subject_set_ids)
+      #
+      # duplicate query clauses which make no sense and can be cleaned up
+      # but it still fails on the limit bind param fails later on :(
+      #
+      # failed fix 3 - tried to merge the AR queries manually
+      #
+      # query.merge(SetMemberSubject.where(subject_set_id: subject_set_ids))
+      #
+      # fixes the duplicate query clause but still fails on the limit bind param later on :(
+      #
+      # failed fix 4 - use an explicit sub query
+      #
+      # SetMemberSubject.where(subject_set_id: subject_set_ids, id: SetMemberSubject.select('id').from(query)).select(:id)
+      #
+      # fixes the subject set id bind param but still fails on the limit bind param later on :(
+
+      # Working solution
+      #
+      # run two queries
+      # one with with a pluck on the query select to pull the avaialble SMS IDs
+      # and then apply the subject set id filter to create a new simple scope
+      # that we pass onto to the selector strategy class which then applies the limit bind params etc
+      SetMemberSubject.where(id: query.pluck(:id), subject_set_id: subject_set_ids).select(:id)
     end
 
     def limit
