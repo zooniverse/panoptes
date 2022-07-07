73917c6da38af2ae68d822fd0fafcb7e1c5e0d84
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-07-07 14:50:01 -0500

    update deprecated testhelpers to test::controllerhelpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "sidekiq/testing"
 require 'flipper/adapters/memory'
+require 'mock_redis'
+require 'database_cleaner'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -15,7 +17,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include APIRequestHelpers, type: :controller
   config.include APIResponseHelpers, type: :controller
   config.include APIRequestHelpers, type: :request
