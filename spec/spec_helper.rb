f8e833317448dcace3189a79bdeb62bf73d52cc9
yuenmichelle1 <michelle@zooniverse.org>
2022-07-08 10:13:55 -0500

    update deprecated testhelpers to test::controllerhelpers (#3903)
    * update deprecated testhelpers to test::controllerhelpers
    
    * removing import from gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include APIRequestHelpers, type: :controller
   config.include APIResponseHelpers, type: :controller
   config.include APIRequestHelpers, type: :request
