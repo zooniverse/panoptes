d3afda7aca63834e3a2782fcdee1ff4440cbc7b4
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-09-16 12:52:34 -0500

    adding tests to check formatting of classification serializer so that it still can go through zoostream via publishclassificationworker

--- spec/serializers/event_stream_serializers/classification_serializer_spec.rb
+++ spec/serializers/event_stream_serializers/classification_serializer_spec.rb
@@ -11,4 +11,24 @@ describe EventStreamSerializers::ClassificationSerializer do
     adapter = described_class.serialize(classification, { include: ['subjects'] })
     expect(adapter.as_json[:linked]['subjects'].size).to eq(1)
   end
+
+  describe "serialize for kinesis stream" do
+    it 'should follow format of kinesis stream' do
+      project = create(:project)
+      workflow = create(:workflow)
+      user = create(:user)
+      subject = create(:subject)
+
+      serialized_data = described_class
+      .serialize(classification, include: ['project', 'workflow', 'user', 'subjects'])
+      .as_json
+      .with_indifferent_access
+
+      sample_kinesis_json = JSON.parse(file_fixture('example_event_stream_serializer_classification.json').read)
+
+      expect(serialized_data.keys).to match_array(["classifications", "linked"])
+      expect(serialized_data[:classifications][0].keys).to match_array(sample_kinesis_json["data"].keys)
+      expect(serialized_data[:linked].keys).to match_array(sample_kinesis_json["linked"].keys)
+    end
+  end
 end
