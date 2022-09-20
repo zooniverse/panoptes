e3eaee352194a2bc3d46ea92c9e75df81498873a
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-09-20 15:13:06 -0500

    Bump AMS to 0.10.13, Related to Related to https://github.com/zooniverse/panoptes/pull/3947/files
    Changes:
     From AMS 0.10.0rc2 to 0.10.13, serializer.each_asociation as well as serializer.type has been removed. Replaced with .associations and .json_key respectively.
    Serializer.associations returns association instance which is an enumerable that can be iterated. .json_key returns in our case ._type defined in each serializer class.
    
    Defining `type` attr for each serializer that extends ActiveModel::Serializer.
    
    Adding fixture based on what was received when tailing kinesis stream and adding appropriate test on classification_serializer_spec.rb to test output of .serialize comes out in the expected format of our kinesis stream.

--- app/serializers/event_stream_serializers/classification_serializer.rb
+++ app/serializers/event_stream_serializers/classification_serializer.rb
@@ -7,6 +7,8 @@ module EventStreamSerializers
 
     attributes :id, :created_at, :updated_at, :user_ip, :workflow_version, :gold_standard,
                :expert_classifier, :annotations, :metadata
+    
+    type :classifications
 
     belongs_to :project,          serializer: EventStreamSerializers::ProjectSerializer
     belongs_to :user,             serializer: EventStreamSerializers::UserSerializer
