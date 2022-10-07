e98eb1a80f247fbef465cd1e952b755eb6ef2071
Campbell Allen <campbell.allen@gmail.com>
2022-10-07 16:17:38 +0100

    fix strong params nested array objects being unpermitted (#3961)
    * add rails backport for nested params
    
    * add spec for nested steps array with json objects

--- lib/gem_ext/gem_ext.rb
+++ lib/gem_ext/gem_ext.rb
@@ -1,3 +1,5 @@
 require_dependency 'gem_ext/doorkeeper/application'
 require 'gem_ext/doorkeeper/server'
 require 'gem_ext/doorkeeper/client_credentials_creator'
+
+require 'gem_ext/rails/strong_parameters'
