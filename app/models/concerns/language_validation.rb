a21e9e2f97df15fb07b0a4e193929918a8682c3d
Campbell Allen <campbell.allen@gmail.com>
2022-10-13 18:22:21 +0700

    update the language code regex for 3 letter subtags
    avoid the use of A-z as it includes non alpha chars

--- app/models/concerns/language_validation.rb
+++ app/models/concerns/language_validation.rb
@@ -2,10 +2,12 @@ module LanguageValidation
   extend ActiveSupport::Concern
 
   def self.lang_regex
-    /\A[a-z]{2}(\z|-[A-z]{2})/
+    # match language codes with possible subtags en-GB, en, en-US, en-us etc
+    # https://en.wikipedia.org/wiki/IETF_language_tag
+    /\A[a-z]{2}-?(?:[a-z]{2,3})?\z/i
   end
 
   included do
-    validates :language, format: {with: LanguageValidation.lang_regex }
+    validates :language, format: { with: LanguageValidation.lang_regex }
   end
 end
