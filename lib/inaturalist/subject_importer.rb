dbee87ac9aa53201508a962092a00df18369214d
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2022-10-19 18:23:52 -0500

    Add SubjectImporter and spec

---
+++ lib/inaturalist/subject_importer.rb
@@ -0,0 +1,37 @@
+module Inaturalist
+  class SubjectImporter
+    class FailedImport < StandardError; end
+
+    def initialize(user_id, subject_set_id)
+      @uploader = User.find(user_id)
+      @subject_set = SubjectSet.find(subject_set_id)
+    end
+
+    def import(obs)
+      subject = find_or_initialize_subject(obs.external_id)
+
+      Subject.transaction do
+        subject.project_id = @subject_set.project_id
+        subject.uploader = @uploader
+        subject.metadata = obs.metadata
+        subject.subject_sets << @subject_set
+
+        Subject.location_attributes_from_params(obs.locations).each do |location_attributes|
+          subject.locations.build(location_attributes)
+        end
+
+        subject.save!
+      end
+      subject
+    rescue ActiveRecord::RecordInvalid => e
+      raise FailedImport, e.message
+    end
+
+    def find_or_initialize_subject(external_id)
+      # Subjects need to be upserted by external ID, which has no index.
+      # So like SubjectSetImports, this scope is limited to the subject set and constrained by SetMemberSubjects
+      # @combined_set_subject_scope.where(external_id: external_id).first_or_initialize
+      @subject_set.subjects.where(external_id: external_id).first_or_initialize
+    end
+  end
+end
---
+++ spec/lib/inaturalist/subject_importer_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe Inaturalist::SubjectImporter do
+
+  describe '#import' do
+    let(:response) { JSON.parse(file_fixture('inat_observations.json').read) }
+    let(:obs) { Inaturalist::Observation.new(response['results'][0]) }
+    let(:subject_set) { create(:subject_set) }
+    let(:importer) { described_class.new(subject_set.project.owner.id, subject_set.id) }
+    let(:locations) {
+      [
+        { "image/jpeg" => "https://static.inaturalist.org/photos/12345/original.JPG" },
+        { "image/jpeg" => "https://static.inaturalist.org/photos/45678/original.JPG" }
+      ]
+    }
+
+    before { importer.import(obs) }
+
+    it 'imports new subjects' do
+      expect(subject_set.subjects.count).to eq(1)
+    end
+
+    it 'sets metadata correctly' do
+      expect(subject_set.subjects.first.metadata).to eq(obs.metadata)
+    end
+
+    describe 'imported locations' do
+      let(:imported_locations) { subject_set.subjects.first.locations.order(:id) }
+
+      it 'sets external_link correctly' do
+        expect(imported_locations.map {|l| l.external_link }).to match_array([true, true])
+      end
+
+      it 'sets content_type correctly' do
+        expect(imported_locations.map {|l| l.content_type }).to match_array(['image/jpeg', 'image/jpeg'])
+      end
+
+      it 'sets src correctly' do
+        expect(imported_locations[0].src).to eq(locations[0]['image/jpeg'])
+        expect(imported_locations[1].src).to eq(locations[1]['image/jpeg'])
+      end
+    end
+
+  context 'when a record fails to save!' do
+    let(:subject_double) { Subject.new }
+
+    before do
+      allow(subject_double).to receive(:save!).and_raise(ActiveRecord::RecordInvalid, subject_double)
+      allow(importer).to receive(:find_or_initialize_subject).and_return(subject_double)
+    end
+
+    it 'raises a relevant error' do
+      expect { importer.import(obs) }.to raise_error(Inaturalist::SubjectImporter::FailedImport)
+    end
+  end
+
+  end
+
+end
