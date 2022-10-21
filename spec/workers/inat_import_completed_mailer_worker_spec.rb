64376efdc351634311b6845fd2481d995c4a1641
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2022-10-21 16:39:39 -0500

    Worker for completion mailer

---
+++ spec/workers/inat_import_completed_mailer_worker_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+RSpec::Matchers.define_negated_matcher :not_change, :change
+
+RSpec.describe InatImportCompletedMailerWorker do
+  let(:ss_import) { create(:subject_set_import) }
+
+  it 'should deliver the mail' do
+    expect{ subject.perform(ss_import) }.to change{ ActionMailer::Base.deliveries.count }.by(1)
+  end
+
+  context "missing attributes" do
+    it "is missing a ss_import and doesn't send" do
+      expect{ subject.perform(nil) }.to not_change{ ActionMailer::Base.deliveries.count }
+        .and raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  context 'with an unknown subject_set_id' do
+    it 'raises an error so we know about it' do
+      expect { subject.perform(nil) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
