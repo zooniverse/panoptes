6b4f6729a9b4ff29d5c0ca0912f32b8f4c441d99
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2022-10-21 16:33:16 -0500

    Add InatImportWorker and spec

---
+++ app/workers/inat_import_worker.rb
@@ -0,0 +1,47 @@
+class InatImportWorker
+  include Sidekiq::Worker
+
+  # skip retries for this job to avoid re-running imports with errors
+  sidekiq_options retry: 0, queue: :data_medium
+
+  def perform(user_id, taxon_id, subject_set_id, updated_since=nil)
+    inat = Inaturalist::ApiInterface.new(taxon_id: taxon_id, updated_since: updated_since)
+    importer = Inaturalist::SubjectImporter.new(user_id, subject_set_id)
+
+    # Use an SSImport instance to track progress & store data
+    ss_import = importer.subject_set_import
+
+    failed_imports = []
+    imported_row_count = 0
+    inat.observations.each do |obs|
+      begin
+        importer.import(obs)
+      rescue SubjectSetImport::Processor::FailedImport
+        ss_import.update_columns( failed_count: failed_count + 1, failed_uuids: failed_uuids | [external_id])
+      end
+
+      imported_row_count += 1
+
+      # update the imported_count as we progress through the import so we can use
+      # this as a progress metric on API resource polling (see SubjectSetWorker)
+      save_imported_row_count(imported_row_count) if (imported_row_count % update_progress_every_rows(inat.total_results)).zero?
+    end
+
+    ss_import.save_imported_row_count(imported_row_count)
+
+    # Count that subject set, like right now
+    SubjectSetSubjectCounterWorker.new.perform(subject_set_id)
+
+    # notify the user about the import success / failure
+    InatImportCompletedMailerWorker.perform_async(ss_import.id)
+
+  end
+
+
+  def update_progress_every_rows(total_results)
+    update_progress_every_rows ||= SubjectSetImport::ProgressUpdateCadence.calculate(total_results)
+  end
+
+
+
+end
---
+++ spec/workers/inat_import_worker_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe InatImportWorker do
+  # avoid re-running the import on failures
+  it 'does not retry imports' do
+    retry_count = described_class.get_sidekiq_options['retry']
+    expect(retry_count).to eq(0)
+  end
+
+  describe '#perform' do
+    # An enumerable proxy for the importer's #observations method
+    let(:obs_array) { [ double('observation'), double('observation') ] }
+    let(:api_double) { instance_double(Inaturalist::ApiInterface, observations: obs_array, total_results: 123) }
+    let(:ss_import) { instance_double(SubjectSetImport, id: 1, update_columns: true, save_imported_row_count: true) }
+    let(:importer_double) do
+      instance_double(Inaturalist::SubjectImporter, import: true, subject_set_import: ss_import)
+    end
+    let(:count_worker_double) do
+      instance_double(SubjectSetSubjectCounterWorker, perform: true)
+    end
+
+    before do
+      allow(Inaturalist::SubjectImporter).to receive(:new).and_return(importer_double)
+      allow(Inaturalist::ApiInterface).to receive(:new).and_return(api_double)
+      allow(SubjectSetImport).to receive(:new).and_return(ss_import)
+      allow(SubjectSetSubjectCounterWorker).to receive(:new).and_return(count_worker_double)
+      allow(InatImportCompletedMailerWorker).to receive(:perform_async)
+    end
+
+    it 'runs SubjectImporter#import on each observation in an enumerable' do
+      described_class.new.perform(1, 1, 1)
+      expect(importer_double).to have_received(:import).with(obs_array[0])
+      expect(importer_double).to have_received(:import).with(obs_array[1])
+    end
+
+
+    it 'calls the subject set import mailer worker' do
+      described_class.new.perform(1, 1, 1)
+      expect(InatImportCompletedMailerWorker).to have_received(:perform_async).with(ss_import.id)
+    end
+
+    it 'calls the subject set counter worker inline' do
+      described_class.new.perform(1, 1, 1)
+      expect(count_worker_double).to have_received(:perform).with(1)
+    end
+  end
+end
