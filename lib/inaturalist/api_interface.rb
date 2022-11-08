bd3b4af4c7dd8909dfe3dc27bfc95d4224ee6f3b
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2022-11-07 18:33:04 -0600

    Remove memory-eating cache (#3997)

--- lib/inaturalist/api_interface.rb
+++ lib/inaturalist/api_interface.rb
@@ -7,12 +7,12 @@ module Inaturalist
     require 'json'
 
     # Set maximum imported subjects, or no limit with -1
-    attr_reader :taxon_id, :total_results, :observation_cache, :params
+    attr_reader :taxon_id, :total_results, :observation_count, :params
 
     def initialize(taxon_id:, updated_since: nil, max_observations: -1)
       @taxon_id = taxon_id
       @max_observations = max_observations
-      @observation_cache = []
+      @observation_count = 0
       @id_above = 0
       @params = { taxon_id: @taxon_id }
       @params[:updated_since] = updated_since unless updated_since.nil?
@@ -41,20 +41,20 @@ module Inaturalist
       # Stop if a) there are no more results
       #         b) the total number of desired subjects is hit
       #         c) the ID of the last seen observation is the same as the last result's id
-      @done = true if results.empty? || max_cache_hit? || @id_above == results.last['id']
+      @done = true if results.empty? || max_count_hit? || @id_above == results.last['id']
       return if @done
 
-      @observation_cache += results
+      @observation_count += 1
       @id_above = results.last['id']
       @params['id_above'] = @id_above
       results
     end
 
-    def max_cache_hit?
+    def max_count_hit?
       # Short circuit to turn off limit
       return false if @max_observations == -1
 
-      @observation_cache.size >= @max_observations
+      @observation_count >= @max_observations
     end
 
     def client
