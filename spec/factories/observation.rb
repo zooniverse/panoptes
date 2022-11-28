299580bd93b90f4b87b883a27fcbe1de02283aca
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2022-11-28 16:03:53 -0600

    Add Observation factory

---
+++ spec/factories/observation.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  sequence :id_sequence, 1
+
+  factory :observation, class: 'Inaturalist::Observation' do
+    external_id { generate(:id_sequence) }
+    metadata { { id: generate(:id_sequence) } }
+    locations {
+      [
+        { 'image/jpeg' => 'https://static.inaturalist.org/photos/12345/original.JPG' },
+        { 'image/jpeg' => 'https://static.inaturalist.org/photos/45678/original.JPG' }
+      ]
+    }
+    initialize_with { new(**attributes) }
+  end
+end
