40d48208a35bbec2ab6daa82a7114da7380354a8
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2022-11-28 16:05:02 -0600

    Refactor importer & worker to insert in batches

--- app/workers/inat_import_worker.rb
+++ app/workers/inat_import_worker.rb
@@ -7,32 +7,41 @@ class InatImportWorker
   sidekiq_options retry: 0, queue: :dumpworker
   sidekiq_options lock: :until_and_while_executing
 
+  def self.import_batch_size
+    ENV.fetch('INAT_IMPORT_BATCH_SIZE', 200)
+  end
+
   def perform(user_id, taxon_id, subject_set_id, updated_since=nil)
-    inat = Inaturalist::ApiInterface.new(taxon_id: taxon_id, updated_since: updated_since)
-    importer = Inaturalist::SubjectImporter.new(user_id, subject_set_id)
+    @inat = Inaturalist::ApiInterface.new(taxon_id: taxon_id, updated_since: updated_since)
+    @importer = Inaturalist::SubjectImporter.new(user_id, subject_set_id)
 
-    # Use a SubjectSetImport instance to track progress & store data
-    ss_import = importer.subject_set_import
+    @imported_row_count = 0
+    subjects_to_import = []
 
-    imported_row_count = 0
-    inat.observations.each do |obs|
-      begin
-        importer.import(obs)
-      rescue Inaturalist::SubjectImporter::FailedImport
-        ss_import.update_columns(
-          failed_count: ss_import.failed_count + 1,
-          failed_uuids: ss_import.failed_uuids | [obs.external_id]
-        )
-      end
+    @inat.observations.each_with_index do |obs, i|
+      subjects_to_import << @importer.to_subject(obs)
+      next unless process_batch?(i)
 
-      imported_row_count += 1
+      # Use activerecord-import to insert in batches
+      subject_import_results = @importer.import_subjects(subjects_to_import)
+      # import_results = Subject.import subjects_to_import, validate: false
 
-      # update the imported_count as we progress through the import so we can use
-      # this as a progress metric on API resource polling (see SubjectSetWorker)
-      ss_import.save_imported_row_count(imported_row_count) if (imported_row_count % update_progress_every_rows(inat.total_results)).zero?
+      # Record import state
+      save_status(subject_import_results)
+
+      # Create/init SMSes and insert in batches
+      @importer.import_smses(subject_import_results)
+      # set_member_subjects_to_import = import_results.ids.map do |subject_id|
+      #   sms = SetMemberSubject.find_or_initialize_by(subject_set_id: subject_set_id, subject_id: subject_id)
+      #   sms.random = rand unless sms.random?
+      #   sms
+      # end
+      # SetMemberSubject.import set_member_subjects_to_import, validate: false
+
+      # Assist ruby GC wherever possible
+      subjects = []
+      import_results, sms = nil
     end
-
-    ss_import.save_imported_row_count(imported_row_count)
 
     # Count that subject set, like right now
     SubjectSetSubjectCounterWorker.new.perform(subject_set_id)
@@ -41,7 +50,32 @@ class InatImportWorker
     InatImportCompletedMailerWorker.perform_async(ss_import.id)
   end
 
+  def process_batch?(index)
+    (index % import_batch_size).zero? && index.positive?
+  end
+
+  def ss_import
+    @ss_import ||= @importer.subject_set_import
+  end
+
+  def save_status(import_results)
+    @imported_row_count += import_results.ids.size
+    ss_import.save_imported_row_count(@imported_row_count)
+    save_failed_import_rows(import_results.failed_instances) if import_results.failed_instances
+  end
+
   def update_progress_every_rows(total_results)
     SubjectSetImport::ProgressUpdateCadence.calculate(total_results)
+  end
+
+  def save_failed_import_rows(failed_instances)
+    failed_import_row_uuids = failed_instances.map(&:external_id)
+    failed_import_row_count = failed_import_row_uuids.size
+    return if failed_import_row_count.zero?
+
+    ss_import.update_columns(
+      failed_count: ss_import.failed_count + failed_import_row_count,
+      failed_uuids: ss_import.failed_uuids | failed_import_row_uuids
+    )
   end
 end
--- spec/workers/inat_import_worker_spec.rb
+++ spec/workers/inat_import_worker_spec.rb
@@ -11,66 +11,71 @@ RSpec.describe InatImportWorker do
 
   describe '#perform' do
     # An enumerable proxy for the importer's #observations method
-    let(:obs_array) { [instance_double('observation'), instance_double('observation')] }
+    let(:obs_array) { build_list(:observation, 2) }
     let(:api_double) { instance_double(Inaturalist::ApiInterface, observations: obs_array, total_results: 123) }
-    let(:ss_import) { instance_double(SubjectSetImport, id: 1, update_columns: true, save_imported_row_count: true) }
+    let(:ss_import) { instance_double(SubjectSetImport, id: 1, save_imported_row_count: true) }
+    let(:import_results_double) do
+      instance_double('ActiveRecord::Import::Result', ids: [1, 2], failed_instances: nil)
+    end
     let(:importer_double) do
-      instance_double(Inaturalist::SubjectImporter, import: true, subject_set_import: ss_import)
+      instance_double(Inaturalist::SubjectImporter, to_subject: true, subject_set_import: ss_import, import_subjects: import_results_double, import_smses: true)
     end
     let(:count_worker_double) do
       instance_double(SubjectSetSubjectCounterWorker, perform: true)
     end
 
+    before do
+      allow(Inaturalist::SubjectImporter).to receive(:new).and_return(importer_double)
+      allow(Inaturalist::ApiInterface).to receive(:new).and_return(api_double)
+      allow(SubjectSetImport).to receive(:new).and_return(ss_import)
+      allow(SubjectSetSubjectCounterWorker).to receive(:new).and_return(count_worker_double)
+      allow(InatImportCompletedMailerWorker).to receive(:perform_async)
+      allow_any_instance_of(described_class).to receive(:import_batch_size).and_return(1)
+      allow_any_instance_of(described_class).to receive(:set_status).and_return(true)
+      allow(Subject).to receive(:import).and_return(import_results_double)
+      allow(SetMemberSubject).to receive(:import).and_return(import_results_double)
+      described_class.new.perform(1, 1, 1)
+    end
+
     context 'when the import is successful' do
-      before do
-        allow(Inaturalist::SubjectImporter).to receive(:new).and_return(importer_double)
-        allow(Inaturalist::ApiInterface).to receive(:new).and_return(api_double)
-        allow(SubjectSetImport).to receive(:new).and_return(ss_import)
-        allow(SubjectSetSubjectCounterWorker).to receive(:new).and_return(count_worker_double)
-        allow(InatImportCompletedMailerWorker).to receive(:perform_async)
+      it 'runs SubjectImporter#to_subject on each observation in an enumerable' do
+        obs_array.each { |obs| expect(importer_double).to have_received(:to_subject).with(obs) }
       end
 
-      it 'runs SubjectImporter#import on each observation in an enumerable' do
-        described_class.new.perform(1, 1, 1)
-        expect(importer_double).to have_received(:import).with(obs_array[0])
-        expect(importer_double).to have_received(:import).with(obs_array[1])
+      it 'invokes the importer to import a batch' do
+        expect(importer_double).to have_received(:import_subjects).with([true, true])
+      end
+
+      it 'invokes the importer to import SetMemberSubjects' do
+        expect(importer_double).to have_received(:import_smses).with(import_results_double)
       end
 
       it 'calls the subject set import mailer worker' do
-        described_class.new.perform(1, 1, 1)
         expect(InatImportCompletedMailerWorker).to have_received(:perform_async).with(ss_import.id)
       end
 
       it 'calls the subject set counter worker inline' do
-        described_class.new.perform(1, 1, 1)
         expect(count_worker_double).to have_received(:perform).with(1)
       end
     end
 
-    context 'when a subject fails to import' do
-      let(:taxon_id) { 12345 }
-      let(:user) { create(:user) }
-      let(:subject_set) { create(:subject_set) }
+    context 'when there is a failure' do
+      let(:failed_instances) { [Subject.new(external_id: 1), Subject.new(external_id: 2)] }
 
       before do
-        allow(Inaturalist::ApiInterface).to receive(:new).and_return(api_double)
-        allow_any_instance_of(Inaturalist::SubjectImporter).to receive(:import).and_raise(Inaturalist::SubjectImporter::FailedImport)
-        allow(obs_array[0]).to receive(:external_id).and_return('123asdf')
-        allow(obs_array[1]).to receive(:external_id).and_return('456ghj')
+        allow(import_results_double).to receive(:failed_instances).and_return(failed_instances)
+        allow(ss_import).to receive(:update_columns).with(any_args).and_return(true)
+        allow(ss_import).to receive(:failed_count).and_return(0)
+        allow(ss_import).to receive(:failed_uuids).and_return([])
       end
 
       it 'continues processing' do
-        expect { described_class.new.perform(user.id, taxon_id, subject_set.id) }.not_to raise_error
+        expect { described_class.new.perform(1, 1, 1) }.not_to raise_error
       end
 
       it 'stores the failed count' do
-        described_class.new.perform(user.id, taxon_id, subject_set.id)
-        expect(SubjectSetImport.where(user_id: user.id, subject_set_id: subject_set.id).first.failed_count).to eq(2)
-      end
-
-      it 'stores the failed UUIDs' do
-        described_class.new.perform(user.id, taxon_id, subject_set.id)
-        expect(SubjectSetImport.where(user_id: user.id, subject_set_id: subject_set.id).first.failed_uuids).to match_array(%w[123asdf 456ghj])
+        described_class.new.perform(1, 1, 1)
+        expect(ss_import).to have_received(:update_columns).with(failed_count: 2, failed_uuids: %w[1 2])
       end
     end
   end
