f2d14b3d8d6256e2a7d2e99e7fe05615e28783c9
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-12-19 16:04:33 -0600

    update doorkepper access to batch delete

--- lib/gem_ext/doorkeeper/access_cleanup.rb
+++ lib/gem_ext/doorkeeper/access_cleanup.rb
@@ -1,34 +1,45 @@
 module Doorkeeper
   class AccessCleanup
     def cleanup!(keep_old_refresh_tokens_for=14)
-      expired_grants.delete_all
-      revoked_grants.delete_all
-      expired_tokens.delete_all
-      revoked_tokens.delete_all
-      old_unused_refreshable_tokens(keep_old_refresh_tokens_for).delete_all
+      remove_expired_grants
+      remove_revoked_grants
+      remove_expired_tokens
+      remove_revoked_tokens
+      remove_old_unused_refreshable_tokens
     end
 
-    def expired_grants
-      Doorkeeper::AccessGrant.where(expired_clause)
+    def remove_expired_grants
+      Doorkeeper::AccessGrant.where(expired_clause).in_batches do |group|
+        group.delete_all
+      end
     end
 
-    def revoked_grants
-      Doorkeeper::AccessGrant.where(revoked_clause)
+    def remove_revoked_grants
+      Doorkeeper::AccessGrant.where(revoked_clause).in_batches do |group|
+        group.delete_all
+      end
     end
 
-    def expired_tokens
-      Doorkeeper::AccessToken.where(refresh_token: nil).where(expired_clause)
+    def remove_expired_tokens
+      Doorkeeper::AccessToken.where(refresh_token: nil).where(expired_clause).in_batches do |group|
+        group.delete_all
+      end
     end
 
-    def revoked_tokens
-      Doorkeeper::AccessToken.where(revoked_clause)
+    def remove_revoked_tokens
+      Doorkeeper::AccessToken.where(revoked_clause).in_batches do |group|
+        group.delete_all
+      end
     end
 
-    def old_unused_refreshable_tokens(keep_for)
+    def remove_old_unused_refreshable_tokens(keep_for)
       Doorkeeper::AccessToken
       .where.not(refresh_token: nil)
       .where(previous_refresh_token: nil)
       .where("created_at < ?", Time.now - keep_for.days)
+      .in_batches do |group|
+        group.delete_all
+      end
     end
 
     private
