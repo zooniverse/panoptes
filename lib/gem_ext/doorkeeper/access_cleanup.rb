c00d6576de377529dc28562f5dea31f7eea043b4
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-12-20 11:02:52 -0600

    clean up per hound sniffs (send delete_aall as an argument to in_batches.. and add forgotten param to remove_old_unused_refreshable_tokens

--- lib/gem_ext/doorkeeper/access_cleanup.rb
+++ lib/gem_ext/doorkeeper/access_cleanup.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Doorkeeper
   class AccessCleanup
     def cleanup!(keep_old_refresh_tokens_for=14)
@@ -5,47 +7,37 @@ module Doorkeeper
       remove_revoked_grants
       remove_expired_tokens
       remove_revoked_tokens
-      remove_old_unused_refreshable_tokens
+      remove_old_unused_refreshable_tokens(keep_old_refresh_tokens_for)
     end
 
     def remove_expired_grants
-      Doorkeeper::AccessGrant.where(expired_clause).in_batches do |group|
-        group.delete_all
-      end
+      Doorkeeper::AccessGrant.where(expired_clause).in_batches(&:delete_all)
     end
 
     def remove_revoked_grants
-      Doorkeeper::AccessGrant.where(revoked_clause).in_batches do |group|
-        group.delete_all
-      end
+      Doorkeeper::AccessGrant.where(revoked_clause).in_batches(&:delete_all)
     end
 
     def remove_expired_tokens
-      Doorkeeper::AccessToken.where(refresh_token: nil).where(expired_clause).in_batches do |group|
-        group.delete_all
-      end
+      Doorkeeper::AccessToken.where(refresh_token: nil).where(expired_clause).in_batches(&:delete_all)
     end
 
     def remove_revoked_tokens
-      Doorkeeper::AccessToken.where(revoked_clause).in_batches do |group|
-        group.delete_all
-      end
+      Doorkeeper::AccessToken.where(revoked_clause).in_batches(&:delete_all)
     end
 
     def remove_old_unused_refreshable_tokens(keep_for)
       Doorkeeper::AccessToken
-      .where.not(refresh_token: nil)
-      .where(previous_refresh_token: nil)
-      .where("created_at < ?", Time.now - keep_for.days)
-      .in_batches do |group|
-        group.delete_all
-      end
+        .where.not(refresh_token: nil)
+        .where(previous_refresh_token: nil)
+        .where('created_at < ?', Time.zone.now - keep_for.days)
+        .in_batches(&:delete_all)
     end
 
     private
 
     def revoked_clause
-      "revoked_at IS NOT NULL"
+      'revoked_at IS NOT NULL'
     end
 
     def expired_clause
