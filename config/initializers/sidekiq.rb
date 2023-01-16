82e35068e616665c7bf64dc19519fd8be0732f2f
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-01-16 13:44:45 -0600

    update sidekiq and sidekiq-unique-jobs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,13 +6,24 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: SidekiqConfig.redis_url }
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
 
 Sidekiq.configure_server do |config|
   config.redis = { url: SidekiqConfig.redis_url }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
   config.server_middleware do |chain|
     chain.add Sidekiq::Congestion::Limiter
+    chain.add SidekiqUniqueJobs::Middleware::Server
   end
+
+  SidekiqUniqueJobs::Server.configure(config)
 
   # Sidekiq-cron: loads recurring jobs from config/schedule.yml
   schedule_file = 'config/schedule.yml'
