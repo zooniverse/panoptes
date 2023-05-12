e357cb7b43f18a43fa0f09453e0fb935fdfc0b44
yuenmichelle1 <michelle@zooniverse.org>
2023-05-12 16:56:58 -0500

    update membership serializer to send roles (#4199)

--- app/serializers/membership_serializer.rb
+++ app/serializers/membership_serializer.rb
@@ -2,6 +2,6 @@ class MembershipSerializer
   include Serialization::PanoptesRestpack
   include CachedSerializer
 
-  attributes :id, :state, :href
+  attributes :id, :state, :roles, :href
   can_include :user, :user_group
 end
