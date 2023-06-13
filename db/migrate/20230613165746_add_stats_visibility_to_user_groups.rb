9deeab5b7c69545cf5625cb4789536d9428c0075
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2023-06-13 12:42:47 -0500

    Update 20230613165746_add_stats_visibility_to_user_groups.rb

--- db/migrate/20230613165746_add_stats_visibility_to_user_groups.rb
+++ db/migrate/20230613165746_add_stats_visibility_to_user_groups.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class AddStatsVisibilityToUserGroups < ActiveRecord::Migration[6.1]
   def change
     add_column :user_groups, :stats_visibility, :integer
     # defaulting to restricted stats_visibility view (where members can view aggregate stats but only admins can view detailed stats)
-    change_column_default :user_groups, :stats_visibility, 0
+    change_column_default :user_groups, :stats_visibility, from: nil, to: 0
   end
 end
