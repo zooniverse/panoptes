968cf88799d43e65aa066296960f8a91d49bc65e
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2023-12-05 15:09:05 -0600

    Allow unconfirmed access forever

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,7 @@ Devise.setup do |config|
   config.skip_session_storage = [:http_auth]
   config.stretches = Rails.env.test? ? 1 : 10
   config.reconfirmable = false
+  config.allow_unconfirmed_access_for = nil
   config.password_length = 8..128
   config.reset_password_within = 6.hours
   config.paranoid = true
