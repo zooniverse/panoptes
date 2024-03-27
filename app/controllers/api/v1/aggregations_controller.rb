03ffb5142e2d517da683662cb4fbd2e8e820ca90
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2024-03-27 16:02:02 -0500

    Controller and spec

--- app/controllers/api/v1/aggregations_controller.rb
+++ app/controllers/api/v1/aggregations_controller.rb
@@ -1,19 +1,23 @@
+# frozen_string_literal: true
+
 class Api::V1::AggregationsController < Api::ApiController
   include JsonApiController::PunditPolicy
 
-  require_authentication :create, :update, scopes: [:project]
-  resource_actions :create, :update, :show, :index
+  require_authentication :index, :show, :update, :create, scopes: [:workflow]
+  resource_actions :index, :show, :create, :update
   schema_type :json_schema
-  before_action :filter_by_subject_set, only: :index
 
-  private
-
-  def filter_by_subject_set
-    subject_set_ids = params.delete(:subject_set_id).try(:split, ',')
-    unless subject_set_ids.blank?
-      @controlled_resources = controlled_resources
-        .joins(workflow: :subject_sets)
-        .where(workflows: { subject_set_id: subject_set_ids } )
+  def create
+    workflow = Workflow.find(create_params['links']['workflow'])
+    response = AggregationClient.new.send_aggregation_request(
+      workflow.project.id,
+      workflow.id,
+      create_params['links']['user']
+    )
+    super do |agg|
+      agg.update({task_id: response.body[:task_id], status: 'pending'})
     end
+  rescue AggregationClient::ConnectionError
+    json_api_render(:service_unavailable, response.body)
   end
 end
