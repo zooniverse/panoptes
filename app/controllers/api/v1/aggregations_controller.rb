e62e60dde6461d9d3e2d8f3b418fa5dae8f0ab47
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2024-06-03 13:32:23 -0500

    Deprecate AggregationsController

--- app/controllers/api/v1/aggregations_controller.rb
+++ app/controllers/api/v1/aggregations_controller.rb
@@ -1,19 +1,12 @@
 class Api::V1::AggregationsController < Api::ApiController
-  include JsonApiController::PunditPolicy
+  # include JsonApiController::PunditPolicy
 
-  require_authentication :create, :update, scopes: [:project]
-  resource_actions :create, :update, :show, :index
-  schema_type :json_schema
-  before_action :filter_by_subject_set, only: :index
+  # THIS FUNCTIONALITY IS BEING DEPRECATED EFFECTIVE IMMEDIATELY
+  # A REPLACEMENT IS FORTHCOMING.
 
-  private
+  # require_authentication :create, :update, scopes: [:project]
+  # resource_actions :create, :update, :show, :index
+  # schema_type :json_schema
+  # before_action :filter_by_subject_set, only: :index
 
-  def filter_by_subject_set
-    subject_set_ids = params.delete(:subject_set_id).try(:split, ',')
-    unless subject_set_ids.blank?
-      @controlled_resources = controlled_resources
-        .joins(workflow: :subject_sets)
-        .where(workflows: { subject_set_id: subject_set_ids } )
-    end
-  end
 end
