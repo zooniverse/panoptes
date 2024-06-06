3d1b3349adf3f0abfc372a49e208e8fc1a329126
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2024-06-06 11:08:49 -0500

    Add safety_assured to migration

--- db/migrate/20240531184258_remove_aggregation_from_workflows.rb
+++ db/migrate/20240531184258_remove_aggregation_from_workflows.rb
@@ -2,6 +2,6 @@
 
 class RemoveAggregationFromWorkflows < ActiveRecord::Migration[6.1]
   def change
-    remove_column :workflows, :aggregation, :jsonb
+    safety_assured { remove_column :workflows, :aggregation, :jsonb }
   end
 end
