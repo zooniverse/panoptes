a92748f54fc1cb0a764ccca8f048277aede0d32b
tooyosi <oluwatoyosi.oyegoke@gmail.com>
2025-04-02 01:31:59 +0100

    update Gemfile-next.lock file, remove authorizations.rb factory and model

--- app/models/authorization.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Authorization < ApplicationRecord
-  belongs_to :user
-
-  validates_presence_of :provider, :uid, :token
-  validates_uniqueness_of :provider, scope: :user_id
-end
