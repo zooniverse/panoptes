4032f69fbfe605e78234d283656e080c522b62b4
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2025-05-01 11:25:39 -0500

    update upp serializer due to hound sniffs

--- app/serializers/user_project_preference_serializer.rb
+++ app/serializers/user_project_preference_serializer.rb
@@ -1,35 +1,37 @@
+# frozen_string_literal: true
+
 class UserProjectPreferenceSerializer
   include Serialization::PanoptesRestpack
   include CachedSerializer
 
   attributes :id, :email_communication, :preferences, :href,
-    :activity_count, :activity_count_by_workflow, :settings,
-    :created_at, :updated_at
+             :activity_count, :activity_count_by_workflow, :settings,
+             :created_at, :updated_at
   can_include :user, :project
   can_sort_by :updated_at, :display_name
   can_filter_by :non_null_activity_count
 
-  CACHE_MINS = (ENV["UPP_ACTIVITY_COUNT_CACHE_MINS"] || 5).freeze
+  CACHE_MINS = (ENV['UPP_ACTIVITY_COUNT_CACHE_MINS'] || 5).freeze
 
   def self.key
-    "project_preferences"
+    'project_preferences'
   end
 
-  def self.page(params = {}, scope = nil, context = {})
+  def self.page(params={}, scope=nil, context={})
     page_with_options CustomScopeFilterOptions.new(self, params, scope, context)
   end
 
   def self.page_with_options(options)
     if options.sorting.key?(:display_name)
-      display_sort, other_sorts = options.sorting.partition do |field, direction|
+      display_sort, other_sorts = options.sorting.partition do |field, _direction|
         field.match(/display_name/)
       end.map(&:to_h)
       options.sorting = {}
       options.scope = options
-        .scope
-        .joins(:project)
-        .order("projects.display_name #{display_sort[:display_name]}")
-        .order(other_sorts)
+                      .scope
+                      .joins(:project)
+                      .order("projects.display_name #{display_sort[:display_name]}")
+                      .order(other_sorts)
     end
     super(options)
   end
@@ -53,9 +55,7 @@ class UserProjectPreferenceSerializer
   end
 
   def count_activity_by_workflow
-    unless project_workflows_ids.empty?
-      UserSeenSubject.activity_by_workflow(@model.user_id, project_workflows_ids)
-    end
+    UserSeenSubject.activity_by_workflow(@model.user_id, project_workflows_ids) unless project_workflows_ids.empty?
   end
 
   def project_workflows_ids
@@ -70,7 +70,7 @@ class UserProjectPreferenceSerializer
   end
 
   class CustomScopeFilterOptions < RestPack::Serializer::Options
-    CUSTOM_SCOPE_FILTERS = %i(non_null_activity_count).freeze
+    CUSTOM_SCOPE_FILTERS = %i[non_null_activity_count].freeze
 
     def scope_with_filters
       filtered_scope = apply_standard_filters
@@ -84,7 +84,7 @@ class UserProjectPreferenceSerializer
     def apply_standard_filters
       scope_filter = {}
       non_custom_filters = @filters.except(*CUSTOM_SCOPE_FILTERS)
-      non_custom_filters.keys.each do |filter|
+      non_custom_filters.each_key do |filter|
         value = query_to_array(@filters[filter])
         scope_filter[filter] = value
       end
