9e3e075a2226d62f4a005864520476d1a69ca729
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2025-05-15 14:21:16 -0500

    add specs for user_seen_subjects_group_worker

---
+++ spec/workers/user_seen_subjects_group_worker_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe UserSeenSubjectsGroupWorker, sidekiq: :inline do
+  let(:workflow) { create(:workflow) }
+  let(:user) { workflow.project.owner }
+
+  it 'queues UserSeenSubjectsGroupWorker' do
+    expect(UserSeenSubjectsWorker)
+      .to receive(:perform_async)
+      .with(
+        user.id,
+        workflow.id,
+        [1, 2]
+      )
+
+    described_class.new.perform([
+                                  { user_id: user.id, workflow_id: workflow.id, subject_ids_arr: [1, 2] }
+                                ])
+  end
+
+  it 'queues UserSeenSubjectsGroupWorker with merged subject_ids_arr of same user and workflow' do
+    expect(UserSeenSubjectsWorker)
+      .to receive(:perform_async)
+      .with(
+        user.id,
+        workflow.id,
+        [1, 2, 3, 4]
+      )
+
+    described_class.new.perform([
+                                  { user_id: user.id, workflow_id: workflow.id, subject_ids_arr: [1, 2] },
+                                  { user_id: user.id, workflow_id: workflow.id, subject_ids_arr: [3, 4] }
+                                ])
+  end
+
+  it 'queues UserSeenSubjectsGroupWorker with unmerged subject_ids_arr for unrelated user-workflow combos' do
+    other_workflow = create(:workflow)
+
+    expect(UserSeenSubjectsWorker)
+      .to receive(:perform_async)
+      .with(
+        user.id,
+        workflow.id,
+        [1, 2]
+      )
+
+    expect(UserSeenSubjectsWorker)
+      .to receive(:perform_async)
+      .with(
+        user.id,
+        other_workflow.id,
+        [3, 4]
+      )
+
+    described_class.new.perform([
+                                  { user_id: user.id, workflow_id: workflow.id, subject_ids_arr: [1, 2] },
+                                  { user_id: user.id, workflow_id: other_workflow.id, subject_ids_arr: [3, 4] }
+                                ])
+  end
+end
