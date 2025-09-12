93327a44166d9d6791f7a4374343a6db641a8a3c
tooyosi <oluwatoyosi.oyegoke@gmail.com>
2025-09-12 21:10:06 +0100

    fix subject dump worker spec

--- spec/workers/subjects_dump_worker_spec.rb
+++ spec/workers/subjects_dump_worker_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe SubjectsDumpWorker do
@@ -17,21 +19,28 @@ RSpec.describe SubjectsDumpWorker do
   end
 
   describe 'cache wiring' do
-    it 'passes a shared cache to scope and formatter' do
+    let(:shared_cache) { SubjectDumpCache.new }
+
+    before do
       allow(CsvDumps::FindsDumpResource).to receive(:find).and_return(project)
-      allow(CsvDumps::FindsMedium).to receive(:new).and_return(double(medium: double(metadata: {}, save!: true, put_file_with_retry: true)))
-      allow(DumpMailer).to receive(:new).and_return(double(send_email: true))
+      medium_instance = instance_double(Medium)
+      finds_medium_instance = instance_double(CsvDumps::FindsMedium, medium: medium_instance)
+      allow(CsvDumps::FindsMedium).to receive(:new).and_return(finds_medium_instance)
+      allow(DumpMailer).to receive(:new).and_return(instance_double(DumpMailer, send_email: true))
       allow_any_instance_of(CsvDumps::DumpProcessor).to receive(:execute).and_return(true)
 
-      shared_cache = SubjectDumpCache.new
       allow(worker).to receive(:cache).and_return(shared_cache)
-
       allow(Formatter::Csv::Subject).to receive(:new).and_call_original
       allow(CsvDumps::SubjectScope).to receive(:new).and_call_original
 
       worker.perform(project.id, 'project')
+    end
 
+    it 'passes a shared cache to the formatter' do
       expect(Formatter::Csv::Subject).to have_received(:new).with(project, shared_cache)
+    end
+
+    it 'passes a shared cache to the scope' do
       expect(CsvDumps::SubjectScope).to have_received(:new).with(project, shared_cache)
     end
   end
