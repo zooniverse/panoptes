b934a7380ff75431e2ee92a147ef6d294929142c
tooyosi <oluwatoyosi.oyegoke@gmail.com>
2025-10-27 21:04:10 +0000

    add not null constraint for oauth_access_grants table to meet  doorkeeper 5.1.x → 5.2.x migration

---
+++ db/migrate/20251027120000_make_oauth_access_grants_scopes_not_null.rb
@@ -0,0 +1,28 @@
+class MakeOauthAccessGrantsScopesNotNull < ActiveRecord::Migration[6.1]
+  def up
+    safety_assured do
+      execute "UPDATE oauth_access_grants SET scopes = '' WHERE scopes IS NULL;"
+    end
+
+    change_column_default :oauth_access_grants, :scopes, ''
+
+    safety_assured do
+      execute <<-SQL
+        ALTER TABLE oauth_access_grants
+        ADD CONSTRAINT check_oauth_access_grants_scopes_not_null
+        CHECK (scopes IS NOT NULL) NOT VALID;
+      SQL
+
+      execute <<-SQL
+        ALTER TABLE oauth_access_grants
+        VALIDATE CONSTRAINT check_oauth_access_grants_scopes_not_null;
+      SQL
+    end
+  end
+
+  def down
+    execute "ALTER TABLE oauth_access_grants DROP CONSTRAINT check_oauth_access_grants_scopes_not_null;"
+    change_column_default :oauth_access_grants, :scopes, nil
+  end
+end
+
