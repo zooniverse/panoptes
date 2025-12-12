c46f54cd500344fe59f12469c0581913b99c8d27
tooyosi <oluwatoyosi.oyegoke@gmail.com>
2025-12-12 17:36:31 +0000

    fix spec and indentation

--- spec/models/subject_groups/virtual_group_subject_spec.rb
+++ spec/models/subject_groups/virtual_group_subject_spec.rb
@@ -9,22 +9,22 @@ describe SubjectGroups::VirtualGroupSubject, type: :model do
   describe '.from_member_subjects' do
     it 'builds a virtual subject with negative id when not provided' do
       virtual_subject_group = described_class.from_member_subjects([s1, s2])
-      expect(virtual.id).to be < 0
+      expect(virtual_subject_group.id).to be < 0
     end
 
     it 'uses the supplied virtual_id when provided' do
       virtual_subject_group = described_class.from_member_subjects([s1, s2], virtual_id: -1)
-      expect(virtual.id).to eq(-1)
+      expect(virtual_subject_group.id).to eq(-1)
     end
 
     it 'sets metadata with #group_subject_ids key' do
       virtual_subject_group = described_class.from_member_subjects([s1, s2], virtual_id: -1)
-      expect(virtual.metadata['#group_subject_ids']).to eq("#{s1.id}-#{s2.id}")
+      expect(virtual_subject_group.metadata['#group_subject_ids']).to eq("#{s1.id}-#{s2.id}")
     end
 
     it 'flattens member media into ordered_locations' do
       virtual_subject_group = described_class.from_member_subjects([s1, s2], virtual_id: -1)
-      expect(virtual.ordered_locations.length).to eq(s1.ordered_locations.length + s2.ordered_locations.length)
+      expect(virtual_subject_group.ordered_locations.length).to eq(s1.ordered_locations.length + s2.ordered_locations.length)
     end
   end
 end
