eed0fca324e74502b30286847088557221dfced0
tooyosi <oluwatoyosi.oyegoke@gmail.com>
2026-03-19 13:43:29 +0000

    rails-7.2: Use sanitize_sql_array in SubjectMetadataWorker for Rails 7.2

--- app/workers/subject_metadata_worker.rb
+++ app/workers/subject_metadata_worker.rb
@@ -24,8 +24,8 @@ class SubjectMetadataWorker
   end
 
   def run_update
-    update_sms_priority_sql =
-      <<-SQL.squish
+    update_sms_priority_sql = ActiveRecord::Base.sanitize_sql_array([
+      <<-SQL.squish,
         UPDATE set_member_subjects
         SET    priority = CAST(subjects.metadata->>'#priority' AS NUMERIC)
         FROM   subjects
@@ -33,22 +33,11 @@ class SubjectMetadataWorker
         AND    subjects.metadata ? '#priority'
         AND    set_member_subjects.id IN (:sms_ids)
       SQL
-    # handle missing bind params for non prepared statements
-    # by manually binding the named vars into the sql via `replace_named_bind_variables`
-    # https://github.com/rails/rails/issues/24893
-    # https://github.com/rails/rails/issues/34183
-    # call to exec_no_cache doesn't pass the bind params - so we need to morph them ourselves via sql
-    # https://github.com/rails/rails/blob/v5.0.7.2/activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb#L587
-    # fixed in rails 6.1 by the look - so in theory can switch to bound params without sql string morphing once there
-    # https://github.com/rails/rails/blob/v6.1.5.1/activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb#L649
-    bound_update_sms_priority_sql = ActiveRecord::Base.send(
-      :replace_named_bind_variables,
-      update_sms_priority_sql,
-      sms_ids: sms_ids
-    )
+      { sms_ids: sms_ids }
+    ])
     ActiveRecord::Base.connection.exec_update(
-      bound_update_sms_priority_sql,
-      'SQL',
+      update_sms_priority_sql,
+      'SubjectMetadataUpdate',
       []
     )
   end
